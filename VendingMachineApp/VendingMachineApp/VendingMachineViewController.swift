6956f738e8782063add406ddafab685265601ce2
yang hee jung <puuurm@naver.com>
2017-11-30 14:43:00 +0900

    Create VendingMachineViewController

---
+++ VendingMachineApp/VendingMachineApp/VendingMachineViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VendingMachineViewController.swift
+//  VendingMachineApp
+//
+//  Created by yangpc on 2017. 11. 30..
+//  Copyright © 2017년 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+class VendingMachineViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

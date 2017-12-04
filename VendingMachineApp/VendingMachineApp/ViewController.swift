ea7bf1405ff76888f16ac78bff4fd08b2341be24
yang hee jung <puuurm@naver.com>
2017-12-04 16:55:06 +0900

    Create event that adds money

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,9 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // 잔돈 추가 컨트롤러 -> User Mode
+    @IBOutlet var remainMoneyLable: UILabel!
+    var vendingMachine: VendingMachine!
+
+    private let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = NumberFormatter.Style.decimal
+        return formatter
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateRemainMoneyLabel()
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let id = segue.identifier, id == "VendingMachineViewController" {
+            guard let vendingMachineViewController = segue.destination as? VendingMachineViewController else {
+                return
+            }
+            vendingMachine = VendingMachine()
+
+            vendingMachineViewController.vendingMachine = vendingMachine
+        }
+    }
+
+    // 천원 추가
+    @IBAction func addTenMoneyButtonDidTap(_ sender: UIButton) {
+        initUserMode()
+        vendingMachine.action(action: Action(option: 1, detail: 1000)!)
+        updateRemainMoneyLabel()
+    }
+
+    // 오천원 추가
+    @IBAction func addFiftyMoneyButtonDidTap(_ sender: UIButton) {
+        initUserMode()
+        vendingMachine.action(action: Action(option: 1, detail: 5000)!)
+        updateRemainMoneyLabel()
+    }
+
+    // User Mode로 초기화
+    func initUserMode() {
+        if vendingMachine.hasMode {
+            vendingMachine.action(action: Action(option: 3, detail: -1)!)
+        }
+        do {
+            try vendingMachine.assignMode(mode: 2)
+        } catch let error{
+            print(error)
+        }
+    }
+
+    // 잔돈 라벨 업데이트
+    func updateRemainMoneyLabel() {
+        guard let menu = vendingMachine.makeMenu() else {
+            remainMoneyLable.text = "0"
+            return
+        }
+        remainMoneyLable.text = "\(numberFormatter.string(from: NSNumber(value: menu.money)) ?? "0")"
+    }
+    
 }
 

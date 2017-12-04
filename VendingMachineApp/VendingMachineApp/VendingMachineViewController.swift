d7ce07566212d8cd2e67a63da294e3b631732c8a
yang hee jung <puuurm@naver.com>
2017-12-04 16:55:06 +0900

    Create event that adds money

--- VendingMachineApp/VendingMachineApp/VendingMachineViewController.swift
+++ VendingMachineApp/VendingMachineApp/VendingMachineViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class VendingMachineViewController: UIViewController {
+    // 음료 추가 컨트롤러 -> Manager Mode
     @IBOutlet var imageViews: [UIImageView]!
     @IBOutlet var inventoryLable: [UILabel]!
     var vendingMachine: VendingMachine!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        initManagerMode()
         self.imageViews.forEach { (imageView: UIImageView) in
             let tap = UITapGestureRecognizer(target: self, action: #selector(VendingMachineViewController.drinkViewDidTap))
             imageView.addGestureRecognizer(tap)
@@ -24,23 +24,28 @@ class VendingMachineViewController: UIViewController {
         updateInventory()
     }
 
-    func initManagerMode() {
-        vendingMachine = VendingMachine()
-        do {
-            try vendingMachine.assignMode(mode: 1)
-        } catch let error {
-            print(error)
-        }
-
-    }
-
+    // 음료 이미지를 클릭했을 경우
     @objc func drinkViewDidTap(_ recognizer: UITapGestureRecognizer) {
+        initManagerMode()
         if let imageView = recognizer.view as? UIImageView {
             vendingMachine.action(action: Action(option: 1, detail: imageView.tag)!)
         }
         updateInventory()
     }
 
+    // Manager Mode로 초기화
+    func initManagerMode() {
+        if vendingMachine.hasMode {
+            vendingMachine.action(action: Action(option: 3, detail: -1)!)
+        }
+        do {
+            try vendingMachine.assignMode(mode: 1)
+        } catch let error {
+            print(error)
+        }
+    }
+
+    // 재고 업데이트
     func updateInventory() {
         let menuContents = vendingMachine.makeMenu()
         for lable in inventoryLable.enumerated() {
@@ -50,4 +55,6 @@ class VendingMachineViewController: UIViewController {
             lable.element.text = "\(menuContents?.inventory[drink] ?? 0 )"
         }
     }
+
+
 }

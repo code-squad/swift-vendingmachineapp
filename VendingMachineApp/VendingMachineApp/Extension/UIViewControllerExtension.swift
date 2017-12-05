6529c6425e8cd8c6e8f5e67d4b818e24edf2731d
yang hee jung <puuurm@naver.com>
2017-12-05 15:58:09 +0900

    Create extension viewcontroller to access app delegate

---
+++ VendingMachineApp/VendingMachineApp/Extension/UIViewControllerExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewControllerExtension.swift
+//  VendingMachineApp
+//
+//  Created by yangpc on 2017. 12. 5..
+//  Copyright © 2017년 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    var appDelegate:AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+}

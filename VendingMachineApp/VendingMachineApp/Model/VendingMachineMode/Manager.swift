267ef566ba3786fb49db9bca9d6e646247924d45
yang hee jung <puuurm@naver.com>
2017-12-08 14:50:13 +0900

    Remove unneccessary mutating keywords

--- VendingMachineApp/VendingMachineApp/Model/VendingMachineMode/Manager.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachineMode/Manager.swift
@@ -15,21 +15,21 @@ struct Manager: EnableMode {
         delegate = target
     }
 
-    mutating func makeMenu() -> MenuContents {
+    func makeMenu() -> MenuContents {
         let income = delegate.howMuchIncome()
         let managerMenu = delegate.AllDrinkList()
         let managerInventory = delegate.listOfInventory()
         return ( .manager, income, managerMenu, managerInventory)
     }
 
-    mutating func add(detail: Int) throws {
+    func add(detail: Int) throws {
         do {
             try delegate.add(productIndex: detail)
         } catch let error {
             throw error
         }
     }
-    mutating func delete(detail: Int) throws {
+    func delete(detail: Int) throws {
         do {
             try delegate.delete(productIndex: detail)
         } catch let error{

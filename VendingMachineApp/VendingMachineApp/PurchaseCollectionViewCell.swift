2b118e868c75743b90923d00650720db9e30e7b0
yang hee jung <puuurm@naver.com>
2017-12-12 14:53:58 +0900

    Modify purchaseList ad a collection view

---
+++ VendingMachineApp/VendingMachineApp/PurchaseCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PurchaseCollectionViewCell.swift
+//  VendingMachineApp
+//
+//  Created by yangpc on 2017. 12. 12..
+//  Copyright © 2017년 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var drinkImageView: UIImageView!
+}

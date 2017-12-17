08de92ec733da341801231a408cefbde2d309a5e
yang hee jung <puuurm@naver.com>
2017-12-18 01:17:27 +0900

    Create PageViewController to add pie graph to Manager Mode

--- VendingMachineApp/VendingMachineApp/Controller/ManagerModeController.swift
+++ VendingMachineApp/VendingMachineApp/Controller/ManagerModeController.swift
@@ -60,9 +60,4 @@ class ManagerModeController: UIViewController {
         }
     }
 
-    // MARK: Actions
-
-    @IBAction func closeButtonDidTap(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
-    }
 }

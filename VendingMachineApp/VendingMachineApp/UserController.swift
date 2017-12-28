1f4686c267d73a39e82e5e62f85bfa43d4ee0be5
DongJin Lee <comets195@gmail.com>
2017-12-28 11:39:59 +0900

    Remove trash code..

--- VendingMachineApp/VendingMachineApp/UserController.swift
+++ VendingMachineApp/VendingMachineApp/UserController.swift
@@ -18,37 +18,4 @@ struct UserController {
     init(with vendingMachineData: VendingMachineData) {
         self.machine = UserVendingMachine(with: vendingMachineData)
     }
-    /*
-    mutating func executeMachine() throws {
-        let condition = true
-        let outputView = OutputView()
-        outputView.printMainMenu(machine: machine)
-        while condition {
-            do {
-                try separateVendingMachineExecution(selector: try InputView.readUserMenu(), view: outputView)
-            } catch ErrorCode.endCode {
-                AdminOutputView.printError(ErrorCode.endCode.description)
-            } catch let Error as ErrorCode {
-                AdminOutputView.printError(Error.description)
-            }
-        }
-    }
-    
-    private mutating func separateVendingMachineExecution(selector: [Int], view: OutputView) throws {
-        guard selector.count > 1 else {
-            throw ErrorCode.validInputString
-        }
-        switch selector[0] {
-        case MenuSelectorUser.insertMode.rawValue:
-            machine.insertMoney(selector[1])
-            view.printAfterInsertMoney(machine: machine)
-        case MenuSelectorUser.purchaseMode.rawValue:
-            let beverage = try machine.buyBeverage(number: selector[1])
-            view.printAfterBuyingBeverage(beverage: beverage)
-            view.printAfterInsertMoney(machine: machine)
-        default:
-            throw ErrorCode.invalidMenu
-        }
-    }
-    */
 }

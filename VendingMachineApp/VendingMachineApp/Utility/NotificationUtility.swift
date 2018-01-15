e36f654490913ae5f5e99c036db4b2ac1f9c8e2e
ChocOZerO <nayalth@gmail.com>
2018-01-15 13:54:38 +0900

    [Update]관리자모드 적용 #37

---
+++ VendingMachineApp/VendingMachineApp/Utility/NotificationUtility.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  VendingMachineApp
+//
+//  Created by TaeHyeonLee on 2018. 1. 15..
+//  Copyright © 2018년 ChocOZerO. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let beverageCounts = Notification.Name("beverageCounts")
+    static let coins = Notification.Name("coins")
+    static let purchase = Notification.Name("purchase")
+}

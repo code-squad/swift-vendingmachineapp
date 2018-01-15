871d2a97e349f84a63cb44c5f2f06b84f54ee0cd
ChocOZerO <nayalth@gmail.com>
2018-01-15 14:50:59 +0900

    [Update]문자열 하드코딩 제거 #39

---
+++ VendingMachineApp/VendingMachineApp/Keyword.swift
@@ -0,0 +1,37 @@
+//
+//  Keyword.swift
+//  VendingMachineApp
+//
+//  Created by TaeHyeonLee on 2018. 1. 15..
+//  Copyright © 2018년 ChocOZerO. All rights reserved.
+//
+
+import Foundation
+
+struct Keyword {
+    enum Key {
+        case beverageCounts
+        case inventory
+        case coins
+        case purchase
+        case image
+        case purchaseListCount
+
+        var value: String {
+            switch self {
+            case .beverageCounts:
+                return "beverageCounts"
+            case .inventory:
+                return "inventory"
+            case .coins:
+                return "coins"
+            case .purchase:
+                return "purchase"
+            case .image:
+                return "image"
+            case .purchaseListCount:
+                return "purchaseListCount"
+            }
+        }
+    }
+}

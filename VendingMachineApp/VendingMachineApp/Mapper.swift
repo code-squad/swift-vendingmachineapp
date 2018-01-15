598290951221963b5d78358598ea967301b8d6b0
undervine <undervine@naver.com>
2018-01-15 23:12:51 +0900

    Replace switch statements with Enum.init()

--- VendingMachineApp/VendingMachineApp/Mapper.swift
+++ VendingMachineApp/VendingMachineApp/Mapper.swift
@@ -10,33 +10,13 @@ import Foundation
 import UIKit
 
 struct Mapper {
-    // balanceButton의 tag 값과 MoneyManager.Unit 모델에 사용될 값을 매핑
+    // 직접 매칭해주지만, sender.tag값이 범위에 들어가면 열거형 타입이 생성되고 아니면 nil값이 됨.
     static func mappingUnit(with sender: UIButton) -> MoneyManager<VendingMachine>.Unit? {
-        var unit = MoneyManager<VendingMachine>.Unit(rawValue: 100)
-        switch sender.tag {
-        case 1: unit = MoneyManager<VendingMachine>.Unit(rawValue: 100)
-        case 2: unit = MoneyManager<VendingMachine>.Unit(rawValue: 500)
-        case 3: unit = MoneyManager<VendingMachine>.Unit(rawValue: 1000)
-        default: break
-        }
-        return unit
+        return MoneyManager.Unit(rawValue: sender.tag)
     }
 
     static func mappingMenu(with sender: UIView) -> VendingMachine.Menu? {
-        var menu: VendingMachine.Menu = .strawberryMilk
-        switch sender.tag {
-        case 1: menu = .strawberryMilk
-        case 2: menu = .bananaMilk
-        case 3: menu = .chocoMilk
-        case 4: menu = .coke
-        case 5: menu = .cider
-        case 6: menu = .fanta
-        case 7: menu = .top
-        case 8: menu = .cantata
-        case 9: menu = .georgia
-        default: break
-        }
-        return menu
+        return VendingMachine.Menu(rawValue: sender.tag)
     }
 
 }

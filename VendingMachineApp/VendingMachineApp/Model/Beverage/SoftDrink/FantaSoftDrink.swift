ae701173b97bf9749622c3a58e917a2eaed7d0ce
undervine <undervine@naver.com>
2018-01-18 22:28:13 +0900

    Modify hard-coded keys

--- VendingMachineApp/VendingMachineApp/Model/Beverage/SoftDrink/FantaSoftDrink.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/SoftDrink/FantaSoftDrink.swift
@@ -18,10 +18,15 @@ class FantaSoftDrink: SoftDrink {
                    carbonContent: carbonContent)
     }
     convenience init(_ menu: VendingMachine.Menu) {
-        self.init(stringData["brands"]![menu]!, intData["volume"]![menu]!, intData["price"]![menu]!,
-                  stringData["productName"]![menu]!, Date(timeIntervalSinceNow: 0),
-                  Date(timeIntervalSinceNow: TimeInterval(intData["expirationDate"]![menu]!)),
-                  intData["calories"]![menu]!, menu, carbonContent: intData["carbonContent"]![menu]!)
+        self.init(stringData[Beverage.CodingKeys.brand]![menu]!,
+                  intData[Beverage.CodingKeys.volume]![menu]!,
+                  intData[Beverage.CodingKeys.price]![menu]!,
+                  stringData[Beverage.CodingKeys.productName]![menu]!,
+                  Date(timeIntervalSinceNow: 0),
+                  Date(timeIntervalSinceNow: TimeInterval(intData[Beverage.CodingKeys.expirationDate]![menu]!)),
+                  intData[Beverage.CodingKeys.calories]![menu]!,
+                  menu,
+                  carbonContent: intData[Beverage.CodingKeys.carbonContent]![menu]!)
     }
     required init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)

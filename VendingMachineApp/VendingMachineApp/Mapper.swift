7f21349e558963d36822d332e5d65ada3de3c8fa
undervine <undervine@naver.com>
2018-01-22 17:30:35 +0900

    Make ViewController to get VendingMachine object, instead of accessing sharedInstance of VendingMachine
    Make ViewController independent of Model-VendingMachine.

--- VendingMachineApp/VendingMachineApp/Mapper.swift
+++ VendingMachineApp/VendingMachineApp/Mapper.swift
@@ -11,12 +11,12 @@ import UIKit
 
 struct Mapper {
     // 직접 매칭해주지만, sender.tag값이 범위에 들어가면 열거형 타입이 생성되고 아니면 nil값이 됨.
-    static func mappingUnit(with sender: UIButton) -> MoneyManager<VendingMachine>.Unit? {
+    static func mappingUnit(with sender: UIButton) -> MoneyManager.Unit? {
         return MoneyManager.Unit(rawValue: sender.tag)
     }
 
-    static func mappingMenu(with sender: UIView) -> VendingMachine.Menu? {
-        return VendingMachine.Menu(rawValue: sender.tag)
+    static func mappingMenu<T: EnumCollection>(with sender: UIView) -> T? {
+        return T.getCase(rawValue: sender.tag) as? T
     }
 
 }

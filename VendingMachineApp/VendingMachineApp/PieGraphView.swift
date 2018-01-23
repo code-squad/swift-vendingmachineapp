97c04af30dac44364d933a2ae3d29c6cd1cc22c5
ChocOZerO <nayalth@gmail.com>
2018-01-23 16:12:57 +0900

    [Add]text for product on the Pie Chart #46

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -29,19 +29,27 @@ class PieGraphView: UIView {
 
     private func drawContext(beverageCounts: [Beverage: Int]) {
         guard let context = UIGraphicsGetCurrentContext() else { return }
-        let center = CGPoint(x: Int(frame.width * 0.5), y: Int(frame.height * 0.5))
-        let radius = CGFloat(min(Int(frame.width * 0.5), Int(frame.height * 0.5)))
         var startAngle: CGFloat = 0
-        for (_, value) in beverageCounts {
+        for (key, value) in beverageCounts {
             let endAngle = startAngle + 2 * .pi * (CGFloat(value) / CGFloat(purchaseList.count))
-            context.setFillColor(getRandomColor())
-            context.move(to: center)
-            context.addArc(center: center, radius: radius,
-                           startAngle: startAngle, endAngle: endAngle,
-                           clockwise: false)
-            context.fillPath()
+            drawEachSegment(context: context, key: key, startAngle: startAngle, endAngle: endAngle)
             startAngle = endAngle
         }
+    }
+
+    private func drawEachSegment(context: CGContext, key: Beverage, startAngle: CGFloat, endAngle: CGFloat) {
+        let center = CGPoint(x: Int(frame.width * 0.5), y: Int(frame.height * 0.5))
+        let radius = CGFloat(min(Int(frame.width * 0.5), Int(frame.height * 0.5)))
+        let color = getRandomColor()
+        context.setFillColor(color)
+        context.move(to: center)
+        context.addArc(center: center, radius: radius,
+                       startAngle: startAngle, endAngle: endAngle,
+                       clockwise: false)
+        context.fillPath()
+        let textPositionAngle = getTextPositionAngle(startAngle: startAngle, endAngle: endAngle)
+        let textPosition = getTextPosition(center: center, radius: radius, angle: textPositionAngle)
+        drawTextOnChart(beverage: key, position: textPosition, color: color)
     }
 
     private func getRandomColor() -> CGColor {
@@ -53,6 +61,30 @@ class PieGraphView: UIView {
 
     private func getRandomCGFloatForColor() -> CGFloat {
         return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+
+    private func getTextPositionAngle(startAngle: CGFloat, endAngle: CGFloat) -> CGFloat {
+        return startAngle + (endAngle - startAngle) * 0.6
+    }
+
+    private func getTextPosition(center: CGPoint, radius: CGFloat, angle: CGFloat) -> CGPoint {
+        let positionRatio: CGFloat = 0.6
+        return CGPoint(x: center.x + radius * positionRatio * cos(angle),
+                       y: center.y + radius * positionRatio * sin(angle))
+    }
+
+    private func drawTextOnChart(beverage: Beverage, position: CGPoint, color: CGColor) {
+        guard let colorComponents = color.components else { return }
+        let averageRGB = (colorComponents[0] + colorComponents[1] + colorComponents[2]) / 3
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        let attributes = [NSAttributedStringKey.paragraphStyle: paragraphStyle,
+                          NSAttributedStringKey.font: UIFont.systemFont(ofSize: 17.0),
+                          NSAttributedStringKey.foregroundColor: (averageRGB > 0.7) ? UIColor.black : UIColor.white]
+        let attrString = NSAttributedString(string: beverage.name,
+                                            attributes: attributes)
+        let renderRect = CGRect(origin: position, size: attrString.size())
+        attrString.draw(in: renderRect)
     }
 
 }

29c7411d51d0adea5c8a5791a2e6dcdb5b797089
DongJin Lee <comets195@gmail.com>
2018-01-23 12:20:29 +0900

    Add Shake Gesture and refactoring..#20.

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 private struct Segment {
-    private (set) var color: UIColor
     private (set) var value: CGFloat
     private (set) var name: String
 }
@@ -18,57 +17,55 @@ class PieGraphView: UIView {
     private var receipts = [Beverage: Int]()
     private var segments = [Segment]()
     private var graphRatio: CGFloat = 0.5
-    private let colors = [UIColor(red: 1.0, green: 31.0/255.0, blue: 73.0/255.0, alpha: 1.0),
+    private var tempColors: [UIColor]!
+    private var colors = [UIColor(red: 1.0, green: 31.0/255.0, blue: 73.0/255.0, alpha: 1.0),
                           UIColor(red: 1.0, green: 222.0/255.0, blue: 0.0, alpha: 1.0),
                           UIColor(red: 0.0, green: 222.0/255.0, blue: 1.0, alpha: 1.0),
                           UIColor(red: 1.0, green: 138.0/255.0, blue: 0.0, alpha: 1.0),
                           UIColor(red: 138.0/255.0, green: 1.0, blue: 222.0/255.0, alpha: 1.0)]
     
     func setPieGraph(receipts: [Beverage: Int]) {
+        self.graphRatio = 0.5
         self.receipts = receipts
         self.segments = [
-            Segment(color: colors[0],
-                    value: CGFloat(receipts[Coke()] ?? 0),
+            Segment(value: CGFloat(receipts[Coke()] ?? 0),
                     name: Coke().description),
-            Segment(color: colors[1],
-                    value: CGFloat(receipts[LightBananaMilk()] ?? 0),
+            Segment(value: CGFloat(receipts[LightBananaMilk()] ?? 0),
                     name: LightBananaMilk().description),
-            Segment(color: colors[2],
-                    value: CGFloat(receipts[Sprite()] ?? 0),
+            Segment(value: CGFloat(receipts[Sprite()] ?? 0),
                     name: Sprite().description),
-            Segment(color: colors[3],
-                    value: CGFloat(receipts[CeylonTea()] ?? 0),
+            Segment(value: CGFloat(receipts[CeylonTea()] ?? 0),
                     name: CeylonTea().description),
-            Segment(color: colors[4],
-                    value: CGFloat(receipts[StarBucksCoffee()] ?? 0),
+            Segment(value: CGFloat(receipts[StarBucksCoffee()] ?? 0),
                     name: StarBucksCoffee().description)
         ]
         setNeedsDisplay()
     }
     
-    func setFillColor() {
-        for index in 0..<segments.count {
-           self.segments[index] = Segment(color: UIColor.black, value: 1, name: "")
-        }
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        drawPieGraph(context: context, colors: colors)
     }
     
-    override func draw(_ rect: CGRect) {
-        let context = UIGraphicsGetCurrentContext()
+    private func drawPieGraph(context: CGContext, colors: [UIColor]) {
         let radius = min(bounds.size.width, bounds.size.height) * graphRatio
         let viewCenter = CGPoint(x: bounds.size.width * 0.5, y: bounds.size.height * 0.5)
         let valueCount = segments.reduce(0) { $0 + $1.value }
         var startAngle = -CGFloat.pi * 0.5
-        
-        for beverage in segments {
-            context?.setFillColor(beverage.color.cgColor)
-            let endAngle = startAngle + 2 * .pi * (beverage.value / valueCount)
-            context?.move(to: viewCenter)
-            context?.addArc(center: viewCenter, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
-            context?.fillPath()
-            let halfAngle = startAngle + (endAngle - startAngle) * 0.5
-            if beverage.value >= 1 {
-                drawLegend(halfAngle: halfAngle, viewCenter: viewCenter, radius: radius, beverage: beverage)
+
+        for index in 0..<segments.count {
+            let endAngle = startAngle + 2 * .pi * (segments[index].value / valueCount)
+            
+            context.setFillColor(colors[index].cgColor)
+            context.move(to: viewCenter)
+            context.addArc(center: viewCenter, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+            context.fillPath()
+            
+            if segments[index].value >= 1 {
+                let halfAngle = startAngle + (endAngle - startAngle) * 0.5
+                drawLegend(halfAngle: halfAngle, viewCenter: viewCenter, radius: radius, beverage: segments[index])
             }
+            
             startAngle = endAngle
         }
     }
@@ -88,12 +85,11 @@ class PieGraphView: UIView {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("began")
-        setFillColor()
+        self.tempColors = self.colors
+        self.colors = [UIColor].init(repeating: UIColor.black, count: 5)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("move")
         if let touch = touches.first {
             let position = touch.location(in: self)
             let xValue = pow(bounds.size.width * 0.5 - position.x, 2)
@@ -106,8 +102,7 @@ class PieGraphView: UIView {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("end")
-        setPieGraph(receipts: self.receipts)
+        self.colors = self.tempColors
+        setNeedsDisplay()
     }
-
 }

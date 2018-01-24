29c4b6eedd7752e5807fb6342fdeab0c461bf731
undervine <undervine@naver.com>
2018-01-25 03:07:19 +0900

    Generate segments and inject to PieGraphView's [Segment]
    Add observer to add new segment to pieGraphView.
    Override viewWillAppear() to update layout of pieGraphView.

--- VendingMachineApp/VendingMachineApp/AdminViewController.swift
+++ VendingMachineApp/VendingMachineApp/AdminViewController.swift
@@ -12,6 +12,7 @@ class AdminViewController: UIViewController {
     var machine: Managable?
     @IBOutlet var addStockButtons: [UIButton]!
     @IBOutlet var stockLabels: [UILabel]!
+    @IBOutlet weak var pieGraphView: PieGraphView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,39 @@ class AdminViewController: UIViewController {
             selector: #selector(updateStockLabels),
             name: .didUpdateStock,
             object: nil)
+        // purchasedHistory의 카운트 증가 시, 차트 세그먼트 추가
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(addSegment),
+            name: .didUpdateRecord,
+            object: nil)
+        // 초기 세그먼트 세팅.
+        pieGraphView.segments = machine?.purchasedList().map { generateSegment($0) }
+    }
+
+    // 세그먼트 생성 함수.
+    private func generateSegment(_ info: HistoryInfo) -> Segment {
+        return Segment(
+            name: info.purchasedMenu.productName,
+            value: info.count,
+            color: UIColor(red: CGFloat(info.count)*100/255,
+                           green: CGFloat(info.count)*100/255,
+                           blue: CGFloat(info.count)*100/255, alpha: 1))
+    }
+
+    // 추가된 구매이력으로 세그먼트 추가.
+    @objc func addSegment(_ notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        if let addedRecord = userInfo[UserInfoKeys.addedRecord] as? HistoryInfo {
+            let newSegment = generateSegment(addedRecord)
+            pieGraphView.segments?.append(newSegment)
+        }
+    }
+
+    // 현재 뷰컨트롤러가 보일 시, 파이그래프 뷰 업데이트.
+    override func viewWillAppear(_ animated: Bool) {
+        // PieGraphView 객체에 구매목록 업데이트
+        pieGraphView.setNeedsLayout()
     }
 
     // 재고 추가 버튼 클릭 시. V -> C -> M

1e5a30914d5f37bb84a582d9600d03fd297a31f6
undervine <undervine@naver.com>
2018-01-29 11:00:19 +0900

    Make PieGraphView touchable and shakable
    Override touches- functions and TouchStatus enum type to separate drawings by touch status.
    Post notification to AdminViewController to request changing pieGraphView's size.
    Override motionEnded() func resetting configurations to display initial pie graph.

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class PieGraphView: UIView {
+    // 원점
+    private var origin: CGPoint {
+        return CGPoint(x: bounds.width/2, y: bounds.height/2)
+    }
+    // 경계 부분이 잘릴 수 있으니 -6 정도로 여유를 준다.
+    private var radius: CGFloat {
+        return CGFloat(min(initialFrameSize.width, initialFrameSize.height)/2-6)
+    }
     // AdminViewController에서 의존성 주입.
     var segments: [Segment]? {
         didSet {
@@ -16,81 +24,148 @@ class PieGraphView: UIView {
             self.setNeedsDisplay()
         }
     }
-
-    private var sum: Int? {
-        return segments?.reduce(0, { $0 + $1.value })
+    // 초기 프레임 사이즈 - 뷰 크기 갱신 위해 고정
+    private var initialFrameSize: CGSize
+    required init?(coder aDecoder: NSCoder) {
+        self.initialFrameSize = CGSize()
+        super.init(coder: aDecoder)
+        self.initialFrameSize = frame.size
     }
-
-    private var textAttributes: [NSAttributedStringKey: Any]? {
+    // 모든 세그먼트들의 합 (총 구매 개수)
+    private var sum: Int? {
+        return segments?.reduce(0, {
+            return $0 + $1.value
+        })
+    }
+    private var textAttributes: [NSAttributedStringKey: Any] {
         return [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 20, weight: .init(rawValue: 5)),
                 NSAttributedStringKey.foregroundColor: UIColor.black]
     }
 
     override func draw(_ rect: CGRect) {
-        let origin = CGPoint(x: bounds.width/2, y: bounds.height/2)
-        // 경계 부분이 잘릴 수 있으니 -6 정도로 여유를 준다.
-        let radius = CGFloat(min(bounds.width, bounds.height)/2-6)
+        switch status {
+        case .none: drawPieGraph(radius: self.radius)
+        case .began: drawBlackCircle(radius: self.radius)
+        case .moved: drawBlackCircle(radius: self.movedRadius)
+        case .ended: drawPieGraph(radius: self.movedRadius)
+        }
+    }
+
+    private func drawPieGraph(radius: CGFloat?) {
+        guard let radius = radius else { return }
+        let context = UIGraphicsGetCurrentContext()
         var startAngle: CGFloat = 0
         segments?.forEach { segment in
-            let endAngle: CGFloat = startAngle + radians(from: degree(of: segment.value))
+            let endAngle: CGFloat = startAngle + segment.value.degrees(in: sum).radians
             let sector = Sector(origin: origin, radius: radius, startAngle: startAngle, endAngle: endAngle)
-            drawGraph(segment, in: sector)
-            drawText(segment, inside: sector)
+            context?.drawSegment(segment, in: sector)
+            let name = NSMutableAttributedString(string: segment.name, attributes: textAttributes)
+            name.draw(on: sector)
             startAngle = endAngle
         }
     }
 
-    private func drawText(_ segment: Segment, inside sector: Sector) {
-        let text = NSMutableAttributedString(string: segment.name, attributes: textAttributes)
-        let halfAngle = sector.startAngle + (sector.endAngle-sector.startAngle)/2
-        // 절반은 너무 좁아서 반지름길이의 2/3 정도로 잡음.
-        let halfRadius = sector.radius*2/3
-        // 텍스트 중앙정렬을 위해 글자 실제길이의 절반을 왼쪽으로 옮김(-).
-        let centerOfSegment = CGPoint(
-            x: sector.origin.x+cos(halfAngle)*halfRadius-text.size().width/2,
-            y: sector.origin.y+sin(halfAngle)*halfRadius)
-        text.draw(at: centerOfSegment)
+    private func drawBlackCircle(radius: CGFloat?) {
+        guard let radius = radius else { return }
+        let context = UIGraphicsGetCurrentContext()
+        context?.move(to: origin)
+        context?.addArc(center: origin, radius: radius, startAngle: 0, endAngle: 360.radians, clockwise: false)
+        context?.setFillColor(UIColor.black.cgColor)
+        context?.drawPath(using: .fill)
     }
 
-    private func drawGraph(_ segment: Segment, in sector: Sector) {
-        let ctx = UIGraphicsGetCurrentContext()
-        ctx?.addArc(
-            center: sector.origin,
-            radius: sector.radius,
-            startAngle: sector.startAngle,
-            endAngle: sector.endAngle,
-            clockwise: false)
-        // 테두리를 그릴 선.
-        ctx?.addLine(to: sector.origin)
-        ctx?.setFillColor(segment.color.cgColor)
-        ctx?.setLineWidth(3)
-        ctx?.setStrokeColor(UIColor.black.cgColor)
-        ctx?.drawPath(using: .fillStroke)
+    enum TouchStatus {
+        case none
+        case began
+        case moved
+        case ended
+    }
+    var status: TouchStatus = .none
+    private var beganRadiusFromOrigin: CGFloat = 0
+    private var movedRadius: CGFloat? {
+        didSet {
+            // 터치를 통해 원 사이즈가 조정되면 뷰 자체의 사이즈도 같이 조정하도록 노티보냄
+            NotificationCenter.default.post(
+                name: .didUpdatePieSize,
+                object: nil,
+                userInfo: [UserInfoKeys.newPieRadius: movedRadius ?? self.radius])
+        }
     }
 
-    private func degree(of value: Int?) -> CGFloat {
-        guard let value = value, let sum = sum else { return 0 }
-        return (CGFloat(value)*360)/CGFloat(sum)
+    // bounds가 세팅될 때 코너를 둥글게 처리.
+    override var bounds: CGRect {
+        didSet {
+            self.layer.cornerRadius = self.bounds.size.width/2
+            self.clipsToBounds = true   // 이걸 해줘야 적용된다.
+        }
     }
 
-    private func radians(from degrees: CGFloat) -> CGFloat {
-        return (CGFloat.pi*degrees)/180
+    // 터치 시작 - 원점으로부터 터치 위치까지의 거리 저장. 그래프를 검은색 원으로 변경하여 표시.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        status = .began
+        guard let thisTouch = touches.first else { return }
+        beganRadiusFromOrigin = thisTouch.location(in: self).distance(from: origin)
+        setNeedsDisplay()
+    }
+
+    // 터치 움직임 - 원점으로부터 현재 터치 위치까지 거리를 radius로 하는 검은색 원 표시.
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        status = .moved
+        guard let thisTouch = touches.first else { return }
+        let currPosition = thisTouch.location(in: self)
+        movedRadius = currPosition.distance(from: origin)
+        setNeedsDisplay()
+    }
+
+    // 터치 끝 - 원점으로부터 현재 터치가 끝난 지점까지 거리를 radius로 하는 파이그래프 표시.
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        status = .ended
+        guard let thisTouch = touches.first else { return }
+        let currPosition = thisTouch.location(in: self)
+        movedRadius = currPosition.distance(from: origin)
+        segments?.forEach({ $0.setRandomColor() })
+        setNeedsDisplay()
+    }
+
+    // 쉐이크 제스처 시 first responder로 지정 가능하도록 설정
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+
+    // 쉐이크 모션 끝날 시, 설정값들을 리셋하여 기본 그래프 표시.
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            status = .none
+            movedRadius = nil
+            setNeedsDisplay()
+        }
     }
 
 }
 
+// 부채꼴 그릴 정보.
 struct Sector {
-    let origin: CGPoint
-    let radius: CGFloat
-    let startAngle: CGFloat
-    let endAngle: CGFloat
+    private(set) var origin: CGPoint
+    private(set) var radius: CGFloat
+    private(set) var startAngle: CGFloat
+    private(set) var endAngle: CGFloat
 }
 
-struct Segment: CustomStringConvertible {
-    let name: String
-    let value: Int
-    let color: UIColor
-    var description: String {
-        return "\(self.name), \(self.value), \(self.color)"
+// 파이 한 조각 정보.
+class Segment {
+    private(set) var name: String
+    private(set) var value: Int
+    private(set) var color: UIColor
+    init(name: String, value: Int, color: UIColor) {
+        self.name = name
+        self.value = value
+        self.color = color
+    }
+}
+
+extension Segment {
+    // 색상 변경 함수.
+    func setRandomColor() {
+        self.color = UIColor.random
     }
 }

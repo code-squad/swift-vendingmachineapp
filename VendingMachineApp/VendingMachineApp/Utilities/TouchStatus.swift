24be51539453d3fc5110fdd2e940645430b46d4e
undervine <undervine@naver.com>
2018-01-29 20:39:57 +0900

    [이동] TouchStatus 열거형을 PieGraphView 밖으로 분리

---
+++ VendingMachineApp/VendingMachineApp/Utilities/TouchStatus.swift
@@ -0,0 +1,16 @@
+//
+//  TouchStatus.swift
+//  VendingMachineApp
+//
+//  Created by 심 승민 on 2018. 1. 29..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import Foundation
+
+enum TouchStatus {
+    case none
+    case began
+    case moved
+    case ended
+}

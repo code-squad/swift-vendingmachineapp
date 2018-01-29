8d4a212eb9a186d9fad604d53c19981ffe6990db
undervine <undervine@naver.com>
2018-01-30 03:18:05 +0900

    [수정] 터치이벤트 시 뷰 크기를 PieGraphView 내부에서 변경하도록 수정
    터치이벤트 moved 시 뷰 크기 변경.
    뷰 크기가 변경되면 해당 뷰 크기의 bounds를 이용하여 원 그림.
    터치이벤트 상태값을 UITouch.phase 프로퍼티를 사용하는 것으로 변경. 기존 TouchStatus 열거형 제거.

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class PieGraphView: UIView, TouchableGraphView {
-    // 원점
+class PieGraphView: UIView {
+    // 원점. 프레임 크기와 상관없이 무조건 뷰의 중점.
     private var origin: CGPoint {
         return CGPoint(x: bounds.width/2, y: bounds.height/2)
     }
     // 경계 부분이 잘릴 수 있으니 -6 정도로 여유를 준다.
     private var radius: CGFloat {
-        return CGFloat(min(initialFrameSize.width, initialFrameSize.height)/2-6)
+        return CGFloat(min(bounds.width, bounds.height)/2-6)
     }
     // AdminViewController에서 의존성 주입.
     var segments: [Segment]? {
@@ -24,13 +24,7 @@ class PieGraphView: UIView, TouchableGraphView {
             self.setNeedsDisplay()
         }
     }
-    // 초기 프레임 사이즈 - 뷰 크기 갱신 위해 고정
-    private var initialFrameSize: CGSize
-    required init?(coder aDecoder: NSCoder) {
-        self.initialFrameSize = CGSize()
-        super.init(coder: aDecoder)
-        self.initialFrameSize = frame.size
-    }
+
     // 모든 세그먼트들의 합 (총 구매 개수)
     private var sum: Int?
     func totalSegmentValues(_ sum: Int?) {
@@ -43,11 +37,11 @@ class PieGraphView: UIView, TouchableGraphView {
     }
 
     override func draw(_ rect: CGRect) {
+        guard let status = status else { return }
         switch status {
-        case .none: drawPieGraph(radius: self.radius)
-        case .began: drawBlackCircle(radius: self.radius)
-        case .moved: drawBlackCircle(radius: self.movedRadius)
-        case .ended: drawPieGraph(radius: self.movedRadius)
+        case .stationary, .ended: drawPieGraph(radius: self.radius)
+        case .began, .moved: drawBlackCircle(radius: self.radius)
+        default: break
         }
     }
 
@@ -68,57 +62,60 @@ class PieGraphView: UIView, TouchableGraphView {
     private func drawBlackCircle(radius: CGFloat?) {
         guard let radius = radius else { return }
         let context = UIGraphicsGetCurrentContext()
-        context?.move(to: origin)
         context?.addArc(center: origin, radius: radius, startAngle: 0, endAngle: 360.radians, clockwise: false)
         context?.setFillColor(UIColor.black.cgColor)
         context?.drawPath(using: .fill)
     }
 
-    var status: TouchStatus = .none
-    private var beganRadiusFromOrigin: CGFloat = 0
-    private var movedRadius: CGFloat? {
-        didSet {
-            // 터치를 통해 원 사이즈가 조정되면 뷰 자체의 사이즈도 같이 조정하도록 노티보냄
-            NotificationCenter.default.post(
-                name: .didUpdatePieSize,
-                object: nil,
-                userInfo: [UserInfoKeys.newPieRadius: movedRadius ?? self.radius])
-        }
-    }
-
-    // bounds가 세팅될 때 코너를 둥글게 처리.
-    override var bounds: CGRect {
-        didSet {
-            self.layer.cornerRadius = self.bounds.size.width/2
-            self.clipsToBounds = true   // 이걸 해줘야 적용된다.
-        }
-    }
+    var status: UITouchPhase?
+    private var beganPosition: CGPoint?
 
     // 터치 시작 - 원점으로부터 터치 위치까지의 거리 저장. 그래프를 검은색 원으로 변경하여 표시.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        status = .began
-        guard let thisTouch = touches.first else { return }
-        beganRadiusFromOrigin = thisTouch.location(in: self).distance(from: origin)
+        status = touches.first?.phase
+        guard let currPosition = touches.first?.location(in: self) else { return }
+        beganPosition = currPosition
         setNeedsDisplay()
     }
 
     // 터치 움직임 - 원점으로부터 현재 터치 위치까지 거리를 radius로 하는 검은색 원 표시.
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        status = .moved
-        guard let thisTouch = touches.first else { return }
-        let currPosition = thisTouch.location(in: self)
-        movedRadius = currPosition.distance(from: origin)
+        status = touches.first?.phase
+        guard let beganPosition = beganPosition else { return }
+        guard let currPosition = touches.first?.location(in: self) else { return }
+        let prevWidth = frame.size.width
+        let prevHeight = frame.size.height
+        let movedDistance = currPosition.distance(from: beganPosition)
+        let scaleFactor: CGFloat = movedDistance/radius*20
+        // 원점과의 거리가 멀어졌다면 scaleFactor 만큼 뷰 크기 확대. 이 때, 부모 뷰의 경계를 넘으면 안 된다.
+        if currPosition.distance(from: origin)-beganPosition.distance(from: origin) > 0 {
+            guard frame.maxX < superview!.frame.maxX && frame.maxY < superview!.frame.maxY else { return }
+            frame.size.width += scaleFactor
+            frame.size.height += scaleFactor
+        } else if currPosition.distance(from: origin)-beganPosition.distance(from: origin) < 0 {
+            // 원점과의 거리가 가까워졌다면 scaleFactor 만큼 뷰 크기 축소.
+            guard radius > 30 else { return }
+            frame.size.width -= scaleFactor
+            frame.size.height -= scaleFactor
+        }
+        frame.origin.x -= (frame.size.width - prevWidth)/2
+        frame.origin.y -= (frame.size.height - prevHeight)/2
         setNeedsDisplay()
     }
 
     // 터치 끝 - 원점으로부터 현재 터치가 끝난 지점까지 거리를 radius로 하는 파이그래프 표시.
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        status = .ended
-        guard let thisTouch = touches.first else { return }
-        let currPosition = thisTouch.location(in: self)
-        movedRadius = currPosition.distance(from: origin)
+        status = touches.first?.phase
         segments?.forEach({ $0.setRandomColor() })
         setNeedsDisplay()
+    }
+
+    // 프레임이 새로 세팅될 때 코너를 둥글게 처리.
+    override var frame: CGRect {
+        didSet {
+            self.layer.cornerRadius = self.bounds.size.width/2
+            self.clipsToBounds = true   // 이걸 해줘야 적용된다.
+        }
     }
 
     // 쉐이크 제스처 시 first responder로 지정 가능하도록 설정
@@ -126,11 +123,20 @@ class PieGraphView: UIView, TouchableGraphView {
         return true
     }
 
+    // 뷰 크기 변경 전 원본 프레임 저장 (복귀 위함)
+    var originalFrame: CGRect
+    required init?(coder aDecoder: NSCoder) {
+        originalFrame = CGRect()
+        super.init(coder: aDecoder)
+        originalFrame = frame
+    }
+
     // 쉐이크 모션 끝날 시, 설정값들을 리셋하여 기본 그래프 표시.
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
-            status = .none
-            movedRadius = nil
+//            self.clearsContextBeforeDrawing = true
+            status = .stationary
+            frame = originalFrame
             setNeedsDisplay()
         }
     }

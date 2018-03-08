d727f29c85694e22d55354d8584d1171e3b14cf5
dely2p <lhyal2p@gmail.com>
2018-03-08 17:54:47 +0900

    add each kind of Beverages object #3
    딸기우유, 바나나우유, 팹시콜라를 생성했다.

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -14,6 +14,17 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        
+        let strawberryMilk: Beverage = StrawberryMilk(brand: "서울우유", weight: 200, price: 1000,
+                        name: "날마다딸기우유", manufactureDate: formatter.date(from: "20171009") ?? Date(),
+                        strawberrySyrup: 5)
+        let bananaMilk: Beverage = BananaMilk(brand: "서울우유", weight: 200, price: 1000, name: "날마다바나나우유",
+                                    manufactureDate: formatter.date(from: "20171012") ?? Date(), bananaSyrup: 3)
+        let coke: Beverage = Coke(brand: "팹시", weight: 350, price: 2000, name: "다이어트콜라",
+                        manufactureDate: formatter.date(from: "20171005") ?? Date(), calorie: 25)
+        
     }
 
     override func didReceiveMemoryWarning() {

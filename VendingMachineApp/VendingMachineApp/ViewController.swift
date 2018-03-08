d8a6641e726e83b0791531a08329edb193eb7b3f
YUAMING <miennes3@gmail.com>
2018-03-08 14:45:19 +0900

    SwiftLint 적용하기

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by yuaming on 08/03/2018.
+//  Copyright © 2018 CodeSquad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

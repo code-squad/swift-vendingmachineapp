2398e10362c89ddbe41ca0b10b9753683328b39c
Mijin Jeon <mayihelpu27@gmail.com>
2018-03-22 15:08:42 +0900

    Move vendingMachine stock description code to Controller

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -12,12 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let coffee = Coffee(brand: "맥심", weight: 350, price: 2000, name: "TOP", manufactured: "20180101")
-        let energyDrink = EnergyDrink(brand: "레드불", weight: 300, price: 3000, name: "레드불", manufactured: "20180201")
-        let chocoMilk = ChocoMilk(brand: "서울우유", weight: 200, price: 900, name: "초코유유", manufactured: "20180319")
-
-        let vendingMachine = VendingMachine(stockItems: [coffee, energyDrink, chocoMilk])
-        print(vendingMachine.showStockDefault())
+        print(Controller().vendingMachineDescription())
     }
 
     override func didReceiveMemoryWarning() {

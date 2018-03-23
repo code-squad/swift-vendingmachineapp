f9b08561fd8247c0665329a7a643fa059e6e4baf
YUAMING <miennes3@gmail.com>
2018-03-23 14:33:51 +0900

    #14: Controllers 그룹 추가

---
+++ VendingMachineApp/VendingMachineApp/VIews/PieGraphView.swift
@@ -0,0 +1,21 @@
+//
+//  PieGraphView.swift
+//  VendingMachineApp
+//
+//  Created by yuaming on 23/03/2018.
+//  Copyright © 2018 CodeSquad. All rights reserved.
+//
+
+import UIKit
+
+class PieGraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

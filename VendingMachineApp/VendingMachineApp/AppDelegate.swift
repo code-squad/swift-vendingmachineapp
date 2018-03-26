48f0868522e1f73b3ddc1fb8e24175d72ecdcce3
Mijin Jeon <mayihelpu27@gmail.com>
2018-03-26 16:06:30 +0900

    Add UserDefaults functions

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -12,22 +12,26 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let vending = VendingMachine(stockItems: Controller().setVendingMachineStock(unit: 1))
+    var vending = VendingMachine(stockItems: Controller().setVendingMachineStock(unit: 1))
 
     // 기존에 저장된 값에서 불러와서 VendingMachine 객체 인스턴스를 생성하기
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+        if UserDefaults.standard.object(forKey: "vendingMachine") != nil {
+            let encodedVending = UserDefaults.standard.data(forKey: "vendingMachine")
+            guard let archivedMachine = NSKeyedUnarchiver.unarchiveObject(with: encodedVending!) as? VendingMachine else { return false }
+            self.vending = archivedMachine
+        }
+            return true
+        }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
+        //  VendingMachine 객체 인스턴스 속성을 저장하기. 저장할 때는 VendingMachine을 아카이브해서 하나의 데이터 값으로 변형한다
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        UserDefaults.standard.set(NSKeyedArchiver.archivedData(withRootObject: self.vending), forKey: "vendingMachine")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -40,9 +44,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     //  VendingMachine 객체 인스턴스 속성을 저장하기. 저장할 때는 VendingMachine을 아카이브해서 하나의 데이터 값으로 변형한다
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        UserDefaults.standard.set(NSKeyedArchiver.archivedData(withRootObject: self.vending), forKey: "vendingMachine")
     }
-
 
 }
 

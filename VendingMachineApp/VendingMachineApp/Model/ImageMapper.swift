54975eeed2bf9c5cbe1fd911b312b1c91419ae90
dely2p <lhyal2p@gmail.com>
2018-03-26 10:29:23 +0900

    fix image name to match class name #41
    이미지 이름 첫번째 문자를 대문자로 고쳐서 클래스 이름과 일치하도록 수정했다. 이를 ImageMapper에서 .jpg를 붙여서 사용하도록 개선함

--- VendingMachineApp/VendingMachineApp/Model/ImageMapper.swift
+++ VendingMachineApp/VendingMachineApp/Model/ImageMapper.swift
@@ -15,9 +15,7 @@ protocol ImageMapper {
 extension ImageMapper {
     var bringImageName: String {
         let name = String(describing: type(of: self))
-        let lowercasedOffirstWord = String(name[name.startIndex]).lowercased()
-        let anotherWords = name.dropFirst()
-        let result = lowercasedOffirstWord + anotherWords + ".jpg"
+        let result = name + ".jpg"
         return result
     }
 }

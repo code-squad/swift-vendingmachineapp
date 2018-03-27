9ba0baeb860a840934cc6906cdf22854369595e6
Mijin Jeon <mayihelpu27@gmail.com>
2018-03-27 14:43:24 +0900

    Add object - DataStorage

---
+++ VendingMachineApp/VendingMachineApp/DataStorage.swift
@@ -0,0 +1,27 @@
+//
+//  DataStorage.swift
+//  VendingMachineApp
+//
+//  Created by YOUTH2 on 2018. 3. 27..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import Foundation
+
+class DataStorage {
+
+    func roadVendingMachine() -> VendingMachine? {
+        // 존재확인
+        if UserDefaults.standard.object(forKey: "vendingMachine") != nil {
+            // key값으로 데이터 가져옴
+            let encodedVending = UserDefaults.standard.data(forKey: "vendingMachine")
+            guard let archivedMachine = NSKeyedUnarchiver.unarchiveObject(with: encodedVending!) as? VendingMachine else { return nil }
+            return archivedMachine
+        }
+        return nil
+    }
+
+    func saveVendingMachine(data: VendingMachine) {
+        UserDefaults.standard.set(NSKeyedArchiver.archivedData(withRootObject: data), forKey: "vendingMachine")
+    }
+}

47184d57c3bc6d1709d1bec3aa7d91a8d30e7a78
YUAMING <miennes3@gmail.com>
2018-03-27 12:18:39 +0900

    #17: 그래프 텍스트 중간 정렬 구현

--- VendingMachineApp/VendingMachineApp/VIews/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/VIews/PieGraphView.swift
@@ -48,7 +48,7 @@ private extension PieGraphItem {
     
     func convertToMenu() -> [String: CGFloat] {
         let result = history.map {
-            return $0.split(separator: ",")[0].trimmingCharacters(in: .whitespaces)
+            return $0.split(separator: ",")[1].trimmingCharacters(in: .whitespaces)
         }
         
         return result.reduce([String: CGFloat]()) { key, count in
@@ -60,7 +60,11 @@ private extension PieGraphItem {
 }
 
 class PieGraphView: UIView {
-    var dataSource: PieGraphViewDataSource?
+    var dataSource: PieGraphViewDataSource? {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
     
     override func layoutSublayers(of layer: CALayer) {
         super.layoutSublayers(of: layer)
@@ -68,37 +72,58 @@ class PieGraphView: UIView {
 
     override func draw(_ rect: CGRect) {
         if let graphItem = self.dataSource?.pieGraphView(self) {
-            drawPieGraphs(graphItem)
+            drawPieGraph(graphItem)
         }
     }
 }
 
 private extension PieGraphView {
-    func drawPieGraphs(_ graphItem: PieGraphItem) {
+    func drawPieGraph(_ graphItem: PieGraphItem) {
         if let context = UIGraphicsGetCurrentContext() {
             let radius = min(frame.size.height, frame.size.height) * 0.5
             let valueCount = graphItem.total
             let viewCenter = CGPoint(x: bounds.size.width * 0.5, y: bounds.size.height * 0.5)
             var startAngle = -CGFloat.pi * 0.5
+            var index = 0
             
             for item in graphItem.convert() {
                 let endAngle = startAngle + 2 * .pi * (item.value / valueCount)
 
-                context.setFillColor(randomColor())
+                context.setFillColor(PieGraphAttribute.pickColor(index))
                 context.move(to: viewCenter)
                 context.addArc(center: viewCenter, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
                 context.fillPath()
                 
+                drawText(startAngle, endAngle, radius, viewCenter, item)
+                
                 startAngle = endAngle
+                index += 1
             }
         }
     }
     
-    func randomColor() -> CGColor {
-        let randomRed: CGFloat = CGFloat(drand48())
-        let randomGreen: CGFloat = CGFloat(drand48())
-        let randomBlue: CGFloat = CGFloat(drand48())
-        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0).cgColor
+    func drawText(_ startAngle: CGFloat, _ endAngle: CGFloat, _ radius: CGFloat, _ viewCenter: CGPoint, _ item: PieGraphItem) {
+        let halfAngle = startAngle + (endAngle - startAngle) * 0.5
+        let textPositionValue : CGFloat = 0.67
+        let textCenter = CGPoint(x: viewCenter.x + radius * textPositionValue * cos(halfAngle), y: viewCenter.y + radius * textPositionValue * sin(halfAngle))
+        let text = "\(item.menu): \(Formatter.ea(Int(item.value)).unit)"
+        
+        var renderRect = CGRect(origin: .zero, size: text.size(withAttributes: PieGraphAttribute.text))
+        
+        renderRect.origin = CGPoint(x: textCenter.x - renderRect.size.width * 0.5, y: textCenter.y - renderRect.size.height * 0.5)
+        
+        text.draw(in: renderRect, withAttributes: PieGraphAttribute.text)
     }
 }
 
+struct PieGraphAttribute {
+    static var text: [NSAttributedStringKey: Any] {
+        return [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 15, weight: .medium),
+                NSAttributedStringKey.foregroundColor: UIColor.black]
+    }
+    
+    static func pickColor(_ index: Int) -> CGColor {
+        let colors: [UIColor] = [UIColor.white, UIColor.brown, UIColor.cyan, UIColor.green, UIColor.magenta, UIColor.purple]
+        return colors[index].cgColor
+    }
+}

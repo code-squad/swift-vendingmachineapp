b4f93a3ac14872e59ac12430b382f2553ba79ee3
Mijin Jeon <mayihelpu27@gmail.com>
2018-04-02 17:31:34 +0900

    Add ImageSelector protocol

---
+++ VendingMachineApp/VendingMachineApp/ImageSelector.swift
@@ -0,0 +1,13 @@
+//
+//  ImageSelector.swift
+//  VendingMachineApp
+//
+//  Created by YOUTH2 on 2018. 4. 2..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import Foundation
+
+protocol ImageSelector {
+    var itemImage: String { get }
+}

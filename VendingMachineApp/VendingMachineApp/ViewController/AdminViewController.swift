ea0ff2f2ef6896888c93ea5062b9f7a3b53c3b5d
Mijin Jeon <mayihelpu27@gmail.com>
2018-04-03 17:21:57 +0900

    Add - AdminViewController

---
+++ VendingMachineApp/VendingMachineApp/ViewController/AdminViewController.swift
@@ -0,0 +1,65 @@
+//
+//  AdminViewController.swift
+//  VendingMachineApp
+//
+//  Created by YOUTH2 on 2018. 4. 3..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+
+    var vending: AdminMode?
+
+    @IBOutlet weak var bananamilkStock: UILabel!
+    @IBOutlet weak var chocomilkStock: UILabel!
+    @IBOutlet weak var coffeeStock: UILabel!
+    @IBOutlet weak var dolcelatteStock: UILabel!
+    @IBOutlet weak var energydrinkStock: UILabel!
+    @IBOutlet weak var softdrinkStock: UILabel!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    private func setIntroView() {
+        self.updateItemNumber()
+    }
+
+    @IBAction func addButtonTouched(sender: UIButton) {
+        guard let item = try? AdminController().pickItem(sender.tag) else { return }
+        vending?.add(inputItem: item)
+    }
+
+    private func updateItemNumber() {
+        guard let vending = self.vending else { return }
+        self.bananamilkStock.text = String(amountFormat: vending.howMany(of: BananaMilk()))
+        self.chocomilkStock.text = String(amountFormat: vending.howMany(of: ChocoMilk()))
+        self.coffeeStock.text = String(amountFormat: vending.howMany(of: Coffee()))
+        self.dolcelatteStock.text = String(amountFormat: vending.howMany(of: DolceLatte()))
+        self.energydrinkStock.text = String(amountFormat: vending.howMany(of: EnergyDrink()))
+        self.softdrinkStock.text = String(amountFormat: vending.howMany(of: SoftDrink()))
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

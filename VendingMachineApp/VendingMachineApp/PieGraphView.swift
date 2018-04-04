ab7495c2dd6323aa1f793b82a834f409747ee41f
Mijin Jeon <mayihelpu27@gmail.com>
2018-04-04 22:57:12 +0900

    Feat: Add PieGraphView

---
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -0,0 +1,23 @@
+//
+//  PieGraphView.swift
+//  VendingMachineApp
+//
+//  Created by YOUTH2 on 2018. 4. 4..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import UIKit
+
+class PieGraphView: UIView {
+
+    var historyData: [Beverage]?
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

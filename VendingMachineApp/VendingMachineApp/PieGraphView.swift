c94d1ad048abf145e022db9473b059697d9c110c
Mijin Jeon <mayihelpu27@gmail.com>
2018-04-05 16:11:49 +0900

    Feat: Add PieGraphView and add one sector shape path in draw()

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -12,12 +12,29 @@ class PieGraphView: UIView {
 
     var historyData: [Beverage]?
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        UIColor.blue.setFill()
+        UIColor.yellow.setStroke()
+        let size = self.bounds
+        let centerX = size.width / 2
+        let centerY = size.height / 2
+        let centerPoint = CGPoint(x: centerX, y: centerY)
+        let radius = min(size.width, size.height) / 2
+        let startAngle: CGFloat = 0
+        let endAngle: CGFloat = CGFloat(120).degreesToRadians
+        let path = UIBezierPath(arcCenter: centerPoint,
+                                radius: radius,
+                                startAngle: startAngle,
+                                endAngle: endAngle,
+                                clockwise: true)
+        path.addLine(to: centerPoint)
+        path.close()
+        path.lineWidth = 10.0
+        path.stroke()
+        path.fill()
     }
-    */
+
+
+
 
 }

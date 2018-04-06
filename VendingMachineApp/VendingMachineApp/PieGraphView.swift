d2cab2cff99229cc56f842ec39542d60a5b7f301
Mijin Jeon <mayihelpu27@gmail.com>
2018-04-07 00:56:46 +0900

    Feat: Add draw Text- beverage label - Added methods: makeText(), coordinatesfromCentralAngle(), textRect() - makeText(): Make and set custom String with specific style and attributes - coordinatesfromCentralAngle(): Get CGpoint between central angle and arc of a pie by using trigonometric functions - textRect(): Make CGRect for drawing beverage lable

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -24,7 +24,14 @@ class PieGraphView: UIView {
         return min(self.bounds.width, self.bounds.height) / 2
     }
 
-
+    var angles: [String: CGFloat]? {
+        let arrangedList = historyData?.reduce(into: [String:Int]()) {
+            $0[$1.type, default: 0] += 1
+        }
+        let angles = arrangedList?.mapValues({CGFloat(CGFloat($0) / CGFloat((historyData?.count)!) * 360)
+        })
+        return angles
+    }
 
     var endRadians: [String: CGFloat]? {
         let arrangedList = historyData?.reduce(into: [String:Int]()) {
@@ -41,12 +48,13 @@ class PieGraphView: UIView {
 
     override func draw(_ rect: CGRect) {
         guard let radiansData = endRadians else {return}
+        guard let anglesData = angles else {return}
         var startArc = CGFloat(0)
         var index = 0
         for list in radiansData.keys {
             let endArc = radiansData[list]!
             self.makePath(from: startArc, to: endArc, cIndex: index)
-
+            makeText(list).draw(in: self.textRect(anglesData[list]!))
             index += 1
             startArc += endArc
         }
@@ -66,9 +74,32 @@ class PieGraphView: UIView {
         return path
     }
 
+    private func makeText(_ myText: String) -> NSAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+
+        let customAttributes = [NSAttributedStringKey.paragraphStyle: paragraphStyle,
+                                NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 23.0),
+                                NSAttributedStringKey.foregroundColor: UIColor.white,
+                                ]
+
+        let customText = NSAttributedString(string: myText,
+                                            attributes: customAttributes)
+        return customText
+    }
+
+    private func coordinatesfromCentralAngle(_ degree: CGFloat) -> (x: CGFloat, y: CGFloat) {
+        let halfDegree = degree / 2
+
+        let pointX = CGFloat(sin(halfDegree * .pi / 180)) * self.radius
+        let pointY = CGFloat(cos(halfDegree * .pi / 180)) * self.radius
+        return (x: pointX, y: pointY)
+    }
 
 
-
-
+    private func textRect(_ degree: CGFloat) -> CGRect {
+        let point = self.coordinatesfromCentralAngle(degree)
+        return CGRect(x: point.x, y: point.x, width: 200, height: 200)
+    }
 
 }

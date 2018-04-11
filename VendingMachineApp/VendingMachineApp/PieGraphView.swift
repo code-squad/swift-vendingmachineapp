41071986a2ee32f340a41a29ae38b192e15f06e7
Mijin Jeon <mayihelpu27@gmail.com>
2018-04-11 15:39:36 +0900

    Style: Arrange codes, pragma mark, Rename variable and functions

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -17,19 +17,8 @@ class PieGraphView: UIView {
         return history
     }
 
-    private var centerPoint: CGPoint {
-        return CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
-    }
-
-    private var radius: CGFloat {
-        return min(self.bounds.width, self.bounds.height) / 2
-    }
-
-    private var changeableRadius: CGFloat = 0.0
-
     private var arrangedHistory: [String:Int] {
         return history.reduce(into: [String:Int]()) { $0[$1.type, default: 0] += 1 }
-
     }
 
     private var endRadians: [String: CGFloat] {
@@ -39,18 +28,28 @@ class PieGraphView: UIView {
         return radians
     }
 
-    private var radiusRatio: CGFloat {
+    private var centerPoint: CGPoint {
+        return CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
+    }
+
+    private var radius: CGFloat {
+        return min(self.bounds.width, self.bounds.height) / 2
+    }
+
+    private var ratioOfNewRadius: CGFloat {
         guard self.drawType == .redrawGraph else {
             return 1.0
         }
-        return self.changeableRadius / self.radius
+        return self.newRadius / self.radius
     }
 
+    private var drawType: DrawType = .defaultGraph
+    private var newRadius: CGFloat = 0.0
     private let degree = 360.0
     private lazy var myFontSize: CGFloat = 23.0
     private lazy var myTextRect = CGSize(width: 130, height: 40)
 
-    enum DrawingType {
+    enum DrawType {
         case defaultGraph
         case blackCircle
         case redrawGraph
@@ -62,21 +61,22 @@ class PieGraphView: UIView {
         case blue
         case purple
 
+        static let allValues = [red, green, blue, purple]
+
         var colorList: [UIColor] {
             switch self {
             case .red:
-                return self.makeColorList(themeColor: UIColor.red)
+                return self.makeList(themeColor: UIColor.red)
             case .green:
-                return self.makeColorList(themeColor: UIColor.green)
+                return self.makeList(themeColor: UIColor.green)
             case .blue:
-                return self.makeColorList(themeColor: UIColor.blue)
+                return self.makeList(themeColor: UIColor.blue)
             case .purple:
-                return self.makeColorList(themeColor: UIColor.purple)
+                return self.makeList(themeColor: UIColor.purple)
             }
         }
-        static let allValues = [red, green, blue, purple]
 
-        func makeColorList(themeColor: UIColor) -> [UIColor] {
+        func makeList(themeColor: UIColor) -> [UIColor] {
             var colors: [UIColor] = []
             for i in 0..<6 {
                 colors.append(themeColor.withAlphaComponent(1.0 - (0.15 * CGFloat(i))))
@@ -85,7 +85,7 @@ class PieGraphView: UIView {
         }
     }
 
-    private var drawType: DrawingType = .defaultGraph
+    // MARK: Draw and Make the graph
 
     override func draw(_ rect: CGRect) {
         switch drawType {
@@ -93,46 +93,61 @@ class PieGraphView: UIView {
             var startArc = CGFloat(0)
             var index = 0
             let colorNumber = Int(arc4random_uniform(UInt32(PieColors.allValues.count)))
+
             for itemName in endRadians.keys {
                 let endArc = endRadians[itemName]!
-                self.makePath(from: startArc, to: endArc, cIndex: index, radius: self.radius, colorNumber: colorNumber)
+                self.makePath(from: startArc,
+                              to: endArc,
+                              colorIndex: index,
+                              radius: self.radius,
+                              colorNumber: colorNumber)
 
-                let arc = self.getArcRadian(startArc: startArc, arc: endArc)
-                makeLabelWithAttributes(of: itemName).draw(in: self.textRect(of: arc))
+                let arc = self.getArcRadian(from: startArc, to: endArc)
+                makeTextWithAttributes(of: itemName).draw(in: self.textRect(of: arc))
 
                 index += 1
                 startArc += endArc
             }
-        case .blackCircle:
-            self.drawBlackCircle(radius: self.changeableRadius)
 
         case .redrawGraph:
             var startArc = CGFloat(0)
             var index = 0
             let colorNumber = Int(arc4random_uniform(UInt32(PieColors.allValues.count)))
+
             for itemName in endRadians.keys {
                 let endArc = endRadians[itemName]!
-                self.makePath(from: startArc, to: endArc, cIndex: index, radius: self.changeableRadius, colorNumber: colorNumber)
+                self.makePath(from: startArc,
+                              to: endArc,
+                              colorIndex: index,
+                              radius: self.newRadius,
+                              colorNumber: colorNumber)
 
-                let arc = self.getArcRadian(startArc: startArc, arc: endArc)
-                makeLabelWithAttributes(of: itemName).draw(in: self.textRect(of: arc))
+                let arc = self.getArcRadian(from: startArc, to: endArc)
+                makeTextWithAttributes(of: itemName).draw(in: self.textRect(of: arc))
 
                 index += 1
                 startArc += endArc
             }
-        }
 
+        case .blackCircle:
+            self.drawBlackCircle(radius: self.newRadius)
+        }
     }
 
-    private func getArcRadian(startArc: CGFloat, arc: CGFloat) -> CGFloat {
-        let endArc = startArc + arc
-        let angle = (startArc + endArc) / 2
+    private func getArcRadian(from start: CGFloat, to end: CGFloat) -> CGFloat {
+        let endArc = start + end
+        let angle = (start + endArc) / 2
         return angle
     }
 
-    private func makePath(from start: CGFloat, to endpoint: CGFloat, cIndex: Int, radius: CGFloat, colorNumber: Int) {
+    private func makePath(from start: CGFloat,
+                          to endpoint: CGFloat,
+                          colorIndex: Int,
+                          radius: CGFloat,
+                          colorNumber: Int)
+    {
         let pieColors = PieColors.allValues[colorNumber].colorList
-        pieColors[cIndex].setFill()
+        pieColors[colorIndex].setFill()
         let end = start + endpoint
         let path = UIBezierPath(arcCenter: centerPoint,
                                 radius: radius,
@@ -142,46 +157,6 @@ class PieGraphView: UIView {
         path.addLine(to: centerPoint)
         path.close()
         path.fill()
-    }
-
-    private func makeLabelWithAttributes(of myText: String) -> NSAttributedString {
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .center
-
-        let shadow = NSShadow()
-        shadow.shadowColor = UIColor.darkGray
-        shadow.shadowOffset = CGSize(width: 2, height: 2)
-        shadow.shadowBlurRadius = 2
-
-        let customAttributes = [NSAttributedStringKey.paragraphStyle: paragraphStyle,
-                                NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: myFontSize * radiusRatio),
-                                NSAttributedStringKey.foregroundColor: UIColor.white,
-                                NSAttributedStringKey.shadow: shadow
-                                ]
-
-        let customText = NSAttributedString(string: myText,
-                                            attributes: customAttributes)
-        return customText
-    }
-
-    private func getOriginOfTextRect(radian: CGFloat, currentRadius: CGFloat) -> CGPoint {
-        let halfRadius = currentRadius / 2
-        let adjustmentX: CGFloat = 75 * radiusRatio
-        let adjustmentY: CGFloat = 25 * radiusRatio
-
-        let pointX = (centerPoint.x + (cos(radian) * halfRadius)) - adjustmentX
-        let pointY = (centerPoint.y + (sin(radian) * halfRadius)) - adjustmentY
-
-        return CGPoint(x: pointX, y: pointY)
-    }
-
-    private func textRect(of radian: CGFloat) -> CGRect {
-        guard self.drawType == .redrawGraph else {
-            let myOrigin = self.getOriginOfTextRect(radian: radian, currentRadius: self.radius)
-            return CGRect(origin: myOrigin, size: self.myTextRect)
-        }
-            let myOrigin = self.getOriginOfTextRect(radian: radian, currentRadius: self.changeableRadius)
-            return CGRect(origin: myOrigin, size: self.myTextRect)
     }
 
     private func drawBlackCircle(radius: CGFloat) {
@@ -197,11 +172,55 @@ class PieGraphView: UIView {
         path.stroke()
     }
 
+     // MARK: Make Text(Labels of the graph)
+
+    private func makeTextWithAttributes(of myText: String) -> NSAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+
+        let shadow = NSShadow()
+        shadow.shadowColor = UIColor.darkGray
+        shadow.shadowOffset = CGSize(width: 2, height: 2)
+        shadow.shadowBlurRadius = 2
+
+        let customAttributes = [NSAttributedStringKey.paragraphStyle: paragraphStyle,
+                                NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: myFontSize * ratioOfNewRadius),
+                                NSAttributedStringKey.foregroundColor: UIColor.white,
+                                NSAttributedStringKey.shadow: shadow
+                                ]
+
+        let customText = NSAttributedString(string: myText,
+                                            attributes: customAttributes)
+        return customText
+    }
+
+    private func textRect(of radian: CGFloat) -> CGRect {
+        guard self.drawType == .redrawGraph else {
+            let myOrigin = self.getOriginOfTextRect(radian: radian, currentRadius: self.radius)
+            return CGRect(origin: myOrigin, size: self.myTextRect)
+        }
+        let myOrigin = self.getOriginOfTextRect(radian: radian, currentRadius: self.newRadius)
+        return CGRect(origin: myOrigin, size: self.myTextRect)
+    }
+
+    private func getOriginOfTextRect(radian: CGFloat, currentRadius: CGFloat) -> CGPoint {
+        let halfRadius = currentRadius / 2
+        let adjustmentX: CGFloat = 75 * ratioOfNewRadius
+        let adjustmentY: CGFloat = 25 * ratioOfNewRadius
+
+        let pointX = (centerPoint.x + (cos(radian) * halfRadius)) - adjustmentX
+        let pointY = (centerPoint.y + (sin(radian) * halfRadius)) - adjustmentY
+
+        return CGPoint(x: pointX, y: pointY)
+    }
+
+    // MARK: Touch events
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         drawType = .blackCircle
         guard let touch = touches.first else { return }
         let newLocation = touch.location(in: self)
-        changeableRadius = sqrt(pow((newLocation.x - centerPoint.x), 2) + pow((newLocation.y - centerPoint.y), 2))
+        newRadius = sqrt(pow((newLocation.x - centerPoint.x), 2) + pow((newLocation.y - centerPoint.y), 2))
         setNeedsDisplay()
     }
 
@@ -209,7 +228,7 @@ class PieGraphView: UIView {
         drawType = .blackCircle
         guard let touch = touches.first else { return }
         let newLocation = touch.location(in: self)
-        changeableRadius = sqrt(pow((newLocation.x - centerPoint.x), 2) + pow((newLocation.y - centerPoint.y), 2))
+        newRadius = sqrt(pow((newLocation.x - centerPoint.x), 2) + pow((newLocation.y - centerPoint.y), 2))
         setNeedsDisplay()
     }
 
@@ -218,7 +237,7 @@ class PieGraphView: UIView {
 
         guard let touch = touches.first else { return }
         let newLocation = touch.location(in: self)
-        changeableRadius = sqrt(pow((newLocation.x - centerPoint.x), 2) + pow((newLocation.y - centerPoint.y), 2))
+        newRadius = sqrt(pow((newLocation.x - centerPoint.x), 2) + pow((newLocation.y - centerPoint.y), 2))
         setNeedsDisplay()
     }
 

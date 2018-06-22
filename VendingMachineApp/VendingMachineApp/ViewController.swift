2333a19d4e560f54259f35709158012e40c8a527
Moon <myssun0325@gmail.com>
2018-06-22 17:53:31 +0900

    Create Project (VendingMachineApp)

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by moon on 2018. 6. 22..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

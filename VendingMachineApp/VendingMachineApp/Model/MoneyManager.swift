65cf868f5ec094d2e5f550ab09791a1140e0013c
Moon <myssun0325@gmail.com>
2018-06-24 18:20:51 +0900

    Add MoneyManager to abstract balance

---
+++ VendingMachineApp/VendingMachineApp/Model/MoneyManager.swift
@@ -0,0 +1,25 @@
+//
+//  MoneyManager.swift
+//  VendingMachineApp
+//
+//  Created by moon on 2018. 6. 24..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import Foundation
+
+class MoneyManager {
+    private var balance: Int = 0
+    
+    func readBalance() -> Int {
+        return self.balance
+    }
+    
+    func increaseBalance(_ price: Int) {
+        self.balance += price
+    }
+    
+    func decreaseBalance(_ price: Int) {
+        self.balance -= price
+    }
+}

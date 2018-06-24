f3408e6054e2d5f7aa48139465e66b7131ce08bd
Moon <myssun0325@gmail.com>
2018-06-24 19:22:25 +0900

    Merge File (Stock > StockManager) to improve readability

--- VendingMachineApp/VendingMachineApp/Model/StockManager.swift
+++ VendingMachineApp/VendingMachineApp/Model/StockManager.swift
@@ -10,7 +10,7 @@ import Foundation
 
 typealias BeverageType = ObjectIdentifier
 
-class StockManager {
+class StockManager: StockManagable {
     private var stock: [BeverageType:Stock]
     
     init(_ stock: [BeverageType:Stock]) {
@@ -20,10 +20,61 @@ class StockManager {
     init() {
         self.stock = [BeverageType:Stock]()
     }
+    
+    var allStock: [Stock] {
+        return self.stock.values.filter { $0.count != 0 }
+    }
+    
+    func addStock(_ beverage: Beverage) {
+        let beverageType = BeverageType(type(of: beverage))
+        if let stock = self.stock[beverageType] {
+            stock.add(beverage)
+        } else {
+            self.stock[beverageType] = Stock([beverage])
+        }
+    }
 }
 
 extension StockManager: Equatable {
     static func == (lhs: StockManager, rhs: StockManager) -> Bool {
         return lhs.stock == rhs.stock
+    }
+}
+
+class Stock {
+    private var beverages: [Beverage]
+    
+    init(_ beverages: [Beverage]) {
+        self.beverages = beverages
+    }
+    
+    convenience init() {
+        self.init([Beverage]())
+    }
+    
+    var count: Int {
+        return self.beverages.count
+    }
+    
+    var beverageName: String {
+        return self.beverages.last?.description ?? ""
+    }
+    
+    func add(_ beverage: Beverage) {
+        self.beverages.append(beverage)
+    }
+}
+
+extension Stock: Equatable {
+    static func == (lhs: Stock, rhs: Stock) -> Bool {
+        return lhs.beverages == rhs.beverages
+    }
+}
+
+extension Stock: Sequence, IteratorProtocol {
+    typealias Element = Beverage
+    
+    func next() -> Element? {
+        return self.beverages.count == 0 ? nil : self.beverages.removeFirst()
     }
 }

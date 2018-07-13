01876b59dd18b2174e0d3d841b950867f9f5922f
cocomilktea <coco_milktea@naver.com>
2018-07-13 15:48:19 +0900

    싱글톤 객체로 직접 접근하는 코드로 변경

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        if let decodeData = DataStorage.roadData() {
+        if let decodeData = DataStorage.loadData() {
             Vendingmachine.loadInstance(decodeData)
         }
         return true

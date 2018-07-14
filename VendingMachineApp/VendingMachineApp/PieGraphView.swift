3ee8e6920bfaf363cf1569663519dcba13d5b491
Moon <myssun0325@gmail.com>
2018-07-14 20:12:30 +0900

    PieGraph 파일 추가

---
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -0,0 +1,21 @@
+//
+//  PieGraphView.swift
+//  VendingMachineApp
+//
+//  Created by moon on 2018. 7. 14..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import UIKit
+
+class PieGraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

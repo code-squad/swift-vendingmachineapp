973d6a8642dde78e233a57a1a5509892112fe671
Moon <myssun0325@gmail.com>
2018-07-16 17:53:45 +0900

    터치를 사용한 그래프 크기 수정기능 구현

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -13,7 +13,9 @@ class PieGraphView: UIView {
     var dataSource: PieGraphViewDataSource?
     
     private let colors = [UIColor.green, UIColor.orange, UIColor.red, UIColor.purple, UIColor.blue]
+    private var isDrawingCircle: Bool = false
     private let lineWidth: CGFloat = 0
+    private var dynamicRadius: CGFloat = 0
     private let textShadow: NSShadow = {
         let shadow = NSShadow()
         shadow.shadowColor = UIColor.gray
@@ -39,19 +41,23 @@ class PieGraphView: UIView {
         var startAngle: CGFloat = 0
         var endAngle: CGFloat = 0
         
-        for (index, count) in dataSource.countList.enumerated() {
-            colors[index].setFill()
-            endAngle = startAngle + (2 * .pi * CGFloat(count.value) / totalCount)
-            drawPie(startAngle, endAngle)
-            let sectionName = NSString(string: count.key)
-            addSectionText(sectionName, startAngle, endAngle)
-            startAngle = endAngle
+        if self.isDrawingCircle {
+            drawBlackCircle(rect)
+        } else {
+            if dynamicRadius == 0 { dynamicRadius = radius }
+            for (index, count) in dataSource.countList.enumerated() {
+                colors[index].setFill()
+                endAngle = startAngle + (2 * .pi * CGFloat(count.value) / totalCount)
+                drawPie(startAngle, endAngle)
+                addSectionText(NSString(string: count.key), startAngle, endAngle)
+                startAngle = endAngle
+            }
         }
     }
     
     private func drawPie(_ startAngle: CGFloat, _ endAngle: CGFloat) {
         let path = UIBezierPath(arcCenter: centerPoint,
-                                radius: radius - lineWidth,
+                                radius: dynamicRadius - lineWidth,
                                 startAngle: startAngle,
                                 endAngle: endAngle,
                                 clockwise: true)
@@ -63,10 +69,56 @@ class PieGraphView: UIView {
     }
     
     private func addSectionText(_ text: NSString, _ startAngle: CGFloat, _ endAngle: CGFloat) {
-        let textPoint = CGPoint(x: centerPoint.x + radius * 0.6 * cos((endAngle + startAngle) / 2 ) - (text.size(withAttributes: nil).width / 2),
-                                y: centerPoint.y + radius * 0.6 * sin((endAngle + startAngle) / 2))
+        let textPoint = CGPoint(x: centerPoint.x + dynamicRadius * 0.6 * cos((endAngle + startAngle) / 2 ) - (text.size(withAttributes: nil).width / 2),
+                                y: centerPoint.y + dynamicRadius * 0.6 * sin((endAngle + startAngle) / 2))
         text.draw(at: textPoint, withAttributes: [NSAttributedStringKey.foregroundColor: UIColor.white,
-                                                  NSAttributedStringKey.font: UIFont.systemFont(ofSize: 20),
+                                                  NSAttributedStringKey.font: UIFont.systemFont(ofSize: 20 * dynamicRadius / radius),
                                                   NSAttributedStringKey.shadow: textShadow])
     }
+    
+    private func drawBlackCircle(_ rect: CGRect) {
+        let path = UIBezierPath(arcCenter: centerPoint,
+                                radius: dynamicRadius,
+                                startAngle: 0,
+                                endAngle: 2 * .pi,
+                                clockwise: true)
+        UIColor.black.setFill()
+        path.stroke()
+        path.fill()
+    }
+    
+    private func calculateDistance(to end: CGPoint, from start: CGPoint) -> CGFloat {
+        let distance = sqrt((end.x - start.x)^^ + (end.y - start.y)^^)
+        if distance > radius {
+            return radius
+        }
+        return distance
+    }
+    
+    // MARK: Touch events
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isDrawingCircle = true
+        guard let location = touches.first?.location(in: self) else { return }
+        let distance = self.calculateDistance(to: location, from: centerPoint)
+        dynamicRadius = distance
+        setNeedsDisplay()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let location = touches.first?.location(in: self) else { return }
+        let distance = self.calculateDistance(to: location, from: centerPoint)
+        dynamicRadius = distance
+        setNeedsDisplay()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isDrawingCircle = false
+        setNeedsDisplay()
+    }
+}
+
+// ^^는 제곱을 하는 사용자 정의 연산자
+postfix operator ^^
+postfix func ^^(value: CGFloat) -> CGFloat {
+    return value * value
 }

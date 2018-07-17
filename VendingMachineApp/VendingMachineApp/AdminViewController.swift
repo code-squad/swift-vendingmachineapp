eeb87dafa003bac77a021966c9f0aa55c3c92e87
cocomilktea <coco_milktea@naver.com>
2018-07-17 17:26:06 +0900

    재고버튼과 재고Label, 음료이미지 AdminViewController에서 동작하도록 구현

--- VendingMachineApp/VendingMachineApp/AdminViewController.swift
+++ VendingMachineApp/VendingMachineApp/AdminViewController.swift
@@ -9,16 +9,48 @@
 import UIKit
 
 class AdminViewController: UIViewController {
+    
+    @IBOutlet var inventoryLabel: [UILabel]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateInventory()
+        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateInventory(notification:)), name: .didUpdateInventory, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    @objc private func didUpdateInventory(notification : Notification) {
+        updateInventory()
+    }
+    
     @IBAction func closeButtonTouched(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addBeverageButtonTouched(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            Vendingmachine.sharedInstance().addPurchases(Coke())
+        case 1:
+            Vendingmachine.sharedInstance().addPurchases(ChocoMilk())
+        case 2:
+            Vendingmachine.sharedInstance().addPurchases(StrawberryMilk())
+        case 3:
+            Vendingmachine.sharedInstance().addPurchases(Top())
+        case 4:
+            Vendingmachine.sharedInstance().addPurchases(Sprite())
+        default:
+            return
+        }
+    }
+    
+    private func updateInventory() {
+        let kinds = Vendingmachine.sharedInstance().makeKindOfBeverage()
+        for index in inventoryLabel.indices {
+            self.inventoryLabel[index].text = "\(Vendingmachine.sharedInstance().countOfInventory(kinds[index]))개"
+        }
     }
 }

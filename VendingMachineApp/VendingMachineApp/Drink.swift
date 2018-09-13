a3292faac673a720ea7b8e4674747866aedfe672
DrakeYang <drake.the.programmer@gmail.com>
2018-09-13 15:46:32 +0900

    기존 자판기 프로젝트의 파일 추가

---
+++ VendingMachineApp/VendingMachineApp/Drink.swift
@@ -0,0 +1,285 @@
+//
+//  Drink.swift
+//  VendingMachine
+//
+//  Created by Yoda Codd on 2018. 7. 19..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+/// 날자 입출력 포맷. 전역변수로 선언
+fileprivate let dateFormat = "yyyyMMdd"
+
+extension Date {
+    func toString ()->String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier:"ko_KR")
+        dateFormatter.dateFormat = dateFormat
+        return dateFormatter.string(from: self)
+    }
+}
+
+extension String{
+    func toDate()->Date?{
+         let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier:"ko_KR")
+        dateFormatter.dateFormat = dateFormat
+        return dateFormatter.date(from: self)
+    }
+}
+
+/// 모든 음료수의 수퍼클래스
+class Drink : CustomStringConvertible {
+    fileprivate let brand : String
+    fileprivate let size : Int
+    fileprivate let price : Int
+    fileprivate let name : String
+    fileprivate let manufacturingDate : Date
+    let drinkType : DrinkType
+    
+    // 직접 생성시 생성자. 날자를 문자열로 입력받기 때문에 잘못된형태를 위해 옵셔널 선언
+    init?(barnd:String, size:Int,price:Int, name:String, manufacturingDateString:String,drinkType:DrinkType) {
+        self.brand = barnd
+        self.size = size
+        self.price = price
+        self.name = name
+        self.drinkType = drinkType
+        guard let newDate = manufacturingDateString.toDate() else {
+            return nil
+        }
+        self.manufacturingDate = newDate
+    }
+    
+    // 복제를 위한 생성자. 생성일자를 Date 로 직접받기 때문에 옵셔널 아님
+    init(barnd:String, size:Int,price:Int, name:String, manufacturingDate:Date,drinkType:DrinkType) {
+        self.brand = barnd
+        self.size = size
+        self.price = price
+        self.name = name
+        self.drinkType = drinkType
+        self.manufacturingDate = manufacturingDate
+    }
+    /// 자기복제 함수
+    func duplicateSelf()->Drink{
+        return Drink(barnd: self.brand, size: self.size, price: self.price, name: self.name
+            , manufacturingDate: self.manufacturingDate, drinkType: self.drinkType)
+    }
+    /// toString 의 역할
+    var description : String  {
+        // 생성자에서 dateFormat 옵셔널 검사가 완료 됬으므로 여기선 강제 래핑
+        return ("\(String(describing: type(of: self))) - \(brand), \(size)ml, \(price)원, \(brand), \(manufacturingDate.toString())")
+    }
+    
+    /// 음료값*주문수량 <= 잔액 인지 체크해주는 함수
+    func calculatePrice(orderCount:Int,balance:Int)throws->Int{
+        if self.price * orderCount <= balance {
+            return self.price * orderCount
+        }
+        else {
+            throw OutputView.errorMessage.notEnoughMoney
+        }
+    }
+}
+
+/// 우유 클래스
+class Milk : Drink {
+    fileprivate let lowFat : Bool
+    
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDateString: String, lowFat: Bool,drinkType:DrinkType) {
+        self.lowFat = lowFat
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDateString: manufacturingDateString,drinkType:drinkType)
+    }
+    init(barnd: String, size: Int, price: Int, name: String, manufacturingDate: Date, lowFat: Bool,drinkType:DrinkType) {
+        self.lowFat = lowFat
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate,drinkType:drinkType)
+    }
+    
+    override func duplicateSelf() -> Milk {
+        return Milk(barnd: super.brand, size:super.size, price: super.price, name: super.name, manufacturingDate: super.manufacturingDate, lowFat: self.lowFat, drinkType: super.drinkType)
+    }
+}
+
+class ChocoMilk : Milk {
+    fileprivate let lowSugar : Bool
+    
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDateString: String, lowFat: Bool, lowSugar:Bool) {
+        self.lowSugar = lowSugar
+        let drinkType : DrinkType = lowSugar ? .lowSugarChocoMilk : .chocoMilk
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDateString: manufacturingDateString, lowFat: lowFat,drinkType:drinkType)
+    }
+    init(barnd: String, size: Int, price: Int, name: String, manufacturingDate: Date, lowFat: Bool, lowSugar:Bool) {
+        self.lowSugar = lowSugar
+        let drinkType : DrinkType = lowSugar ? .lowSugarChocoMilk : .chocoMilk
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate, lowFat: lowFat,drinkType:drinkType)
+    }
+    
+    override func duplicateSelf() -> ChocoMilk {
+        return ChocoMilk(barnd: super.brand, size:super.size, price: super.price, name: super.name, manufacturingDate: super.manufacturingDate, lowFat: super.lowFat, lowSugar: self.lowSugar)
+    }
+}
+
+/// 탄산음료 클래스
+class Soda : Drink {
+    fileprivate let usingPET : Bool
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDateString: String, usingPET: Bool,drinkType:DrinkType) {
+        self.usingPET = usingPET
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDateString: manufacturingDateString,drinkType:drinkType)
+    }
+    init(barnd: String, size: Int, price: Int, name: String, manufacturingDate: Date, usingPET: Bool,drinkType:DrinkType) {
+        self.usingPET = usingPET
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate,drinkType:drinkType)
+    }
+    
+    override func duplicateSelf() -> Soda {
+        return Soda(barnd: super.brand, size:super.size, price: super.price, name: super.name, manufacturingDate: super.manufacturingDate, usingPET: self.usingPET, drinkType: super.drinkType)
+    }
+}
+
+
+class Coke : Soda {
+    fileprivate let zeroCalorie : Bool
+    
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDateString: String, usingPET: Bool,zeroCalorie:Bool) {
+        self.zeroCalorie = zeroCalorie
+        let drinkType : DrinkType = zeroCalorie ? .zeroCalorieCoke : .coke
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDateString: manufacturingDateString, usingPET: usingPET,drinkType:drinkType)
+    }
+    init(barnd: String, size: Int, price: Int, name: String, manufacturingDate: Date, usingPET: Bool,zeroCalorie:Bool) {
+        self.zeroCalorie = zeroCalorie
+        let drinkType : DrinkType = zeroCalorie ? .zeroCalorieCoke : .coke
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate, usingPET: usingPET, drinkType:drinkType)
+    }
+    
+    override func duplicateSelf() -> Coke {
+        return Coke(barnd: super.brand, size:super.size, price: super.price, name: super.name, manufacturingDate: super.manufacturingDate, usingPET: super.usingPET,zeroCalorie:self.zeroCalorie)
+    }
+}
+
+/// 커피 클래스
+class Coffee : Drink{
+    fileprivate let hot : Bool
+    
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDateString: String, hot: Bool,drinkType:DrinkType) {
+        self.hot = hot
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDateString: manufacturingDateString,drinkType:drinkType)
+    }
+    
+    init(barnd: String, size: Int, price: Int, name: String, manufacturingDate: Date, hot: Bool,drinkType:DrinkType) {
+        self.hot = hot
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate,drinkType:drinkType)
+    }
+    
+    override func duplicateSelf() -> Coffee {
+        return Coffee(barnd: super.brand, size:super.size, price: super.price, name: super.name, manufacturingDate: super.manufacturingDate, hot: self.hot, drinkType: super.drinkType)
+    }
+}
+
+
+class TopCoffee : Coffee {
+    fileprivate let zeroSugar : Bool
+    
+    init?(brand: String, size: Int, price: Int, name: String, manufacturingDateString: String,hot: Bool, zeroSugar: Bool) {
+        self.zeroSugar = zeroSugar
+        let drinkType : DrinkType  = zeroSugar ? .hotTopCoffee : .none
+        super.init(barnd: brand, size: size, price: price, name: name, manufacturingDateString: manufacturingDateString,hot: hot,drinkType:drinkType)
+    }
+    
+    init(brand: String, size: Int, price: Int, name: String, manufacturingDate: Date,hot: Bool, zeroSugar: Bool) {
+        self.zeroSugar = zeroSugar
+        let drinkType : DrinkType  = zeroSugar ? .hotTopCoffee : .none
+        super.init(barnd: brand, size: size, price: price, name: name, manufacturingDate: manufacturingDate,hot: hot,drinkType:drinkType)
+    }
+    
+    override func duplicateSelf() -> TopCoffee {
+        return TopCoffee(brand: super.brand, size:super.size, price: super.price, name: super.name, manufacturingDate: super.manufacturingDate, hot: super.hot, zeroSugar: self.zeroSugar)
+    }
+}
+
+/// 에너지 드링크 클래스
+class EnergyDrink : Drink {
+    fileprivate let zeroCaffeine : Bool
+    
+    init?(barnd: String, size: Int, price: Int, name: String, manufacturingDateString: String, zeroCaffeine: Bool) {
+        self.zeroCaffeine = zeroCaffeine
+        let drinkType : DrinkType = zeroCaffeine ? .energyDrink : .none
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDateString: manufacturingDateString, drinkType:drinkType)
+    }
+    
+    init(barnd: String, size: Int, price: Int, name: String, manufacturingDate: Date, zeroCaffeine: Bool) {
+        self.zeroCaffeine = zeroCaffeine
+        let drinkType : DrinkType = zeroCaffeine ? .energyDrink : .none
+        super.init(barnd: barnd, size: size, price: price, name: name, manufacturingDate: manufacturingDate, drinkType:drinkType)
+    }
+    
+    override func duplicateSelf()->EnergyDrink{
+        return EnergyDrink(barnd: self.brand, size: self.size, price: self.price, name: self.name, manufacturingDate: self.manufacturingDate, zeroCaffeine:self.zeroCaffeine)
+    }
+}
+
+
+/// 재고정보를 담당할 구조체. 음료의 정보를 받아서 이름,가격,개수,음료타입
+struct StoredDrinkDetail {
+    let drinkName : String
+    let drinkPrice : Int
+    var drinkCount : Int
+    let drinkType : DrinkType
+    
+    init(drink:Drink,drinkCount: Int){
+        self.drinkName = drink.name
+        self.drinkPrice = drink.price
+        self.drinkCount = drinkCount
+        self.drinkType = drink.drinkType
+    }
+    
+    func isEnoughDrink(orderCount:Int)->Bool{
+        return self.drinkCount >= orderCount
+    }
+}
+
+/// 재고정보를 배열로 가짐
+struct InventoryDetail {
+    var storedDrinksDetail : [StoredDrinkDetail] = []
+    init(){}
+    init(drinkDetails:[StoredDrinkDetail]){
+        self.storedDrinksDetail = drinkDetails
+    }
+    
+    /// 재고 출력 메세지
+    func getAllDrinkDetails()->String{
+        // 결과출력을 위한 변수
+        var result = ""
+        // 번호를 위한 변수
+        var number = 1
+        result += ("---현재 구매가능한 음료수---\n")
+        for drink in storedDrinksDetail {
+            result += ("\(number). \(drink.drinkName)-\(drink.drinkPrice)원-\(drink.drinkCount)개\n")
+            number += 1
+        }
+        result += ("----------------------")
+        return result
+    }
+    
+    mutating func takeDrinkDetail(drinkInventory:StoredDrinkDetail?){
+        if let drinkDetail = drinkInventory {
+            self.storedDrinksDetail.append(drinkDetail)
+        }
+    }
+    
+    func selectDrinkDetail(drinkNumber:Int)throws->StoredDrinkDetail{
+        if drinkNumber > storedDrinksDetail.count {
+            throw OutputView.errorMessage.wrongMenu
+        }
+        return storedDrinksDetail[drinkNumber-1]
+    }
+}
+
+/// 음료주문에 쓰이는 객체. 음료타입, 개수만 가지고 있다
+class OrderDetail{
+    let drinkType : DrinkType
+    let drinkCount : Int
+    init(drinkType : DrinkType,drinkCount : Int){
+        self.drinkType = drinkType
+        self.drinkCount = drinkCount
+    }
+}
---
+++ VendingMachineApp/VendingMachineApp/DrinkSlot.swift
@@ -0,0 +1,143 @@
+//
+//  DrinkSlot.swift
+//  VendingMachine
+//
+//  Created by Yoda Codd on 2018. 8. 24..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+
+/// 음료 종류
+enum DrinkType{
+    case
+     lowSugarChocoMilk
+    ,chocoMilk
+    ,coke
+    ,zeroCalorieCoke
+    ,hotTopCoffee
+    ,energyDrink
+    ,none
+}
+
+/// 음료 한종류를 가지고 있는 객체
+class DrinkSlot<T:Drink> {
+    // 음료배열을 가진다
+    private var drinks:[T]=[]
+    
+    // 어떤 음료의 슬롯인지
+    let drinkType : DrinkType
+    
+    init(drinkType:DrinkType){
+        self.drinkType = drinkType
+    }
+    
+    /// 음료 추가
+    func addDrink(drink:T)throws{
+        if drink.drinkType == self.drinkType {
+            drinks.append(drink)
+        }
+        else {
+           throw OutputView.errorMessage.wrongDrink
+        }
+    }
+    
+    /// 재고 출력
+    func getDrinkDetail()-> StoredDrinkDetail?{
+        // 재고가 있을경우
+        if let drink = self.drinks.first {
+            let inventoryDetail = StoredDrinkDetail(drink:drink, drinkCount: self.drinks.count)
+            return inventoryDetail
+        } // 없을경우
+        else {
+            return nil
+        }
+    }
+    
+    /// 음료 배출
+    func popDrink()throws->T{
+        // 재고 체크
+        if drinks.isEmpty {
+            throw OutputView.errorMessage.notEnoughDrink
+        }
+        // 리턴용 슬롯
+        return drinks.removeFirst()
+    }
+    
+    /// 음료 배출
+    func popDrinks(drinkCount:Int)throws->DrinkSlot<Drink>{
+        // 재고 체크
+        if drinks.count < drinkCount {
+            throw OutputView.errorMessage.notEnoughDrink
+        }
+        // 리턴용 슬롯
+        let restulDrinkSlot = DrinkSlot<Drink>(drinkType: self.drinkType)
+        // 입력개수 만큼 기존 음료슬롯에서 음료를 빼서 결과변수에 추가
+        for _ in 1...drinkCount {
+            try restulDrinkSlot.addDrink(drink: popDrink())
+        }
+        // 결과 리턴
+        return restulDrinkSlot
+    }
+    
+    /// 음료 복제
+    func duplicate(drinkCount:Int)throws->StoredDrinkDetail{
+        // 재고가 있는지 체크
+        guard let firstDrink = self.drinks.first
+            // 없을경우
+            else {
+                throw OutputView.errorMessage.notEnoughDrink
+        }
+        
+        // 입력개수 만큼 증가
+        for _ in 1...drinkCount {
+            // 복제해서 추가할 음료정보
+            let newDrinkInformation = firstDrink.duplicateSelf() as! T
+            
+            // 음료정보를 음료로 변환하여 추가한다
+            drinks.append(newDrinkInformation)
+        }
+        return StoredDrinkDetail(drink: firstDrink, drinkCount: drinkCount)
+    }
+    
+    /// 음료 구입 가능 금액 여부 확인 함수 
+    func calculatePrice(orderCount:Int,balance:Int)throws->Int{
+        if self.drinks.isEmpty == false  {
+            return try drinks[0].calculatePrice(orderCount: orderCount, balance: balance)
+        }
+        throw OutputView.errorMessage.notEnoughMoney
+    }
+    
+    /// 음료슬롯을 받아서 전부 추가
+    func addDrinkSlot(drinkSlot:DrinkSlot<T>)throws->StoredDrinkDetail{
+        // 옮겨진 음료들의 정보를 담는 음료정보 변수
+        guard let result = drinkSlot.getDrinkDetail() else {
+            // 0개면 에러
+            throw OutputView.errorMessage.notEnoughDrink
+        }
+        // 서로 타입이 다른 음료일 경우 에러
+        if self.drinkType != result.drinkType {
+            throw OutputView.errorMessage.wrongDrink
+        }
+        
+        // 음료 개수만큼 음료를 추출한다. 이경우는 전부
+        for _ in 1...result.drinkCount {
+            try self.addDrink(drink: drinkSlot.popDrink())
+        }
+        // 옴기는 과정이 완료되면 결과정보 리턴
+        return result
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+

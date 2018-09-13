52593ece4785da5062333bf28d7fae4c8bcf138b
DrakeYang <drake.the.programmer@gmail.com>
2018-09-13 15:26:01 +0900

    프로젝트 생성

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by Yoda Codd on 2018. 9. 13..
+//  Copyright © 2018년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

312ffdad13762464cfd1fda3922b13eb8e663239
DrakeYang <drake.the.programmer@gmail.com>
2018-09-14 11:58:02 +0900

    자판기 생성함수 삭제

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -16,18 +16,6 @@ class ViewController: UIViewController {
         super.init(coder: aDecoder)!
     }
     
-    // 자판기 생성 및 음료추가 함수
-    func makeVendingMachine()throws->VendingMachine {
-        let vendingMachine = VendingMachine()
-        // 음료수 추가
-        _ = try vendingMachine.addDrink(drink: ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "저과당초코우유", manufacturingDateString: "20171009", lowFat: true, lowSugar: true)!)
-        _ = try vendingMachine.addDrink(drink: ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "저과당초코우유", manufacturingDateString: "20171009", lowFat: true, lowSugar: true)!)
-        _ = try vendingMachine.addDrink(drink: Coke(barnd: "팹시", size: 350, price: 2000, name: "다이어트콜라", manufacturingDateString: "20171005", usingPET: false, zeroCalorie: true)!)
-        _ = try vendingMachine.addDrink(drink: Coke(barnd: "팹시", size: 350, price: 2000, name: "다이어트콜라", manufacturingDateString: "20171005", usingPET: false, zeroCalorie: true)!)
-        _ = try vendingMachine.addDrink(drink: EnergyDrink(barnd: "핫식스", size: 200, price: 1000, name: "핫식스", manufacturingDateString: "20171012", zeroCaffeine: true)!)
-        _ = try vendingMachine.addDrink(drink: EnergyDrink(barnd: "핫식스", size: 200, price: 1000, name: "핫식스", manufacturingDateString: "20171012", zeroCaffeine: true)!)
-        return vendingMachine
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         do {

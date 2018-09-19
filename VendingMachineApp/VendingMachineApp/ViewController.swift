4b6a1c26ed542eece5d8a52ad899915b24571966
DrakeYang <drake.the.programmer@gmail.com>
2018-09-19 14:50:17 +0900

    음료사진 파일면 변경

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,14 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    /// 자판기 잔액표기 갱신 함수
+    func refreshBalance(){
+        self.balance.text =  "\(vendingMachine.getMoney()) 원"
+    }
+    // 자판기 선언
     var vendingMachine : VendingMachine
-    
+    // 생성자
     required init(coder aDecoder: NSCoder) {
         self.vendingMachine = VendingMachine()
         super.init(coder: aDecoder)!
     }
+    // 음료1번 이미지뷰
+    @IBOutlet weak var drink01View: UIImageView!
+    
+    @IBOutlet weak var balance: UILabel!
+    
+    func addBalance(uiButton:UIButton){
+        _ = vendingMachine.plusMoney(money: uiButton.tag)
+        refreshBalance()
+    }
+    @IBAction func addBalance5000(_ sender: UIButton) {
+        addBalance(uiButton: sender)
+    }
+    @IBAction func addBalance10000(_ sender: UIButton) {
+        addBalance(uiButton: sender)
+    }
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
+        self.drink01View.layer.masksToBounds = true
+        self.drink01View.layer.cornerRadius = 30
+        self.drink01View.image = UIImage(named: "Drink01.jpg")
         super.viewDidLoad()
         do {
             _ = try vendingMachine.addDrink(drink: ChocoMilk(barnd: "서울우유", size: 200, price: 1000, name: "저과당초코우유", manufacturingDateString: "20171009", lowFat: true, lowSugar: true)!)
@@ -26,6 +54,8 @@ class ViewController: UIViewController {
             _ = try vendingMachine.addDrink(drink: EnergyDrink(barnd: "핫식스", size: 200, price: 1000, name: "핫식스", manufacturingDateString: "20171012", zeroCaffeine: true)!)
             _ = try vendingMachine.addDrink(drink: EnergyDrink(barnd: "핫식스", size: 200, price: 1000, name: "핫식스", manufacturingDateString: "20171012", zeroCaffeine: true)!)
             print(vendingMachine.getAllAvailableDrinks().getAllDrinkDetails())
+            
+            self.balance.text = "\(vendingMachine.getMoney()) 원"
             
         }
         catch OutputView.errorMessage.failMakingVendingMachine {

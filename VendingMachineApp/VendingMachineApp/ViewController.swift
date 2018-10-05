b10752477a0fb6785804ba2d862486e9229e38d6
oingbong <oingbong@gmail.com>
2018-10-05 14:18:11 +0900

    Add print stock list in ViewController

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let vendingMachine = VendingMachine(with: Stock.prepareStock())
+        if let stockList = vendingMachine.stockList() {
+            for stock in stockList {
+                print("\(stock[0].beverageName()) (\(stock.count)개)")
+            }
+        }
     }
 
 }

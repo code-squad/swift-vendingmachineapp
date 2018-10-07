1df899523e613c349a342c68a5c2eb7b23826ee2
oingbong <oingbong@gmail.com>
2018-10-07 14:05:05 +0900

    1. Add ignore : *.pbxproj / 2. Connect IBAction / 3. Display inventory

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,16 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func addStrawBerryMilkBtn(_ sender: UIButton) {
+    }
+    @IBAction func addChocolateMilkBtn(_ sender: UIButton) {
+    }
+    @IBAction func addBananaMilkBtn(_ sender: UIButton) {
+    }
+    @IBAction func addCokeBtn(_ sender: UIButton) {
+    }
+    @IBAction func addCiderBtn(_ sender: UIButton) {
+    }
+    @IBAction func addFantaBtn(_ sender: UIButton) {
+    }
+    @IBAction func addTopCoffeeBtn(_ sender: UIButton) {
+    }
+    @IBAction func addCantataCoffeeBtn(_ sender: UIButton) {
+    }
+    @IBAction func addGeorgiaCoffeeBtn(_ sender: UIButton) {
+    }
+    
+    @IBOutlet var beverageStock: [UILabel]!
+    @IBOutlet var beverageImages: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let vendingMachine = VendingMachine(with: Stock.prepareStock())
         if let stockList = vendingMachine.stockList() {
-            for stock in stockList {
-                print("\(stock[0].beverageName()) (\(stock.count)개)")
+            for index in 0..<stockList.count {
+                beverageStock[index].text = "\(stockList[index].count)개"
             }
         }
+        
     }
 
 }

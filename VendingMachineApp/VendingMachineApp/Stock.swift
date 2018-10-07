8bf61e7d0b9a85bbd38db7f00fbaa05075a5cd67
oingbong <oingbong@gmail.com>
2018-10-07 13:52:23 +0900

    Edit add beverage : random -> all for matching index in main.storyboard

--- VendingMachineApp/VendingMachineApp/Stock.swift
+++ VendingMachineApp/VendingMachineApp/Stock.swift
@@ -12,17 +12,10 @@ struct Stock {
     // 재고 준비
     public static func prepareStock() -> [[Beverage]] {
         var beverages = [[Beverage]]()
-        let numberOfBeverageType = Int.random(in: 4...8)
-        for _ in 1...numberOfBeverageType {
+        let numberOfBeverageType = Product.allCases.count
+        for index in 1...numberOfBeverageType {
             let numberPerItem = Int.random(in: 1...9)
-            let beverage = AddingBeverage.random(target: numberPerItem)
-            // 음료 중복 확인
-            var isDuplicate = false
-            for savedItem in beverages where beverage[0].className == savedItem[0].className {
-                    isDuplicate = true
-                    break
-            }
-            if isDuplicate { continue }
+            let beverage = AddingBeverage.select(target: index, amount: numberPerItem)
             beverages.append(beverage)
         }
         

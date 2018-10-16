dac2d8797e813a8aeed52da75c96cb194fdd7345
oingbong <oingbong@gmail.com>
2018-10-16 16:44:34 +0900

    Edit : default init -> convenience init

--- VendingMachineApp/VendingMachineApp/Soda.swift
+++ VendingMachineApp/VendingMachineApp/Soda.swift
@@ -34,14 +34,13 @@ class Soda: Beverage {
 class Coke: Soda {
     private var calorie: Int // 칼로리 (kcal)
     
-    init(dateOfManufacture: Date) {
-        self.calorie = 90
-        super.init(sodium: 200, brand: Brand.pepsi.description, capacity: 350, price: 1500, name: Product.dietCoke.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.pepsi.description)
-    }
-    
     init(calorie: Int, sodium: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
         self.calorie = calorie
         super.init(sodium: sodium, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
+    }
+    
+    convenience init(dateOfManufacture: Date) {
+        self.init(calorie: 90, sodium: 200, brand: Brand.pepsi.description, capacity: 350, price: 1500, name: Product.dietCoke.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.pepsi.description)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -62,14 +61,13 @@ class Coke: Soda {
 class Cider: Soda {
     private var transFat: Int // 트랜스지방 (g)
     
-    init(dateOfManufacture: Date) {
-        self.transFat = 0
-        super.init(sodium: 300, brand: Brand.lotteChilsung.description, capacity: 140, price: 1100, name: Product.chilsungCider.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.lotteChilsung.description)
-    }
-    
     init(transFat: Int, sodium: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
         self.transFat = transFat
         super.init(sodium: sodium, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
+    }
+    
+    convenience init(dateOfManufacture: Date) {
+        self.init(transFat: 0, sodium: 300, brand: Brand.lotteChilsung.description, capacity: 140, price: 1100, name: Product.chilsungCider.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.lotteChilsung.description)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -90,14 +88,13 @@ class Cider: Soda {
 class Fanta: Soda {
     private var taste: FantaTaste // 색상 (pineapple , orange , grape , etc..)
     
-    init(dateOfManufacture: Date) {
-        self.taste = FantaTaste.orange
-        super.init(sodium: 100, brand: Brand.cocacola.description, capacity: 355, price: 900, name: Product.orangeFanta.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.cocacola.description)
-    }
-    
     init(taste: FantaTaste, sodium: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
         self.taste = taste
         super.init(sodium: sodium, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
+    }
+    
+    convenience init(dateOfManufacture: Date) {
+        self.init(taste: FantaTaste.orange, sodium: 100, brand: Brand.cocacola.description, capacity: 355, price: 900, name: Product.orangeFanta.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.cocacola.description)
     }
     
     required init?(coder aDecoder: NSCoder) {

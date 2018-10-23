86ef1f49cf72ae625a5df927d8b905cdba96deec
oingbong <oingbong@gmail.com>
2018-10-23 12:10:05 +0900

    Add identifier for using in hash

--- VendingMachineApp/VendingMachineApp/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Coffee.swift
@@ -11,9 +11,9 @@ import Foundation
 class Coffee: Beverage {
     private var caffeine: Int // 카페인함량 (mg)
     
-    init(caffeine: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
+    init(caffeine: Int, identifier: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
         self.caffeine = caffeine
-        super.init(brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
+        super.init(identifier: identifier, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -34,13 +34,13 @@ class Coffee: Beverage {
 class TOP: Coffee {
     private var hot: Bool // 뜨거움 여부
     
-    init(hot: Bool, caffeine: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
+    init(hot: Bool, caffeine: Int, identifier: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
         self.hot = hot
-        super.init(caffeine: caffeine, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
+        super.init(caffeine: caffeine, identifier: identifier, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
     }
     
     convenience init(dateOfManufacture: Date) {
-        self.init(hot: true, caffeine: 120, brand: Brand.maxim.description, capacity: 275, price: 1800, name: Product.topCoffee.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.maxim.description)
+        self.init(hot: true, caffeine: 120, identifier: 301, brand: Brand.maxim.description, capacity: 275, price: 1800, name: Product.topCoffee.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.maxim.description)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -61,13 +61,13 @@ class TOP: Coffee {
 class Cantata: Coffee {
     private var sugarFree: Bool // 무가당여부
     
-    init(sugarFree: Bool, caffeine: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
+    init(sugarFree: Bool, caffeine: Int, identifier: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
         self.sugarFree = sugarFree
-        super.init(caffeine: caffeine, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
+        super.init(caffeine: caffeine, identifier: identifier, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
     }
     
     convenience init(dateOfManufacture: Date) {
-        self.init(sugarFree: true, caffeine: 100, brand: Brand.lotteChilsung.description, capacity: 275, price: 1400, name: Product.cantataCoffee.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.lotteChilsung.description)
+        self.init(sugarFree: true, caffeine: 100, identifier: 302, brand: Brand.lotteChilsung.description, capacity: 275, price: 1400, name: Product.cantataCoffee.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.lotteChilsung.description)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -88,13 +88,13 @@ class Cantata: Coffee {
 class Georgia: Coffee {
     private var packageMaterial: Material // 패키지재질 ( Can , Plastic , Glass , etc..)
 
-    init(packageMaterial: Material, caffeine: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
+    init(packageMaterial: Material, caffeine: Int, identifier: Int, brand: String, capacity: Int, price: Int, name: String, dateOfManufacture: Date, manufacturer: String) {
         self.packageMaterial = packageMaterial
-        super.init(caffeine: caffeine, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
+        super.init(caffeine: caffeine, identifier: identifier, brand: brand, capacity: capacity, price: price, name: name, dateOfManufacture: dateOfManufacture, manufacturer: manufacturer)
     }
     
     convenience init(dateOfManufacture: Date) {
-        self.init(packageMaterial: Material.can, caffeine: 80, brand: Brand.cocacola.description, capacity: 240, price: 1800, name: Product.georgiaCoffee.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.cocacola.description)
+        self.init(packageMaterial: Material.can, caffeine: 80, identifier: 303, brand: Brand.cocacola.description, capacity: 240, price: 1800, name: Product.georgiaCoffee.description, dateOfManufacture: dateOfManufacture, manufacturer: Brand.cocacola.description)
     }
     
     required init?(coder aDecoder: NSCoder) {

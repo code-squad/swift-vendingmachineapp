6e948edfeb7446a3bd4549384d5d88a61b815989
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-29 12:29:23 +0900

    Create Project

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 이동건 on 29/10/2018.
+//  Copyright © 2018 이동건. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

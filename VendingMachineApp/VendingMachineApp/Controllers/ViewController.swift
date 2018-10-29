b553d08d6a79ac5f03f52d8f5681920e2d647206
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-29 18:01:04 +0900

    Setup Methods and Handle deposit

--- VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
@@ -15,17 +15,43 @@ class ViewController: UIViewController {
     private let itemsOnRow = 4
     
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var accountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupVendingMachine()
+    }
+    
+    //MARK: Setup
+    private func setupVendingMachine() {
+        setupNotificationObserver()
+        setupVendingMachineAccount()
+    }
+    
+    private func setupNotificationObserver() {
         NotificationCenter.default.addObserver(self, selector: #selector(handleBeverageAdded), name: VendingMachineNotification.didAdd.name, object: nil)
     }
     
+    private func setupVendingMachineAccount(with money: Int? = nil) {
+        let money: Int = money ?? 0
+        vendingMachine.deposit(money)
+        accountLabel.text = "금액 \(vendingMachine.remain)원"
+    }
+    
+    //MARK: Actions
     @objc func handleBeverageAdded(_ notification: Notification) {
         guard let className = notification.object as? String else { return }
         let beverage = WareHouse.generateBeverage(by: className)
         vendingMachine.add(beverage)
         collectionView.reloadData()
+    }
+    
+    @IBAction func oneThousandDidTapped(_ sender: Any) {
+        setupVendingMachineAccount(with: 1000)
+        
+    }
+    @IBAction func fiveThousandDidTapped(_ sender: Any) {
+        setupVendingMachineAccount(with: 5000)
     }
 }
 

81e04cbc205cc2b823ead7e816bd0f65322d2f40
DrakeYang <drake.the.programmer@gmail.com>
2018-11-20 17:17:36 +0900

    자판기 생성위치 AppDelegate 로 이동. 백그라운드,앱종료 시 자판기저장하도록 추가

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -13,9 +13,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var sharedVendingMachine = VendingMachine.shared()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if let loadedVendingMachine = VendingMachineDataManager.loadVendingMachineData() {
+            sharedVendingMachine = loadedVendingMachine
+        }
+        
         return true
     }
 
@@ -27,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        
+        VendingMachineDataManager.saveVendingMachineData(sharedVendingMachine)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -40,7 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        
+        VendingMachineDataManager.saveVendingMachineData(sharedVendingMachine)
     }
 
 

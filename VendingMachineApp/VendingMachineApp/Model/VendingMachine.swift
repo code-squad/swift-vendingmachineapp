7b6655af01382b7f7128398c1d71ccaf78cc52fd
DrakeYang <drake.the.programmer@gmail.com>
2018-11-20 14:17:27 +0900

    객체내부의 저장관련 함수,변수 삭제

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -27,28 +27,14 @@ class VendingMachine : vendinMachineMenu  {
     private var insertedMoney = 0
     
     /// 재고창고
-    private var drinkInventory = DrinkInventory()
+    private var drinkInventory : DrinkInventory
     
     /// 주문한 음료수가 쌓이는 곳
     private var orderedDrinks = DrinkInventory()
     
-    /// 잔액저장 위치
-    private let moneySavingFile = "money.save"
-    /// 재고창고 저장 위치
-    private let drinkInventorySavingFile = "drinkInventory.save"
-    /// 주문창고 저장 위치
-    private let orderedDrinksSavingFile = "orderedDrinks.save"
-    
     /// 생성자
     init(){
-        // 저장된 내용이 있는지 체크
-        if checkArchavingFiles() {
-            // 저장된 데이터가 자판기객체가 맞다면
-            self.insertedMoney = NSKeyedUnarchiver.unarchiveObject(withFile: moneySavingFile) as! Int
-            self.drinkInventory = NSKeyedUnarchiver.unarchiveObject(withFile: drinkInventorySavingFile) as! DrinkInventory
-            self.orderedDrinks = NSKeyedUnarchiver.unarchiveObject(withFile: orderedDrinksSavingFile) as! DrinkInventory
-        }
-        // 없으면 추가없음
+        drinkInventory = DrinkInventory()
     }
     
     /// 디코더
@@ -65,43 +51,9 @@ class VendingMachine : vendinMachineMenu  {
         aCoder.encode(orderedDrinks, forKey: "orderedDrinks")
     }
     
-    /// 아카이빙 저장 위치
-    func filePath(fileName:String)->String{
-            let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
-            return documentDirectory + fileName
-    }
-    /// 아카이빙 파일이 전부 존재하는지 체크
-    func checkArchavingFiles()->Bool{
-        let fileManager = FileManager()
-        // 저장된 돈,재고,주문내역 내용이 있는지체크
-        if fileManager.fileExists(atPath: filePath(fileName: self.drinkInventorySavingFile)) && fileManager.fileExists(atPath: filePath(fileName: self.moneySavingFile)) && fileManager.fileExists(atPath: filePath(fileName: self.orderedDrinksSavingFile)) {
-            return true
-        }
-        else {
-            return false
-        }
-    }
-    
-    
-    /// 아카이빙 함수
-//    private func save(file:String){
-//        NSKeyedArchiver.archiveRootObject(self, toFile: self.filePath(fileName: file))
-//    }
-    private func saveMoney(){
-        NSKeyedArchiver.archiveRootObject(self.insertedMoney, toFile: self.filePath(fileName: moneySavingFile))
-    }
-    private func saveDrinkInventory(){
-        NSKeyedArchiver.archiveRootObject(self.drinkInventory, toFile: self.filePath(fileName: drinkInventorySavingFile))
-    }
-    private func saveOrderedInventory(){
-        NSKeyedArchiver.archiveRootObject(self.orderedDrinks, toFile: self.filePath(fileName: orderedDrinksSavingFile))
-    }
-    
     /// 금액 추가 함수
     func plusMoney(money:Int)->String{
         self.insertedMoney += money
-        // 아카이빙
-        self.saveMoney()
         return "\(money)원을 추가하였습니다."
     }
     
@@ -112,8 +64,6 @@ class VendingMachine : vendinMachineMenu  {
             throw OutputView.errorMessage.notEnoughMoney
         }
         self.insertedMoney = resultMoney
-        // 아카이빙
-        self.saveMoney()
     }
     
     /// 금액 출력 함수
@@ -128,9 +78,7 @@ class VendingMachine : vendinMachineMenu  {
     
     /// 재고 추가
     func addDrink(drink:Drink)throws->StoredDrinkDetail?{
-        let result = try self.drinkInventory.addInventory(undefinedDrink: drink)
-        // 아카이빙
-        self.saveDrinkInventory()
+        let result = try self.drinkInventory.addDrink(undefinedDrink: drink)
         return result
     }
     
@@ -160,9 +108,6 @@ extension VendingMachine : VendingMachineUserMenu {
         let movedDrinks = try drinkInventory.popDrinks(orderDetail: orderDetail)
         // 이동된 음료를 주문리스트에 넣고 옮겨진 음료정보를 기록한다
         let movedDrinksDetail : StoredDrinkDetail = try self.orderedDrinks.addDrinks(drinks: movedDrinks)
-        // 아카이빙
-        self.saveDrinkInventory()
-        self.saveOrderedInventory()
         // 옮겨진 음료정보를 리턴한다
         return movedDrinksDetail
     }
@@ -177,10 +122,6 @@ extension VendingMachine : VendingMachineUserMenu {
         // 인벤토리->주문내역 으로 음료 이동. 이동된 음료의 정보 저장
         let movedDrinksDetail = try orderedDrinks.addDrinks(drinks: drinkInventory.popDrinks(orderDetail: orderDetail))
         
-        // 아카이빙
-        self.saveDrinkInventory()
-        self.saveOrderedInventory()
-        
         // 완료 메세지 리턴
         return "\(movedDrinksDetail.drinkName) \(movedDrinksDetail.drinkCount)개를 \(totalOrderPrice)원에 구입하였습니다."
     }
@@ -192,9 +133,6 @@ extension VendingMachine : VendingMachineAdminMenu {
         // 음료타입과 개수를 받아서 해당 음료를 리스트로 옮긴다
         let movedDrinks = try drinkInventory.popDrinks(orderDetail: orderDetail)
         
-        // 아카이빙
-        self.saveDrinkInventory()
-        
         // 리스트의 정보를 출력한다
         return movedDrinks.getDrinkDetail()
     }
@@ -205,8 +143,7 @@ extension VendingMachine : VendingMachineAdminMenu {
         guard let movedDrinkDetail = try removeDrinks(orderDetail: orderDetail) else {
             throw OutputView.errorMessage.notEnoughDrink
         }
-        // 아카이빙
-        self.saveDrinkInventory()
+        
         // 완료 메세지 리턴
         return "\(movedDrinkDetail.drinkName) \(movedDrinkDetail.drinkCount)개를 제거하였습니다."
     }
@@ -215,9 +152,6 @@ extension VendingMachine : VendingMachineAdminMenu {
     func duplicateDrink(orderDetail:OrderDetail) throws->String{
         // 음료 추가.
         let resultDetail = try drinkInventory.addDrinkSelfDuplicate(orderDetail: orderDetail)
-        
-        // 아카이빙
-        self.saveDrinkInventory()
         
         // 완료 메세지 리턴
         return "\(resultDetail.drinkName) \(resultDetail.drinkCount)개를 추가하였습니다."
@@ -243,7 +177,30 @@ extension VendingMachine : VendingMachineAdminMenu {
         }
         // 음료 추가
         _ = try addDrink(drink: newDrink)
-        // 아카이빙
-        self.saveDrinkInventory()
+    }
+    
+    /// 자판기 생성용 기본 음료들 추가함수
+    func addBasicDrinkSet()throws{
+        try addBasicDrink(drinkType: DrinkType.chocoMilk)
+        try addBasicDrink(drinkType: DrinkType.lowSugarChocoMilk)
+        try addBasicDrink(drinkType: DrinkType.lowSugarChocoMilk)
+        try addBasicDrink(drinkType: DrinkType.coke)
+        try addBasicDrink(drinkType: DrinkType.coke)
+        try addBasicDrink(drinkType: DrinkType.coke)
+        try addBasicDrink(drinkType: DrinkType.zeroCalorieCoke)
+        try addBasicDrink(drinkType: DrinkType.zeroCalorieCoke)
+        try addBasicDrink(drinkType: DrinkType.zeroCalorieCoke)
+        try addBasicDrink(drinkType: DrinkType.zeroCalorieCoke)
+        try addBasicDrink(drinkType: DrinkType.hotTopCoffee)
+        try addBasicDrink(drinkType: DrinkType.hotTopCoffee)
+        try addBasicDrink(drinkType: DrinkType.hotTopCoffee)
+        try addBasicDrink(drinkType: DrinkType.hotTopCoffee)
+        try addBasicDrink(drinkType: DrinkType.hotTopCoffee)
+        try addBasicDrink(drinkType: DrinkType.energyDrink)
+        try addBasicDrink(drinkType: DrinkType.energyDrink)
+        try addBasicDrink(drinkType: DrinkType.energyDrink)
+        try addBasicDrink(drinkType: DrinkType.energyDrink)
+        try addBasicDrink(drinkType: DrinkType.energyDrink)
+        try addBasicDrink(drinkType: DrinkType.energyDrink)
     }
 }

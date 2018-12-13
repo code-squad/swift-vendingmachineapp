fe80ecb4a8b501a186944405a16b0993864a6a1b
DrakeYang <drake.the.programmer@gmail.com>
2018-12-13 18:45:20 +0900

    원그래프 담당객체 추가

---
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -0,0 +1,21 @@
+//
+//  PieGraphView.swift
+//  VendingMachineApp
+//
+//  Created by Yoda Codd on 2018. 12. 11..
+//  Copyright © 2018년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class PieGraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

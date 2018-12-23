9a7da395df6e078291a61d745a8f590d71cfada3
popsmile <popsmile7@gmail.com>
2018-12-24 00:29:10 +0900

    Create a new Xcode project named VendingMachineApp
    The project is a Single View App.

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 윤지영 on 24/12/2018.
+//  Copyright © 2018 윤지영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

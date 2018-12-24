bc1b00b41c82c0c3b75d38fcabae92ee9c785f39
popsmile <popsmile7@gmail.com>
2018-12-24 11:17:43 +0900

    Add a VendingMachine to the ViewController as a property

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var vendingMachine: VendingMachine
+
+    required init?(coder aDecoder: NSCoder) {
+        let emptyList = [ObjectIdentifier: Pack]()
+        let inventory = Inventory(list: emptyList)
+        self.vendingMachine = VendingMachine(initialInventory: inventory)
+        super.init(coder: aDecoder)
+    }
+
+    private func showBeverages() {
+        let beverages = [Sprite(), Sprite(), Sprite(),
+                         StrawberryMilk(), StrawberryMilk(),
+                         Cantata(), Cantata()]
+        beverages.forEach { vendingMachine.add(beverage: $0) }
+        let form = { (name: String, stock: Int, check: Bool) in
+            print("\(check ? "✅" : "🚫") \(name)(\(stock)개)")
+        }
+        vendingMachine.showListOfAll(with: form)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showBeverages()
     }
-
 
 }
 

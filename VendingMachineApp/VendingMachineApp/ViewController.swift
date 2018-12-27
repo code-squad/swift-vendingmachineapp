78a29102ed492e16dad550baf3dff3ff227da6d9
popsmile <popsmile7@gmail.com>
2018-12-27 16:19:32 +0900

    Add IBActions to insert money to VendingMachine

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     private var vendingMachine: VendingMachine
-
+    @IBOutlet weak var quantityOfStrawberryMilk: UILabel!
+    @IBOutlet weak var quantityOfChocolateMilk: UILabel!
+    @IBOutlet weak var quantityOfSprite: UILabel!
+    @IBOutlet weak var quantityOfPepsi: UILabel!
+    @IBOutlet weak var quantityOfGeorgia: UILabel!
+    @IBOutlet weak var quantityOfCantata: UILabel!
+    @IBOutlet weak var balance: UILabel!
+    
     required init?(coder aDecoder: NSCoder) {
         let emptyList = [ObjectIdentifier: Pack]()
         let inventory = Inventory(list: emptyList)
@@ -32,6 +39,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         showBeverages()
+        vendingMachine.showBalance(with: balanceForm(money:))
+    }
+
+    private func balanceForm(money: Int) {
+        self.balance.text = "잔액: \(money)원"
+    }
+
+    private func insert(money: Int = 0) {
+        guard vendingMachine.insert(money: money) else { return }
+        vendingMachine.showBalance(with: balanceForm(money:))
+    }
+
+    @IBAction func insertOneThousand(_ sender: Any) {
+        insert(money: 1000)
+    }
+
+    @IBAction func insertFiveThousands(_ sender: Any) {
+        insert(money: 5000)
     }
 
 }

51e40e2f308518b9770384e152ee185935a79053
hngfu <djsl1018@naver.com>
2018-12-27 16:23:41 +0900

    자판기 앱 프로젝트 생성

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 조재흥 on 18. 12. 27..
+//  Copyright © 2018 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

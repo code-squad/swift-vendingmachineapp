653416122bce11239552a795591306f0015f97ef
hngfu <djsl1018@naver.com>
2018-12-27 17:25:40 +0900

    자판기에 음료를 넣고 출력했습니다.

--- VendingMachineApp/VendingMachineApp/VendingMachineManagerMode.swift
+++ VendingMachineApp/VendingMachineApp/VendingMachineManagerMode.swift
@@ -1,38 +1,38 @@
+////
+////  VendingMachineManagerMode.swift
+////  VendingMachine
+////
+////  Created by 조재흥 on 18. 12. 27..
+////  Copyright © 2018 JK. All rights reserved.
+////
 //
-//  VendingMachineManagerMode.swift
-//  VendingMachine
+//import Foundation
 //
-//  Created by 조재흥 on 18. 12. 27..
-//  Copyright © 2018 JK. All rights reserved.
+//struct VendingMachineManagerMode {
+//    static func run(_ vendingMachineOfManagerMode: VendingMachineManagerFunction) -> VendingMachine? {
+//        var vendingMachine = vendingMachineOfManagerMode
+//        while true {
+//            let addableList = AddableBeverage.allCases
+//            let inputMent = MentMaker.makeManagerInputMent(addableList: addableList)
+//            let inputString = InputView.readInput(ment: inputMent)
+//            if VendingMachineMode.back.isEqual(input: inputString) {
+//                guard let vendingMachine = vendingMachine as? VendingMachine else {return nil}
+//                return vendingMachine
+//            }
+//            guard ValidChecker.checkManagerInput(string: inputString, countOfBeverage: addableList.count)
+//                else {return nil}
+//            let input = VendingMachineInput(input: inputString)
 //
-
-import Foundation
-
-struct VendingMachineManagerMode {
-    static func run(_ vendingMachineOfManagerMode: VendingMachineManagerFunction) -> VendingMachine? {
-        var vendingMachine = vendingMachineOfManagerMode
-        while true {
-            let addableList = AddableBeverage.allCases
-            let inputMent = MentMaker.makeManagerInputMent(addableList: addableList)
-            let inputString = InputView.readInput(ment: inputMent)
-            if VendingMachineMode.back.isEqual(input: inputString) {
-                guard let vendingMachine = vendingMachine as? VendingMachine else {return nil}
-                return vendingMachine
-            }
-            guard ValidChecker.checkManagerInput(string: inputString, countOfBeverage: addableList.count)
-                else {return nil}
-            let input = VendingMachineInput(input: inputString)
-
-            if input.isModeEqual(1) {
-                guard let addableProduct = input.readBeverage(addableList: addableList) else {return nil}
-                let product = BeverageFactory.produce(addableBeverage: addableProduct)
-                vendingMachine.add(product: product)
-            }
-
-            if input.isModeEqual(2) {
-                vendingMachine.removeExpiredProducts()
-                OutputView.show(result: "유통기한이 지난 음료들을 제거하였습니다.\n")
-            }
-        }
-    }
-}
+//            if input.isModeEqual(1) {
+//                guard let addableProduct = input.readBeverage(addableList: addableList) else {return nil}
+//                let product = BeverageFactory.produce(addableBeverage: addableProduct)
+//                vendingMachine.add(product: product)
+//            }
+//
+//            if input.isModeEqual(2) {
+//                vendingMachine.removeExpiredProducts()
+//                OutputView.show(result: "유통기한이 지난 음료들을 제거하였습니다.\n")
+//            }
+//        }
+//    }
+//}
--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var vendingMachine: VendingMachine = VendingMachine()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        vendingMachine.add(product: CocaCola())
+        vendingMachine.add(product: CocaCola())
+        vendingMachine.add(product: CocaColaZero())
+        vendingMachine.add(product: StarbucksDoubleShot())
+        let inventory = vendingMachine.checkInventory()
+        for (product, count) in inventory {
+            print("\(product)(\(count)개)")
+        }
     }
-
-
 }
 

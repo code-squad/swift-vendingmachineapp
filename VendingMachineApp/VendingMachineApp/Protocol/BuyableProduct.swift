3da8835189d4d0a3f05154d2e20db4ac354fe872
hngfu <djsl1018@naver.com>
2018-12-31 15:54:18 +0900

    BuyableProduct 추가

---
+++ VendingMachineApp/VendingMachineApp/Protocol/BuyableProduct.swift
@@ -0,0 +1,13 @@
+//
+//  BuyableProduct.swift
+//  VendingMachineApp
+//
+//  Created by 조재흥 on 18. 12. 31..
+//  Copyright © 2018 hngfu. All rights reserved.
+//
+
+import Foundation
+
+protocol BuyableProduct {
+    init()
+}
---
+++ VendingMachineApp/VendingMachineApp/Protocol/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  VendingMachineApp
+//
+//  Created by 조재흥 on 18. 12. 30..
+//  Copyright © 2018 hngfu. All rights reserved.
+//
+
+import Foundation

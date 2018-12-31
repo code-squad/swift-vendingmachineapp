f26e315bb03f743c5718e237bc3f6870232ffb7c
hngfu <djsl1018@naver.com>
2018-12-31 16:51:37 +0900

    tapAddBeverageButton(_:)으로 이름 변경

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func tapAddBeverage(_ sender: UIButton) {
+    @IBAction func tapAddBeverageButton(_ sender: UIButton) {
         let beverage: Beverage
         
         switch sender.restorationIdentifier {

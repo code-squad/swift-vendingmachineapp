e38957e70a556f335f30867915728aa793879f25
hngfu <djsl1018@naver.com>
2018-12-31 16:49:53 +0900

    tapAddBeverage(_:) 추가
    버튼을 누르면 상품을 추가

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -14,5 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    @IBAction func tapAddBeverage(_ sender: UIButton) {
+        let beverage: Beverage
+        
+        switch sender.restorationIdentifier {
+        case "mandarineMilk":
+            beverage = Beverage.produce(product: MandarineMilk.self)
+        case "lactoseFreeMilk":
+            beverage = Beverage.produce(product: LactoseFreeMilk.self)
+        case "starbucksDoubleShot":
+            beverage = Beverage.produce(product: StarbucksDoubleShot.self)
+        case "topTheBlack":
+            beverage = Beverage.produce(product: TOPTheBlack.self)
+        case "cocaCola":
+            beverage = Beverage.produce(product: CocaCola.self)
+        case "cocaColaZero":
+            beverage = Beverage.produce(product: CocaColaZero.self)
+        default:
+            return
+        }
+        
+        vendingMachine.add(product: beverage)
+    }
 }
 

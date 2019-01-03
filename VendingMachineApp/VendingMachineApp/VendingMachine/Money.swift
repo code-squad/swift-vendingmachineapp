be5b89118aa9ea80652eeb3468c1b619d918928e
popsmile <popsmile7@gmail.com>
2019-01-03 11:37:59 +0900

    Create a struct named Money

---
+++ VendingMachineApp/VendingMachineApp/VendingMachine/Money.swift
@@ -0,0 +1,48 @@
+//
+//  Money.swift
+//  VendingMachineApp
+//
+//  Created by 윤지영 on 03/01/2019.
+//  Copyright © 2019 윤지영. All rights reserved.
+//
+
+import Foundation
+
+struct Money {
+    private var balance: Int
+
+    enum Unit: Int {
+        case oneThousand = 1000
+        case fiveThousands = 5000
+    }
+
+    init(initialBalance: Int = 0) {
+        self.balance = initialBalance
+    }
+
+    init(unit: Money.Unit) {
+        self.balance = unit.rawValue
+    }
+
+    func isPositive() -> Bool {
+        return balance > 0
+    }
+
+    static func +(_ lhs: Money, _ rhs: Money) -> Money {
+        let sum = lhs.balance + rhs.balance
+        return Money(initialBalance: sum)
+    }
+
+    mutating func deductedPrice(of beverage: Beverage) {
+        balance = beverage.subtractPrice(from: balance)
+    }
+
+    func show(with show: (Int) -> Void) {
+        show(balance)
+    }
+
+    func isEnoughToBuy(pack: Pack) -> Bool {
+        return pack.isBuyable(with: balance)
+    }
+
+}

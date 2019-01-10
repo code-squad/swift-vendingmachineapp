1e175acf33103480d4eb50cebb421d5e7ad7e940
hngfu <djsl1018@naver.com>
2019-01-10 12:26:05 +0900

    BeverageArchiveKey enum -> struct 다시 만듦

---
+++ VendingMachineApp/VendingMachineApp/BeverageArchiveKey.swift
@@ -0,0 +1,19 @@
+//
+//  BeverageArchiveKey.swift
+//  VendingMachineApp
+//
+//  Created by 조재흥 on 19. 1. 10..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct BeverageArchiveKey {
+    //MARK: - Beverage
+    static let brand: String = "brand"
+    static let size: String = "size"
+    static let price: String = "price"
+    static let name: String = "name"
+    static let openDate: String = "openDate"
+    static let expiryDate: String = "expiryDate"
+}

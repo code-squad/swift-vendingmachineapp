82b7420d611d252aba2089d41c33dd6ea976eab9
hngfu <djsl1018@naver.com>
2019-01-10 12:13:08 +0900

    BeverageArchiveKey 생성

---
+++ VendingMachineApp/VendingMachineApp/Enum/BeverageArchiveKey.swift
@@ -0,0 +1,18 @@
+//
+//  BeverageArchiveKey.swift
+//  VendingMachineApp
+//
+//  Created by 조재흥 on 19. 1. 10..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+enum BeverageArchiveKey: String {
+    case brand = "brand"
+    case size = "size"
+    case price = "price"
+    case name = "name"
+    case openDate = "openDate"
+    case expiryDate = "expiryDate"
+}

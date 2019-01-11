17549bf6535f188470db3bdc4b631fbd44d7f11d
hngfu <djsl1018@naver.com>
2019-01-11 21:24:33 +0900

    HistoryOfPurchase 추가

---
+++ VendingMachineApp/VendingMachineApp/HistoryOfPurchase.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryOfPurchase.swift
+//  VendingMachineApp
+//
+//  Created by 조재흥 on 19. 1. 11..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+class HistoryOfPurchase {
+    private var historyOfPurchase: [Beverage] = []
+    
+    
+}

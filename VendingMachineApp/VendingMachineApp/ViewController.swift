fad022ed57b4e26ce8951cff0cfd5938e1c33897
Dongmin.yoon <ehdals8965@naver.com>
2019-01-15 11:57:43 +0900

    Change Setting iPhone -> iPad 10.5

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 윤동민 on 15/01/2019.
+//  Copyright © 2019 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

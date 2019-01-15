43cf79ad047cac00d4b1256a147bb15d77d47ab0
popsmile <popsmile7@gmail.com>
2019-01-15 17:30:44 +0900

    Remove the direct access to the instance of VendingMachine in the ViewController

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let viewController = window?.rootViewController as? ViewController
+        viewController?.set(vendingMachine: VendingMachine.shared)
         return true
     }
 

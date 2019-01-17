c77c18280d70f79df738a36f691b6028ce89541a
popsmile <popsmile7@gmail.com>
2019-01-17 14:25:48 +0900

    Create a new UIViewController named AdminViewController

---
+++ VendingMachineApp/VendingMachineApp/AdminViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdminViewController.swift
+//  VendingMachineApp
+//
+//  Created by 윤지영 on 17/01/2019.
+//  Copyright © 2019 윤지영. All rights reserved.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

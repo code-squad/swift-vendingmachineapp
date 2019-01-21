e351883c9f0af01b2ceb9aaf093cb14fca248ba3
popsmile <popsmile7@gmail.com>
2019-01-21 12:22:50 +0900

    Add the override method to draw a pie graph

--- VendingMachineApp/VendingMachineApp/CustomView/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/CustomView/PieGraphView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class PieGraphView: UIView {
     private var purchases: [Beverage] = []
+    private let palette: [UIColor] = [.green, .red, .blue, .yellow, .gray, .purple]
 
     var countOfPurchase: Int {
         return purchases.count
@@ -17,6 +18,32 @@ class PieGraphView: UIView {
 
     func updated(with beverages: ArraySlice<Beverage>) {
         beverages.forEach { purchases.append($0) }
+    }
+
+    private var classifiedPurchase: [String: Int] {
+        return purchases.reduce(into: [:]) { $0[$1.title, default: 0] += 1 }
+    }
+
+    override func draw(_ rect: CGRect) {
+        let center = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        let radius = self.frame.width / 2
+        var colorIndex = 0
+        let angleInCircle = { (count: Int) in 2 * .pi * (CGFloat(count) / CGFloat(self.purchases.count)) }
+        var currentAngle: CGFloat = 0
+        for purchase in classifiedPurchase {
+            let angle = angleInCircle(purchase.value)
+            let endAngle = currentAngle + angle
+            let path = UIBezierPath(arcCenter: center,
+                                    radius: radius,
+                                    startAngle: currentAngle,
+                                    endAngle: endAngle,
+                                    clockwise: true)
+            currentAngle = endAngle
+            palette[colorIndex].setFill()
+            colorIndex += 1
+            path.addLine(to: center)
+            path.fill()
+        }
     }
 
 }

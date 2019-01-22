039fbfd160eea486e71a3bb06b591fa325347099
hngfu <djsl1018@naver.com>
2019-01-22 11:25:41 +0900

    ManagerViewController를 추가하고 새로 만든 ViewController에 연결

---
+++ VendingMachineApp/VendingMachineApp/ManagerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ManagerViewController.swift
+//  VendingMachineApp
+//
+//  Created by 조재흥 on 19. 1. 22..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class ManagerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

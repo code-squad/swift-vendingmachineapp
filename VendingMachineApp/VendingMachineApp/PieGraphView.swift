357c60d147b4d45c975b895cc19d5f9bc57f4c2a
hngfu <djsl1018@naver.com>
2019-01-23 18:26:40 +0900

    PieGraphView생성

---
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -0,0 +1,21 @@
+//
+//  PieGraphView.swift
+//  VendingMachineApp
+//
+//  Created by 조재흥 on 19. 1. 23..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class PieGraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

bd167a7e76693aeecbb929c6decb4391f3f24870
hngfu <djsl1018@naver.com>
2019-01-25 02:10:08 +0900

    makeLabel(name:startAngle:angleFromStartToEnd:) 생성 및 각 차트 영역위에 이름 레이블이 오도록 만듦

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -14,7 +14,7 @@ class PieGraphView: UIView {
     //MARK: - Properties
     
     private var purchaseInfo: [String: CGFloat] = ["가": 1, "나": 2, "다": 3, "라": 4, "마": 5, "사": 6,]
-    private var colors: [UIColor] = [.black, .green, .orange, .red, .purple, .blue,]
+    private var colors: [UIColor] = [.black, .gray, .orange, .red, .purple, .blue,]
     
     //MARK: - Methods
     
@@ -23,26 +23,53 @@ class PieGraphView: UIView {
         let countOfAll = purchaseInfo.values.reduce(0) {$0 + $1}
         var startAngle: CGFloat = 0
         
-        for (_, count) in purchaseInfo {
-            let endAngle = startAngle + (count / countOfAll)  * .pi * 2
-            let path = makePath(startAngle: startAngle, endAngle: endAngle)
-            startAngle = endAngle
+        for (name, count) in purchaseInfo {
+            let angleFromStartToEnd = (count / countOfAll)  * .pi * 2
+            let path = makePath(startAngle: startAngle, angleFromStartToEnd: angleFromStartToEnd)
             colors.popLast()?.setFill()
             path.fill()
+            let label = makeLabel(name: name, startAngle: startAngle, angleFromStartToEnd: angleFromStartToEnd / 2)
+            startAngle += angleFromStartToEnd
+            addSubview(label)
         }
     }
     
-    private func makePath(startAngle: CGFloat, endAngle: CGFloat) -> UIBezierPath {
+    //MARK: Private
+    
+    private func makePath(startAngle: CGFloat, angleFromStartToEnd: CGFloat) -> UIBezierPath {
         
         let path = UIBezierPath()
         path.move(to: CGPoint(x: bounds.midX, y: bounds.midY))
         path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY),
                     radius: bounds.width / 2,
                     startAngle: startAngle,
-                    endAngle: endAngle,
+                    endAngle: startAngle + angleFromStartToEnd,
                     clockwise: true)
         path.close()
         
         return path
+    }
+    
+    private func makeLabel(name: String, startAngle: CGFloat, angleFromStartToEnd: CGFloat) -> UILabel {
+        
+        let label = UILabel()
+        label.text = name
+        label.textAlignment = .center
+        label.textColor = UIColor.white
+        label.frame.size = CGSize(width: 20, height: 20)
+        
+        let path = UIBezierPath(arcCenter: CGPoint(x: bounds.midX, y: bounds.midY),
+                                radius: bounds.width / 2,
+                                startAngle: startAngle,
+                                endAngle: startAngle + angleFromStartToEnd,
+                                clockwise: true)
+        
+        let x = (path.currentPoint.x - bounds.midX) / 2 + bounds.midX
+        let y = (path.currentPoint.y - bounds.midY) / 2 + bounds.midY
+        
+        let position = CGPoint(x: x, y: y)
+        label.center = position
+        
+        return label
     }
 }

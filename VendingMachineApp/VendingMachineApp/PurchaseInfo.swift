131d15f474c61659561e030c08c624af0da42312
hngfu <djsl1018@naver.com>
2019-01-25 02:54:00 +0900

    PurchaseInfo에 count(name:) 생성

--- VendingMachineApp/VendingMachineApp/PurchaseInfo.swift
+++ VendingMachineApp/VendingMachineApp/PurchaseInfo.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 struct PurchaseInfo {
+    
+    //MARK: - Properties
+    //MARK: Private
+    
     private var purchaseInfo: [String: CGFloat]
+    
+    //MARK: - Methods
+    
+    mutating func count(name: String) {
+        
+        if purchaseInfo[name] == nil {
+            purchaseInfo[name] = 0
+        }
+        
+        guard let number = purchaseInfo[name] else { return }
+        
+        purchaseInfo[name] = number + 1
+    }
+    
 }

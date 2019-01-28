ba5d871b061a43ec21af4b55d01b1f3ffc8495ca
Dongmin.yoon <ehdals8965@naver.com>
2019-01-28 17:08:58 +0900

    Add New ViewController and Implement ViewTransition

---
+++ VendingMachineApp/VendingMachineApp/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ManagerModeViewController.swift
+//  VendingMachineApp
+//
+//  Created by 윤동민 on 28/01/2019.
+//  Copyright © 2019 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func exit(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

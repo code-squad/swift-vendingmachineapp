f2b1478185b90d7c28a7125d10c2343938b6f062
Dongmin.yoon <ehdals8965@naver.com>
2019-01-28 18:06:55 +0900

    Implement MangerMode Function which add Stocks in machine and Delete AddStock Function in UserMode

--- VendingMachineApp/VendingMachineApp/SecondViewController.swift
+++ VendingMachineApp/VendingMachineApp/SecondViewController.swift
@@ -9,25 +9,55 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    @IBOutlet var addButtons: [UIButton]!
+    @IBOutlet var drinkLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(updateDrinkLabel), name: .stockChanged, object: nil)
+        initialAddButtonTag()
+        initialLabel()
+    }
+    
+    @objc func updateDrinkLabel() {
+        let commonMode: CommonAvailableMachine = VendingMachine.sharedInstance
+        for menu in DrinkCategory.allCases {
+            commonMode.markDrinkLabel(menu) { drinkCounts in
+                self.drinkLabels[menu.rawValue-1].text = "\(drinkCounts)개"
+            }
+        }
+    }
+    
+    private func initialAddButtonTag() {
+        var tag = 1
+        for button in addButtons {
+            button.tag = tag
+            tag += 1
+        }
+    }
+    
+    private func initialLabel() {
+        let machine: CommonAvailableMachine = VendingMachine.sharedInstance
+        for menu in DrinkCategory.allCases {
+            machine.markDrinkLabel(menu) { drinkCounts in
+                self.drinkLabels[menu.rawValue-1].text = "\(drinkCounts)개"
+            }
+        }
+    }
+    
+    @IBAction func addStocks(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+        guard let menu = DrinkCategory(rawValue: button.tag) else { return }
+        addEachDrink(of: menu)
+    }
+    
+    private func addEachDrink(of menu: DrinkCategory) {
+        let managerMode: ManageableMode = VendingMachine.sharedInstance
+        managerMode.addStock(menu: menu)
     }
     
     @IBAction func exit(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

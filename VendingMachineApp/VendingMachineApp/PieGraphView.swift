9b5b4e127c1aac2efa9b8b020458986a1ee9f3ac
Dongmin.yoon <ehdals8965@naver.com>
2019-01-31 14:19:09 +0900

    Add UIView and Create PipGraghView Class

---
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -0,0 +1,23 @@
+//
+//  PieGraphView.swift
+//  VendingMachineApp
+//
+//  Created by 윤동민 on 31/01/2019.
+//  Copyright © 2019 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class PieGraphView: UIView {
+    
+    var purchaseList: [Beverage] = []
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

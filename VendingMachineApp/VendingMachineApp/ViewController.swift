cfb1f7f83052a89be5f09fa85c268ecef936b292
conyconydev <conyconydev@gmail.com>
2019-05-10 17:28:14 +0900

    update : ViewController 재고 목록을 print()

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var vendingMachine: VendingMachine
+    
+    // 'required' initializer 'init(coder:)' must be provided by subclass of 'UIViewController'
+    
+    required init?(coder: NSCoder) {
+        self.vendingMachine = VendingMachine(list: Inventory(list: [ObjectIdentifier: Packages]()))
+        super.init(coder: coder)
+    }
+    
+    private func showBeverageList() {
+        let beverages = [Sprite(), Sprite(), Sprite(), CocaCola(),
+                         CocaCola(), CocaCola(),CocaCola(),
+                         ChocolateMilk(), ChocolateMilk(),
+                         BananaMilk(), BananaMilk(),
+                         CantataCoffee(), CantataCoffee(), CantataCoffee(),
+                         StarbucksCoffee(), StarbucksCoffee()]
+        
+        beverages.forEach { beverage in vendingMachine.add(beverage: beverage) }
+        
+        let form = { (name: String, goods: Int, mark: Bool)
+            in
+            print("\(mark ? "✅" : "🚫") \(name)(\(goods)개)")
+        }
+        vendingMachine.showListOfAll(list: form)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showBeverageList()
     }
 
 
 }
-

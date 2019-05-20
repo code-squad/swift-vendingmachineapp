4a77d6604c5959f0dd10186685732f782dbde1a9
conyconydev <conyconydev@gmail.com>
2019-05-20 19:05:00 +0900

    update : RoundImageView 분리

--- VendingMachineApp/VendingMachineApp/Mode/View/RoundImageView.swift
+++ VendingMachineApp/VendingMachineApp/Mode/View/RoundImageView.swift
@@ -5,3 +5,16 @@
 //  Created by Elena on 20/05/2019.
 //  Copyright © 2019 elena. All rights reserved.
 //
+
+import UIKit
+
+class RoundImageView: UIViewController{
+    
+    static func makeRound(index: [UIImageView]) {
+        for num in index {
+            num.layer.borderColor = UIColor.gray.cgColor
+            num.layer.borderWidth = 2
+            num.layer.cornerRadius = 10
+        }
+    }
+}

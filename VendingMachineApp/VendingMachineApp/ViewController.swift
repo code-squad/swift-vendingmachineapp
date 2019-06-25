8dee4d39f0895c4f53b399f8898b8b41ee2fd11b
kiljoonho <kiljh@me.com>
2019-06-25 16:58:55 +0900

    생성 - 자판기 앱 프로젝트 생성

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by joon-ho kil on 6/25/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var vendingMachine = VendingMachine()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

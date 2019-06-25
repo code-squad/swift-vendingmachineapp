6569e9c030d0ca095c158639e7eca30b095adfa3
kiljoonho <kiljh@me.com>
2019-06-25 21:11:51 +0900

    구현 - 음료 추가 버튼 연동

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -14,13 +14,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        vendingMachine.supply(1, amount: 1)
-        vendingMachine.supply(2, amount: 2)
+        
+        
         vendingMachine.supply(6, amount: 1)
         
         print(vendingMachine.getStockList())
     }
 
-
+    @IBAction func bananaMilkSupply(_ sender: Any) {
+        vendingMachine.supply(1, amount: 1)
+    }
+    @IBAction func strawberryMilkSupply(_ sender: Any) {
+        vendingMachine.supply(2, amount: 1)
+    }
+    @IBAction func fantaSupply(_ sender: Any) {
+        vendingMachine.supply(3, amount: 1)
+    }
+    @IBAction func topSupply(_ sender: Any) {
+        vendingMachine.supply(4, amount: 1)
+    }
+    @IBAction func hot6Supply(_ sender: Any) {
+        vendingMachine.supply(5, amount: 1)
+    }
+    @IBAction func pepsiCokeSupply(_ sender: Any) {
+        vendingMachine.supply(6, amount: 1)
+    }
+    
 }
 

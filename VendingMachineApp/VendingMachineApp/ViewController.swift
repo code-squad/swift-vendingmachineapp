8d7c72e3203cd1397a1914a5d3c3497ccdf5a78e
kiljoonho <kiljh@me.com>
2019-06-26 11:23:24 +0900

    수정 - 음료를 tag 값을 가지고 공급하도록 수정

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -27,43 +27,39 @@ class ViewController: UIViewController {
         
         
     }
+    
+    func refreshDrinkCount () {
+        let stock = vendingMachine.getStockList()
+        
+        for (drink, count) in stock {
+            if drink is BananaMilk {
+                bananaMilkCount.text = "\(count)개"
+            }
+            if drink is StrawberryMilk {
+                strawberryMilkCount.text = "\(count)개"
+            }
+            if drink is Fanta {
+                fantaCount.text = "\(count)개"
+            }
+            if drink is TOP {
+                topCount.text = "\(count)개"
+            }
+            if drink is Hot6 {
+                hot6Count.text = "\(count)개"
+            }
+            if drink is PepsiCoke {
+                pepsiCokeCount.text = "\(count)개"
+            }
+        }
+    }
 
-    @IBAction func bananaMilkSupply(_ sender: Any) {
-        vendingMachine.supply(1, amount: 1)
-        let stock = vendingMachine.getStockList()
-        let bananaMilk = BananaMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190301", farmCode: 5, expirationDate: "20190930")
-        self.bananaMilkCount.text = "\(stock[bananaMilk]!)개"
+   
+
+    @IBAction func drinkSupply(_ sender: UIButton) {
+        vendingMachine.supply(sender.tag, amount: 1)
+        refreshDrinkCount()
     }
-    @IBAction func strawberryMilkSupply(_ sender: Any) {
-        vendingMachine.supply(2, amount: 1)
-        let stock = vendingMachine.getStockList()
-        let strawberryMilk = StrawberryMilk(brand: "빙그레", ml: 200, price: 1300, productDate: "20190405", farmCode: 3, expirationDate: "20190925")
-        self.strawberryMilkCount.text = "\(stock[strawberryMilk]!)개"
-    }
-    @IBAction func fantaSupply(_ sender: Any) {
-        vendingMachine.supply(3, amount: 1)
-        let stock = vendingMachine.getStockList()
-        let fanta = Fanta(brand: "롯데", ml: 350, price: 2000, productDate: "20190505", orangeIncense: 0.7, expirationDate: "20190921")
-        self.fantaCount.text = "\(stock[fanta]!)개"
-    }
-    @IBAction func topSupply(_ sender: Any) {
-        vendingMachine.supply(4, amount: 1)
-        let stock = vendingMachine.getStockList()
-        let top = TOP(brand: "맥심", ml: 400, price: 3000, productDate: "20190606", hot: false, expirationDate: "20190920")
-        self.topCount.text = "\(stock[top]!)개"
-    }
-    @IBAction func hot6Supply(_ sender: Any) {
-        vendingMachine.supply(5, amount: 1)
-        let stock = vendingMachine.getStockList()
-        let hot6 = Hot6(brand: "롯데", ml: 240, price: 1000, productDate: "20190529", expirationDate: "20191029")
-        self.hot6Count.text = "\(stock[hot6]!)개"
-    }
-    @IBAction func pepsiCokeSupply(_ sender: Any) {
-        vendingMachine.supply(6, amount: 1)
-        let stock = vendingMachine.getStockList()
-        let pepsiCoke = PepsiCoke(brand: "펩시", ml: 255, price: 1200, productDate: "20190610", expirationDate: "20191010")
-        self.pepsiCokeCount.text = "\(stock[pepsiCoke]!)개"
-    }
+    
     @IBAction func insertOneThousand(_ sender: Any) {
         vendingMachine.insertCoin(1000)
         let balance = vendingMachine.getBalance()

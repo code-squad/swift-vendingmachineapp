3bdb2a824f339bee441528ffcc25e34c57a73eb4
kiljoonho <kiljh@me.com>
2019-06-28 14:05:34 +0900

    수정 - 뷰가 열릴 때 저장되있는 재고와 잔고 업데이트

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -17,15 +17,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var pepsiCokeCount: UILabel!
     @IBOutlet weak var balance: UILabel!
     
-    
-    private var vendingMachine = VendingMachine()
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var vendingMachine = VendingMachine()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        vendingMachine = appDelegate.vendingMachine
         
+        let stock = UserDefaults.standard.object(forKey: "stock") as? [Int]
+        let balance = UserDefaults.standard.object(forKey: "balance") as? Int
         
+        if let stock = stock {
+            for (index, amount) in stock.enumerated() {
+                vendingMachine.supply(index, amount: amount)
+            }
+        }
+        
+        if let balance = balance {
+            vendingMachine.insertCoin(balance)
+        }
+        
+        refreshDrinkCount()
+        refreshBalance()
     }
     
     private func refreshDrinkCount () {
@@ -57,6 +72,13 @@ class ViewController: UIViewController {
         let balance = vendingMachine.getBalance()
         self.balance.text = "\(balance)원"
     }
+    
+//    private func drinkToLabel(_ drink: Drink) -> UILabel {
+//        let supplyableDrinks = SupplyableDrinkList.getSupplyableDrinkList
+////        let drinkLabels = [supplyableDrinks[0]: bananaMilkCount]
+//
+//
+//    }
 
     @IBAction func drinkSupply(_ sender: UIButton) {
         vendingMachine.supply(sender.tag, amount: 1)

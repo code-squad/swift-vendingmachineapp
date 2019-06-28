96df27a7fb8a94cf8d76ff44832bc40645a5291a
kiljoonho <kiljh@me.com>
2019-06-28 14:07:25 +0900

    추가 - 화면을 떠날 때 현재 재고와 잔고 저장

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -12,16 +12,30 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var vendingMachine = VendingMachine()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        let stock = vendingMachine.getStockList()
+        let ableDrinks = vendingMachine.getAbleDrinks()
+        let balance = vendingMachine.getBalance()
+        
+        let stockToSave = ableDrinks.map { (drink) -> Int in
+            return stock[drink] ?? 0
+        }
+        
+        let balanceToSave = Int("\(balance)")
+        UserDefaults.standard.set(stockToSave, forKey: "stock")
+        UserDefaults.standard.set(balanceToSave, forKey: "balance")
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -30,17 +44,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
+        
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 

e3270c2773984fa1253a0c22af554afd4c57ac95
conyconydev <conyconydev@gmail.com>
2019-07-01 22:45:09 +0900

    update : 관리자모드 추가 , 뷰 전환하면서 데이터를 전달하기 위해 prepare 구현

--- VendingMachineApp/VendingMachineApp/AdminViewController.swift
+++ VendingMachineApp/VendingMachineApp/AdminViewController.swift
@@ -6,4 +6,55 @@
 //  Copyright © 2019 elena. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AdminViewController: UIViewController {
+    // MARK: - private variable
+    private weak var vendingMachine: VendingMachine?
+    
+    // MARK: - @IBOutlet
+    @IBOutlet var beverageImageView: [RoundImageView]!
+    @IBOutlet var beverageLabel: [UILabel]!
+    
+    // MARK: - private
+    private func showQuantity(index: Int) {
+        let count = vendingMachine?.count(beverage: index)
+        beverageLabel[index].text = "\(count ?? 0)개"
+    }
+    
+    // MARK: - func
+    func set(vendingMachine: VendingMachine?) {
+        self.vendingMachine = vendingMachine
+    }
+    
+    // MARK: - @IBAction
+    @IBAction func addBeverage(_ sender: UIButton) {
+        let beverage = sender.tag
+        guard vendingMachine?.add(beverage: beverage) ?? false else { return }
+    }
+    
+    @IBAction func closeButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(objcShowQuantity(_:)), name: .addBeverage, object: nil)
+        
+        for index in beverageLabel.indices {
+            showQuantity(index: index)
+        }
+    }
+    
+    // MARK: - @objc
+    @objc private func objcShowQuantity(_ notification: Notification) {
+        if let data = notification.userInfo?[Notification.NotiKey.purchaseIndex] as? Int {
+            showQuantity(index: data)
+            return
+        }
+        for data in beverageLabel.indices {
+            showQuantity(index: data)
+        }
+    }
+}

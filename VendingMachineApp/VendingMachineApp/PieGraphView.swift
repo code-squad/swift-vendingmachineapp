b8c60f26308839602c0717345c5a5337b3170245
conyconydev <conyconydev@gmail.com>
2019-07-02 13:47:09 +0900

    update : UIView 상속받는 PieGraphView 클래스 생성

---
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -0,0 +1,21 @@
+//
+//  PieGraphView.swift
+//  VendingMachineApp
+//
+//  Created by Elena on 02/07/2019.
+//  Copyright © 2019 elena. All rights reserved.
+//
+
+import UIKit
+
+class PieGraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

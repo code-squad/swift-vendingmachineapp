20fbd315750ee105eaf42df25a2a037ffb296119
kiljoonho <kiljh@me.com>
2019-07-02 21:53:34 +0900

    생성 - 하드코딩된 한글을 enum 의 rawValue 로 구현

---
+++ VendingMachineApp/VendingMachineApp/KoreanUnit.swift
@@ -0,0 +1,14 @@
+//
+//  KoreanUnit.swift
+//  VendingMachineApp
+//
+//  Created by joon-ho kil on 7/2/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import Foundation
+
+enum KoreanUnit: String {
+    case count = "개"
+    case won = "원"
+}

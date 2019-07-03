4fb42dcbcda1fa73d4982b02a4d37ad7c3bab792
kiljoonho <kiljh@me.com>
2019-07-03 11:38:58 +0900

    생성 - 자판기 잔고를 출력할 수 있는 프로토콜 생성

---
+++ VendingMachineApp/VendingMachineApp/BalancePrintable.swift
@@ -0,0 +1,13 @@
+//
+//  BalancePrintable.swift
+//  VendingMachineApp
+//
+//  Created by joon-ho kil on 7/3/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import Foundation
+
+protocol BalancePrintable {
+    func printBalance () -> Money
+}

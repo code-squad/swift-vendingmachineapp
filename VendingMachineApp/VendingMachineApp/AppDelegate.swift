7715d866e8822deedda7ee9b7771066380a1bbca
kiljoonho <kiljh@me.com>
2019-07-08 18:38:52 +0900

    추가 - 루트 뷰 컨트롤러에 벤딩 머신 인스턴스를 보내는 코드 추가

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -16,9 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+    
         loadVendingMachine()
+        let viewController = window!.rootViewController as! ViewController
         
+        viewController.vendingMachine = vendingMachine
         return true
     }
 

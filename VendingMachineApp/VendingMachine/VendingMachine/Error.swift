dde797e015488921114aef88688d4b4dfc8472cc
김지나 <jinagold.k@gmail.com>
2019-07-09 14:29:49 +0900

    음료를 가지고 처리하는 위치 수정

--- VendingMachineApp/VendingMachine/VendingMachine/Error.swift
+++ VendingMachineApp/VendingMachine/VendingMachine/Error.swift
@@ -19,12 +19,12 @@ enum InputError: String, Error {
     }
 }
 
-typealias userChoice = (menu: Inventory.Menu, value: Int)
+typealias userChoice = (menu: VendingMachine.Menu, value: Int)
 
 // 첫 입력 확인
 func incorrect(_ input: String) throws -> userChoice {
     let UserInput = input.split(separator: " ").map { String($0) }
-    guard let menuNum = UserInput.first, let menu = Inventory.Menu(rawValue: menuNum) else { throw InputError.incorrect }
+    guard let menuNum = UserInput.first, let menu = VendingMachine.Menu(rawValue: menuNum) else { throw InputError.incorrect }
     guard let inputNum = UserInput.last, let value = Int(inputNum) else { throw InputError.incorrect }
     if Int(menuNum)! > 2 { throw InputError.incorrect }
     return (menu, value)
@@ -33,12 +33,12 @@ func incorrect(_ input: String) throws -> userChoice {
 // 두 번째 입력 확인
 func checkAvailability(of machine: VendingMachine, _ select: Int) throws {
     let balance = machine.balance()
-    let status = machine.currentBeverageStatus()
+//    let status = machine.currentBeverageStatus()
     
     // 올바른 음료를 선택했는지 확인
-    if select >= status.count { throw InputError.incorrect }
-    // 현재의 잔액으로 선택한 음료의 구매 가능 여부 확인
-    if status[select].beveragaPrice > balance { throw InputError.notEnoughBalance }
-    // 선택한 음료의 구매 가능한 재고의 존재 여부
-    if status[select].beverageCount == 0 { throw InputError.outOfStock }
+//    if select >= status.count { throw InputError.incorrect }
+//    // 현재의 잔액으로 선택한 음료의 구매 가능 여부 확인
+//    if status[select].beveragaPrice > balance { throw InputError.notEnoughBalance }
+//    // 선택한 음료의 구매 가능한 재고의 존재 여부
+//    if status[select].beverageCount == 0 { throw InputError.outOfStock }
 }

b90ed7be318dfc1fcbd272f34ef79e85c3a3379e
김지나 <jinagold.k@gmail.com>
2019-07-10 16:45:57 +0900

    파일 참조 관계 변경

---
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -0,0 +1,92 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by 김지나 on 22/04/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class VendingMachine {
+    enum Menu: String {
+        case insertMoney = "1"
+        case buyBeverage = "2"
+    }
+    
+    private var list: [Beverage]
+    private var currentBalance: Int
+    private var purchase = [String]()
+    
+    init(_ list: [Beverage], currentBalance: Int = 0) {
+        self.list = list
+        self.currentBalance = currentBalance
+    }
+    
+    // 현재 보유 중인 음료 리턴
+    func drinks() -> [Beverage] {
+        return self.list
+    }
+    
+    // 자판기 금액을 변경해주는 메소드
+    func insert(money: Int) {
+        self.currentBalance = currentBalance + money
+    }
+    
+    // 현재 잔액 리턴
+    func balance() -> Int {
+        return self.currentBalance
+    }
+    
+
+    /// 음료 내역
+//    // 현재 음료 현황 리턴
+//    func currentBeverageStatus() -> [Inventory] {
+//        return currentBeverage
+//    }
+    
+    
+    /// 음료수 구매
+    //구매한 음료 처리
+    func sell(beverageNumber: Int) {
+        for inner in 0 ..< list.count {
+            if beverageNumber == inner {
+//                subtract(from: inner)
+//                purchaseList(from: inner)
+//                deduct(money: inner)
+            }
+        }
+    }
+
+    // 시작이후 구매 상품 이력을 배열로 리턴하는 메소드
+//    private func purchaseList(from productNumber: Int) {
+//        self.purchase.append(currentBeverage[productNumber].beverageName)
+//    }
+//
+//    // 재고 마이너스
+//    private func subtract(from inventory: Int) {
+//        self.currentBeverage[inventory].beverageCount -= 1
+//    }
+//
+//    // 잔액 마이너스
+//    private func deduct(money: Int) {
+//        self.currentBalance = currentBalance - currentBeverage[money].beveragaPrice
+//    }
+
+    
+    /// 특정 음료 구분
+    // 유통기한이 지난 재고만 리턴하는 메소드
+    private func notValidDate() -> [Beverage] {
+        return list.filter{ $0.validate() == false }
+    }
+
+    // 따뜻한 음료만 리턴하는 메소드
+    private func hotBeverage() -> [Beverage] {
+        let coffee = list.filter{
+            if let drink = $0 as? Coffee, drink.isHot() { return drink.isHot() }
+            return false
+        }
+        return coffee
+    }
+}
+

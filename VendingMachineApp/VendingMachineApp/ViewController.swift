c9f15ce8cb20872e7ce051f8e679d6ec145a9617
gukjin jang <jgj455@gmail.com>
2019-07-15 21:24:38 +0900

    Modify: viewDidLoad() 에 출력문 구현

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let strawberryMilk = StrawberryMilk()
+    let chocolateMilk = ChocolateMilk()
+    let coke = Coke()
+    let sprite = Sprite()
+    let topCoffee = TOPCoffee()
+    let cantataCoffee = CantataCoffee()
     
+    var vendingMachine = VendingMachine()
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        vendingMachine.stockUp(of: strawberryMilk, count: 2)
+        vendingMachine.stockUp(of: chocolateMilk, count: 1)
+        vendingMachine.stockUp(of: coke, count: 1)
+        
+        for (key, value) in vendingMachine.isFullStock() {
+            print("\(key.getName()) (\(value)개)", terminator: " ")
+        }
     }
 
 }

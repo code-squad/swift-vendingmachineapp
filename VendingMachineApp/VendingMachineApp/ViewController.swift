c1717de3d1afae28a4273ccdb7533c4003d6c939
Sunghyun Kim <sunghyun_k@icloud.com>
2019-07-16 16:06:37 +0900

    프로젝트 생성

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 김성현 on 16/07/2019.
+//  Copyright © 2019 김성현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

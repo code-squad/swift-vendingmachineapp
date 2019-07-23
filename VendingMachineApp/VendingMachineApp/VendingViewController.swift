2688972982ec21d74d8911cfdf68181f6f57c7fc
hwj0623 <hwj0623@naver.com>
2019-07-23 15:48:51 +0900

    자판기 잔액 충전 버튼기능 추가

--- VendingMachineApp/VendingMachineApp/VendingViewController.swift
+++ VendingMachineApp/VendingMachineApp/VendingViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class VendingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
     private var vendingMachine: VendingMachine!
+    @IBOutlet weak var balanceInfo: UILabel!
+    
     override func viewDidLoad() {
         vendingMachine = MockVendingMachineCreator.initializeVendingMachine()
         printInitialDrinkMenuList()
+        updateBalance()
         super.viewDidLoad()
+    }
+    
+    private func updateBalance(){
+        vendingMachine.showCurrentBalanceInfo(printFormat: { (balance) in
+            balanceInfo.text = "\(balance)원"
+        })
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -24,7 +33,6 @@ class VendingViewController: UIViewController, UICollectionViewDelegate, UIColle
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let info = vendingMachine.showDrinkStockTable()
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as! GridCell
-        print(indexPath.item)
         cell.updateDrinkInfo(drinkStock: info, index: indexPath.item)
         return cell
     }
@@ -35,11 +43,26 @@ class VendingViewController: UIViewController, UICollectionViewDelegate, UIColle
             sortedMenutable.forEach{ (key: Int, value: DrinkItemList) in
                 print( "\(value.drinkName)(\(value.drinkStockList.count)개)", terminator: " ")
             }
-            print()
         }
         vendingMachine.displayDrinkMenuList(printFormat: printDrinkMenuListFormat)
     }
+    
+    @IBAction func handleChargeBalanceButton(_ sender: UIButton) {
+        guard let balanceSize = sender.currentTitle else {
+            return
+        }
+        var chargeInput = 0
+        if balanceSize == "+1000"{
+            chargeInput = 1000
+        }else if balanceSize == "+5000"{
+            chargeInput = 5000
+        }
+        vendingMachine.chargeBalance(chargeInput)
+        updateBalance()
+    }
+
 }
+
 extension VendingViewController: UICollectionViewDelegateFlowLayout {
     ///cell size를 기기에 맞게 설정
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -53,6 +76,17 @@ class GridCell: UICollectionViewCell{
     @IBOutlet var imgView: UIImageView!
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var stockInfoLabel: UILabel!
+    private var itemIndex: Int!
+    
+    @IBAction func testButton(_ sender: UIButton) {
+        guard let currentStock = stockInfoLabel.text else{
+            return
+        }
+        guard let stockSize = Int(currentStock.components(separatedBy: "개")[0]) else{
+            return
+        }
+        stockInfoLabel.text = "\(stockSize+1)개"
+    }
     
     func updateDrinkInfo(drinkStock: DrinkStockTable, index: Int){
         guard let img: UIImage = UIImage.init(named: "\(index+1).jpg") else{
@@ -63,5 +97,6 @@ class GridCell: UICollectionViewCell{
             return
         }
         stockInfoLabel.text = "\(drinkItemList.drinkStockList.count)개"
+        itemIndex = index
     }
 }

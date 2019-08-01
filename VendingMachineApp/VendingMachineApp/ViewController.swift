9dafb3a5f93a2c17487bc98f44b762238a18d0da
gukjin jang <jgj455@gmail.com>
2019-08-01 17:13:43 +0900

    Add: 각 Button과 Label에 IBAction과 IBOutlet 추가

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -17,16 +17,68 @@ class ViewController: UIViewController {
     let cantataCoffee = CantataCoffee()
     
     private var vendingMachine = VendingMachine()
+    
+    @IBOutlet weak var stockOfStrawberryMilk: UILabel!
+    @IBOutlet weak var stockOfChocolateMilk: UILabel!
+    @IBOutlet weak var stockOfCoke: UILabel!
+    @IBOutlet weak var stockOfSprite: UILabel!
+    @IBOutlet weak var StockOfTOPCoffee: UILabel!
+    @IBOutlet weak var stockOfCantataCoffee: UILabel!
+    @IBOutlet weak var depositedMoney: UILabel!
+    
+    @IBAction func addStrawberryMilk(_ sender: Any) {
+        vendingMachine.stockUp(of: strawberryMilk, count: 1)
+        self.stockOfStrawberryMilk.text = "\(vendingMachine.isFullStock()[strawberryMilk]!)개"
+    }
+    @IBAction func addChocolateMilk(_ sender: Any) {
+        vendingMachine.stockUp(of: chocolateMilk, count: 1)
+        self.stockOfChocolateMilk.text = "\(vendingMachine.isFullStock()[chocolateMilk]!)개"
+    }
+    @IBAction func addCoke(_ sender: Any) {
+        vendingMachine.stockUp(of: coke, count: 1)
+        self.stockOfCoke.text = "\(vendingMachine.isFullStock()[coke]!)개"
+    }
+    @IBAction func addSprite(_ sender: Any) {
+        vendingMachine.stockUp(of: sprite, count: 1)
+        self.stockOfSprite.text = "\(vendingMachine.isFullStock()[sprite]!)개"
+    }
+    @IBAction func addTOPCoffee(_ sender: Any) {
+        vendingMachine.stockUp(of: topCoffee, count: 1)
+        self.StockOfTOPCoffee.text = "\(vendingMachine.isFullStock()[topCoffee]!)개"
+    }
+    @IBAction func addCantataCoffee(_ sender: Any) {
+        vendingMachine.stockUp(of: cantataCoffee, count: 1)
+        self.stockOfCantataCoffee.text = "\(vendingMachine.isFullStock()[cantataCoffee]!)개"
+    }
+    @IBAction func inputSmallMoney(_ sender: Any) {
+        vendingMachine.pay(of: 1000)
+        self.depositedMoney.text = "잔액: \(vendingMachine.checkBalance())"
+    }
+    @IBAction func inputLargeMoney(_ sender: Any) {
+        vendingMachine.pay(of: 5000)
+        self.depositedMoney.text = "잔액: \(vendingMachine.checkBalance())"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        vendingMachine.stockUp(of: strawberryMilk, count: 2)
-        vendingMachine.stockUp(of: chocolateMilk, count: 1)
-        vendingMachine.stockUp(of: coke, count: 1)
+        vendingMachine.stockUp(of: strawberryMilk, count: 5)
+        vendingMachine.stockUp(of: chocolateMilk, count: 5)
+        vendingMachine.stockUp(of: coke, count: 5)
+        vendingMachine.stockUp(of: sprite, count: 5)
+        vendingMachine.stockUp(of: topCoffee, count: 5)
+        vendingMachine.stockUp(of: cantataCoffee, count: 5)
         
         for (key, value) in vendingMachine.isFullStock() {
             print("\(key.getName()) (\(value)개)", terminator: " ")
         }
+        
+        self.stockOfStrawberryMilk.text = "\(vendingMachine.isFullStock()[strawberryMilk]!)개"
+        self.stockOfChocolateMilk.text = "\(vendingMachine.isFullStock()[chocolateMilk]!)개"
+        self.stockOfCoke.text = "\(vendingMachine.isFullStock()[coke]!)개"
+        self.stockOfSprite.text = "\(vendingMachine.isFullStock()[sprite]!)개"
+        self.StockOfTOPCoffee.text = "\(vendingMachine.isFullStock()[topCoffee]!)개"
+        self.stockOfCantataCoffee.text = "\(vendingMachine.isFullStock()[cantataCoffee]!)개"
+        self.depositedMoney.text = "잔액: \(vendingMachine.checkBalance())"
     }
-
 }
 

c4876571b40d087d3995cd01cfde46ae77f10a0e
Sunghyun Kim <sunghyun_k@icloud.com>
2019-08-02 14:58:24 +0900

    우유 클래스 수정

--- VendingMachineApp/VendingMachineApp/Milk.swift
+++ VendingMachineApp/VendingMachineApp/Milk.swift
@@ -1,22 +1,22 @@
-import Foundation
+import UIKit
 
 class Milk: Beverage {
     private(set) var milkFactoryIdentifier: String
     
-    init(brand: String, packageSize: Int, price: Int, name: String, milkFactoryIdentifier: String, dateOfManufacture: Date) {
-        self.milkFactoryIdentifier = milkFactoryIdentifier
-        super.init(brand: brand, packageSize: packageSize, price: price, name: name, dateOfManufacture: dateOfManufacture, expirationPeriod: Date.timeInterval(fromDays: 14), beverageTemperature: 10)
+    init(name: String, price: Coin, photo: UIImage?, brand: String, packageSize: Int, milkFactoryIdentifier: String) {
+        
+        super.init(name: name, price: price, photo: photo, brand: brand, packageSize: packageSize, beverageTemperature: 10, expirationPeriod: Date.timeInterval(fromDays: 14))
     }
 }
 
 class StrawberryMilk: Milk {
-    init(milkFactoryIdentifier: String, dateOfManufacture: Date) {
-        super.init(brand: "매일유업", packageSize: 200, price: 500, name: "딸기 우유", milkFactoryIdentifier: milkFactoryIdentifier, dateOfManufacture: dateOfManufacture)
+    init() {
+        super.init(name: "딸기 우유", price: 500, photo: UIImage(named: "Strawberry"), brand: "매일유업", packageSize: 200, milkFactoryIdentifier: "매일우유목장")
     }
 }
 
 class ChocolateMilk: Milk {
-    init(milkFactoryIdentifier: String, dateOfManufacture: Date) {
-        super.init(brand: "남양", packageSize: 180, price: 600, name: "초코 우유", milkFactoryIdentifier: milkFactoryIdentifier, dateOfManufacture: dateOfManufacture)
+    init() {
+        super.init(name: "초코 우유", price: 600, photo: UIImage(named: "Chocolate"), brand: "남양", packageSize: 180, milkFactoryIdentifier: "남양우유목장")
     }
 }

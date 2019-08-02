934e4ba0a73b7f0559d1908c790fba5be2f1470b
hwj0623 <hwj0623@naver.com>
2019-08-02 18:53:52 +0900

    모델에 (구매 이벤트 발생시) 잔액 변경 notification 추가

--- VendingMachineApp/VendingMachineDTO/VendingMachineNotification.swift
+++ VendingMachineApp/VendingMachineDTO/VendingMachineNotification.swift
@@ -10,4 +10,5 @@ import Foundation
 
 extension Notification.Name {
     static let notifyDrinkStockSizeUpdate = Notification.Name(rawValue: "notifyDrinkStockSizeUpdate")
+    static let notifyBalanceInfoUpdate = Notification.Name(rawValue: "notifyBalanceInfoUpdate")
 }

cd33ad5300b4000cb19e077b1797347365e0289a
Sunghyun Kim <sunghyun_k@icloud.com>
2019-08-12 14:15:23 +0900

    커피에 카페인 프로퍼티를 추가함

--- VendingMachineApp/VendingMachineApp/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Coffee.swift
@@ -2,8 +2,10 @@ import UIKit
 
 class Coffee: Beverage {
     
-    init(name: String, price: Coin, photo: UIImage?, brand: String, packageSize: Int, beverageTemperature: Int) {
-        
+    var caffeine: Int
+    
+    init(name: String, price: Coin, photo: UIImage?, brand: String, packageSize: Int, beverageTemperature: Int, caffeine: Int) {
+        self.caffeine = caffeine
         super.init(name: name, price: price, photo: photo, brand: brand, packageSize: packageSize, beverageTemperature: beverageTemperature, expirationPeriod: Date.timeInterval(fromDays: 90))
         
     }
@@ -11,12 +13,12 @@ class Coffee: Beverage {
 
 class TOPCoffee: Coffee {
     init() {
-        super.init(name: "TOP", price: 1000, photo: #imageLiteral(resourceName: "TOP"), brand: "맥심", packageSize: 275, beverageTemperature: 10)
+        super.init(name: "TOP", price: 1000, photo: #imageLiteral(resourceName: "TOP"), brand: "맥심", packageSize: 275, beverageTemperature: 10, caffeine: 50)
     }
 }
 
 class CantataCoffee: Coffee {
     init() {
-        super.init(name: "칸타타", price: 1500, photo: #imageLiteral(resourceName: "Cantata"), brand: "롯데", packageSize: 500, beverageTemperature: 80)
+        super.init(name: "칸타타", price: 1500, photo: #imageLiteral(resourceName: "Cantata"), brand: "롯데", packageSize: 500, beverageTemperature: 80, caffeine: 40)
     }
 }

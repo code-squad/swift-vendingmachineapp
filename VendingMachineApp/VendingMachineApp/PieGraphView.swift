3637fa540dde4613e47ab78e9f3e0493ae541b23
hwj0623 <hwj0623@naver.com>
2019-08-13 15:40:47 +0900

    구매이력을 딕셔너리형태로 변환

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -8,14 +8,41 @@
 
 import UIKit
 
+
 class PieGraphView: UIView {
+    private var historySet: [String: Int] = [String: Int]()
+    
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initializeHistorySet()
     }
-    */
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initializeHistorySet()
+        isOpaque = false
+    }
+    
+    
+    func initializeHistorySet() {
+        let list = VendingMachine.sharedInstance.showShoppingHistory().list
+        for drink in list {
+            guard let value = historySet[drink.name] else{
+                historySet.updateValue(1, forKey: drink.name)
+                continue
+            }
+            historySet.updateValue(value+1, forKey: drink.name)
+        }
+    }
+    
+    private func calculateTotalCount() -> Int{
+        let temporalMap = historySet.map{ (key: String, value: Int) -> Int in return value }
+        let totalCount = temporalMap.reduce(0, {(value1: Int, value2: Int) -> Int in return value1+value2})
+        return totalCount
+    }
+    
+    override func draw(_ rect: CGRect) {
+        
+    }
 }

5bc74c49284a3b228c6101deb152f687f0d0cadd
hwj0623 <hwj0623@naver.com>
2019-08-13 15:43:19 +0900

    파이 그래프 기능 구현

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -11,7 +11,13 @@ import UIKit
 
 class PieGraphView: UIView {
     private var historySet: [String: Int] = [String: Int]()
-    
+    let colorList = [
+        UIColor.orange,
+        UIColor.init(red: 0.7, green: 0.0, blue: 0.7, alpha: 1),
+        UIColor.init(red: 0.1, green: 0.3, blue: 0.9, alpha: 1),
+        UIColor.init(red: 0, green: 0.8, blue: 0.2, alpha: 1),
+        UIColor.init(red: 0.9,green: 0.1, blue: 0, alpha: 1)
+    ]
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,7 +29,6 @@ class PieGraphView: UIView {
         self.initializeHistorySet()
         isOpaque = false
     }
-    
     
     func initializeHistorySet() {
         let list = VendingMachine.sharedInstance.showShoppingHistory().list
@@ -43,6 +48,34 @@ class PieGraphView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
+        let initialAngle: CGFloat = -CGFloat.pi * 0.5
+
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        let frameSize = min(frame.size.height, frame.size.width)
+        let boundSize = min(bounds.size.height, bounds.size.width)
+        let radius = frameSize * 0.5
+        let center = CGPoint(x: boundSize * 0.5, y: boundSize * 0.5)   //원점
         
+        let totalCount = calculateTotalCount()
+        var startAngle = initialAngle
+
+        var index = 0
+        for drink in historySet.enumerated(){
+            if index >= colorList.count {
+                index = 0
+            }
+            
+            context.setFillColor(colorList[index].cgColor)
+            let endAngle = startAngle + 2 * .pi * (CGFloat(drink.element.value)/CGFloat(totalCount))
+      
+            context.move(to: center)
+            context.addArc(center: center, radius: radius,
+                       startAngle: startAngle, endAngle: endAngle, clockwise: false)
+            context.fillPath()
+            startAngle = endAngle
+            index += 1
+        }
     }
 }

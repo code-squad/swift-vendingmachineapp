55f937e5065cb82743d90e7aa56fc2fd83c9b497
hwj0623 <hwj0623@naver.com>
2019-08-13 16:05:38 +0900

    메서드 분리 및 가독성 개선

--- VendingMachineApp/VendingMachineApp/PieGraphView.swift
+++ VendingMachineApp/VendingMachineApp/PieGraphView.swift
@@ -19,23 +19,25 @@ class PieGraphView: UIView {
         UIColor.init(red: 0.9,green: 0.1, blue: 0, alpha: 1)
     ]
     
+    private let paragraphStyle: NSParagraphStyle = makeParagrapheStyle()
+    
+    private lazy var textAttributes: [NSAttributedString.Key: Any] = [
+        .paragraphStyle: self.paragraphStyle,
+        .font: UIFont.systemFont(ofSize: 20),
+        .foregroundColor: UIColor.black
+    ]
+    
     private static func makeParagrapheStyle() -> NSParagraphStyle {
         let style = NSMutableParagraphStyle()
         style.alignment = .center
         return style.mutableCopy() as! NSParagraphStyle
     }
     
-    private let paragraphStyle: NSParagraphStyle = makeParagrapheStyle()
-
-    private lazy var textAttributes: [NSAttributedString.Key: Any] = [
-        .paragraphStyle: self.paragraphStyle, .font: UIFont.systemFont(ofSize: 20)
-    ]
-
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.initializeHistorySet()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.initializeHistorySet()
@@ -62,61 +64,64 @@ class PieGraphView: UIView {
     
     override func draw(_ rect: CGRect) {
         let initialAngle: CGFloat = -CGFloat.pi * 0.5
-
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return
-        }
         let frameSize = min(frame.size.height, frame.size.width)
         let boundSize = min(bounds.size.height, bounds.size.width)
         let radius = frameSize * 0.5
-        let center = CGPoint(x: boundSize * 0.5, y: boundSize * 0.5)   //원점
-        
+        let center = CGPoint(x: boundSize * 0.5, y: boundSize * 0.5)
         let totalCount = calculateTotalCount()
         var startAngle = initialAngle
-
+        makePieGraph(start: startAngle, radius: radius, center: center, totalCount: totalCount)
+        startAngle = initialAngle
+        makeLabelsForPieGraph(start: startAngle, radius: radius, center: center, totalCount: totalCount)
+    }
+    
+    private func makePieGraph(start: CGFloat, radius: CGFloat, center: CGPoint, totalCount: Int){
+        var startAngle = start
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
         var index = 0
         for drink in historySet.enumerated(){
             if index >= colorList.count {
                 index = 0
             }
-            
             context.setFillColor(colorList[index].cgColor)
             let endAngle = startAngle + 2 * .pi * (CGFloat(drink.element.value)/CGFloat(totalCount))
-      
             context.move(to: center)
             context.addArc(center: center, radius: radius,
-                       startAngle: startAngle, endAngle: endAngle, clockwise: false)
+                           startAngle: startAngle, endAngle: endAngle, clockwise: false)
             context.fillPath()
             startAngle = endAngle
             index += 1
         }
-        
-        index = 0
-        startAngle = initialAngle
+    }
+    
+    private func makeLabelsForPieGraph(start: CGFloat, radius: CGFloat, center: CGPoint, totalCount: Int){
+        var startAngle = start
         for drink in historySet.enumerated(){
             let endAngle = startAngle + 2 * .pi * (CGFloat(drink.element.value)/CGFloat(totalCount))
             let halfAngle = startAngle + (endAngle - startAngle)/2
             //text attribute
-            let textLabel = drink.element.key as NSString
-            let textRenderSize = textLabel.size(withAttributes: textAttributes)
+            let labelText = drink.element.key as NSString
+            let textRenderSize = labelText.size(withAttributes: textAttributes)
             let textLabelCenter = center
-            // text rect size          
+            // text rect size
             let rectOriginPoint = textLabelCenter.project(by: radius, center: center, angle: halfAngle)
-            let rectCenterPoint = CGPoint.init(x: rectOriginPoint.x - textRenderSize.width * 0.5, y: rectOriginPoint.y - textRenderSize.height*0.5)
-            let textRect = CGRect(origin: rectCenterPoint, size: textRenderSize)
-            textAttributes[.foregroundColor] = UIColor.black
-            textLabel.draw(in: textRect, withAttributes: textAttributes)
-            index += 1
+            let rectCenterPoint = CGPoint.init(x: rectOriginPoint.x - textRenderSize.width * 0.5,
+                                               y: rectOriginPoint.y - textRenderSize.height * 0.5)
+            let textLabelRect = CGRect(origin: rectCenterPoint, size: textRenderSize)
+            
+            labelText.draw(in: textLabelRect, withAttributes: textAttributes)
             startAngle = endAngle
         }
     }
 }
 
 extension CGPoint {
-    /// 중점 좌표로부터 보정된 rad만큼의 각도를 따라
+    /// 중점 좌표로부터 보정된 반지름 만큼의 각도를 따라
     /// x방향으로 radius * cos(angle)
     /// y방향으로 radius * sin(angle)
-    /// 만큼 떨어진 좌표를 label의 중점좌표로 두기 위함.
+    /// 만큼 떨어진 좌표를 text label의 origin 좌표로 둔다.
     func project(by radius: CGFloat, center: CGPoint, angle: CGFloat) -> CGPoint {
         let tunedRadius = 0.6 * radius
         let labelX = center.x + tunedRadius * cos(angle)

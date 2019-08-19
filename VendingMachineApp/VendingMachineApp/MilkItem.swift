1794aece1d1df91acf51437f1ee275e5f6948fdd
Sunghyun Kim <sunghyun_k@icloud.com>
2019-08-19 20:32:47 +0900

    milkItem 인코딩 디코딩 작성

--- VendingMachineApp/VendingMachineApp/MilkItem.swift
+++ VendingMachineApp/VendingMachineApp/MilkItem.swift
@@ -7,4 +7,21 @@ class MilkItem: BeverageItem {
         self.milkFactoryIdentifier = milkFactoryIdentifier
         super.init(name: name, price: price, brand: brand, packageSize: packageSize)
     }
+    
+    struct PropertyKey {
+        static let milkFactoryIdentifier = "milkFactoryIdentifier"
+    }
+    
+    override func encode(with coder: NSCoder) {
+        super.encode(with: coder)
+        coder.encode(milkFactoryIdentifier, forKey: PropertyKey.milkFactoryIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        guard let milkFactoryIdentifier = coder.decodeObject(forKey: PropertyKey.milkFactoryIdentifier) as? String else {
+            return nil
+        }
+        self.milkFactoryIdentifier = milkFactoryIdentifier
+        super.init(coder: coder)
+    }
 }

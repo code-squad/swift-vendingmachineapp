e57103ebfb3b87b4ab53079a3d24ec8e853464ed
Sunghyun Kim <sunghyun_k@icloud.com>
2019-08-19 20:07:21 +0900

    BeverageItem NSCoding 준수

--- VendingMachineApp/VendingMachineApp/BeverageItem.swift
+++ VendingMachineApp/VendingMachineApp/BeverageItem.swift
@@ -23,6 +23,39 @@ class BeverageItem: NSObject, NSCoding {
         self.packageSize = packageSize
     }
     
+    struct PropertyKey {
+        static let name = "name"
+        static let price = "price"
+        static let brand = "brand"
+        static let packageSize = "packageSize"
+        static let beverages = "beverages"
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: PropertyKey.name)
+        coder.encode(price, forKey: PropertyKey.price)
+        coder.encode(brand, forKey: PropertyKey.brand)
+        coder.encode(packageSize, forKey: PropertyKey.packageSize)
+        coder.encode(beverages, forKey: PropertyKey.beverages)
+    }
+    
+    required convenience init?(coder: NSCoder) {
+        guard let name = coder.decodeObject(forKey: PropertyKey.name) as? String else {
+            return nil
+        }
+        let price = coder.decodeInteger(forKey: PropertyKey.price)
+        guard let brand = coder.decodeObject(forKey: PropertyKey.brand) as? String else {
+            return nil
+        }
+        let packageSize = coder.decodeInteger(forKey: PropertyKey.packageSize)
+        guard let beverages = coder.decodeObject(forKey: PropertyKey.packageSize) as? [Beverage] else {
+            return nil
+        }
+        self.init(name: name, price: price, brand: brand, packageSize: packageSize)
+        self.beverages = beverages
+    }
+    
+    
     //MARK: 연산 프로퍼티
     
     var count: Int {

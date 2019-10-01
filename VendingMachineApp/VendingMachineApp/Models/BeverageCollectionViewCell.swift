fa6af7b4515e2eb63a7a4a62919d61ee00e90601
cmindy <minjidev@naver.com>
2019-10-01 22:01:54 +0900

    feat: Add BeverageCollectionViewCell

---
+++ VendingMachineApp/VendingMachineApp/Models/BeverageCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BeverageCollectionViewCell.swift
+//  VendingMachineApp
+//
+//  Created by CHOMINJI on 01/10/2019.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class BeverageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var quantityLabel: UILabel!
+    
+}

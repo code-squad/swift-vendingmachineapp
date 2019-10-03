2ebc5099d88212cd8bc84a8a44a10f2a0c57a804
cmindy <minjidev@naver.com>
2019-10-03 16:32:17 +0900

    feat: Add delegate to BeverageCollectionViewCell for touch add button

--- VendingMachineApp/VendingMachineApp/Models/BeverageCollectionViewCell.swift
+++ VendingMachineApp/VendingMachineApp/Models/BeverageCollectionViewCell.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
+protocol BeverageCollectionViewCellDelegate: class {
+    func addStock(with indexPath: IndexPath)
+}
+
 class BeverageCollectionViewCell: UICollectionViewCell {
+    var indexPath: IndexPath!
+    weak var delegate: BeverageCollectionViewCellDelegate?
+    
     @IBOutlet weak var beverageImageView: UIImageView!
     @IBOutlet weak var quantityLabel: UILabel!
+    
+    @IBAction func touchAddButton(_ sender: Any) {
+        delegate?.addStock(with: indexPath)
+    }
 }

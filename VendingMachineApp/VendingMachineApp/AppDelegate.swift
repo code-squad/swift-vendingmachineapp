29948284e6a109d946bc3807105cac6c67b77228
cmindy <cmindy.soft@gmail.com>
2019-10-22 22:17:31 +0900

    feat: Add save and load feature to Appdelegate for before iOS 13.0

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -10,20 +10,37 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        guard let rootViewController = window?.rootViewController as? ViewController else {
+            return false
+        }
+        
+        rootViewController.vendingMachine = VendingMachine.sharedInstance
         return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        UserDefaultManager<VendingMachine>(key: vendingMachineID).save()
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        UserDefaultManager<VendingMachine>(key: vendingMachineID).save()
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.

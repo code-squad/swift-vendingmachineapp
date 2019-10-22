920df3a2eb53ac9c7e8902ae8da6ce876fdba997
cmindy <cmindy.soft@gmail.com>
2019-10-22 22:55:56 +0900

    refactor: Modify concrete to protocol type for dependency inversion in Ddelegates

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        guard let rootViewController = window?.rootViewController as? ViewController else {
+        guard var rootViewController = window?.rootViewController as? VendingMachineViewType else {
             return false
         }
         

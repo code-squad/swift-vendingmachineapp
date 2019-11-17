c1479fc978c50b1cf0e780df21f085744682b0d0
cmindy <cmindy.soft@gmail.com>
2019-11-17 19:06:13 +0900

    feat: Add ManagerViewController for manage beverages

---
+++ VendingMachineApp/VendingMachineApp/Controller/ManagerViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ManagerViewController.swift
+//  VendingMachineApp
+//
+//  Created by CHOMINJI on 15/11/2019.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class ManagerViewController: UIViewController, VendingMachineViewType {
+    
+    var vendingMachine: VendingMachineType!
+    private let cellReuseID = "BeverageCollectionViewCell"
+    private var beverages: [Item] {
+        return vendingMachine.beverages
+    }
+    
+    @IBOutlet weak var beverageCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(refreshStock),
+                                               name: NSNotification.Name(NotificationID.stockAdded),
+                                               object: nil)
+
+        beverageCollectionView.dataSource = self
+    }
+    
+    @IBAction func touchCloseButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc
+    private func refreshStock() {
+        beverageCollectionView.reloadData()
+    }
+}
+
+// MARK: - Extensions
+// MARK: CollectionViewDataSource
+
+extension ManagerViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return beverages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseID, for: indexPath) as? BeverageCollectionViewCell else {
+            return .init()
+        }
+        cell.delegate = self
+        cell.indexPath = indexPath
+        
+        let beverage = beverages[indexPath.row]
+        cell.quantityLabel.text = "\(beverage.count)개"
+        cell.beverageImageView.image = UIImage(named: beverage.imageName ?? "")
+        
+        return cell
+    }
+}
+
+extension ManagerViewController: BeverageCollectionViewCellDelegate {
+    func beverageCell(_ cell: UICollectionViewCell, addItemAt indexPath: IndexPath) {
+        guard let beverage =  vendingMachine.fetchBeverage(at: indexPath.item) else {
+            return
+        }
+        vendingMachine.addStock(of: beverage, count: 1)
+    }
+    
+    func beverageCell(_ cell: UICollectionViewCell, purchaseItemAt indexPath: IndexPath) {
+        guard let beverage = vendingMachine.fetchBeverage(at: indexPath.item) else {
+                return
+        }
+         vendingMachine.purchase(beverage: beverage, completion: nil)
+    }
+}

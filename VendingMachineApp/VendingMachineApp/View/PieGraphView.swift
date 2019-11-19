cade98fea7645cdc86300446ed7abfb6375e4b86
cmindy <cmindy.soft@gmail.com>
2019-11-19 17:15:03 +0900

    feat: Add PieGraphView to storyboard

---
+++ VendingMachineApp/VendingMachineApp/View/PieGraphView.swift
@@ -0,0 +1,21 @@
+//
+//  PieGraphView.swift
+//  VendingMachineApp
+//
+//  Created by CHOMINJI on 2019/11/19.
+//  Copyright © 2019 cmindy. All rights reserved.
+//
+
+import UIKit
+
+class PieGraphView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

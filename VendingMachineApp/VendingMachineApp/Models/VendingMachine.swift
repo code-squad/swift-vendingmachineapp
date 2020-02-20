1bd1eb4368b94109817244375fb53011c19c50fd
corykim0829 <corykim0829@gmail.com>
2020-02-20 21:47:37 +0900

    Feat: VendingMachine class
    - VendingMachine will show beverages

---
+++ VendingMachineApp/VendingMachineApp/Models/VendingMachine.swift
@@ -0,0 +1,24 @@
+//
+//  VendingMachine.swift
+//  VendingMachineApp
+//
+//  Created by Cory Kim on 2020/02/20.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+class VendingMachine {
+    
+    init() {
+        showBeverages()
+    }
+    
+    private func showBeverages() {
+        [
+        Milk(name: "바나나는 원래 하얗다", brand: "매일우유", servingSize: 150, price: 1200, dateOfManufacture: Date(), milkType: .bananaMilk),
+        Soda(name: "펩시", brand: "PEPSI", servingSize: 120, price: 900, dateOfManufacture: Date(), sodaType: .coke),
+        Coffee(name: "TOP아메리카노", brand: "TOP", servingSize: 180, price: 1500, dateOfManufacture: Date(), coffeeType: .TOP)
+        ].forEach{ print($0) }
+    }
+}
--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var vendingMachine: VendingMachine?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        [
-        Milk(name: "바나나는 원래 하얗다", brand: "매일우유", servingSize: 150, price: 1200, dateOfManufacture: Date(), milkType: .bananaMilk),
-        Soda(name: "펩시", brand: "PEPSI", servingSize: 120, price: 900, dateOfManufacture: Date(), sodaType: .coke),
-        Coffee(name: "TOP아메리카노", brand: "TOP", servingSize: 180, price: 1500, dateOfManufacture: Date(), coffeeType: .TOP)
-        ].forEach{ print($0) }
+        vendingMachine = VendingMachine()
     }
 
 

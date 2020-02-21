21a6f08d7cf51de7ec37d72fa7712de3db5beccc
TTOzzi <devttozzi@gmail.com>
2020-02-21 23:28:01 +0900

    ViewController 에 음료 추가와 재고 출력 코드 작성

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -12,7 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let vendingMachine = VendingMachine()
+        let strawberryMilk = Milk(brand: "서울우유", amount: 200, price: 1000, name: "날마다딸기우유", flavor: .strawberry)
+        let cola = Soda(brand: "팹시", amount: 350, price: 2000, name: "다이어트콜라", category: .cola)
+        let top = Coffee(brand: "맥심", amount: 400, price: 3000, name: "TOP아메리카노", category: .top)
+        vendingMachine.addBeverage(beverage: strawberryMilk)
+        vendingMachine.addBeverage(beverage: cola)
+        vendingMachine.addBeverage(beverage: top)
+        vendingMachine.printStock()
+        
     }
 
 

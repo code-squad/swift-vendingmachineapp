d08cb1d575c1f76e0e5adcef29d1e495338d018e
TTOzzi <devttozzi@gmail.com>
2020-02-24 18:27:09 +0900

    불필요 코드 정리

--- VendingMachineApp/VendingMachineApp/Model/Coffee/Top.swift
+++ VendingMachineApp/VendingMachineApp/Model/Coffee/Top.swift
@@ -9,7 +9,5 @@
 import Foundation
 
 class Top: Coffee {
-    static func americano() -> Top {
-        return Top(brand: "맥심", amount: 400, price: 3000, name: "TOP아메리카노")
-    }
+    
 }
--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -12,14 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let vendingMachine = VendingMachine()
-        let strawberryMilk = StrawberryMilk.everyDayStrawberryMilk()
-        let cola = Cola.pepsiDietCoke()
-        let top = Top.americano()
-        vendingMachine.addBeverage(beverage: strawberryMilk)
-        vendingMachine.addBeverage(beverage: cola)
-        vendingMachine.addBeverage(beverage: top)
-        vendingMachine.printStock()
     }
 }
 

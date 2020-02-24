d3c6f097d34535c033634b4c0fad806ba53c8ef3
TTOzzi <devttozzi@gmail.com>
2020-02-24 13:34:17 +0900

    변경된 코드에 맞게 출력 코드 수정

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -13,16 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let vendingMachine = VendingMachine()
-        let strawberryMilk = Milk(brand: "서울우유", amount: 200, price: 1000, name: "날마다딸기우유", flavor: .strawberry)
-        let cola = Soda(brand: "팹시", amount: 350, price: 2000, name: "다이어트콜라", category: .cola)
-        let top = Coffee(brand: "맥심", amount: 400, price: 3000, name: "TOP아메리카노", category: .top)
+        let strawberryMilk = StrawberryMilk.everyDayStrawberryMilk()
+        let cola = Cola.pepsiDietCoke()
+        let top = Top.americano()
         vendingMachine.addBeverage(beverage: strawberryMilk)
         vendingMachine.addBeverage(beverage: cola)
         vendingMachine.addBeverage(beverage: top)
         vendingMachine.printStock()
-        
     }
-
-
 }
 

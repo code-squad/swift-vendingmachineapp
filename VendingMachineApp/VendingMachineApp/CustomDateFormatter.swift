1ed0290217c1d98a05bb125230732bfd88110596
1Consumption <hanseop95@gmail.com>
2020-02-24 17:42:04 +0900

    date를 사용자가 원하는 형식으로 반환 해주는 클래스

---
+++ VendingMachineApp/VendingMachineApp/CustomDateFormatter.swift
@@ -0,0 +1,18 @@
+//
+//  dateFormatter.swift
+//  VendingMachineApp
+//
+//  Created by 신한섭 on 2020/02/24.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+class CustomDateFormatter {
+    func convertDate(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        
+        return dateFormatter.string(from: date)
+    }
+}

40e3f2ef70113a569dcd16db84ed59f68f471a87
TTOzzi <devttozzi@gmail.com>
2020-02-24 17:03:23 +0900

    저지방 우유 확인 추가
    지방 비율을 표시하는 fatRatio
    지방 비율이 3퍼센트 미만이면 저지방 우유

--- VendingMachineApp/VendingMachineApp/Model/Milk/Milk.swift
+++ VendingMachineApp/VendingMachineApp/Model/Milk/Milk.swift
@@ -9,5 +9,14 @@
 import Foundation
 
 class Milk: Beverage {
-
+    private var fatRatio: Double
+    
+    func isLowFat() -> Bool {
+        return fatRatio < 3
+    }
+    
+    init(brand: String, amount: Int, price: Int, name: String, fatRatio: Double) {
+        self.fatRatio = fatRatio
+        super.init(brand: brand, amount: amount, price: price, name: name)
+    }
 }

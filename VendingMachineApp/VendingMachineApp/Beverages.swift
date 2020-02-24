dd45f97cf56f9a3479f27073a3e2f5871061bb8f
1Consumption <hanseop95@gmail.com>
2020-02-24 16:44:28 +0900

    Beverages 클래스 생성
    Beverage 객체 배열을 프로퍼티로 가지고, 동작에 대한 메소드를 가지고 있음

---
+++ VendingMachineApp/VendingMachineApp/Beverages.swift
@@ -0,0 +1,13 @@
+//
+//  Beverages.swift
+//  VendingMachineApp
+//
+//  Created by 신한섭 on 2020/02/24.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+class Beverages{
+    
+}

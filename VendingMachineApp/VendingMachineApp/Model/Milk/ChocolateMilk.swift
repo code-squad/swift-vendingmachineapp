2d719cad6c725b28be6f3089e4e4901c3f3f5575
TTOzzi <devttozzi@gmail.com>
2020-02-24 11:33:08 +0900

    enum 으로 나눈 종류들을 클래스로 생성

---
+++ VendingMachineApp/VendingMachineApp/Model/Milk/ChocolateMilk.swift
@@ -0,0 +1,13 @@
+//
+//  ChocolateMilk.swift
+//  VendingMachineApp
+//
+//  Created by TTOzzi on 2020/02/24.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+class ChocolateMilk: Milk {
+    
+}

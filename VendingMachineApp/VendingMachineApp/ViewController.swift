3026874062df73ee7a33095140e904ec689fa615
1Consumption <hanseop95@gmail.com>
2020-02-24 11:12:59 +0900

    프로젝트 생성
    자판기 앱을 위한 프로젝트 생성

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 신한섭 on 2020/02/24.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

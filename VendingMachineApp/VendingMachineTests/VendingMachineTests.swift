5dffeb78664c6aaa932ed7e8560dad56efe68fe4
TTOzzi <devttozzi@gmail.com>
2020-02-25 22:46:27 +0900

    재고 종류별 분류 테스트 코드 추가

--- VendingMachineApp/VendingMachineTests/VendingMachineTests.swift
+++ VendingMachineApp/VendingMachineTests/VendingMachineTests.swift
@@ -12,10 +12,14 @@ import XCTest
 class VendingMachineTests: XCTestCase {
 
     var myVendingMachine: VendingMachine!
+    var milk: BananaMilk!
+    var coffee: Cantata!
     
     override func setUp() {
         super.setUp()
         myVendingMachine = VendingMachine()
+        milk = BananaMilk(brand: "빙그레", amount: 240, price: 1000, name: "바나나맛우유", calorie: 208, saleablePeriod: 14, fatRatio: 15, bananaContent: 3)
+        coffee = Cantata(brand: "롯데", amount: 500, price: 3000, name: "콘트라베이스 콜드브루", calorie: 20, saleablePeriod: 270, caffeineContent: 179, isHot: true)
     }
     
     func testPutMoney() {
@@ -24,17 +28,17 @@ class VendingMachineTests: XCTestCase {
     }
     
     func testAddBeverage() {
-        myVendingMachine.addBeverage(beverage: BananaMilk(brand: "빙그레", amount: 240, price: 1000, name: "바나나맛우유", calorie: 208, saleablePeriod: 14, fatRatio: 15, bananaContent: 3))
+        myVendingMachine.addBeverage(beverage: milk)
         XCTAssertEqual(myVendingMachine.stock.beverages.count, 1)
         XCTAssertTrue(myVendingMachine.stock.beverages.contains { $0.name == "바나나맛우유" })
-        myVendingMachine.addBeverage(beverage: Cantata(brand: "롯데", amount: 500, price: 3000, name: "콘트라베이스 콜드브루", calorie: 20, saleablePeriod: 270, caffeineContent: 179, isHot: true))
+        myVendingMachine.addBeverage(beverage: coffee)
         XCTAssertEqual(myVendingMachine.stock.beverages.count, 2)
         XCTAssertTrue(myVendingMachine.stock.beverages.contains { $0.name == "콘트라베이스 콜드브루" })
     }
     
     func testPurchasableList() {
-        myVendingMachine.stock.beverages.append(BananaMilk(brand: "빙그레", amount: 240, price: 1000, name: "바나나맛우유", calorie: 208, saleablePeriod: 14, fatRatio: 15, bananaContent: 3))
-        myVendingMachine.stock.beverages.append( Cantata(brand: "롯데", amount: 500, price: 3000, name: "콘트라베이스 콜드브루", calorie: 20, saleablePeriod: 270, caffeineContent: 179, isHot: true))
+        myVendingMachine.stock.beverages.append(milk)
+        myVendingMachine.stock.beverages.append(coffee)
         myVendingMachine.money = 2000
         XCTAssertTrue(myVendingMachine.purchasableList.contains { $0.name == "바나나맛우유" })
         XCTAssertFalse(myVendingMachine.purchasableList.contains { $0.name == "콘트라베이스 콜드브루" })
@@ -44,8 +48,8 @@ class VendingMachineTests: XCTestCase {
     }
     
     func testBuy() {
-        myVendingMachine.stock.beverages.append(BananaMilk(brand: "빙그레", amount: 240, price: 1000, name: "바나나맛우유", calorie: 208, saleablePeriod: 14, fatRatio: 15, bananaContent: 3))
-        myVendingMachine.stock.beverages.append( Cantata(brand: "롯데", amount: 500, price: 3000, name: "콘트라베이스 콜드브루", calorie: 20, saleablePeriod: 270, caffeineContent: 179, isHot: true))
+        myVendingMachine.stock.beverages.append(milk)
+        myVendingMachine.stock.beverages.append(coffee)
         myVendingMachine.money = 2000
         XCTAssertNil(myVendingMachine.buy(beverageName: "콘트라베이스 콜드브루"))
         XCTAssertNil(myVendingMachine.buy(beverageName: "딸기맛우유"))
@@ -57,5 +61,15 @@ class VendingMachineTests: XCTestCase {
     func testCheckBalance() {
         myVendingMachine.money = 3000
         XCTAssertEqual(myVendingMachine.checkBalance(), 3000)
+    }
+    
+    func testStockList() {
+        myVendingMachine.stock.beverages.append(milk)
+        myVendingMachine.stock.beverages.append(milk)
+        myVendingMachine.stock.beverages.append(milk)
+        myVendingMachine.stock.beverages.append(coffee)
+
+        XCTAssertEqual(myVendingMachine.stockList[milk], 3)
+        XCTAssertEqual(myVendingMachine.stockList[coffee], 1)
     }
 }

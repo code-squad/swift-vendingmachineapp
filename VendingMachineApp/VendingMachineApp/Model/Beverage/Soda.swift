839af3a235425123d64782c8f20a0da475616e47
1Consumption <hanseop95@gmail.com>
2020-02-25 15:18:32 +0900

    Soda 클래스 추가
    Beverage클래스를 상속받음
    zeroCalorie 프로퍼티를 가지고 있음

---
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Soda.swift
@@ -0,0 +1,22 @@
+//
+//  Soda.swift
+//  VendingMachineApp
+//
+//  Created by 신한섭 on 2020/02/25.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+class Soda: Beverage {
+    var zeroCalorie: Bool
+    
+    init(brand: String, capacity: Int, price: Int, name: String, manufacturingDate: Date, expirationDate: Date, calorie: Double, zeroCalorie: Bool) {
+        self.zeroCalorie = zeroCalorie
+        super.init(brand: brand, capacity: capacity, price: price, name: name, manufacturingDate: manufacturingDate, expirationDate: expirationDate, calorie: calorie)
+    }
+    
+    func isZeroCalorie() -> Bool {
+        return zeroCalorie
+    }
+}

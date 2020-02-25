3104717df2123517900b154e8e4ba7997cb18fe6
TTOzzi <devttozzi@gmail.com>
2020-02-25 18:24:20 +0900

    코드 정리

--- VendingMachineApp/VendingMachineApp/Model/Coffee/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Model/Coffee/Coffee.swift
@@ -11,12 +11,12 @@ import Foundation
 class Coffee: Beverage {
     private let caffeineContent: Double
     
-    func isDecaffeinated() -> Bool {
-        return caffeineContent < 10
-    }
-    
     init(brand: String, amount: Int, price: Int, name: String, calorie: Double, saleablePeriod: Int, caffeineContent: Double, isHot: Bool) {
         self.caffeineContent = caffeineContent
         super.init(brand: brand, amount: amount, price: price, name: name, calorie: calorie, saleablePeriod: saleablePeriod, isHot: isHot)
+    }
+    
+    func isDecaffeinated() -> Bool {
+        return caffeineContent < 10
     }
 }

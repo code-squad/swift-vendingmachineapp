6c62517c9512b9d0f1c8f0907e1532899bf7b769
1Consumption <hanseop95@gmail.com>
2020-02-25 22:54:19 +0900

    Georgia 클래스 추가
    블랙 커피인지 아닌지를 표시하는 black 프로퍼티를 가짐

---
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Georgia.swift
@@ -0,0 +1,22 @@
+//
+//  Georgia.swift
+//  VendingMachineApp
+//
+//  Created by 신한섭 on 2020/02/25.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+class Georgia: Coffee {
+    private var black: Bool
+    
+    init(brand: String, capacity: Int, price: Int, name: String, manufacturingDate: Date, expirationDate: Date, calorie: Double, hot: Bool, decaffeinated: Bool, black: Bool) {
+        self.black = black
+        super.init(brand: brand, capacity: capacity, price: price, name: name, manufacturingDate: manufacturingDate, expirationDate: expirationDate, calorie: calorie, hot: hot, decaffeinated: decaffeinated)
+    }
+    
+    func isBlack() -> Bool {
+        return black
+    }
+}

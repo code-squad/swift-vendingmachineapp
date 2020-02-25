c0e06de65f398fa95a2629e7c228a2cae6fa66c2
corykim0829 <corykim0829@gmail.com>
2020-02-25 21:29:27 +0900

    Feat: ProductsStackView using Storyboard

--- VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
@@ -10,14 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var vendingMachine = VendingMachine(products: [])
-    let vendingMachineTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Cory's Vending Machine"
-        label.textColor = .white
-        label.font = UIFont.boldSystemFont(ofSize: 36)
-        return label
-    }()
-    let productStackView = ProductStackView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,15 +19,6 @@ class ViewController: UIViewController {
     }
     
     private func setupUI() {
-        view.backgroundColor = #colorLiteral(red: 0.9411766529, green: 0.3607842624, blue: 0.2039215267, alpha: 1)
-        view.addSubview(vendingMachineTitleLabel)
-        vendingMachineTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        vendingMachineTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
-        vendingMachineTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
-        view.addSubview(productStackView)
-        productStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        productStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
     }
 }
 

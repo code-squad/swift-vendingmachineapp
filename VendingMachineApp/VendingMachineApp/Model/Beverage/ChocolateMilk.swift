f2317aa0e7b3e2a108f6bc12f7415a546b08a16c
1Consumption <hanseop95@gmail.com>
2020-02-25 21:01:55 +0900

    ChocolateMilk 클래스 생성
    패키지 종류를 Package enum으로 선언하여 package 프로퍼티가 가지고 있음
    패키지 종류가 같은지를 검사하는 인터페이스 가짐

---
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/ChocolateMilk.swift
@@ -0,0 +1,28 @@
+//
+//  ChocolateMilk.swift
+//  VendingMachineApp
+//
+//  Created by 신한섭 on 2020/02/25.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+class ChocolateMilk: Milk {
+    enum Package: String {
+        case plastic = "plastic"
+        case paper = "paper"
+        case vinyl = "vinyl"
+    }
+    
+    private var package: Package
+    
+    init(brand: String, capacity: Int, price: Int, name: String, manufacturingDate: Date, expirationDate: Date, calorie: Double, farmCode: String, package: Package) {
+        self.package = package
+        super.init(brand: brand, capacity: capacity, price: price, name: name, manufacturingDate: manufacturingDate, expirationDate: expirationDate, calorie: calorie, farmCode: farmCode)
+    }
+    
+    func isSamePackage(as package: Package) -> Bool {
+        return self.package == package
+    }
+}

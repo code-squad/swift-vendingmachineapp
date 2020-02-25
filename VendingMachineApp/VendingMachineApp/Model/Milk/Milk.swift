d03d34b7ff5f985a6e811da0c757d75758208002
TTOzzi <devttozzi@gmail.com>
2020-02-25 18:24:20 +0900

    코드 정리

--- VendingMachineApp/VendingMachineApp/Model/Milk/Milk.swift
+++ VendingMachineApp/VendingMachineApp/Model/Milk/Milk.swift
@@ -11,12 +11,12 @@ import Foundation
 class Milk: Beverage {
     private let fatRatio: Double
     
-    func isLowFat() -> Bool {
-        return fatRatio < 3
-    }
-    
     init(brand: String, amount: Int, price: Int, name: String, calorie: Double, saleablePeriod: Int, fatRatio: Double) {
         self.fatRatio = fatRatio
         super.init(brand: brand, amount: amount, price: price, name: name, calorie: calorie, saleablePeriod: saleablePeriod)
+    }
+    
+    func isLowFat() -> Bool {
+        return fatRatio < 3
     }
 }

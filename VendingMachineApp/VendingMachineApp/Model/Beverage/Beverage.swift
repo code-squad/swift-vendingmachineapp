345a75c84ee722d79532f4f60bd768261190c08d
1Consumption <hanseop95@gmail.com>
2020-02-25 15:04:37 +0900

    속성 추가
    유통기한, 칼로리 속성 추가

--- VendingMachineApp/VendingMachineApp/Model/Beverage/Beverage.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Beverage.swift
@@ -14,13 +14,17 @@ class Beverage {
     private var price: Int
     private var name: String
     private var manufacturingDate: Date
+    private var expirationDate: Date
+    private var calorie: Double
     
-    init(brand: String, capacity: Int, price: Int, name: String, manufacturingDate: Date){
+    init(brand: String, capacity: Int, price: Int, name: String, manufacturingDate: Date, expirationDate: Date, calorie: Double){
         self.brand = brand
         self.capacity = capacity
         self.price = price
         self.name = name
         self.manufacturingDate = manufacturingDate
+        self.expirationDate = expirationDate
+        self.calorie = calorie
     }
 }
 

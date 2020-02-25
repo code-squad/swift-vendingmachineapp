b41b2aab6d52a3757c57a978371359ed419e0c3e
1Consumption <hanseop95@gmail.com>
2020-02-25 21:27:02 +0900

    Cider 클래스 추가
    맛을 표현하는 Taste enum을 가짐
    taste 속성이 맛을 가짐
    taste가 같은지 검사하는 인터페이스 메소드 있음

---
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Cider.swift
@@ -0,0 +1,27 @@
+//
+//  Cider.swift
+//  VendingMachineApp
+//
+//  Created by 신한섭 on 2020/02/25.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+class Cider: Soda {
+    enum Taste: String {
+        case lemon = "lemon"
+        case pear = "pear"
+    }
+    
+    private var taste: Taste
+    
+    init(brand: String, capacity: Int, price: Int, name: String, manufacturingDate: Date, expirationDate: Date, calorie: Double, zeroCalorie: Bool, taste: Taste) {
+        self.taste = taste
+        super.init(brand: brand, capacity: capacity, price: price, name: name, manufacturingDate: manufacturingDate, expirationDate: expirationDate, calorie: calorie, zeroCalorie: zeroCalorie)
+    }
+    
+    func isSameTaste(as taste: Taste) -> Bool {
+        return self.taste == taste
+    }
+}

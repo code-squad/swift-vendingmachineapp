3deecbb1462a0725b5be535831e7d0cb1c821e23
TTOzzi <devttozzi@gmail.com>
2020-02-25 18:12:01 +0900

    각 우유들에 원재료 함량 속성 추가

--- VendingMachineApp/VendingMachineApp/Model/Milk/ChocolateMilk.swift
+++ VendingMachineApp/VendingMachineApp/Model/Milk/ChocolateMilk.swift
@@ -9,5 +9,10 @@
 import Foundation
 
 class ChocolateMilk: Milk {
+    private let chocolateContent: Double
     
+    init(brand: String, amount: Int, price: Int, name: String, calorie: Double, saleablePeriod: Int, fatRatio: Double, chocolateContent: Double) {
+        self.chocolateContent = chocolateContent
+        super.init(brand: brand, amount: amount, price: price, name: name, calorie: calorie, saleablePeriod: saleablePeriod, fatRatio: fatRatio)
+    }
 }

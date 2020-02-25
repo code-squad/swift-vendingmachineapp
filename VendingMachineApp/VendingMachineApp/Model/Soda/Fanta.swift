930354a297a2bdf5e210b3be459ffd6c4de43651
TTOzzi <devttozzi@gmail.com>
2020-02-25 18:28:29 +0900

    환타 맛 추가

--- VendingMachineApp/VendingMachineApp/Model/Soda/Fanta.swift
+++ VendingMachineApp/VendingMachineApp/Model/Soda/Fanta.swift
@@ -9,5 +9,10 @@
 import Foundation
 
 class Fanta: Soda {
+    private let flavor: String
     
+    init(brand: String, amount: Int, price: Int, name: String, calorie: Double, saleablePeriod: Int, sugarContent: Double, flavor: String) {
+        self.flavor = flavor
+        super.init(brand: brand, amount: amount, price: price, name: name, calorie: calorie, saleablePeriod: saleablePeriod, sugarContent: sugarContent)
+    }
 }

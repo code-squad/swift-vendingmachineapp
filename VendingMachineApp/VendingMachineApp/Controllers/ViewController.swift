8931a90afbe5ae8be31534a5143ff47ef9a8cc4c
corykim0829 <corykim0829@gmail.com>
2020-02-26 21:03:03 +0900

    Feat: UI with Storyboard

--- VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     var vendingMachine = VendingMachine(products: [])
-    
+
+    @IBOutlet var productsStackView: [UIStackView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()

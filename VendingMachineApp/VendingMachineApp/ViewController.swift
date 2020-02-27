d8996281c78515e5c6a05437521f0339ca63a787
seizze <cwcwc3@gmail.com>
2020-02-27 19:01:57 +0900

    feat: Create new vending machine app project

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by Chaewan Park on 2020/02/27.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

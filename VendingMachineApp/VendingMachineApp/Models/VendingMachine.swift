581f5e19ed99995a211014748e16a35aacd9a911
seizze <cwcwc3@gmail.com>
2020-02-27 21:32:45 +0900

    feat: Add class vending machine

---
+++ VendingMachineApp/VendingMachineApp/Models/VendingMachine.swift
@@ -0,0 +1,21 @@
+//
+//  VendingMachine.swift
+//  VendingMachineApp
+//
+//  Created by Chaewan Park on 2020/02/27.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+class VendingMachine {
+    private var stock = [Beverage]()
+    
+    var stockList: String {
+        return stock.map { "\($0)" }.joined(separator: "\n")
+    }
+    
+    func fill(beverage: Beverage) {
+        stock.append(beverage)
+    }
+}

a6d682db8c5fa19205926f7b0b313295746446ef
delma <sdy2856@gmail.com>
2020-02-29 15:50:39 +0900

    Feat: print vending machine beverage list

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -8,13 +8,13 @@
 
 import Foundation
 class VendingMachine {
-    let topCoffee = Coffee(manufacturer: "동서식품", brand: "TOP", capacity: 275, price: 2500, name: "마스터라떼", manufacturedDate: Date(), isContainMilk: true, flavor: .latte, package: .can)
-    let bananaMilk = Milk(manufacturer: "빙그레", brand: "빙그레", capacity: 250, price: 1400, name: "빙그레 바나나맛 우유", manufacturedDate: Date(), fatRatio: .original, flavor: .banana, isLactoFree: false)
-    let splite = Soda(manufacturer: "코카콜라", brand: "스프라이트", capacity: 355, price: 1500, name: "스프라이트", manufacturedDate: Date(), sugarRatio:.original, flavor: .cider)
+    private let topCoffee = Coffee(manufacturer: "동서식품", brand: "TOP", capacity: 275, price: 2500, name: "마스터라떼", manufacturedDate: Date(), isContainMilk: true, flavor: .latte, package: .can)
+    private let bananaMilk = Milk(manufacturer: "빙그레", brand: "빙그레", capacity: 250, price: 1400, name: "빙그레 바나나맛 우유", manufacturedDate: Date(), fatRatio: .original, flavor: .banana, isLactoFree: false)
+    private let splite = Soda(manufacturer: "코카콜라", brand: "스프라이트", capacity: 355, price: 1500, name: "스프라이트", manufacturedDate: Date(), sugarRatio:.original, flavor: .cider)
     
     func showBeverages() {
-        topCoffee.description
-        bananaMilk.description
-        splite.description
+        print(topCoffee.description)
+        print(bananaMilk.description)
+        print(splite.description)
     }
 }
--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let vendingMachine = VendingMachine()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        vendingMachine.showBeverages()
     }
 
 

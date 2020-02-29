b01b84b245f8ed2c8e414f5bac3f3584ba1b51ae
delma <sdy2856@gmail.com>
2020-02-29 15:13:21 +0900

    Add: Class Coda's properties

--- VendingMachineApp/VendingMachineApp/Model/Soda.swift
+++ VendingMachineApp/VendingMachineApp/Model/Soda.swift
@@ -8,5 +8,25 @@
 
 import Foundation
 class Soda: Beverage {
+    private var isLowerSugar: SugarRatio
+    private var flavor: Flavor
+
+    init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, isLowerSugar: SugarRatio, flavor: Flavor) {
+        self.isLowerSugar = isLowerSugar
+        self.flavor = flavor
+        super.init(manufacturer: manufacturer, brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate)
+    }
     
+    enum Flavor {
+        case coke
+        case cider
+        case strawberry
+        case apple
+    }
+
+    enum SugarRatio {
+        case original
+        case lower
+        case free
+    }
 }

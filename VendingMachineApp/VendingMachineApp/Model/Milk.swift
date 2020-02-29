a389a980454099174c1366ec793564aa27e4a5a5
delma <sdy2856@gmail.com>
2020-02-29 15:14:44 +0900

    Modify: Class Milk's properites

--- VendingMachineApp/VendingMachineApp/Model/Milk.swift
+++ VendingMachineApp/VendingMachineApp/Model/Milk.swift
@@ -9,14 +9,21 @@
 import Foundation
 class Milk: Beverage {
     private var fatRatio: FatRatio
-    private var flavor: Int
+    private var flavor: Flavor
     private var isLactoFree: Bool
 
-    init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, fatRatio: FatRatio, flavor: Int, isLactoFree: Bool) {
+    init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, fatRatio: FatRatio, flavor: Flavor, isLactoFree: Bool) {
         self.fatRatio = fatRatio
         self.flavor = flavor
         self.isLactoFree = isLactoFree
         super.init(manufacturer: manufacturer, brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate)
+    }
+    
+    enum Flavor {
+        case normal
+        case banana
+        case choco
+        case strawberry
     }
     
     enum FatRatio {

6c90e2062e9d05020bf61b911e6b4258df57da2e
delma <sdy2856@gmail.com>
2020-02-29 15:59:15 +0900

    Feat: print beverage list in VendingMachine

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -12,9 +12,9 @@ class VendingMachine {
     private let bananaMilk = Milk(manufacturer: "빙그레", brand: "빙그레", capacity: 250, price: 1400, name: "빙그레 바나나맛 우유", manufacturedDate: Date(), fatRatio: .original, flavor: .banana, isLactoFree: false)
     private let splite = Soda(manufacturer: "코카콜라", brand: "스프라이트", capacity: 355, price: 1500, name: "스프라이트", manufacturedDate: Date(), sugarRatio:.original, flavor: .cider)
     
+    lazy var beverages: [Beverage] = [topCoffee, bananaMilk, splite]
+    
     func showBeverages() {
-        print(topCoffee.description)
-        print(bananaMilk.description)
-        print(splite.description)
+        beverages.forEach { print($0.description) }
     }
 }

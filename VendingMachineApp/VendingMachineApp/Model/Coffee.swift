3427da647d0f468f42fa174f1be087162e598375
delma <sdy2856@gmail.com>
2020-02-29 15:19:56 +0900

    Add: Class Coffee's properites

--- VendingMachineApp/VendingMachineApp/Model/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Model/Coffee.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 class Coffee: Beverage {
+    private var isContainMilk: Bool
+    private var flavor: Flavor
+    private var package: Packages
     
+    init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, isContainMilk: Bool, flavor: Flavor, package: Packages) {
+        self.isContainMilk = isContainMilk
+        self.flavor = flavor
+        self.package = package
+        super.init(manufacturer: manufacturer, brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate)
+    }
+    
+    enum Flavor {
+        case americano
+        case vanilla
+        case mocha
+    }
+    
+    enum Packages{
+        case glass
+        case can
+        case plastic
+    }
 }

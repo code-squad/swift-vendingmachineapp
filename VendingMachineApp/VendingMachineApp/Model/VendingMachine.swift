f1d9381025613d57b0588de2edcdd1ceb62786e6
TTOzzi <devttozzi@gmail.com>
2020-03-02 22:27:48 +0900

    음료 이미지 업데이트 구현
    음료의 이름과 이미지 이름을 매칭해서 이미지뷰를 만들도록 구현함
    재고를 Dictionary 로 관리하려니 정렬이 제멋대로라 튜플 배열로 정렬하여줌

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -15,14 +15,24 @@ struct VendingMachine {
     var purchasableList: [Beverage] {
         stock.getPurchasableList(money: money)
     }
-    var stockList: [Beverage: Int] {
-        stock.sortOut()
+    var stockList: [(key: Beverage, value: Int)] {
+        stock.sortOut().sorted {
+            $0.key.name < $1.key.name
+        }
     }
     var dateExpiredBeverages: [Beverage] {
         stock.getDateExpiredBeverages()
     }
     var hotBeverages: [Beverage] {
         stock.getHotBeverages()
+    }
+    
+    init() {
+        self.stock.add(beverage: StrawberryMilk(brand: "서울우유", amount: 180, price: Money(amount: 800), name: "딸기꿀단지", calorie: 145, saleablePeriod: 70, fatRatio: 9, strawberryContent: 1))
+        self.stock.add(beverage: Cola(brand: "코카콜라", amount: 250, price: Money(amount: 1200), name: "코카콜라 제로", calorie: 0, saleablePeriod: 600, sugarContent: 0))
+        self.stock.add(beverage: Cantata(brand: "롯데", amount: 500, price: Money(amount: 2500), name: "칸타타 콘트라베이스", calorie: 20, saleablePeriod: 150, caffeineContent: 179, isHot: false))
+        self.stock.add(beverage: BananaMilk(brand: "빙그레", amount: 240, price: Money(amount: 1000), name: "바나나맛우유", calorie: 208, saleablePeriod: 90, fatRatio: 15, bananaContent: 3))
+        self.stock.add(beverage: Fanta(brand: "코카콜라", amount: 250, price: Money(amount: 1000), name: "환타 오렌지", calorie: 136, saleablePeriod: 600, sugarContent: 34, flavor: "오렌지"))
     }
     
     func addBeverage(beverage: Beverage) {

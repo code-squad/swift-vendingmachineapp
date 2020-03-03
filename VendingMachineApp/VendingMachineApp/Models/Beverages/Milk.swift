6f4cd1aad16e9386a3aaab25def942d43d7f75a4
seizze <cwcwc3@gmail.com>
2020-03-03 20:36:16 +0900

    feat: Implement milk that inherits beverage

---
+++ VendingMachineApp/VendingMachineApp/Models/Beverages/Milk.swift
@@ -0,0 +1,31 @@
+//
+//  Milk.swift
+//  VendingMachineApp
+//
+//  Created by Chaewan Park on 2020/03/03.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+class Milk: Beverage {
+    
+    enum MilkFarmCode: String {
+        case seoulCowFarm = "COW001"
+        case gyeonggiBullFarm = "BUL002"
+        case incheonRhinoFarm = "RNO003"
+    }
+    
+    var farmCode: MilkFarmCode
+    var expirationDate: Date
+    
+    init(manufacture: String, size: Int, price: Int, name: String, farmCode: MilkFarmCode) {
+        self.farmCode = farmCode
+        self.expirationDate = Date()
+        super.init(manufacturer: manufacture, size: size, price: price, name: name)
+        
+        let week = DateComponents(day: 7)
+        self.expirationDate = Calendar.current.date(byAdding: week, to: self.productionDate)!
+    }
+}
+

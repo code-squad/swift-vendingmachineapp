6c6105bfb85ca95159ce5d5fe8dfb8d40cf57358
seizze <cwcwc3@gmail.com>
2020-03-04 03:21:39 +0900

    feat: Add coffee that inherits beverage

---
+++ VendingMachineApp/VendingMachineApp/Models/Beverages/Coffee.swift
@@ -0,0 +1,25 @@
+//
+//  Coffee.swift
+//  VendingMachineApp
+//
+//  Created by Chaewan Park on 2020/03/04.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+class Coffee: Beverage {
+    let caffeineContent: Int
+    let temperature: Int
+    
+    init(manufacture: String,
+         size: Int,
+         price: Int,
+         name: String,
+         caffeineContent: Int,
+         temperature: Int) {
+        self.caffeineContent = caffeineContent
+        self.temperature = temperature
+        super.init(manufacturer: manufacture, size: size, price: price, name: name)
+    }
+}

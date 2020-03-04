e7c23376ace2394a9237587b6b391946ab8ef7ff
ehgud0670 <kimdo2297@gmail.com>
2020-03-04 22:13:06 +0900

    Feat: 최상위 객체인 Beverage를 상속하는 상위 클래스들 코드 작성함 	Milk: 우유를 나타내는 객체 	Coffee: 커피를 나타내는 객체 	Soda: 탄산음료를 나타내는 객체

---
+++ VendingMachineApp/VendingMachineApp/View/Coffee.swift
@@ -0,0 +1,19 @@
+//
+//  Coffee.swift
+//  VendingMachineApp
+//
+//  Created by kimdo2297 on 2020/03/04.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import Foundation
+
+class Coffee: Beverage {
+    
+    override init(brand: String, volume: Int, price: Int,
+                  name: String, manufacturingDate: Date) {
+        super.init(brand: brand, volume: volume, price: price,
+                   name: name, manufacturingDate: manufacturingDate)
+    }
+    
+}

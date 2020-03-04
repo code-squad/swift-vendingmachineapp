66870a9520dcbe60e50c75c2a03ddc56e962c863
ehgud0670 <kimdo2297@gmail.com>
2020-03-04 21:33:51 +0900

    Feat: 자판기 구조체 추가

---
+++ VendingMachineApp/VendingMachineApp/View/VendingMachine.swift
@@ -0,0 +1,13 @@
+//
+//  VendingMachine.swift
+//  VendingMachineApp
+//
+//  Created by kimdo2297 on 2020/03/04.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import Foundation
+
+struct VendingMachine {
+    
+}

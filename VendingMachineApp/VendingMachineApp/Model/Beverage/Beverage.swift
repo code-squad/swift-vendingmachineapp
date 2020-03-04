fc77948b19446eb06ac4f2055e94bc5a02d89f6b
delma <sdy2856@gmail.com>
2020-03-04 22:40:33 +0900

    Add: a property related expirationDate and methods realted DateFormat

--- VendingMachineApp/VendingMachineApp/Model/Beverage/Beverage.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Beverage.swift
@@ -14,22 +14,32 @@ class Beverage: CustomStringConvertible {
     private var price: Int
     private var name: String
     private var manufacturedDate: Date
+    private var expirationDate: Date
+    
     var description: String {
         return "\(manufacturer), \(capacity), \(price), \(brand), \(dateToString(manufacturedDate))"
     }
 
-    init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date) {
+    init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, expirationDate: Date) {
         self.manufacturer = manufacturer
         self.brand = brand
         self.capacity = capacity
         self.price = price
         self.name = name
         self.manufacturedDate = manufacturedDate
+        self.expirationDate = expirationDate
     }
     
-    func dateToString(_ manufacturedDate: Date) -> String {
+    func dateToString(_ date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyyMMdd"
-        return dateFormatter.string(from: manufacturedDate)
+        return dateFormatter.string(from: date)
+    }
+    
+    func stringToDate(_ date: String) -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        dateFormatter.timeZone = NSTimeZone(name: "UTC") as TimeZone?
+        return dateFormatter.date(from: date)!
     }
 }

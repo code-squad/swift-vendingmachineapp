92bf1fe638d95f1419fcf04ea9611cfe52ada1df
delma <sdy2856@gmail.com>
2020-03-05 17:42:19 +0900

    Add: Beverage Test Case

---
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -0,0 +1,14 @@
+//
+//  VendingMachineAppTests.swift
+//  VendingMachineAppTests
+//
+//  Created by delma on 05/03/2020.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import XCTest
+@testable import VendingMachineApp
+
+class VendingMachineAppTests: XCTestCase {
+
+}

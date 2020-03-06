bcda10137379682b98b58ba79f28a3bde3b87961
ehgud0670 <kimdo2297@gmail.com>
2020-03-06 12:57:38 +0900

    Feat: VendingMachine 구조체를 프로퍼티로 추가, let으로 선언      이유: let으로 일단 선언하고, 이후 step에서 프로퍼티를 바꿔야 한다면 	  var 으로 재선언 할 것이다.

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let vendingMachine = VendingMachine()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

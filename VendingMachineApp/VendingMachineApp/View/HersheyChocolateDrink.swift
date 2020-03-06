a58c7dcb5e9992ea9e02c546f606438ffae0913f
ehgud0670 <kimdo2297@gmail.com>
2020-03-06 12:53:45 +0900

    Feat: Milk 클래스를 상속하는 HersheyChocolateDrink 클래스 구현       1. 회사는 "매일유업"가 확실하므로 convenience init을 이용하여          default value로 선정함       2. 나머지 프로퍼티들은 무슨 허쉬초콜릿드링크 음료냐에 따라서 다 다른 값이므로          생성자에서 매개변수로 받게끔 코드 구현

---
+++ VendingMachineApp/VendingMachineApp/View/HersheyChocolateDrink.swift
@@ -0,0 +1,22 @@
+//
+//  Chocoemon.swift
+//  VendingMachineApp
+//
+//  Created by kimdo2297 on 2020/03/04.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import Foundation
+
+class HersheyChocolateDrink: Milk {
+    
+    override init(brand: String, volume: Int, price: Int, name: String, manufacturingDate: Date) {
+        super.init(brand: brand, volume: volume, price: price, name: name, manufacturingDate: manufacturingDate)
+    }
+    
+    convenience init(volume: Int, price: Int, name: String, manufacturingDate: Date) {
+        self.init(brand: "매일유업", volume: volume, price: price, name: name,
+                  manufacturingDate: manufacturingDate)
+    }
+    
+}

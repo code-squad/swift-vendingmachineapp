a6c16534bc9c5942f282defa2ecea25e0493aef6
ehgud0670 <kimdo2297@gmail.com>
2020-03-06 14:17:29 +0900

    Feat: viewDidLoad에 음료 객체 3가지 추가하고 음료 객체의 내용을 print함

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let primiumLatte = Cantata(volume: 275, price: 2200, name: "프리미엄 라떼", dateInfo: "20200102")
+        let dietCola = Pepsi(volume: 350, price: 2000, name: "다이어트 콜라", dateInfo: "20190928")
+        let cookieCream = HersheyChocolateDrink(volume: 190, price: 1500, name: "쿠키 앤 크림", dateInfo: "20191116")
+        
+        let nonInitMessage = "non-init"
+        print(primiumLatte?.description ?? nonInitMessage)
+        print(dietCola?.description ?? nonInitMessage)
+        print(cookieCream?.description ?? nonInitMessage)
     }
     
 }

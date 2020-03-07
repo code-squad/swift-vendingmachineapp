dcc7fa384b604373df37b31db57e25e7efa7f915
delma <sdy2856@gmail.com>
2020-03-07 12:18:01 +0900

    Refactor: remove property flavor in Soda

--- VendingMachineApp/VendingMachineApp/Model/Beverage/Soda/Cider.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Soda/Cider.swift
@@ -9,10 +9,9 @@
 import Foundation
 class Cider: Soda {
     private var sugarRatio: SugarRatio = .original
-    private var flavor: Flavor = .cider
     
     init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, expirationDate: Date, temperature: Int) {
 
-        super.init(manufacturer: manufacturer, brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate, expirationDate: expirationDate, sugarRatio: sugarRatio, flavor: flavor, temperature: temperature)
+        super.init(manufacturer: manufacturer, brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate, expirationDate: expirationDate, sugarRatio: sugarRatio, temperature: temperature)
     }
 }

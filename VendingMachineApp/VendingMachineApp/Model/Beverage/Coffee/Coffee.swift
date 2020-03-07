a2776582cba45ba519cf9d4d7fb301e5c82218f2
delma <sdy2856@gmail.com>
2020-03-07 12:28:29 +0900

    Refactor: Coffee classes

--- VendingMachineApp/VendingMachineApp/Model/Beverage/Coffee/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Coffee/Coffee.swift
@@ -9,23 +9,12 @@
 import Foundation
 class Coffee: Beverage {
     private var isContainMilk: Bool
-    private var flavor: Flavor
     private var package: Packages
-//    private var isHot: Bool
     
-    init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, expirationDate: Date, isContainMilk: Bool, flavor: Flavor, package: Packages, temperature: Int) {
+    init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, expirationDate: Date, isContainMilk: Bool, package: Packages, temperature: Int) {
         self.isContainMilk = isContainMilk
-        self.flavor = flavor
         self.package = package
-//        self.isHot = isHot
         super.init(manufacturer: manufacturer, brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate, expirationDate: expirationDate, temperature: temperature)
-    }
-    
-    enum Flavor {
-        case americano
-        case vanilla
-        case mocha
-        case latte
     }
     
     enum Packages{

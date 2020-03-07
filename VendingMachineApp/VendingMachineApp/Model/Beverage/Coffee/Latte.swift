cbab1b65dad476e13f49e1c6b1310a66cdad8194
delma <sdy2856@gmail.com>
2020-03-07 12:28:29 +0900

    Refactor: Coffee classes

---
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Coffee/Latte.swift
@@ -0,0 +1,15 @@
+//
+//  Latte.swift
+//  VendingMachineApp
+//
+//  Created by delma on 04/03/2020.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+class Latte: Coffee {
+    override init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, expirationDate: Date, isContainMilk: Bool, package: Packages, temperature: Int) {
+        super.init(manufacturer: manufacturer, brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate, expirationDate: expirationDate, isContainMilk: isContainMilk, package: package, temperature: temperature)
+        
+    }
+}

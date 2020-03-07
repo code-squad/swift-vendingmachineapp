b44e15fe21860c3537c62a80b70f33640dbf73aa
delma <sdy2856@gmail.com>
2020-03-07 21:47:58 +0900

    Refactor properties inherit from Soda & Add Milkis inherit from Soda

--- VendingMachineApp/VendingMachineApp/Model/Beverage/Soda/Milkis.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Soda/Milkis.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+class Milkis: Soda {
+    private var milkRatio: Double
+    
+    init(manufacturer: String, brand: String, capacity: Int, price: Int, name: String, manufacturedDate: Date, expirationDate: Date, sugarRatio: SugarRatio, temperature: Int, milkRatio: Double) {
+        self.milkRatio = milkRatio
+        super.init(manufacturer: manufacturer, brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate, expirationDate: expirationDate, sugarRatio: sugarRatio, temperature: temperature)
+    }
+}

aa633f779b17e157347ff2e7c63668fe22c77f79
delma <sdy2856@gmail.com>
2020-03-08 20:50:24 +0900

    Refactor vendingMachine product list

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -11,6 +11,16 @@ struct VendingMachine {
     private var beverages: Beverages
     private(set) var balance: Money = Money()
     private var purchasedList: [Beverage] = []
+    let bananaMilk = BananMilk(manufacturer: "연세우유", brand: "곰곰", capacity: 200, price: Money(balance: 1200), name: "곰곰 바나나우유", manufacturedDate: Date(), expirationDate: Date(), fatRatio: .original, isLactoFree: false, temperature: 8, bananaCountry: "케냐")
+    let chocoMilk = ChocoMilk(manufacturer: "덴마크우유", brand: "덴마크우유", capacity: 300, price: Money(balance: 1600), name: "초코초코우유", manufacturedDate: Date(), expirationDate: Date(), fatRatio: .lower, isLactoFree: false, temperature: 8, chocolateRatio: 30.0)
+    let strawberryMilk = StrawberryMilk(manufacturer: "덴마크우유", brand: "덴마크우유", capacity: 300, price: Money(balance: 1600), name: "딸기딸기우유", manufacturedDate: Date(), expirationDate: Date(), fatRatio: .free, temperature: 8, isLactoFree: true)
+    let americano = Americano(manufacturer: "맥심", brand: "티오피", capacity: 255, price: Money(balance: 1400), name: "스위트 아메리카노", manufacturedDate: Date(), expirationDate: Date(), isContainMilk: false, package: .can, temperature: 70, coffeeBean: "에티오피아")
+    let latte = Latte(manufacturer: "매일유업", brand: "마이카페라떼", capacity: 200, price: Money(balance: 1400), name: "마이카페라떼 마일드", manufacturedDate: Date(), expirationDate: Date(), isContainMilk: true, package: .plastic, temperature: 8, isContainSugar: true)
+    let mocha = Mocha(manufacturer: "동서식품", brand: "스타벅스", capacity: 300, price: Money(balance: 2200), name: "스타벅스 프라푸치노 모카", manufacturedDate: Date(), expirationDate: Date(), isContainMilk: true, package: .glass, temperature: 8, isContainWheepCream: true)
+    let coke = Coke(manufacturer: "코카콜라", brand: "코카콜라", capacity: 355, price: Money(balance: 1800), name: "코카콜라", manufacturedDate: Date(), expirationDate: Date(), sugarRatio: .original, temperature: 8, cocaLeafRatio: 10.0)
+    let cider = Cider(manufacturer: "코카콜라", brand: "스프라이트", capacity: 355, price: Money(balance: 1800), name: "스프라이트", manufacturedDate: Date(), expirationDate: Date(), sugarRatio: .lower, temperature: 8, flavor: .lemon)
+    let milkis = Milkis(manufacturer: "롯데", brand: "밀키스", capacity: 355, price: Money(balance: 1400), name: "밀키스", manufacturedDate: Date(), expirationDate: Date(), sugarRatio: .original, temperature: 4, milkRatio: 20.0)
+    lazy var products: [Beverage] = [bananaMilk, chocoMilk, strawberryMilk, americano, latte, mocha, coke, cider, milkis]
 
     init() {
         beverages = Beverages()
@@ -24,18 +34,18 @@ struct VendingMachine {
         balance.raiseMoney(fiveThousandCount: fiveThousandCount, thousandCount: thousandCount, fiveHundredCount: fiveHundredCount, hundredCount: hundredCount)
     }
 
-    func addStock(_ beverage: Beverage) {
-        beverages.addBeverage(beverage)
+    mutating func addStock(_ index: Int) {
+        beverages.addBeverage(products[index])
     }
 
     mutating func reportAvailableBeverageNowMoney() -> [Beverage] {
         return beverages.reportAvailableBeverageNowMoney(confirmBalance())
     }
 
-    mutating func purchaseBeverage(beverage: Beverage, price: Money) {
-        balance.subtract(price)
-        beverages.removeBeverage(beverage)
-        purchasedList.append(beverage)
+    mutating func purchaseBeverage(index: Int) {
+        balance.subtract(products[index].price)
+        beverages.removeBeverage(products[index])
+        purchasedList.append(products[index])
     }
 
     func confirmBalance() -> Money {
--- VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -11,14 +11,11 @@ import XCTest
 
 class VendingMachineAppTests: XCTestCase {
     var vendingMachine = VendingMachine()
-   let coffee = Mocha(manufacturer: "동서식품", brand: "스타벅스", capacity: 355, price: Money(balance: 2700), name: "스타벅스 모카", manufacturedDate: Date(), expirationDate:  Date(), isContainMilk: true, package: .glass, temperature: 8, isContainWheepCream: true)
-    let soda = Coke(manufacturer: "코카콜라", brand: "코카콜라", capacity: 200, price: Money(balance: 2000), name: "코카콜라", manufacturedDate: Date(), expirationDate: Date(), sugarRatio: .lower, temperature: 2, cocaLeafRatio: 8.8)
-    let milk = StrawberryMilk(manufacturer: "서울우유", brand: "서울우유", capacity: 200, price: Money(balance: 1200), name: "딸기맛 우유", manufacturedDate: Date(), expirationDate:  Date(), fatRatio: .original, temperature: 8, isLactoFree: false)
     
     override func setUp() {
-        vendingMachine.addStock(coffee)
-        vendingMachine.addStock(soda)
-        vendingMachine.addStock(milk)
+        vendingMachine.addStock(0)
+        vendingMachine.addStock(3)
+        vendingMachine.addStock(6)
     }
     
     func testRaiseMoney() {
@@ -29,24 +26,24 @@ class VendingMachineAppTests: XCTestCase {
     
     func testAvailableBeverageNowMoney() {
          vendingMachine.raiseMoney(fiveThousandCount: 2, thousandCount: 2, fiveHundredCount: 1, hundredCount: 2)
-        let availabeBeverage = vendingMachine.reportAvailableBeverageNowMoney()
-        XCTAssertEqual(availabeBeverage.contains(coffee), true)
-        XCTAssertEqual(availabeBeverage.contains(soda), true)
-        XCTAssertEqual(availabeBeverage.contains(milk), true)
+        let availableBeverage = vendingMachine.reportAvailableBeverageNowMoney()
+        XCTAssertEqual(availableBeverage.contains(vendingMachine.bananaMilk), true)
+        XCTAssertEqual(availableBeverage.contains(vendingMachine.americano), true)
+        XCTAssertEqual(availableBeverage.contains(vendingMachine.coke), true)
     }
-    
+
     func testPurchaseBeverage() {
         vendingMachine.raiseMoney(fiveThousandCount: 2, thousandCount: 2, fiveHundredCount: 1, hundredCount: 2)
-        vendingMachine.purchaseBeverage(beverage: coffee, price: coffee.price)
-        XCTAssertEqual(vendingMachine.balance.balance, 10000)
+        vendingMachine.purchaseBeverage(index: 0)
+        XCTAssertEqual(vendingMachine.balance.balance, 11500)
     }
     
     func testPurchaseHistory() {
-        vendingMachine.purchaseBeverage(beverage: coffee, price: coffee.price)
+        vendingMachine.purchaseBeverage(index: 0)
         let history = vendingMachine.reportPurchasedHistory()
-        XCTAssertEqual(history.contains(coffee), true)
-        XCTAssertEqual(history.contains(milk), false)
-        XCTAssertEqual(history.contains(soda), false)
+        XCTAssertEqual(history.contains(vendingMachine.bananaMilk), true)
+        XCTAssertEqual(history.contains(vendingMachine.americano), false)
+        XCTAssertEqual(history.contains(vendingMachine.coke), false)
     }
     
 }

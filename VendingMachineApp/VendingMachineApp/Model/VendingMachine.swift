fdad72a9e09259c491b0b431566a38624005943b
delma <sdy2856@gmail.com>
2020-03-08 19:27:43 +0900

    delete: meaningless codes

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -11,7 +11,7 @@ struct VendingMachine {
     private var beverages: Beverages
     private(set) var balance: Money = Money()
     private var purchasedList: [Beverage] = []
-//    private var inputMoney: Money
+
     init() {
         beverages = Beverages()
     }
@@ -20,7 +20,6 @@ struct VendingMachine {
         beverages.forEachBeverages { print($0.description) }
     }
     
-    //자판기에 돈 넣어서 잔액 올림
     mutating func raiseMoney(fiveThousandCount: Int, thousandCount: Int, fiveHundredCount: Int, hundredCount: Int) {
         balance.raiseMoney(fiveThousandCount: fiveThousandCount, thousandCount: thousandCount, fiveHundredCount: fiveHundredCount, hundredCount: hundredCount)
     }
@@ -29,19 +28,16 @@ struct VendingMachine {
         beverages.addBeverage(beverage)
     }
 
-    //현재 잔액으로 구매 가능한 음료들
     mutating func reportAvailableBeverageNowMoney() -> [Beverage] {
         return beverages.reportAvailableBeverageNowMoney(confirmBalance())
     }
 
-    //음료 금액만큼 잔액 차감
     mutating func purchaseBeverage(beverage: Beverage, price: Money) {
         balance.subtract(price)
         beverages.removeBeverage(beverage)
         purchasedList.append(beverage)
     }
 
-    //현재 잔액 확인
     func confirmBalance() -> Money {
         return balance
     }

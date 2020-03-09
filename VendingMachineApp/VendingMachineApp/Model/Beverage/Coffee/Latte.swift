bd6597d342304a51c87be7d83d2a94ef003f4334
delma <sdy2856@gmail.com>
2020-03-09 19:27:02 +0900

    Refactor: properties that type is Bool

--- VendingMachineApp/VendingMachineApp/Model/Beverage/Coffee/Latte.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverage/Coffee/Latte.swift
@@ -8,10 +8,10 @@
 
 import Foundation
 class Latte: Coffee {
-      private var isContainSugar: Bool
+      private var sugarAmount: Int
       
-      init(manufacturer: String, brand: String, capacity: Int, price: Money, name: String, manufacturedDate: Date, expirationDate: Date, isContainMilk: Bool, package: Packages, temperature: Int, isContainSugar: Bool) {
-          self.isContainSugar = isContainSugar
+      init(manufacturer: String, brand: String, capacity: Int, price: Money, name: String, manufacturedDate: Date, expirationDate: Date, isContainMilk: Bool, package: Packages, temperature: Int, sugarAmount: Int) {
+          self.sugarAmount = sugarAmount
           super.init(manufacturer: manufacturer, brand: brand, capacity: capacity, price: price, name: name, manufacturedDate: manufacturedDate, expirationDate: expirationDate, package: package, temperature: temperature)
           
       }

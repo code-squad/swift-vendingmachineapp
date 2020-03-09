ee39811edc80d4864e352f3cbcef3a9bcf0b2920
ehgud0670 <kimdo2297@gmail.com>
2020-03-09 17:34:41 +0900

    Feat: 우유 객체의 유통기한 검사하는 테스트 코드 작성함 	* 성공하는 경우, 실패하는 경우 모두 테스트 코드 작성함

--- VendingMachineApp/VendingMachineAppTests/MilkTest.swift
+++ VendingMachineApp/VendingMachineAppTests/MilkTest.swift
@@ -10,20 +10,32 @@ import XCTest
 @testable import VendingMachineApp
 
 class MilkTest: XCTestCase {
-
+    
     var milk: Milk!
     
     override func setUp() {
         super.setUp()
         milk = HersheyChocolateDrink(volume: 190, price: 1500,
-        name: "쿠키 앤 크림",
-        manufacturingDateInfo: "20191116",
-        expirationDateInfo: "20191123")
+                                     name: "쿠키 앤 크림",
+                                     manufacturingDateInfo: "20191116",
+                                     expirationDateInfo: "20191123")
     }
-
+    
     override func tearDown() {
         milk = nil
         super.tearDown()
+    }
+    
+    func testValidateSuccess() {
+        if let date = Date.dateFormatter.date(from: "20191120") {
+            XCTAssertTrue(milk.validate(with: date))
+        }
+    }
+    
+    func testValidateFail() {
+        if let date = Date.dateFormatter.date(from: "20191128") {
+            XCTAssertFalse(milk.validate(with: date))
+        }
     }
     
 }

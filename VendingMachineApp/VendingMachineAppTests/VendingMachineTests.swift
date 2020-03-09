8b3477ee05d6c68e7ee4c5ac64cb1787a512f127
seizze <cwcwc3@gmail.com>
2020-03-09 15:16:32 +0900

    test: Add vending machine test methods
    Test using mock cashier that conforms protocol calculable

--- VendingMachineApp/VendingMachineAppTests/VendingMachineTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineTests.swift
@@ -12,9 +12,30 @@ import XCTest
 class VendingMachineTests: XCTestCase {
     
     var vendingMachine: VendingMachine!
+    var mockCashier: MockCashier!
     
     override func setUp() {
-        vendingMachine = VendingMachine()
+        mockCashier = MockCashier()
+        vendingMachine = VendingMachine(cashier: mockCashier)
+    }
+    
+    func testInsert() {
+        vendingMachine.insert(money: 1000)
+        XCTAssertEqual(mockCashier.cash, 1000)
+        XCTAssertEqual(mockCashier.addToBalanceCalled, 1)
+    }
+    
+    func testSell() {
+        let items = [StrawberryMilk(), Coke(), Fanta(), Top()]
+        items.forEach { vendingMachine.fill(beverage: $0) }
+        
+        vendingMachine.insert(money: 2000)
+        vendingMachine.sell(beverage: "Coke")
+        
+        XCTAssertEqual(mockCashier.cash, 400)
+        XCTAssertEqual(mockCashier.balanceCalled, 1)
+        XCTAssertEqual(mockCashier.addToBalanceCalled, 1)
+        XCTAssertEqual(mockCashier.sellCalled, 1)
     }
     
     func testSalesLog() {
@@ -24,8 +45,34 @@ class VendingMachineTests: XCTestCase {
         vendingMachine.insert(money: 2000)
         vendingMachine.sell(beverage: "Coke")
         
-        var salesLog = [Beverage]()
-        vendingMachine.repeatForSalesLog { salesLog.append($0) }
-        XCTAssert(salesLog[0] === items[1])
+        vendingMachine.repeatForSalesLog { _ in return }
+        XCTAssertEqual(mockCashier.salesLogCalled, 1)
+    }
+}
+
+class MockCashier: Calculable {
+    var cash = 0
+    var balanceCalled = 0
+    var addToBalanceCalled = 0
+    var sellCalled = 0
+    var salesLogCalled = 0
+    
+    func balance() -> Int {
+        balanceCalled += 1
+        return cash
+    }
+    
+    func addToBalance(_ money: Int) {
+        addToBalanceCalled += 1
+        cash += money
+    }
+    
+    func sell(beverage: Beverage) {
+        sellCalled += 1
+        cash -= beverage.price
+    }
+    
+    func forEachSalesLog(_ block: (Beverage) -> Void) {
+        salesLogCalled += 1
     }
 }

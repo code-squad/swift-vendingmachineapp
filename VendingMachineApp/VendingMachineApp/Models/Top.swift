d3d15cd85618c82435bfd3d11c0087c8b9e81d50
GangWoon <gangwoon2018@gmail.com>
2020-03-10 14:21:52 +0900

    Top 클래스 수정
    top의 특징인 coffeeVarieties를 속성으로 추가했습니다.
    커피 품종이라는 의미로 사용했습니다.

--- VendingMachineApp/VendingMachineApp/Models/Top.swift
+++ VendingMachineApp/VendingMachineApp/Models/Top.swift
@@ -10,6 +10,13 @@ import Foundation
 
 class Top: ElementsOfBeverage {
 
+    private var coffeeVarieties: String
+    
+    init(brand: String, weight: Int, price: Int, name: String, dateOfManufacture: Date, coffeeVarieties: String) {
+        self.coffeeVarieties = coffeeVarieties
+        super.init(brand: brand, weight: weight, price: price, name: name, dateOfManufacture: dateOfManufacture)
+        apply(featureOfBeverage: [coffeeVarieties])
+    }
 }
 
 

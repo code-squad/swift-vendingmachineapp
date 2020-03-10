849bbda5cc5c86757c4b6287be1d8ecfe08cd68c
kjoonk <kjoonk@gmail.com>
2020-03-10 15:39:50 +0900

    프로젝트 생성 및 설정
    - iPad용으로 설정

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by Viet on 2020/03/10.
+//  Copyright © 2020 Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

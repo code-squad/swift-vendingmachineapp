a460d257416e1506833e8ca0252a625539dd06f1
GangWoon <gangwoon2018@gmail.com>
2020-03-10 11:11:20 +0900

    BananaMilk 클래스 생성
    ElementsOfBeverage를 상속받습니다.

---
+++ VendingMachineApp/VendingMachineApp/Models/BananaMilk.swift
@@ -0,0 +1,13 @@
+//
+//  BananaMilk.swift
+//  VendingMachineApp
+//
+//  Created by Cloud on 2020/03/10.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+
+class BananaMilk: ElementsOfBeverage {
+    
+}

d5820807ee36af324a32fc4ef5d04018861043de
GangWoon <gangwoon2018@gmail.com>
2020-03-11 18:56:56 +0900

    ViewController 클래스 수정
    테스트 코드를 추가해서 viewcontroller에 추가할 필요가 없다고 생각했습니다.

--- VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
@@ -9,18 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    private var beverages: [Beverage] = []
-    private let cola = Cola(brand: "코카콜라", weight: 1332, price: 1500, name: "코카콜라 제로", dateOfManufacture: Date(), pondusHydrogenii: 2.5)
-    private let top = Top(brand: "맥심", weight: 500, price: 2058, name: "스위트 아메리카", dateOfManufacture: Date(), coffeeVarieties: "아라비카")
-    private let bananaMilk = BananaMilk(brand: "빙그레", weight: 250, price: 1300, name: "바나나맛 우유", dateOfManufacture: Date(), flavor: "바나나")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        beverages.append(cola)
-        beverages.append(top)
-        beverages.append(bananaMilk)
-        beverages.forEach { print($0) }
+        
     }
 }
 

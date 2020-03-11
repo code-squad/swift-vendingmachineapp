d1a6d811f198d0d61f423e3944aa392a1a127987
seizze <cwcwc3@gmail.com>
2020-03-11 22:00:52 +0900

    refactor: Separate notification name extension

---
+++ VendingMachineApp/VendingMachineApp/Utils/NotificationNameExtension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationNameExtension.swift
+//  VendingMachineApp
+//
+//  Created by Chaewan Park on 2020/03/11.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let balanceDidChange = Notification.Name("balanceDidChange")
+}

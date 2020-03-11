486e15f90a910f0a9541f30e1f29e068a1a922b5
ehgud0670 <kimdo2297@gmail.com>
2020-03-11 19:12:18 +0900

    Feat: 펩시의 패키지 재질에 대한 속성 추가

--- VendingMachineApp/VendingMachineApp/Models/Pepsi.swift
+++ VendingMachineApp/VendingMachineApp/Models/Pepsi.swift
@@ -10,12 +10,26 @@ import Foundation
 
 class Pepsi: Soda {
     
+    private let package: Package
+    
     init?(volume: Int, price: Int, name: String,
-          manufacturingDateInfo: String, kiloCalorie: Int) {
+          manufacturingDateInfo: String, kiloCalorie: Int,
+          package: Package) {
+        self.package = package
         super.init(brand: "팹시", volume: volume,
                    price: price, name: name,
                    manufacturingDateInfo: manufacturingDateInfo,
                    kiloCalorie: kiloCalorie)
+    }
+    
+}
+
+extension Pepsi {
+    
+    enum Package {
+        case glass
+        case can
+        case pet
     }
     
 }

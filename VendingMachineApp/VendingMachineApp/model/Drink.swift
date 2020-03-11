cd74c5df2bad9a7e46123580b8d410243c8f4eb6
limwin94 <amuguna94@gmail.com>
2020-03-11 20:19:42 +0900

    Drink Class 추가

---
+++ VendingMachineApp/VendingMachineApp/model/Drink.swift
@@ -0,0 +1,25 @@
+//
+//  Drink.swift
+//  VendingMachineApp
+//
+//  Created by 임승혁 on 2020/03/11.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import Foundation
+
+class Drink {
+    private let brand: String
+    private let capacity: Int
+    private let price: Int
+    private let name: String
+    private let manufacturingDate: Date
+    
+    init(brand: String, capacity: Int, price: Int, name: String, manufacturingDate: Date) {
+        self.brand = brand
+        self.capacity = capacity
+        self.price = price
+        self.name = name
+        self.manufacturingDate = manufacturingDate
+    }
+}

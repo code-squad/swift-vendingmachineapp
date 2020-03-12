e8d857377383118aa3e68d6f8df5dc20d467ea83
limwin94 <amuguna94@gmail.com>
2020-03-12 16:34:58 +0900

    Drink Class를 상속받은 ChocolateMilk, Fanta, Georgia Class 생성

---
+++ VendingMachineApp/VendingMachineApp/model/ChocolateMilk.swift
@@ -0,0 +1,13 @@
+//
+//  ChocolateMilk.swift
+//  VendingMachineApp
+//
+//  Created by 임승혁 on 2020/03/12.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import Foundation
+
+class ChocolateMilk: Drink {
+    
+}

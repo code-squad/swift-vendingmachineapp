d163765a7d7c43201dd3b8b6062d85f09e03767e
TTOzzi <devttozzi@gmail.com>
2020-03-12 15:56:51 +0900

    기본 음료 채우는 코드 위치 변경
    VendingMachineManager 가 생성될 때 추가하도록 변경

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -9,8 +9,8 @@
 import Foundation
 
 class VendingMachine: NSObject, NSCoding {
-    private var stock = Stock()
-    private var money = Money() {
+    private var stock: Stock
+    private var money: Money {
         didSet {
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "MoneyChanged"), object: self, userInfo: ["balance":"\(money)"])
         }
@@ -32,11 +32,8 @@ class VendingMachine: NSObject, NSCoding {
     }
     
     override init() {
-        self.stock.add(beverage: StrawberryMilk(brand: "서울우유", amount: 180, price: Money(amount: 800), name: "딸기꿀단지", calorie: 145, saleablePeriod: 70, fatRatio: 9, strawberryContent: 1))
-        self.stock.add(beverage: Cola(brand: "코카콜라", amount: 250, price: Money(amount: 1200), name: "코카콜라 제로", calorie: 0, saleablePeriod: 600, sugarContent: 0))
-        self.stock.add(beverage: Cantata(brand: "롯데", amount: 500, price: Money(amount: 2500), name: "칸타타 콘트라베이스", calorie: 20, saleablePeriod: 150, caffeineContent: 179, isHot: false))
-        self.stock.add(beverage: BananaMilk(brand: "빙그레", amount: 240, price: Money(amount: 1000), name: "바나나맛우유", calorie: 208, saleablePeriod: 90, fatRatio: 15, bananaContent: 3))
-        self.stock.add(beverage: Fanta(brand: "코카콜라", amount: 250, price: Money(amount: 1000), name: "환타 오렌지", calorie: 136, saleablePeriod: 600, sugarContent: 34, flavor: "오렌지"))
+        self.stock = Stock()
+        self.money = Money()
     }
     
     func addBeverage(beverage: Beverage) {

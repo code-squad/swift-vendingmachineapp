b6478a405eb6899371b65923ea894829c4059716
ehgud0670 <kimdo2297@gmail.com>
2020-03-15 16:34:39 +0900

    Feat: 앞으로 사용할 커스텀 뷰들 생성함 	* 상속이 필요없으므로 final로 선언함

---
+++ VendingMachineApp/VendingMachineApp/Views/BeverageNumberLabel.swift
@@ -0,0 +1,13 @@
+//
+//  BeverageNumberLabel.swift
+//  VendingMachineApp
+//
+//  Created by kimdo2297 on 2020/03/15.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class BeverageNumberLabel: UILabel {
+
+}

0147b1405f8057a1e037a323121b4eb1b0711394
GangWoon <gangwoon2018@gmail.com>
2020-03-15 16:26:17 +0900

    Notification.Name  확장
    노티피케이션을 사용할때 필요한 이름을 static으로 만들었습니다.

---
+++ VendingMachineApp/VendingMachineApp/Extension/NotificationName.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationName.swift
+//  VendingMachineApp
+//
+//  Created by Cloud on 2020/03/15.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let vendingMachine = Notification.Name("vendingMachine")
+}

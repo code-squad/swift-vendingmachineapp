3e2170de83e5f5f35c6bceb09ecf7c0683fcddbe
ehgud0670 <kimdo2297@gmail.com>
2020-03-15 21:52:56 +0900

    Feat: 현재 잔액을 보여주는 커스텀 레이블 구현함

---
+++ VendingMachineApp/VendingMachineApp/Views/BalanceLabel.swift
@@ -0,0 +1,24 @@
+//
+//  BalanceLabel.swift
+//  VendingMachineApp
+//
+//  Created by kimdo2297 on 2020/03/15.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import UIKit
+
+final class BalanceLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func update(currentMoney :Int) {
+        text = "\(currentMoney)원"
+    }
+}

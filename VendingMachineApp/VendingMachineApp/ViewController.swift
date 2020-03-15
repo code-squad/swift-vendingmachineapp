425005c82b56ef54cae1e67646fda48feae924ba
ehgud0670 <kimdo2297@gmail.com>
2020-03-15 17:56:20 +0900

    Deleted: 자판기의 초기 재고들 삭제함

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -10,30 +10,8 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    private var vendingMachine = VendingMachine(stock: [Cantata(cantataBuilder:
-                                                                Cantatas.builderPrimiumLatte175ml,
-                                                                manufacturingDateInfo: "20200102",
-                                                                celsius: 65)!,
-                                                        Cantata(cantataBuilder:
-                                                                Cantatas.builderPrimiumLatte275ml,
-                                                                manufacturingDateInfo: "20200102",
-                                                                celsius: 65)!,
-                                                        Pepsi(pepsiBuilder:
-                                                              Pepsis.builderDietCola250ml,
-                                                              manufacturingDateInfo: "20190928")!,
-                                                        Pepsi(pepsiBuilder:
-                                                              Pepsis.builderDietCola591ml,
-                                                              manufacturingDateInfo: "20190928")!,
-                                                        HersheyChocolateDrink(hersheyBuilder:
-                                                                              HersheyChocolateDrinks
-                                                                              .builderCookieCream235ml,
-                                                                              manufacturingDateInfo: "20191116",
-                                                                              expirationDateInfo: "20191123")!,
-                                                        HersheyChocolateDrink(hersheyBuilder:
-                                                                              HersheyChocolateDrinks
-                                                                              .builderCookieCream350ml,
-                                                                              manufacturingDateInfo: "20191116",
-                                                                              expirationDateInfo: "20191123")!])
+    
+    private var vendingMachine = VendingMachine()
     
     override func viewDidLoad() {
         super.viewDidLoad()

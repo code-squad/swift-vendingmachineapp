294cf67f70753499dad4107636f4829070b7ca85
GangWoon <gangwoon2018@gmail.com>
2020-03-15 16:32:27 +0900

    ViewController 클래스 수정
    모델 클래스와 뷰 클래스 생성하고 두 클래스끼리의 관계를 생성했습니다.
    모델 클래스의 변화를 감지하기 위해서 NotifcationCenter를 사용했습니다.

--- VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/Controllers/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var manager: DataManager = DataManager()
+    var vendingMachineView: VendingMachineView {
+        view as! VendingMachineView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        vendingMachineView.datasource = manager
+        addNotificationObserver()
+    }
+    
+    private func addNotificationObserver() {
+        NotificationCenter
+            .default
+            .addObserver(
+                self,
+                selector: #selector(pushInformationToView),
+                name: .vendingMachine,
+                object: nil
+        )
+    }
+    
+    @objc private func pushInformationToView(notification: Notification) {
+        if let balance = notification.userInfo?["balance"] {
+            self.vendingMachineView.update(balance: "잔액: \(balance)")
+        } else {
+            let valueTuple = notification.userInfo?["beverageCounts"] as? (tag: Int, counts: Int)
+            self.vendingMachineView.update(tag: valueTuple?.tag ?? 0, counts: String(valueTuple?.counts ?? 0))
+        }
     }
 }
-

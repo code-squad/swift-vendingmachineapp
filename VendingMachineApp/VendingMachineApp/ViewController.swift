c7c9d31df8f269aa9aa56b69f9bdad031503c93e
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-02-22 22:02:08 +0900

    Create subclasses
    Implemented subclass's designated initializer
    Tried to implement convenience inits to the each of classes
    Temporarily deleted generateDate method

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let beverage1 = Beverage(brandName: "종근당", sizeInMilliliter: 150, itemPrice: 9999, itemName: "쌍화차", manufacturingDate: Date())
+    let milk1 = Milk(brandName: "서울우유", sizeInMilliliter: 200, itemPrice: 1000, itemName: "날마다딸기우유", milkCategory: .딸기우유)
+    let soda1 = Soda(brandName: "펩시", sizeInMilliliter: 1000, itemPrice: 2000, itemName: "스쿼드콜라", sodaCategory: .콜라)
+    let coffee1 = Coffee(brandName: "맥심", sizeInMilliliter: 250, itemPrice: 1500, itemName: "TOP이탈리아노", coffeeCategory: .TOP)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(beverage1)
+        print(milk1)
+        print(soda1)
+        print(coffee1)
     }
-
-
 }
 

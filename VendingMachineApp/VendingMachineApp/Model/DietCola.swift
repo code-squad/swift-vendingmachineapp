850df5deeac87ae4ed60ae761bf69354d89b0c55
hoonha choi <sosqsz2@naver.com>
2021-02-23 21:58:18 +0900

    inputDate함수 static 추가 및 enum 브랜드 제거

--- VendingMachineApp/VendingMachineApp/Model/DietCola.swift
+++ VendingMachineApp/VendingMachineApp/Model/DietCola.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class DietCola : Soda {
-    init(liter: Int, price: Int, name: String, manufactured: Date) {
-        super.init(brand: Brand.Soda(.Pepsi) , liter: liter, price: price, name: name, manufactured: manufactured)
+    override init(brand : String, liter: Int, price: Int, name: String, manufactured: Date) {
+        super.init(brand: brand, liter: liter, price: price, name: name, manufactured: manufactured)
     }
 }

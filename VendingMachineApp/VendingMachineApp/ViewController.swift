c7e24a41ab5d8986f74854f1817995ebf29a61ae
lena <sue822330@gmail.com>
2021-02-23 12:00:33 +0900

    add: project setting

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by Ador on 2021/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

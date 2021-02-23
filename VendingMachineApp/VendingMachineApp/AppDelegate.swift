bcf19c0a6d3c6633738c273e0cec0580afef8197
hoonha choi <sosqsz2@naver.com>
2021-02-23 11:35:48 +0900

    Project init

---
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  VendingMachineApp
+//
+//  Created by HOONHA CHOI on 2021/02/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+}
+
---
+++ VendingMachineApp/VendingMachineAppUITests/VendingMachineAppUITests.swift
@@ -0,0 +1,11 @@
+//
+//  VendingMachineAppUITests.swift
+//  VendingMachineAppUITests
+//
+//  Created by HOONHA CHOI on 2021/02/23.
+//
+
+import XCTest
+
+class VendingMachineAppUITests: XCTestCase {
+}

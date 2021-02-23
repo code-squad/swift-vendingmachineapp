e66b6ef9b74aa5650e015c14324b1ebf0dc54145
hoonha choi <sosqsz2@naver.com>
2021-02-23 14:36:47 +0900

    하위 클래스 밀크,소다,커피 생성

---
+++ VendingMachineApp/VendingMachineApp/Model/Milk.swift
@@ -0,0 +1,14 @@
+//
+//  Milk.swift
+//  VendingMachineApp
+//
+//  Created by HOONHA CHOI on 2021/02/23.
+//
+
+import Foundation
+
+class Milk : Beverage {
+    override init(brand: String, liter: Int, price: Int, name: String, date: String) {
+        super.init(brand: brand, liter: liter, price: price, name: name, date: date)
+    }
+}

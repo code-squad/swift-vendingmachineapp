96c8c803fbeea1086b54e6d7a1f1832a4b54e435
hoonha choi <sosqsz2@naver.com>
2021-02-23 14:32:38 +0900

    음료 클래스 생성 및 date,string extension 추가

---
+++ VendingMachineApp/VendingMachineApp/Model/Beverage.swift
@@ -0,0 +1,28 @@
+//
+//  Beverage.swift
+//  VendingMachineApp
+//
+//  Created by HOONHA CHOI on 2021/02/23.
+//
+
+import Foundation
+
+class Beverage : CustomStringConvertible {
+    private let brand : String
+    private let liter : Int
+    private let price : Int
+    private let name : String
+    private let date : Date
+    
+    init(brand : String, liter : Int, price : Int, name : String, date : String) {
+        self.brand = brand
+        self.liter = liter
+        self.price = price
+        self.name = name
+        self.date = date.convertDate()
+    }
+    
+    var description: String {
+        return "\(brand) \(liter) \(price) \(name) \(date.description)"
+    }
+}

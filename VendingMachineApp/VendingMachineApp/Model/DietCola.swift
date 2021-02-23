5973cc5dc596e7d440020521d4ad7140fd91b418
hoonha choi <sosqsz2@naver.com>
2021-02-23 19:53:32 +0900

    date 타입 입력 값 변경

--- VendingMachineApp/VendingMachineApp/Model/DietCola.swift
+++ VendingMachineApp/VendingMachineApp/Model/DietCola.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class DietCola : Soda {
-    init(liter: Int, price: Int, name: String, manufactured: String) {
+    init(liter: Int, price: Int, name: String, manufactured: Date) {
         super.init(brand: Brand.Soda(.Pepsi) , liter: liter, price: price, name: name, manufactured: manufactured)
     }
 }
--- VendingMachineApp/VendingMachineApp/Model/TopAmericano.swift
+++ VendingMachineApp/VendingMachineApp/Model/TopAmericano.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class TopAmericano : Coffee {
-    init(liter: Int, price: Int, name: String, manufactured: String) {
+    init(liter: Int, price: Int, name: String, manufactured: Date) {
         super.init(brand: Brand.Coffee(.Maxim), liter: liter, price: price, name: name, manufactured: manufactured)
     }
 }

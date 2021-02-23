c8b7c528a9f114e427dc587df84c8ff2e029db4c
dahun-lee-daji <ssss6596@gmail.com>
2021-02-23 17:58:33 +0900

    Init project

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 이다훈 on 2021/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

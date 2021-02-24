199a401f9a6b04cced4b42730bf413cdb87829e6
Ray <readingod@naver.com>
2021-02-24 10:45:07 +0900

    Add Beverage Class and Brand Structure
    자판기 앱을 만들기 위해 Beverage Class와 Brand 구조체 생성, Brand 구조체 안에는 enum으로 우유, 탄산음료, 커피의 경우를 나눠서 타입으로 선언

---
+++ VendingMachineApp/VendingMachineApp/Model/Brand.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+struct Brand {
+    
+    enum Milk: String {
+        case seoul = "서울우유"
+        case bing = "빙그레"
+        case namyang = "남양유업"
+    }
+    
+    enum SoftDrik: String {
+        case coca = "코카콜라"
+        case pepsi = "펩시"
+        case lotte = "롯데칠성음료"
+    }
+    
+    enum Coffee: String {
+        case maxim = "맥심"
+        case lotte = "롯데칠성음료"
+        case coca = "코카롤라"
+    }
+}

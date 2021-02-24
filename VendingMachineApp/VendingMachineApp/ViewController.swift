5692fa70f437949719e900642b9be91df440805e
Jisoo Kim <ghis22130@naver.com>
2021-02-24 11:26:25 +0900

    Script 작성

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let drinks:[Drink] = [ Milk(brand: "서울우유", capacity: 200, price: 1000, name: "날마다서울우유", date: Date().date("20171009")),
+                       Milk(brand: "서울우유", capacity: 200, price: 1000, name: "날마다서울우유", date: Date().date("20171012")),
+                       Soda(brand: "펩시", capacity: 350, price: 2000, name: "다이어트콜라", date: Date().date("20171005")),
+                       Coffee(brand: "맥심", capacity: 400, price: 3000, name: "TOP아메리카노", date: Date().date("20171010"))
+        ]
+        
+        let vendingMachine = VendingMachine(drinks: drinks)
     }
-
-
+    
+    
 }
 

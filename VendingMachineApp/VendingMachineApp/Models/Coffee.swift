5d4afab2afd78a2d846606667192adbc8169f6cf
Jisoo Kim <ghis22130@naver.com>
2021-02-24 10:41:39 +0900

    하위 음료 객체 생성

---
+++ VendingMachineApp/VendingMachineApp/Models/Coffee.swift
@@ -0,0 +1,15 @@
+//
+//  Coffee.swift
+//  VendingMachineApp
+//
+//  Created by 지북 on 2021/02/24.
+//
+
+import Foundation
+
+class Coffee: Drink {
+    
+    override init(brand: String, capacity: Int, price: Int, name: String, date: Date) {
+        super.init(brand: brand, capacity: capacity, price: price, name: name, date: date)
+    }
+}
---
+++ VendingMachineApp/VendingMachineApp/Models/Milk.swift
@@ -0,0 +1,15 @@
+//
+//  Milk.swift
+//  VendingMachineApp
+//
+//  Created by 지북 on 2021/02/24.
+//
+
+import Foundation
+
+class Milk: Drink {
+    
+    override init(brand: String, capacity: Int, price: Int, name: String, date: Date) {
+        super.init(brand: brand, capacity: capacity, price: price, name: name, date: date)
+    }
+}

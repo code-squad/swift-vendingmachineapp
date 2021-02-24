15f059ca2b466b3f0d6cfd498502683f80341df1
zeke <yangsin1031@naver.com>
2021-02-24 15:51:04 +0900

    VendingMachine 클래스 생성
    VendingMachine 클래스 생성하고 음료들을 담는 drinks속성 추가

---
+++ VendingMachineApp/VendingMachineApp/Model/VendinMachine.swift
@@ -0,0 +1,16 @@
+//
+//  VendinMachine.swift
+//  VendingMachineApp
+//
+//  Created by 양준혁 on 2021/02/24.
+//
+
+import Foundation
+
+class VendingMachine {
+    var drinks: [Beverage]
+    
+    init() {
+        drinks = []
+    }
+}

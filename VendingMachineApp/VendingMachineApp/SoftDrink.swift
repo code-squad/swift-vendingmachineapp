0c187af527c5b1b1270c90da00a0265c7d75ad77
1song2 <1song2918@gmail.com>
2021-02-24 11:08:48 +0900

    Create a subclass 'SoftDrink' of 'Beverage'

---
+++ VendingMachineApp/VendingMachineApp/SoftDrink.swift
@@ -0,0 +1,24 @@
+//
+//  SoftDrink.swift
+//  VendingMachineApp
+//
+//  Created by Song on 2021/02/24.
+//
+
+import Foundation
+
+class SoftDrink: Beverage {
+    
+    enum SoftDrinkType {
+        case cola
+        case sprite
+        case fanta
+    }
+    
+    private let type: SoftDrinkType
+    
+    init(brand: String, volume: Int, price: Int, name: String, manufactureDate: Date, type: SoftDrinkType) {
+        self.type = type
+        super.init(brand: brand, volume: volume, price: price, name: name, manufactureDate: manufactureDate)
+    }
+}

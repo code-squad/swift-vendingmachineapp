0ba87642d9d0c54a2c31c4a678db8e7c9c86de21
dahun-lee-daji <ssss6596@gmail.com>
2021-02-24 21:50:08 +0900

    폴더 정리 : enum, struct VendingMachine추가

---
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -0,0 +1,15 @@
+//
+//  VendingMachine.swift
+//  VendingMachineApp
+//
+//  Created by 이다훈 on 2021/02/24.
+//
+
+import Foundation
+
+struct VendingMachine {
+    private var money : Int
+    private var stock : [Drink]
+    private var purchased : [Drink]
+    
+}

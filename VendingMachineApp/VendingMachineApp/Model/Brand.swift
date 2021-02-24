6167702590a2079e15ed79f2b69a49fabce1a16d
Ray <readingod@naver.com>
2021-02-24 10:56:19 +0900

    CustomStringConvetible 프로토콜 채택
    Beverage Class 및 Brand 구조체의 enum타입들에 대해 CustomStringConvetible 프로토콜 채택

--- VendingMachineApp/VendingMachineApp/Model/Brand.swift
+++ VendingMachineApp/VendingMachineApp/Model/Brand.swift
@@ -2,21 +2,27 @@ import Foundation
 
 struct Brand {
     
-    enum Milk: String {
+    enum Milk: String, CustomStringConvertible {
         case seoul = "서울우유"
         case bing = "빙그레"
         case namyang = "남양유업"
+        
+        var description: String {return "\(rawValue)"}
     }
     
-    enum SoftDrik: String {
+    enum SoftDrik: String, CustomStringConvertible {
         case coca = "코카콜라"
         case pepsi = "펩시"
         case lotte = "롯데칠성음료"
+        
+        var description: String {return "\(rawValue)"}
     }
     
-    enum Coffee: String {
+    enum Coffee: String, CustomStringConvertible {
         case maxim = "맥심"
         case lotte = "롯데칠성음료"
         case coca = "코카롤라"
+        
+        var description: String {return "\(rawValue)"}
     }
 }

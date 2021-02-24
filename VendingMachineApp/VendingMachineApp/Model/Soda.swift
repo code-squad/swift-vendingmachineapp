8c427c2a7479e8852ee24fdb37c5debd7a222781
dahun-lee-daji <ssss6596@gmail.com>
2021-02-24 17:41:30 +0900

    Add class Cola : Soda
    Modify Soda.init
    New property in Cola = unsweetened
    New method = isUnsweetened()

--- VendingMachineApp/VendingMachineApp/Model/Soda.swift
+++ VendingMachineApp/VendingMachineApp/Model/Soda.swift
@@ -15,9 +15,9 @@ class Soda: Drink {
     //사이다 - 칼로리 추가
     private let package : Materials
     
-    init() {
-        self.package = .steel
-        super.init(brand: "CoCa-Cola", volume: 215, charge: 574, name: "코카콜라제로", manufacturing: Date.init()) //제조일자 : now
+    init(brand : String, volume : Int, charge : Int, name :String, manufacturing : Date, package : Materials) {
+        self.package = package
+        super.init(brand: brand, volume: volume, charge: charge, name: name, manufacturing: manufacturing)
     }
     
     func packingMaterial() -> Materials {

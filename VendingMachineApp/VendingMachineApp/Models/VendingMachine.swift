25650c11ab2dc9a575bd67a59d14018cf24cd181
Jisoo Kim <ghis22130@naver.com>
2021-02-24 10:51:19 +0900

    VenDingMachine Class 생성

---
+++ VendingMachineApp/VendingMachineApp/Models/VendingMachine.swift
@@ -0,0 +1,16 @@
+//
+//  VendingMachine.swift
+//  VendingMachineApp
+//
+//  Created by 지북 on 2021/02/24.
+//
+
+import Foundation
+
+class VendingMachine {
+    private var drinks: [Drink]
+    
+    init(drinks: [Drink]) {
+        self.drinks = drinks
+    }
+}

301ac9976bb833c384e84cf4f047f79859e06ecc
lena <sue822330@gmail.com>
2021-02-24 23:59:51 +0900

    make Drink class to Hashable

--- VendingMachineApp/VendingMachineApp/Drink.swift
+++ VendingMachineApp/VendingMachineApp/Drink.swift
@@ -7,12 +7,25 @@
 
 import Foundation
 
-class Drink {
+class Drink: Hashable {
+    static func == (lhs: Drink, rhs: Drink) -> Bool {
+        return lhs.dateOfManufacture == rhs.dateOfManufacture && lhs.manufacturer == rhs.manufacturer
+            && lhs.volume == rhs.volume && lhs.name == rhs.name && lhs.price == rhs.price
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(manufacturer)
+        hasher.combine(volume)
+        hasher.combine(name)
+        hasher.combine(dateOfManufacture)
+        hasher.combine(price)
+    }
+    
     private var manufacturer: String
     private var volume: Int
     private var name: String
     private var dateOfManufacture: Date
-    private var price: Int
+    private(set) var price: Int
     var description: String {
         return "\(manufacturer), \(volume)ml, \(price)원, \(name), \(Date.dateFormatter(date: dateOfManufacture))"
     }

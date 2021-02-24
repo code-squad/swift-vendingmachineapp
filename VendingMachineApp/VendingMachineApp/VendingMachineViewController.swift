ca00c925dde4ba93898ed43609ff7869ff8cbeb3
Ray <readingod@naver.com>
2021-02-24 15:58:46 +0900

    Step1 간단한 피드백 내용 반영

--- VendingMachineApp/VendingMachineApp/VendingMachineViewController.swift
+++ VendingMachineApp/VendingMachineApp/VendingMachineViewController.swift
@@ -4,9 +4,6 @@ class VendingMachineViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(Beverage(brand: Brand.Brand.seoul, volume: 200, price: 1000, productName: "날마다딸기우유", manufactureDate: Date()))
-        print(Beverage(brand: Brand.Brand.lotte, volume: 500, price: 900, productName: "콘트라베이스 콜드브루 블랙", manufactureDate: Date(timeIntervalSinceNow: -86400 * 10)))
-        print(Beverage(brand: Brand.Brand.maxim, volume: 400, price: 3000, productName: "TOP아메리카노", manufactureDate: Date(timeIntervalSinceNow: -86400 * 2)))
     }
 
 }

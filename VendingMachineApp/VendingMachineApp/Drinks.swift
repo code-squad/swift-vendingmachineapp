347a9a918d9c3d9538ea4c42ccb17414b06d4432
lena <sue822330@gmail.com>
2021-02-25 00:01:03 +0900

    feat: add methods for VendingMachine

--- VendingMachineApp/VendingMachineApp/Drinks.swift
+++ VendingMachineApp/VendingMachineApp/Drinks.swift
@@ -8,15 +8,31 @@
 import Foundation
 
 class Drinks {
-    private var drinks: [Drink]
+    private var drinks: [Drink: Int]
     
-    init(drinks: [Drink]) {
+    init(drinks: [Drink: Int]) {
         self.drinks = drinks
     }
     
-    func retrieveDrinks(completion: (Drink) -> Void) {
-        drinks.forEach { drink in
-            completion(drink)
+    func add(drink: Drink) {
+        guard var stock = drinks[drink] else {
+            drinks[drink] = 1
+            return
         }
+        stock += 1
+    }
+    
+    func getAvailableDrinks(with coins: Int) -> [Drink] {
+        let availableDrinks = drinks.keys.filter { (drink: Drink) -> Bool in
+            return drink.price <= coins
+        }
+        return availableDrinks
+    }
+    
+    func remove(drink: Drink) {
+        guard var stock = drinks[drink] else {
+            return
+        }
+        stock -= 1
     }
 }

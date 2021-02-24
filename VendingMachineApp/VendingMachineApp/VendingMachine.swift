f6d5040373040511c822891e151392aad9ea4aff
1song2 <1song2918@gmail.com>
2021-02-24 11:27:13 +0900

    Add class 'VendingMachine' to manage inventory

---
+++ VendingMachineApp/VendingMachineApp/VendingMachine.swift
@@ -0,0 +1,16 @@
+//
+//  VendingMachine.swift
+//  VendingMachineApp
+//
+//  Created by Song on 2021/02/24.
+//
+
+import Foundation
+
+class VendingMachine {
+    private let inventory: [Beverage]
+    
+    init(inventory: [Beverage]) {
+        self.inventory = inventory
+    }
+}

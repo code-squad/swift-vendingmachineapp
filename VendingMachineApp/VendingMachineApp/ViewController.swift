0679abab7da68801e179beffdda1bd2bae505c98
HongzCloud <jihong21511@gmail.com>
2021-02-24 22:54:57 +0900

    🎉 Create VendingMachineApp project

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 오킹 on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+

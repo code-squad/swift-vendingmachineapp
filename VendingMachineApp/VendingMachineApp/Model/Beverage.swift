c41cbb133324916c5b9b8f38a4b9acab90cf7bae
Ray <readingod@naver.com>
2021-02-24 10:51:56 +0900

    Beverage 클래스에 프로퍼티 생성 및 초기화
    일단 브랜드는 Brand구조체 및 enum타입 활용하여 초기화하였고, 나머지 프로퍼티에 대해서는 추가적인 타입선언이 필요한지 고민 중.

--- VendingMachineApp/VendingMachineApp/Model/Beverage.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverage.swift
@@ -1,5 +1,18 @@
 import Foundation
 
 class Beverage {
-
+    
+    private var brand: Brand
+    private var volume: Int
+    private var price: Int
+    private var productName: String
+    private var manufactureDate: Date
+    
+    init(brand:Brand, volume: Int, price: Int, productName: String, manufactureDate: Date) {
+        self.brand = brand
+        self.volume = volume
+        self.price = price
+        self.productName = productName
+        self.manufactureDate = manufactureDate
+    }
 }

bacf98d742c6caae3bd5695b22aacc976399ca3f
zeke <yangsin1031@naver.com>
2021-02-24 14:56:07 +0900

    Milk, Soda, Coffee 클래스 생성
    Milk, Soda, Coffee 클래스 생성 후 상위 속성들을 초기화

--- VendingMachineApp/VendingMachineApp/Model/Soda.swift
+++ VendingMachineApp/VendingMachineApp/Model/Soda.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+class Soda: Beverage {
+    override init(brand: String, capacity: Int, price: Int, name: String, date: Date) {
+        super.init(brand: brand, capacity: capacity, price: price, name: name, date: date)
+    }
+}

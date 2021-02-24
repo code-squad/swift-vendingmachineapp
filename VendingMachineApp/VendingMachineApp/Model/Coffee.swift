62848a37d6233b66df9b46f9fbd47589fd990a17
dahun-lee-daji <ssss6596@gmail.com>
2021-02-24 18:46:57 +0900

    Add class TOP : Coffee
    New property in TOP = hot
    New method = isHot()

--- VendingMachineApp/VendingMachineApp/Model/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Model/Coffee.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-//http://emart.ssg.com/item/itemView.ssg?itemId=0000007323904&siteNo=6001&salestrNo=2034&tlidSrchWd=TOP&srchPgNo=1&src_area=elist
+
 class Coffee: Drink {
     
     // 카페인 추가 -> optional 선언으로 decaffein 구현
@@ -19,10 +19,6 @@ class Coffee: Drink {
     init(brand : String, volume : Int, charge : Int, name :String, manufacturing : Date, caffeine : Double?) {
         self.caffeine = caffeine
         super.init(brand: brand, volume: volume, charge: charge, name: name, manufacturing: manufacturing)
-    }
-    
-    convenience init() {
-        self.init(brand: "Maxim", volume: 275, charge: 980, name: "TOP 스위트 아메리카노", manufacturing: Date.init(), caffeine : 123.45) //제조일자 : now
     }
     
     func productCaffeine() -> String {

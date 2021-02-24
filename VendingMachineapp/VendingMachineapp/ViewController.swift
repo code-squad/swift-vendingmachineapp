07f3af532dbf7e21d7e71dca2b377c1abdac2c8d
shimyoungmin <dudals6965@gmail.com>
2021-02-24 21:41:26 +0900

    VendingMachine Class 작성 & test 코드 작성

--- VendingMachineapp/VendingMachineapp/ViewController.swift
+++ VendingMachineapp/VendingMachineapp/ViewController.swift
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        run()
     }
-
-
+    
+    func run() {
+        let vendingMachine = VendingMachine()
+        
+        let milk1: Milk = StrawberryMilk(manufacturer: "서울우유", volume: 200, price: 1000, brand: "날마다딸기우유", manufactoringDate: Date(), type: .strawberry)
+        let milk2: Milk = StrawberryMilk(manufacturer: "서울우유", volume: 200, price: 1000, brand: "날마다딸기우유", manufactoringDate: Date(), type: .strawberry)
+        let soda: Soda = Coke(manufacturer: "팹시", volume: 350, price: 2000, brand: "다이어트콜라", manufactoringDate: Date(), type: .coke)
+        let coffee: Coffee = Top(manufacturer: "맥심", volume: 400, price: 3000, brand: "TOP아메리카노", manufactoringDate: Date(), type: .top)
+        
+        vendingMachine.addBeverage(beverage: milk1)
+        vendingMachine.addBeverage(beverage: milk2)
+        vendingMachine.addBeverage(beverage: soda)
+        vendingMachine.addBeverage(beverage: coffee)
+        
+        vendingMachine.printList()
+    }
 }
 

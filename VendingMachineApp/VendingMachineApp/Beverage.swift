306544bf2755f268c1a49553dd6a6573b5b8aef6
Song Lee <songlish233@gmail.com>
2021-02-24 16:35:54 +0900

    Date 포맷 변경 메소드 개선

--- VendingMachineApp/VendingMachineApp/Beverage.swift
+++ VendingMachineApp/VendingMachineApp/Beverage.swift
@@ -24,6 +24,6 @@ class Beverage: CustomStringConvertible {
     }
 
     var description: String {
-        return "[\(brand)] \(name): ₩\(price), \(size)ml (\(manufactured.description))"
+        return "[\(brand)] \(name): ₩\(price), \(size)ml (\(manufactured.inFormat("yyMMdd")))"
     }
 }

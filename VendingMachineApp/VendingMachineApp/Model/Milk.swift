36c3c6d9788872b3aadd5d8c58f2c207df34ceee
Ray <readingod@naver.com>
2021-02-24 17:00:54 +0900

    Milk Class 프로퍼티 생성 및 초기화
    농장코드를 반환하는 원산지를 알 수 있는 메서드 생성

--- VendingMachineApp/VendingMachineApp/Model/Milk.swift
+++ VendingMachineApp/VendingMachineApp/Model/Milk.swift
@@ -1,5 +1,15 @@
 import Foundation
 
-class Milk {
+class Milk: Beverage {
     
+    private var farmCode: String
+    
+    init(brand: Brand.Name, volume: Int, price: Int, name: String, manufacturedDay: Date, farmCode: String) {
+        self.farmCode = farmCode
+        super.init(brand: brand, volume: volume, price: price, productName: name, manufacturedDay: manufacturedDay)
+    }
+    
+    func origin() -> String {
+        return farmCode
+    }
 }

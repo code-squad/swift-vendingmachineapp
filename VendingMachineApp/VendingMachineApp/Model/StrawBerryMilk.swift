98e538f947013f30d9791306a5a1d76475b0cb56
Ray <readingod@naver.com>
2021-02-24 17:52:01 +0900

    StrawBerry Class 및 Chocolate Class에 프로퍼티 생성 및 초기화
    기존에 3단계 객체들에서 채택 예정이었던 sellByDate프로토콜을 2단계 객체들에서 채택하는 것으로 수정, 우선 Milk에서 채택하였고 이후 SoftDrink 및 Coffee에서 채택 예정
    
    유통기한은 세부상품으로 나눠지기 전에 커피, 우유 등으로 나눠졌을 때 분류하는 것이 적절한 것으로 보임

--- VendingMachineApp/VendingMachineApp/Model/StrawBerryMilk.swift
+++ VendingMachineApp/VendingMachineApp/Model/StrawBerryMilk.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-class StrawBerryMilk {
+class StrawBerryMilk: Milk {
     
+    private var additive: String
+    
+    init(brand: Brand.Name, volume: Int, price: Int, productName: String, manufacturedDay: Date, farmCode: String, sellByDate: Date, additive: String) {
+        self.additive = additive
+        super.init(brand: brand, volume: volume, price: price, productName: productName, manufacturedDay: manufacturedDay, farmCode: farmCode, sellByDate: sellByDate)
+    }
 }

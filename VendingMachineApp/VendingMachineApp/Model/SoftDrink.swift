752e37362e68c9835648496c7de59ba14c9a9bf5
Ray <readingod@naver.com>
2021-02-24 17:07:24 +0900

    SoftDrink Class 프로퍼티 생성 및 초기화
    제로 칼러리 여부를 반환하는 메서드 생성

--- VendingMachineApp/VendingMachineApp/Model/SoftDrink.swift
+++ VendingMachineApp/VendingMachineApp/Model/SoftDrink.swift
@@ -1,5 +1,15 @@
 import Foundation
 
-class SoftDrink {
+class SoftDrink: Beverage {
+    private var zeroCalories: Bool
     
+    init(brand: Brand.Name, volume: Int, price: Int, name: String, manufacturedDay: Date, zeroCalories: Bool) {
+        self.zeroCalories = zeroCalories
+        super.init(brand: brand, volume: volume, price: price, productName: name, manufacturedDay: manufacturedDay)
+    }
+    
+    func discriminateZeroCalories() -> String {
+        if zeroCalories { return "YES" }
+        else { return "NO" }
+    }
 }

b3029d910d749177dc545b2548f479cbd4c73e2b
Jisoo Kim <ghis22130@naver.com>
2021-02-24 10:41:28 +0900

    Drink Class 생성

---
+++ VendingMachineApp/VendingMachineApp/Models/Drink.swift
@@ -0,0 +1,25 @@
+//
+//  Drink.swift
+//  VendingMachineApp
+//
+//  Created by 지북 on 2021/02/22.
+//
+
+import Foundation
+
+class Drink {
+    
+    private var brand: String
+    private var capacity: Int
+    private var price: Int
+    private var name: String
+    private var date: Date
+    
+    init(brand: String, capacity: Int, price: Int, name: String, date: Date){
+        self.brand = brand
+        self.capacity = capacity
+        self.price = price
+        self.name = name
+        self.date = date
+    }
+}

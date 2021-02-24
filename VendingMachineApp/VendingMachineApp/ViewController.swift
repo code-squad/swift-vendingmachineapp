26b8e35910acab315594ed95b87462b27b6aa33d
hoonha choi <sosqsz2@naver.com>
2021-02-24 12:45:57 +0900

    현재 금액의 구매가능한 음류수 목록 리턴 메소드 추가

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -8,38 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let beverage : [Beverage] = [StrawberryMilk.init(brand : "서울우유",
-                                                         liter: 200,
-                                                         price: 1000,
-                                                         name: "딸기우유",
-                                                         manufactured: Date.inputDate(date: "20210207"),
-                                                         mlikFarmCode: 023,
-                                                         expiryDate: Date.inputDate(date: "20210210")),
-                                     StrawberryMilk.init(brand : "서울우유",
-                                                         liter: 200,
-                                                         price: 100,
-                                                         name: "딸기우유",
-                                                         manufactured: Date.inputDate(date: "20210211"),
-                                                         mlikFarmCode: 024,
-                                                         expiryDate: Date.inputDate(date: "20210218")),
-                                     DietCola.init(brand : "팹시",
-                                                   liter: 350,
-                                                   price: 2000,
-                                                   name: "다이어트콜라",
-                                                   manufactured: Date.inputDate(date: "20210222"),calory: 123),
-                                     TopAmericano.init(brand : "맥심",
-                                                       liter: 400,
-                                                       price: 3000,
-                                                       name: "TOP아메리카노",
-                                                       manufactured: Date.inputDate(date: "20210217"),
-                                                       caffeineContent: 130,
-                                                       hotNCold: "hot")
-        ]
+        let strStrawberryMilk = StrawberryMilk.init(brand : "서울우유",
+                                                    liter: 200,
+                                                    price: 1000,
+                                                    name: "딸기우유",
+                                                    manufactured: Date.inputDate(date: "20210207"),
+                                                    mlikFarmCode: 023,
+                                                    expiryDate: Date.inputDate(date: "20210210"))
+        let dietCola =  DietCola.init(brand : "팹시",
+                                      liter: 350,
+                                      price: 2000,
+                                      name: "다이어트콜라",
+                                      manufactured: Date.inputDate(date: "20210222"),calory: 123)
+        let topAmericano =  TopAmericano.init(brand : "맥심",
+                                              liter: 400,
+                                              price: 3000,
+                                              name: "TOP아메리카노",
+                                              manufactured: Date.inputDate(date: "20210217"),
+                                              caffeineContent: 130,
+                                              hotNCold: "hot")
+        
         
         let vendingMachine = VendingMachine()
+        vendingMachine.putPayMoney(money: 5000)
+        vendingMachine.addBevergeStock(beverage: strStrawberryMilk)
+        vendingMachine.addBevergeStock(beverage: dietCola)
+        vendingMachine.addBevergeStock(beverage: topAmericano)
+        
     }
     
 }

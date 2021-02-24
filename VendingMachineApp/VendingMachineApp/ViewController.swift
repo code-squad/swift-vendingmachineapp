eb4ac1e95f87e63f92656ba333b457deef25f58f
1song2 <1song2918@gmail.com>
2021-02-24 12:18:22 +0900

    Create instances of subclasses of 'Beverage'
    Use a do-catch statement to handle errors.

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -11,9 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        do {
+            let seoulStrawberryMilk1 = try Milk(brand: "서울우유", volume: 200, price: 1000, name: "날마다딸기우유", manufactureDate: "20171009", type: .strawberry)
+            let seoulStrawberryMilk2 = try Milk(brand: "서울우유", volume: 200, price: 1000, name: "날마다딸기우유", manufactureDate: "20171012", type: .strawberry)
+            let pepsiDietCola = try SoftDrink(brand: "팹시", volume: 350, price: 2000, name: "다이어트콜라", manufactureDate: "20171005", type: .cola)
+            let topAmericano = try Coffee(brand: "맥심", volume: 400, price: 3000, name: "TOP아메리카노", manufactureDate: "20171010", type: .top)
+        } catch DateError.invalidInputFormat(let validFormat) {
+            print("Invalid date string format, a valid format is: \(validFormat)")
+        } catch {
+            print("Unexpectd error: \(error)")
+        }
     }
-
-
 }
-

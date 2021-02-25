4729a2e68a60d0ca662ca4ef4435518030d652fb
Song Lee <songlish233@gmail.com>
2021-02-25 21:20:41 +0900

    Beverages class 생성: 재고 관리 기능 위임

---
+++ VendingMachineApp/VendingMachineApp/Beverages.swift
@@ -0,0 +1,79 @@
+//
+//  Beverages.swift
+//  VendingMachineApp
+//
+//  Created by Song on 2021/02/25.
+//
+
+import Foundation
+
+class Beverages {
+    
+    private var storage: [Beverage]
+    private var stockList: [Beverage: Int]
+    
+    init() {
+        storage = []
+        stockList = [:]
+    }
+    
+    func add(_ beverage: Beverage) {
+        storage.append(beverage)
+        updateStockList(of: beverage, by: 1)
+    }
+    
+    private func updateStockList(of beverage: Beverage, by amount: Int) {
+        if stockList[beverage] != nil {
+            stockList[beverage]! += amount
+        } else {
+            stockList[beverage] = amount
+        }
+        checkSoldout(for: beverage)
+    }
+    
+    private func checkSoldout(for beverage: Beverage) {
+        if stockList[beverage] == 0 {
+            stockList[beverage] = nil
+        }
+    }
+    
+    func pullOut(_ beverage: Beverage) -> Beverage? {
+        guard stockList[beverage] != nil else { return nil }
+        
+        if let targetIdx = storage.firstIndex(of: beverage) {
+            let beverageToPullOut = storage[targetIdx]
+            storage.remove(at: targetIdx)
+            updateStockList(of: beverage, by: -1)
+            return beverageToPullOut
+        } else {
+            return nil
+        }
+    }
+    
+    func listByTime(filter: (([Beverage: Int]) -> [Beverage])?) -> [Beverage] {
+        if let filter = filter {
+            return filter(stockList)
+        } else {
+            return storage
+        }
+    }
+    
+    func listByType(filter: (([Beverage]) -> [Beverage: Int])?) -> [Beverage: Int] {
+        if let filter = filter {
+            return filter(storage)
+        } else {
+            return stockList
+        }
+    }
+    
+    func affordable(with money: Int) -> [Beverage] {
+        var purchashableList = [Beverage]()
+        
+        stockList.forEach { (beverage: Beverage, _: Int) in
+            if beverage.isPurchashable(with: money){
+                purchashableList.append(beverage)
+            }
+        }
+        return purchashableList
+    }
+}

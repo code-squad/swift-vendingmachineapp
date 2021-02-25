121642a6d3b32f6305b0a2701707827eda452ed5
Ray <readingod@naver.com>
2021-02-25 15:04:09 +0900

    3단계 하위 클래스 top, contata 생성으로 마무리 및 유제품 첨가 여부 확인 프로토콜 생성

---
+++ VendingMachineApp/VendingMachineApp/Model/IsDairyProducts.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+protocol IsDairyProducts {
+    func isDiaryProducts () -> Bool
+}
--- VendingMachineApp/VendingMachineApp/Model/Top.swift
+++ VendingMachineApp/VendingMachineApp/Model/Top.swift
@@ -1,5 +1,15 @@
 import Foundation
 
-class Top: Coffee {
+class Top: Coffee, IsDairyProducts {
     
+    private var dairyProducts: Bool
+    
+    init(brand: Brand.Name, volume: Int, price: Int, productName: String, manufacturedDay: Date, decaffeinated: Bool, dairyProducts: Bool) {
+        self.dairyProducts = dairyProducts
+        super.init(brand: brand, volume: volume, price: price, productName: productName, manufacturedDay: manufacturedDay, decaffeinated: decaffeinated)
+    }
+    
+    func isDiaryProducts() -> Bool {
+        return dairyProducts
+    }
 }

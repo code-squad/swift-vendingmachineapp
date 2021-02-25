a5993e8ec497d6dcbe22075bd8e93dc5351fac54
hoonha choi <sosqsz2@naver.com>
2021-02-25 17:13:26 +0900

    테스트 코드 추가 및 메소드 수정

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -11,40 +11,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let strStrawberryMilk = StrawberryMilk.init(brand : "서울우유",
-                                                    liter: 200,
-                                                    price: 1000,
-                                                    name: "딸기우유",
-                                                    manufactured: Date.inputDate(date: "20210207"),
-                                                    mlikFarmCode: 023,
-                                                    expiryDate: Date.inputDate(date: "20210210"), hot: false ,calory: 150, productCode: 001)
-        let dietCola =  DietCola.init(brand : "팹시",
-                                      liter: 350,
-                                      price: 2000,
-                                      name: "다이어트콜라",
-                                      manufactured: Date.inputDate(date: "20210222"),sugar: false,expiryDate: Date.inputDate(date: "20210303"),hot: false,calory: 123,taste: "lemon")
-        
-        let topAmericano =  TopAmericano.init(brand : "맥심",
-                                              liter: 400,
-                                              price: 3000,
-                                              name: "TOP아메리카노",
-                                              manufactured: Date.inputDate(date: "20210217"),
-                                              caffeineContent: 130,
-                                              expiryDate: Date.inputDate(date: "20210227"),
-                                              hot: true, calory : 100,isPream: false)
-        
-        
-        let vendingMachine = VendingMachine()
-        vendingMachine.putPayMoney(money: 5000)
-        vendingMachine.addBevergeStock(beverage: strStrawberryMilk)
-        vendingMachine.addBevergeStock(beverage: dietCola)
-        vendingMachine.addBevergeStock(beverage: topAmericano)
-        vendingMachine.addBevergeStock(beverage: topAmericano)
-        vendingMachine.addBevergeStock(beverage: topAmericano)
-        
-        vendingMachine.showBeverageList {
-            print($0)
-        }
     }
     
 }
---
+++ VendingMachineApp/VendingMachineAppTests/BeverageTests.swift
@@ -0,0 +1,40 @@
+//
+//  BeverageTests.swift
+//  VendingMachineAppTests
+//
+//  Created by HOONHA CHOI on 2021/02/25.
+//
+
+import XCTest
+@testable import VendingMachineApp
+
+class BeverageTests: XCTestCase {
+    
+    func testBeverageClass() {
+        let strawberryMilk = StrawberryMilk.init(brand : "서울우유",
+                                                    liter: 200,
+                                                    price: 1000,
+                                                    name: "딸기우유",
+                                                    manufactured: Date.inputDate(date: "20210207"),
+                                                    mlikFarmCode: 023,
+                                                    expiryDate: Date.inputDate(date: "20210210"), hot: false ,calory: 150, productCode: 001)
+        let dietCola =  DietCola.init(brand : "팹시",
+                                      liter: 350,
+                                      price: 2000,
+                                      name: "다이어트콜라",
+                                      manufactured: Date.inputDate(date: "20210222"),sugar: false,expiryDate: Date.inputDate(date: "20210303"),hot: false,calory: 123,taste: "lemon")
+        
+        let topAmericano =  TopAmericano.init(brand : "맥심",
+                                              liter: 400,
+                                              price: 3000,
+                                              name: "TOP아메리카노",
+                                              manufactured: Date.inputDate(date: "20210217"),
+                                              caffeineContent: 130,
+                                              expiryDate: Date.inputDate(date: "20210227"),
+                                              hot: true, calory : 100,isPream: false)
+        
+        XCTAssertEqual(strawberryMilk.validate(with: Date.inputDate(date: "20210209")), true)
+        XCTAssertEqual(dietCola.isLowCalorie(), true)
+        XCTAssertEqual(topAmericano.isHot(), true)
+    }
+}

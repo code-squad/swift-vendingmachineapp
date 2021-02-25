d6bdaf85cdc815a531acdac64a8d18c8ccfe6ed7
Ray <readingod@naver.com>
2021-02-25 15:42:56 +0900

    유통기한 프로퍼티 Beverage Class로 옮기고 isHot과 저칼로리 프로퍼티 추가

--- VendingMachineApp/VendingMachineApp/Model/Top.swift
+++ VendingMachineApp/VendingMachineApp/Model/Top.swift
@@ -4,9 +4,9 @@ class Top: Coffee, IsDairyProducts {
     
     private var dairyProducts: Bool
     
-    init(brand: Brand.Name, volume: Int, price: Int, productName: String, manufacturedDay: Date, decaffeinated: Bool, dairyProducts: Bool) {
+    init(brand: Brand.Name, volume: Int, price: Int, productName: String, manufacturedDay: Date, sellByDate: Date, lowCalories: Bool, isHot: Bool, decaffeinated: Bool, dairyProducts: Bool) {
         self.dairyProducts = dairyProducts
-        super.init(brand: brand, volume: volume, price: price, productName: productName, manufacturedDay: manufacturedDay, decaffeinated: decaffeinated)
+        super.init(brand: brand, volume: volume, price: price, productName: productName, manufacturedDay: manufacturedDay, sellByDate: sellByDate, lowCalories: lowCalories, isHot: isHot, decaffeinated: decaffeinated)
     }
     
     func isDiaryProducts() -> Bool {

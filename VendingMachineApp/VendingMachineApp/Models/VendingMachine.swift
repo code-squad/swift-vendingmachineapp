153dd08e5b50cbdbd152ca09c21afcbd386df665
Jisoo Kim <ghis22130@naver.com>
2021-02-25 14:41:14 +0900

    VendingMachine Coint Type, insertCoint Method 구현

--- VendingMachineApp/VendingMachineApp/Models/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Models/VendingMachine.swift
@@ -8,15 +8,30 @@
 import Foundation
 
 class VendingMachine {
+    enum Coin: Int {
+        case fifty = 50
+        case hundred = 100
+        case fivehundred = 500
+        case thousand = 1000
+        case fifvethousand = 5000
+    }
+    
     private var drinks: [Drink]
+    private var credit: Int
+    
     
     init(drinks: [Drink]) {
         self.drinks = drinks
+        self.credit = 0
     }
     
     func show(handler: (Drink) -> Void) {
         drinks.forEach {
             handler($0)
         }
+    }
+    
+    func insertCoin(coin: Coin) {
+        credit += coin.rawValue
     }
 }

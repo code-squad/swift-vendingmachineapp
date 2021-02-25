b85c0a8acfcf5828db5d793f1547d49f655bcc44
ellyheetov <yaaajcm@naver.com>
2021-02-25 18:09:10 +0900

    Add Milk class

--- VendingMachineApp/Models/Milk.swift
+++ VendingMachineApp/Models/Milk.swift
@@ -16,7 +16,7 @@ class Milk : Beverage {
     
     private var kind : Kind
     
-    init(brand : String, capacity : Int, price : Int, name : String, createDate : Date, kind : Milk.Kind){
+    init(brand : String, capacity : Int, price : Int, name : String, createDate : String, kind : Milk.Kind){
         self.kind = kind
         super.init(brand: brand,
                    capacity: capacity,

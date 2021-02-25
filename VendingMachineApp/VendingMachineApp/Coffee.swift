ce4d7f7a151ce4579c62d6130397e247d93e1939
lena <sue822330@gmail.com>
2021-02-25 23:04:45 +0900

    chore: update var declaration for immutable value

--- VendingMachineApp/VendingMachineApp/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Coffee.swift
@@ -8,10 +8,9 @@
 import Foundation
 
 class Coffee: Drink, CustomStringConvertible {
-    private var caffeineContent: Int
+    private let caffeineContent: Int
     
-    init(manufacturer: String, volume: Int, name: String,
-         dateOfManufacture: Date, price: Int, caffeineContent: Int) {
+    init(manufacturer: String, volume: Int, name: String, dateOfManufacture: Date, price: Int, caffeineContent: Int) {
         self.caffeineContent = caffeineContent
         super.init(manufacturer: manufacturer, volume: volume, name: name, dateOfManufacture: dateOfManufacture, price: price)
     }

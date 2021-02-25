d8f8117e4a7b87cdb6ae032a3ba226356646c7de
BMO <b38238496@gmail.com>
2021-02-26 00:07:25 +0900

    ViewController에 vendingMachine 추가

--- VendinMachineApp/VendinMachineApp/ViewController.swift
+++ VendinMachineApp/VendinMachineApp/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  VendinMachineApp
-//
-//  Created by user on 2021/02/25.
-//
+
 
 import UIKit
 
@@ -12,6 +7,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var vendingMachine = VendingMachine()
+        vendingMachine.addBeverage(BananaMilk("빙그레", 200, 1000, "바나나우유", Date()))
+        vendingMachine.addBeverage(BananaMilk("빙그레", 200, 1000, "바나나우유", Date()))
+        vendingMachine.addBeverage(Coke("코카콜라", 500, 1500, "코카콜라", Date()))
+        vendingMachine.addBeverage(TOP("맥심", 300, 2000, "TOP 스위트 아메리카노", Date()))
+        
+        vendingMachine.checkStock()
     }
 
 

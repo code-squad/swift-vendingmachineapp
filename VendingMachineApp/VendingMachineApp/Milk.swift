3976df48dce3586ff94791ea7104d3802addcf48
HongzCloud <jihong21511@gmail.com>
2021-02-25 17:53:54 +0900

    ✨ Add Milk, Soda, Coffee class and Add subClass

---
+++ VendingMachineApp/VendingMachineApp/Milk.swift
@@ -0,0 +1,29 @@
+//
+//  Milk.swift
+//  VendingMachineApp
+//
+//  Created by 오킹 on 2021/02/24.
+//
+
+import Foundation
+
+class Milk: CustomStringConvertible {
+    
+    var description: String {
+        return "\(brand), \(miliLiter)ml, \(price)원, \(name), \(dateOfManufacture)"
+    }
+    
+    private var brand: String
+    private var miliLiter: Int
+    private var price: Int
+    private var name: String
+    private var dateOfManufacture: Date
+    
+    init(brand: String, miliLiter: Int, price: Int, name: String, dateOfManufacture: Date) {
+        self.brand = brand
+        self.miliLiter = miliLiter
+        self.price = price
+        self.name = name
+        self.dateOfManufacture = dateOfManufacture
+    }
+}

711ae75b115d5b4b13c214232f192cca0e1c3aff
lena <sue822330@gmail.com>
2021-02-25 23:38:00 +0900

    test: add test code

--- VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import VendingMachineApp
 
 class VendingMachineAppTests: XCTestCase {
 
@@ -28,5 +29,38 @@ class VendingMachineAppTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testVendingMachine() {
+        // 유통기한 지난 상품
+        let stringDate = "20201020"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        let date = dateFormatter.date(from: stringDate)
+       
+        // 음료 객체
+        let coffee = Coffee(manufacturer: "맥심", volume: 200, name: "맥심아메리카노", dateOfManufacture: date!, price: 1000, caffeineContent: 20)
+        let drinks = Drinks()
+        drinks.add(drink: coffee)
+        
+        var vm = VendingMachine(drinks: drinks)
+        
+        // 현재 충전된 금액으로 구매 가능한 음료 목록
+        XCTAssertEqual(vm.getAvailableDrinks(), [])
+        
+        // 유통기한 지난 음료
+        XCTAssertEqual(vm.getExpiredDrinks(), [coffee:1])
+        
+        // 자판기 충전
+        vm.charge(coins: 1000)
 
+        // 현재 충전 금액으로 구입 가능한 음료 목록 출력
+        XCTAssertEqual(vm.getAvailableDrinks(), [coffee])
+
+        // 음료 구매
+        guard let drink = vm.getAvailableDrinks().first else { return }
+        vm.purchase(drink: drink)
+        XCTAssertEqual(vm.getAvailableDrinks(), [])
+
+        XCTAssertEqual(vm.checkRemainCoins(), 0)
+    }
 }

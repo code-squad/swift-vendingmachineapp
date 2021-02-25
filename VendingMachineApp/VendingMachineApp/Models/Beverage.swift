d9a19d9389f02009e866bfe576916e4471b18ef2
zzisun <jisunkim98@gmail.com>
2021-02-25 18:55:55 +0900

    step1. implement Beverage

--- VendingMachineApp/VendingMachineApp/Models/Beverage.swift
+++ VendingMachineApp/VendingMachineApp/Models/Beverage.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+class Beverage: CustomStringConvertible {
+    
+    private let brand: String
+    private let capacity: Int
+    private let price: Int
+    private let name: String
+    private let manufactureDate: Date
+    
+    var description: String {
+        return "\(brand), \(capacity)ml, \(price)원, \(name), \(manufactureDate.productDisplayFormat)"
+    }
+    
+    init(brand: String, capacity: Int, price: Int, name: String, dateString: String){
+        self.brand = brand
+        self.capacity = capacity
+        self.price = price
+        self.name = name
+        self.manufactureDate = dateString.converToDate(from: dateString)
+    }
+}
+
+extension String {
+    func converToDate(from manufactureDate: String) -> Date {
+        let manufactureDateFormatter = DateFormatter()
+        manufactureDateFormatter.dateFormat = "yyyyMMdd"
+        return manufactureDateFormatter.date(from: manufactureDate)!
+    }
+}
+
+extension Date {
+    var productDisplayFormat: String {
+        let manufactureDateFormatter = DateFormatter()
+        manufactureDateFormatter.dateFormat = "yyyyMMdd"
+        return manufactureDateFormatter.string(from: self)
+    }
+}

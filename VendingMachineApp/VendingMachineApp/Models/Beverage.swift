85eaaf611266180995abf135ccafaf3e5983af65
zzisun <jisunkim98@gmail.com>
2021-02-25 18:53:59 +0900

    step1. make class for VendingMachine and Beverage

---
+++ VendingMachineApp/VendingMachineApp/Models/Beverage.swift
@@ -0,0 +1,8 @@
+//
+//  Beverage.swift
+//  VendingMachineApp
+//
+//  Created by 김지선 on 2021/02/25.
+//
+
+import Foundation
---
+++ VendingMachineApp/VendingMachineApp/Models/VendingMachine.swift
@@ -0,0 +1,8 @@
+//
+//  VendingMachine.swift
+//  VendingMachineApp
+//
+//  Created by 김지선 on 2021/02/25.
+//
+
+import Foundation

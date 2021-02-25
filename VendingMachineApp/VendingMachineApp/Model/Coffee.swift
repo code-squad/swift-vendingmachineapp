2d12edf111abf952918f7f291fab6e75c45a3cf3
Ray <readingod@naver.com>
2021-02-25 14:51:38 +0900

    Coke Class프로퍼티 및 enum 타입 생성

--- VendingMachineApp/VendingMachineApp/Model/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Model/Coffee.swift
@@ -4,9 +4,9 @@ class Coffee:Beverage {
     
     private var decaffeinated: Bool
     
-    init(brand: Brand.Name, volume: Int, price: Int, name: String, manufacturedDay: Date, decaffeinated: Bool) {
+    init(brand: Brand.Name, volume: Int, price: Int, productName: String, manufacturedDay: Date, decaffeinated: Bool) {
         self.decaffeinated = decaffeinated
-        super.init(brand: brand, volume: volume, price: price, productName: name, manufacturedDay: manufacturedDay)
+        super.init(brand: brand, volume: volume, price: price, productName: productName, manufacturedDay: manufacturedDay)
     }
     
     func discriminateDecaffeinated () -> String {
--- VendingMachineApp/VendingMachineApp/Model/Top.swift
+++ VendingMachineApp/VendingMachineApp/Model/Top.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-class Top {
+class Top: Coffee {
     
 }

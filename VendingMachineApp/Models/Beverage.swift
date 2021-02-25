e3d10b21c804d4f1ac540296c4411f67a654a0d0
ellyheetov <yaaajcm@naver.com>
2021-02-25 18:14:50 +0900

    Add Date extension

--- VendingMachineApp/Models/Beverage.swift
+++ VendingMachineApp/Models/Beverage.swift
@@ -25,21 +25,3 @@ class Beverage : CustomStringConvertible {
         self.createDate = Date.getDate(with: createDate)
     }
 }
-
-extension Date {
-    static func getDate(with str : String) -> Date{
-        let dateFormatter = setDateFormat(with: "yyyyMMdd")
-        let date = dateFormatter.date(from: str) ?? Date()
-        return date
-    }
-    
-    static func setDateFormat(with str : String) -> DateFormatter{
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = str
-        return dateFormatter
-    }
-    static func toString(date : Date) -> String{
-        let dateFormatter = setDateFormat(with: "yyyyMMdd")
-        return dateFormatter.string(from: date)
-    }
-}

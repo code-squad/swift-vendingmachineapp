b9e10611504e4cbc948a10aa341db4c6bb646ed3
zzisun <jisunkim98@gmail.com>
2021-02-26 06:39:33 +0900

    💬 Edit ReadMe file

---
+++ VendingMachineApp/VendingMachineApp/Models/SoftDrink.swift
@@ -0,0 +1,8 @@
+//
+//  SoftDrink.swift
+//  VendingMachineApp
+//
+//  Created by 김지선 on 2021/02/26.
+//
+
+import Foundation
---
+++ VendingMachineApp/VendingMachineApp/Models/Staff.swift
@@ -0,0 +1,8 @@
+//
+//  Staff.swift
+//  VendingMachineApp
+//
+//  Created by 김지선 on 2021/02/26.
+//
+
+import Foundation

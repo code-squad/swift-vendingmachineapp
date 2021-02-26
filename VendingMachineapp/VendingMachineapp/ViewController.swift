2e8391aed8f4a2ba2cd1553c745d4316dbbd48d5
shimyoungmin <dudals6965@gmail.com>
2021-02-26 10:40:38 +0900

    Date의 stringToDate 타입메서드로 변경

--- VendingMachineapp/VendingMachineapp/ViewController.swift
+++ VendingMachineapp/VendingMachineapp/ViewController.swift
@@ -17,10 +17,10 @@ class ViewController: UIViewController {
     func run() {
         let vendingMachine = VendingMachine()
         
-        let milk1: Milk = StrawberryMilk(manufacturer: "서울우유", volume: 200, price: 1000, brand: "날마다딸기우유", manufactoringDate: Date().stringToDate(date: "20171009"), type: .strawberry)
-        let milk2: Milk = StrawberryMilk(manufacturer: "서울우유", volume: 200, price: 1000, brand: "날마다딸기우유", manufactoringDate: Date().stringToDate(date: "20171012"), type: .strawberry)
-        let soda: Soda = Coke(manufacturer: "팹시", volume: 350, price: 2000, brand: "다이어트콜라", manufactoringDate: Date().stringToDate(date: "20171005"), type: .coke)
-        let coffee: Coffee = Top(manufacturer: "맥심", volume: 400, price: 3000, brand: "TOP아메리카노", manufactoringDate: Date().stringToDate(date: "20171010"), type: .top)
+        let milk1: Milk = StrawberryMilk(manufacturer: "서울우유", volume: 200, price: 1000, brand: "날마다딸기우유", manufactoringDate: Date.stringToDate(date: "20171009"), type: .strawberry)
+        let milk2: Milk = StrawberryMilk(manufacturer: "서울우유", volume: 200, price: 1000, brand: "날마다딸기우유", manufactoringDate: Date.stringToDate(date: "20171012"), type: .strawberry)
+        let soda: Soda = Coke(manufacturer: "팹시", volume: 350, price: 2000, brand: "다이어트콜라", manufactoringDate: Date.stringToDate(date: "20171005"), type: .coke)
+        let coffee: Coffee = Top(manufacturer: "맥심", volume: 400, price: 3000, brand: "TOP아메리카노", manufactoringDate: Date.stringToDate(date: "20171010"), type: .top)
         
         vendingMachine.addBeverage(beverage: milk1)
         vendingMachine.addBeverage(beverage: milk2)

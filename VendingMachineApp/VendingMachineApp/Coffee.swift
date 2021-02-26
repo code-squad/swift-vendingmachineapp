4c8905f98ce4ec2a5ca8429a3884ee52dfbe4772
jhpark-steve <park9368@naver.com>
2021-02-27 06:12:59 +0900

    서브클래스 정의
    커피, 소다, 우유, 에너지 드링크 beverage의 서브클래스로 정의함

---
+++ VendingMachineApp/VendingMachineApp/Coffee.swift
@@ -0,0 +1,14 @@
+//
+//  Coffee.swift
+//  VendingMachineApp
+//
+//  Created by 박정하 on 2021/02/27.
+//
+
+import Foundation
+
+class Coffee : Beverage {
+    enum taste {
+        case americano, latte
+    }
+}
---
+++ VendingMachineApp/VendingMachineApp/Soda.swift
@@ -0,0 +1,14 @@
+//
+//  Soda.swift
+//  VendingMachineApp
+//
+//  Created by 박정하 on 2021/02/27.
+//
+
+import Foundation
+
+class Soda : Beverage {
+    enum tatse {
+        case lemon, lime
+    }
+}

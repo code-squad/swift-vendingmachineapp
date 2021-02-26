24cab6e559a5f993a9359156b05068d981697329
BMO <b38238496@gmail.com>
2021-02-27 03:26:33 +0900

    하위 클래스 별도 파일로 분리

--- VendinMachineApp/VendinMachineApp/Model/Beverages/Coke.swift
+++ VendinMachineApp/VendinMachineApp/Model/Beverages/Coke.swift
@@ -1,17 +1,10 @@
 
 import Foundation
 
-class Soda : Beverage {
-    
-    override init(brand: String, size: Int, price: Int, name: String, manufactureDate: Date) {
-        super.init(brand: brand, size: size, price: price, name: name, manufactureDate: manufactureDate)
-    }
-}
-
-
 class Coke : Soda {
     
     override init(brand: String, size: Int, price: Int, name: String, manufactureDate: Date) {
         super.init(brand: brand, size: size, price: price, name: name, manufactureDate: manufactureDate)
     }
+    
 }
--- VendinMachineApp/VendinMachineApp/Model/Beverages/TOP.swift
+++ VendinMachineApp/VendinMachineApp/Model/Beverages/TOP.swift
@@ -1,17 +1,10 @@
 
 import Foundation
 
-class Coffee : Beverage {
-    
-    override init(brand: String, size: Int, price: Int, name: String, manufactureDate: Date) {
-        super.init(brand: brand, size: size, price: price, name: name, manufactureDate: manufactureDate)
-    }
-}
-
-
 class TOP : Coffee {
     
     override init(brand: String, size: Int, price: Int, name: String, manufactureDate: Date) {
         super.init(brand: brand, size: size, price: price, name: name, manufactureDate: manufactureDate)
     }
+    
 }

578bb40f4e32245299ea3da9ca58ee99ad60cad2
zombietux <>
2021-02-26 17:29:46 +0900

    VendingMachine 추가 후 output 객체와 OutputPrintable로 소통하는 코드 추가

--- VendingMachineApp/VendingMachineApp/OutputView.swift
+++ VendingMachineApp/VendingMachineApp/OutputView.swift
@@ -1,8 +1,16 @@
 //
-//  Output.swift
+//  OutputView.swift
 //  VendingMachineApp
 //
 //  Created by zombietux on 2021/02/26.
 //
 
 import Foundation
+
+class OutputView {
+    static func printBeverages(vendingMachine: OutputViewPrintable) {
+        vendingMachine.printBeverage { beverage in
+            print(beverage.description)
+        }
+    }
+}
--- VendingMachineApp/VendingMachineApp/OutputViewPrintable.swift
+++ VendingMachineApp/VendingMachineApp/OutputViewPrintable.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+protocol OutputViewPrintable {
+    func printBeverage(handler: (Beverage) -> ())
+}

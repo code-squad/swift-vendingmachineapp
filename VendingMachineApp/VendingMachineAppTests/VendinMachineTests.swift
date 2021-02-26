c051bfa8bf878b7db8a097d4ea7aae55574f1183
hoonha choi <sosqsz2@naver.com>
2021-02-26 14:48:46 +0900

    Date static 인스턴스 메소드로 변경

--- VendingMachineApp/VendingMachineAppTests/VendinMachineTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendinMachineTests.swift
@@ -15,22 +15,22 @@ class VendingMachineTests: XCTestCase {
                                                     liter: 200,
                                                     price: 1000,
                                                     name: "딸기우유",
-                                                    manufactured: Date.inputDate(date: "20210207"),
+                                                    manufactured: Date().insert(date: "20210207"),
                                                     mlikFarmCode: 023,
-                                                    expiryDate: Date.inputDate(date: "20210210"), hot: false ,calory: 150, strawberryContent: 10)
+                                                    expiryDate: Date().insert(date: "20210210"), hot: false ,calory: 150, strawberryContent: 10)
         let dietCola =  DietCola.init(brand : "팹시",
                                       liter: 350,
                                       price: 2000,
                                       name: "다이어트콜라",
-                                      manufactured: Date.inputDate(date: "20210222"),sugar: false,expiryDate: Date.inputDate(date: "20210303"),hot: false,calory: 123,taste: .lemon)
+                                      manufactured: Date().insert(date: "20210222"),sugar: false,expiryDate: Date().insert(date: "20210303"),hot: false,calory: 123,taste: .lemon)
         
         let topAmericano =  TopAmericano.init(brand : "맥심",
                                               liter: 400,
                                               price: 3000,
                                               name: "TOP아메리카노",
-                                              manufactured: Date.inputDate(date: "20210217"),
+                                              manufactured: Date().insert(date: "20210217"),
                                               caffeineContent: 130,
-                                              expiryDate: Date.inputDate(date: "20210227"),
+                                              expiryDate: Date().insert(date: "20210227"),
                                               hot: true, calory : 100,coffeeSolids: 0.7)
         
         let vendingMachine = VendingMachine()

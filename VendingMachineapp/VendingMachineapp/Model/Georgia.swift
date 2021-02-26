37e5bbb991ae2d59d22db28fccb6e5fa91c828dc
shimyoungmin <dudals6965@gmail.com>
2021-02-26 14:37:55 +0900

    Coffee Type의 3단계 클래스 파일 분리 & caffeineContent 변수추가로 인한 init() 수정

---
+++ VendingMachineapp/VendingMachineapp/Model/Georgia.swift
@@ -0,0 +1,15 @@
+//
+//  Georgia.swift
+//  VendingMachineapp
+//
+//  Created by 심영민 on 2021/02/26.
+//
+
+import Foundation
+
+class Georgia: Coffee {
+    
+    override init(manufacturer: String, volume: Int, price: Int, brand: String, manufactured: Date, type: CoffeeType, caffeineContent: Int) {
+        super.init(manufacturer: manufacturer, volume: volume, price: price, brand: brand, manufactured: manufactured, type: type, caffeineContent: caffeineContent)
+    }
+}

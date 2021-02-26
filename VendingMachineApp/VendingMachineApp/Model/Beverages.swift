881031e972f1cedbc6661d31a933d9f8a843ce3b
Ray <readingod@naver.com>
2021-02-26 14:21:54 +0900

    VendingMachine 구조체에 잔액, 재고, 뜨거운 음료 여부 확인할 수 있는 메서드 추가 완료

--- VendingMachineApp/VendingMachineApp/Model/Beverages.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverages.swift
@@ -14,4 +14,21 @@ class Beverages {
     func forEachBeverage(_ transform: (Beverage)->()) {
         beverages.forEach { transform($0) }
     }
+    
+    func removeProduct(_ product: Beverage) {
+        beverages.remove(at: beverages.firstIndex(of: product)!)
+    }
+    
+    func countOfBeverages() -> [String: Int]{
+        var stockList = [String:Int]()
+        for beverage in beverages {
+            let productName = beverage.canbuy()
+            if stockList[productName] == nil {
+                stockList[productName] = 1
+            } else {
+                stockList[productName]! += 1
+            }
+        }
+        return stockList
+    }
 }

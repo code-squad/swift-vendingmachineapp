eb6784b924b4c3806b5947aa147b77408b068fa4
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-02-26 16:52:09 +0900

    Create HotBeverageAvailable protocol
    Applied the protocol to Subclasses of Coffee class
    Combined CaffeineSuppliable and HotBeverageAvailable protocol into EnvergyBoostable protocol
    Combined CaffeineSuppliable and FoodColoringApplicable into IsHazardous protocol and applied it on Fanta class

--- VendingMachineApp/VendingMachineApp/BeverageModel/SodaModel/SodaTasteModel/Fanta.swift
+++ VendingMachineApp/VendingMachineApp/BeverageModel/SodaModel/SodaTasteModel/Fanta.swift
@@ -7,15 +7,25 @@
 
 import Foundation
 
-class Fanta: Soda & FoodColoringApplicable {
+class Fanta: Soda & IsHazardous {
     var foodColoring: FoodColoring?
+    var caffeine: Int
     
-    init(brand: String, size: Milli_Liter, price: Int, name: String, packDate: Date, zeroCalories: Bool, foodColoring: FoodColoring?) {
+    init(brand: String, size: Milli_Liter, price: Int, name: String, packDate: Date, zeroCalories: Bool, foodColoring: FoodColoring?, caffeine: Int) {
         self.foodColoring = foodColoring
+        self.caffeine = caffeine
         super.init(brand: brand, size: size, price: price, name: name, packDate: packDate, zeroCalories: zeroCalories)
     }
     
     func isUsingFoodColoring() -> Bool {
         return foodColoring != nil
+    }
+
+    func highCaffeine() -> Bool {
+        return caffeine > 100
+    }
+    
+    func isBadForBody() -> Bool {
+        isUsingFoodColoring() && highCaffeine()
     }
 }

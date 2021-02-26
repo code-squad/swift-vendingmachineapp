06289f07a0b78cafe0828b2e1fbde99a2caa3cc7
Jisoo Kim <ghis22130@naver.com>
2021-02-26 13:14:38 +0900

    DrinkDictionary 구현

--- VendingMachineApp/VendingMachineApp/Models/Drinks.swift
+++ VendingMachineApp/VendingMachineApp/Models/Drinks.swift
@@ -10,13 +10,17 @@ import Foundation
 class Drinks {
     
     private var drinks: [Drink]
+    private var dictionry: DrinksDictionary
     
     init() {
         self.drinks = []
+        self.dictionry = DrinksDictionary()
     }
     
     init(_ drinks: [Drink]) {
         self.drinks = drinks
+        self.dictionry = DrinksDictionary()
+        classify(with: drinks)
     }
     
     func show(handler: (Drink) -> Void) {
@@ -27,12 +31,14 @@ class Drinks {
     
     func append(_ drink: Drink) {
         drinks.append(drink)
+        classify(with: drink)
     }
     
     func append(_ newDrinks: [Drink]) {
         newDrinks.forEach {
             drinks.append($0)
         }
+        classify(with: drinks)
     }
     
     func possibleDrinks(with credit: Int) -> Drinks {
@@ -57,5 +63,20 @@ class Drinks {
         return Drinks(drinks.filter {
             $0.isLowCalore()
         })
+    }
+    
+    func classify(with drink: Drink) {
+        dictionry.append(drink)
+    }
+    
+    func classify(with drinks: [Drink]){
+        drinks.forEach {
+            dictionry.append($0)
+        }
+    }
+    
+    //음료를 하나하나 출력하는 show 메소드가 불필요해지면 show로 바꿀 예정
+    func showStock() -> Dictionary<Drink, Int>{
+        return dictionry.drinkDictionary
     }
 }

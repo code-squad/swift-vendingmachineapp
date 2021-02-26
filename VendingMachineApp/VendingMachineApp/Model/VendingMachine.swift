88b3481459589e4b6a32eb6ddcab585d3c1a9582
Ray <readingod@naver.com>
2021-02-26 11:58:01 +0900

    beverageList 및 음료추가 메서드 구현
    음료 추가 메서드는 추가할 음료의 수를 파라미터로 전달받으며 해당 파라미터만큼 for-loop로 beverage list를 탐색하며 음료수를 추가

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -5,8 +5,18 @@ struct VendingMachine {
     private var numsOfBeverages: Int
     private var beverages: Beverages
     
-    mutating func addBeverage(beverage: Beverage) {
-        beverages.append(from: beverage)
+    let beverageList = [ChocolateMilk(brand: .namyang, volume: 180, price: 1000, productName: "초코에몽", manufacturedDay: Date(), sellByDate: Date(), lowCalories: false, isHot: false, farmCode: "1111", additive: "Chocolate"),
+                        StrawBerryMilk(brand: .seoul, volume: 180, price: 700, productName: "딸기꿀단지", manufacturedDay: Date(), sellByDate: Date(), lowCalories: false, isHot: false, farmCode: "1111", additive: "StrawBerry"),
+                        Coke(brand: .coca, volume: 250, price: 1500, productName: "코카콜라 제로", manufacturedDay: Date(), sellByDate: Date(), lowCalories: true, isHot: false, zeroCalories: true, flavor: .Original),
+                        Sprite(brand: .coca, volume: 190, price: 340, productName: "스프라이트", manufacturedDay: Date(), sellByDate: Date(), lowCalories: false, isHot: false, zeroCalories: false, container: .Can),
+                        Top(brand: .maxim, volume: 380, price: 990, productName: "맥심TOP", manufacturedDay: Date(), sellByDate: Date(), lowCalories: false, isHot: true, decaffeinated: false, dairyProducts: false),
+                        Cantata(brand: .lotte, volume: 500, price: 2000, productName: "칸타타콘트라베이스블랙", manufacturedDay: Date(), sellByDate: Date(), lowCalories: true, isHot: false, decaffeinated: true, dairyProducts: false)]
+    
+    mutating func addBeverage(count: Int) {
+        for i in 0..<count {
+            let idx = i % beverageList.count
+            beverages.append(from: beverageList[idx])
+        }
     }
     
     mutating func getTheMoney(from customer: Int) {
@@ -23,3 +33,4 @@ struct VendingMachine {
         return availableList
     }
 }
+

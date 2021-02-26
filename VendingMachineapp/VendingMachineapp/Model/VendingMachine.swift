5d22f36486e6869351c7b969406a0178fa17a6a2
shimyoungmin <dudals6965@gmail.com>
2021-02-26 22:51:28 +0900

    VendingMachine method 작성

--- VendingMachineapp/VendingMachineapp/Model/VendingMachine.swift
+++ VendingMachineapp/VendingMachineapp/Model/VendingMachine.swift
@@ -7,15 +7,54 @@
 
 import Foundation
 
-class VendingMachine {
+struct VendingMachine {
     private var inventory = Inventory()
+    private var money: Int
+    
+    init(money: Int) {
+        self.money = money
+    }
+    
+    mutating func increaseMoney(moeny: Int) {
+        self.money += money
+    }
     
     func addBeverage(beverage: Beverage) {
         inventory.addBeverage(beverage: beverage)
     }
+    
+    func availablePurchaseList() -> [Beverage] {
+        return inventory.availablePurchaseList(money: money)
+    }
+    
+    func purchaseBeverage(beverage: Beverage) {
+        inventory.subtractBeverage(beverage: beverage)
+    }
+    
+    func checkChagne() -> Int {
+        return money
+    }
+    
+    func purchasedList() -> [Beverage] {
+        return inventory.purchasedList()
+    }
+    
+    func hotBeverageList() -> [Beverage] {
+        return inventory.hotBeverageList()
+    }
+    
+    func validateList(with date: Date) -> [Beverage] {
+        return inventory.validateList(with: date)
+    }
+    
     func printList() {
         inventory.eachBeverage(handler: { (beverage) in
             print(beverage)
         })
     }
+    
+    func wholeBeverage() -> [String:Int] {
+        return inventory.wholeBeverage()
+    }
+    
 }

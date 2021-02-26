1d1bfba6856f695acb1eee4d65e3d834cf218972
zombietux <unknown>
2021-02-26 17:30:48 +0900

    Date포맷 변경 코드 추가

--- VendingMachineApp/VendingMachineApp/DateExtension.swift
+++ VendingMachineApp/VendingMachineApp/DateExtension.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension Date {
+    func formattedDate() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        return dateFormatter.string(from: self)
+    }
+}

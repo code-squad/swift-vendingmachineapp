2de020aa2eee8bc75ba2e79346eb1e154b9867d9
ellyheetov <yaaajcm@naver.com>
2021-02-27 16:39:17 +0900

    Update EnergyDrink : remove enum

--- VendingMachineApp/Model/EnergyDrink/EnergyDrink.swift
+++ VendingMachineApp/Model/EnergyDrink/EnergyDrink.swift
@@ -8,19 +8,5 @@
 import Foundation
 
 class EnergyDrink : Beverage {
-    enum Kind {
-        case Monster
-        case Hotsix
-    }
-    private var kind : Kind
-    
-    init(brand : String, capacity : Int, price : Int, name : String, create : String, expire: String, kind : EnergyDrink.Kind){
-        self.kind = kind
-        super.init(brand: brand,
-                   capacity: capacity,
-                   price: price,
-                   name: name,
-                   create: create,
-                   expire: expire)
-    }
+
 }

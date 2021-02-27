ec69181c41501750533679c10db10aa90cb55fcc
ellyheetov <yaaajcm@naver.com>
2021-02-27 22:17:46 +0900

    Test VendingMachineApp : test one senario

--- VendingMachineAppTests/VendingMachineAppTests.swift
+++ VendingMachineAppTests/VendingMachineAppTests.swift
@@ -9,63 +9,51 @@ import XCTest
 @testable import VendingMachineApp
 
 class VendingMachineAppTests: XCTestCase {
-
-    var vendinMachine : VendingMachine!
+    
+    var vendingMachine : VendingMachine!
+    
+    var energyDrinkFactory : EnergyDrinkFactory!
+    var coffeeFactory : CoffeeFactory!
+    var sodaFactory : SodaFactory!
+    var milkFactory : MilkFactory!
     
     override func setUp() {
         super.setUp()
-    
-        vendinMachine = VendingMachine(coins: 10000)
-        vendinMachine.append(product: StroberryMilk(create:"20210403", expire: "20210412"))
-        vendinMachine.append(product: ChocolateMilk(create: "20200211", expire: "20200220"))
-        vendinMachine.append(product: Georgia(create :"20210202", expire: "20210210", hot: true))
-        vendinMachine.append(product: Coke(create: "20200101", expire: "20200824", flaver: .cherry))
-
-    }
-
-    func testGetBalance() {
-        let coins = 10000
-        vendinMachine.charge(coins: coins)
-        let expect = 20000
+        vendingMachine = VendingMachine()
         
-        XCTAssertEqual(expect, vendinMachine.getbalance(), "잔액이 일치하지 않습니다.")
+        energyDrinkFactory = EnergyDrinkFactory()
+        coffeeFactory = CoffeeFactory()
+        sodaFactory = SodaFactory()
+        milkFactory = MilkFactory()
     }
     
-    func testAvailableProducts(){
+    func testsSenario(){
+        // 재고를 추가한다.
+        vendingMachine.append(product: coffeeFactory.createBeverage(with: "Top"))
+        vendingMachine.append(product: milkFactory.createBeverage(with: "Chocolate"))
+        vendingMachine.append(product: coffeeFactory.createBeverage(with: "Georgia"))
+        vendingMachine.append(product: sodaFactory.createBeverage(with: "Sprite"))
         
-        let expect = 4
-        let actual = vendinMachine.availableProducts().count
+        // 잔액을 추가한다.
+        vendingMachine.charge(coins: 10000)
         
-        XCTAssertEqual(expect, actual, "구매 가능한 음료 목록이 올바르지 않습니다.")
-    }
-    
-    func testBuyProduct(){
-        let georgia = Georgia(create: "20210202", expire: "20210210")
-        vendinMachine.buyProduct(product: georgia)
+        // 재고를 확인한다.
+        vendingMachine.getTotalStock().forEach{print($0)}
         
-        let expect = 3
-        let actual = vendinMachine.getTotalStock().count
-        XCTAssertEqual(expect, actual, "음료를 구매할 수 없습니다.")
-    }
-    func testExpiredProduct(){
-        let actual = vendinMachine.expiredProduct().count
-        let expect = 3
+        // 구매 가능한 상품을 출력한다.
+        vendingMachine.availableProducts().forEach{print($0)}
         
-        XCTAssertEqual(expect, actual, "유통기한 체크 목록이 올바르지 않습니다.")
-    }
-    func testGetHotDrink(){
-        let actual = vendinMachine.getHotDrink().count
-        let expect = 1
+        // 상품을 구매한다.
+        vendingMachine.sellProduct(with: "조지아오리지널")
         
-        XCTAssertEqual(expect, actual, "뜨거운 음료 목록을 받아오지 못하였습니다.")
-    }
-    func testGetSoldProducts(){
-        vendinMachine.charge(coins: 10000)
-    
-        vendinMachine.buyProduct(product: StroberryMilk(create:"20210403", expire: "20210412"))
-        let expect = 1
-        let actual = vendinMachine.getSoldProducts().count
+        // 잔액을 확인한다.
+        let guess = 9000
+        XCTAssertEqual(vendingMachine.getbalance(), guess, "잔액이 올바르지 않습니다.")
         
-        XCTAssertEqual(expect, actual, "구매 이력을 받아오지 못하였습니다.")
+        // 재고 상태를 확인한다.
+        vendingMachine.getTotalStock().forEach{print($0)}
+        
+        // 구매 이력을 확인한다.
+        vendingMachine.getSoldProducts().forEach{print($0)}
     }
 }

9e67cec7bcd968aa9ba1aef2d34eca8e919c27c2
ellyheetov <yaaajcm@naver.com>
2021-02-27 16:40:05 +0900

    Update Soda : remove enum

--- VendingMachineApp/Model/Soda/Soda.swift
+++ VendingMachineApp/Model/Soda/Soda.swift
@@ -8,20 +8,5 @@
 import Foundation
 
 class Soda : Beverage {
-    enum Kind {
-        case Coke
-        case Sprite
-    }
-    
-    private var kind : Kind
-    
-    init(brand : String, capacity : Int, price : Int, name : String, create : String, expire: String, kind : Soda.Kind){
-        self.kind = kind
-        super.init(brand: brand,
-                   capacity: capacity,
-                   price: price,
-                   name: name,
-                   create: create,
-                   expire: expire)
-    }
+
 }

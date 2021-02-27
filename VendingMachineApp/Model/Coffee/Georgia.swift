89f66d58d487171c77f12a9105333619a0a0291c
ellyheetov <yaaajcm@naver.com>
2021-02-27 17:58:32 +0900

    Update Top : insert default argument in init method

--- VendingMachineApp/Model/Coffee/Georgia.swift
+++ VendingMachineApp/Model/Coffee/Georgia.swift
@@ -8,5 +8,7 @@
 import Foundation
 
 class Georgia : Coffee {
-    
+    init(create : String, expire : String, hot : Bool = false){
+        super.init(brand: "조지아", capacity: 275, price: 1000, name: "조지아오리지널", create: create, expire: expire, caffeine: 100, hot: hot)
+    }
 }

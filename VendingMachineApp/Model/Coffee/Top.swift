4c9a02ad7dcce2b0110517fb3566a1ed13a53c1a
ellyheetov <yaaajcm@naver.com>
2021-02-27 18:01:05 +0900

    Update Top : insert default argument in init method

--- VendingMachineApp/Model/Coffee/Top.swift
+++ VendingMachineApp/Model/Coffee/Top.swift
@@ -9,4 +9,7 @@ import Foundation
 
 class Top : Coffee {
 
+    init(create : String, expire : String, hot : Bool = false){
+        super.init(brand: "맥심", capacity: 275, price: 1700, name: "맥심TOP", create: create, expire: expire, caffeine: 100, hot: hot)
+    }
 }

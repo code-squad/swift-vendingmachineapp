2ec621f86e93c6763de0b9234976a980dc60a1e6
okstring <milkoi7801@gmail.com>
2021-02-28 22:01:19 +0900

    Output에서 DrinkFactory로 변경, 음료 생성 시 사용할 enum 추가

---
+++ VendingMachineApp/VendingMachineApp/Model/Productization/enum.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+enum KinkOfDrink: String {
+    case blueBottleColdBrew = "BlueBottle Cold Brew"
+    case starBucksColdBrew = "StarBucks Cold Brew"
+    case seoulStrawberryMilk = "서울 우유 딸기맛"
+    case bingBananaMilk = "빙그레 바나나우유"
+    case sanpellegrino = "Sanpellegrino"
+    case fanta = "Fanta"
+}

5a245c79fc6573ea61b94dc2a5bd9322891bcf01
BMO <b38238496@gmail.com>
2021-03-03 02:03:22 +0900

    BananaMilk SubClass에 속성, 메소드 추가

--- VendinMachineApp/VendinMachineApp/Model/Beverages/BananaMilk.swift
+++ VendinMachineApp/VendinMachineApp/Model/Beverages/BananaMilk.swift
@@ -2,9 +2,25 @@
 import Foundation
 
 class BananaMilk : Milk {
+
+    enum BananaMilkColor {
+        case yellow
+        case white
+    }
     
-    override init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, validWithin days: Int) {
+    private var bananaMilkColor: BananaMilkColor
+    
+    init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, validWithin days: Int, bananaMilkColor: BananaMilkColor) {
+        self.bananaMilkColor = bananaMilkColor
         super.init(brand: brand, size: size, price: price, name: name, manufactureAt: manufactureAt, validWithin: days)
+    }
+    
+    convenience init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, bananaMilkColor: BananaMilkColor) {
+        self.init(brand: brand, size: size, price: price, name: name, manufactureAt: manufactureAt, validWithin: 3, bananaMilkColor: bananaMilkColor)
+    }
+    
+    func isRealBananaMilk() -> Bool {
+        return self.bananaMilkColor == .yellow ? true : false
     }
     
 }

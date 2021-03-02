e4793487b80627e91d25f0177606a97a3f0c3b54
dahun-lee-daji <ssss6596@gmail.com>
2021-03-02 17:47:18 +0900

    무가당 여부 판별 로직 수정
    존 bool -> 함유량 Double
    가당 함유량 2% 미만일 경우 무가당으로 판별함.

--- VendingMachineApp/VendingMachineApp/Model/Cola.swift
+++ VendingMachineApp/VendingMachineApp/Model/Cola.swift
@@ -9,18 +9,18 @@ import Foundation
 
 class Cola: Soda {
     
-    private let unsweetened : Bool
+    private let sugarContent : Double
     
-    init(brand : String, volume : Int, charge : Int, name :String, manufacturing : Date, package : Materials, unsweetened : Bool) {
-        self.unsweetened = unsweetened
+    init(brand : String, volume : Int, charge : Int, name :String, manufacturing : Date, package : Materials, sugarContent : Double) {
+        self.sugarContent = sugarContent
         super.init(brand: brand, volume: volume, charge: charge, name: name, manufacturing: manufacturing, package: package)
     }
     
     convenience init() {
-        self.init(brand: "CoCa-Cola", volume: 215, charge: 574, name: "코카콜라제로", manufacturing: Date.init(), package : Materials.steel, unsweetened : true) //제조일자 : now
+        self.init(brand: "CoCa-Cola", volume: 215, charge: 574, name: "코카콜라제로", manufacturing: Date.init(), package : Materials.steel, sugarContent : 0) //제조일자 : now
     }
     
     func isUnsweetened() -> Bool {
-        return unsweetened
+        return sugarContent < 2
     }
 }

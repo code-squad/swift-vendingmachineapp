9db41d0abaecea6a44191a6cfbe74817e4f2a07f
zeke <yangsin1031@naver.com>
2021-03-02 16:38:03 +0900

    XCTest 추가
    XCTest를 생성하여 타겟 지정

---
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -0,0 +1,45 @@
+//
+//  VendingMachineAppTests.swift
+//  VendingMachineAppTests
+//
+//  Created by 양준혁 on 2021/03/02.
+//
+
+import XCTest
+
+class VendingMachineAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+    
+    func test_자판기금액충전() {
+    
+    }
+    func test_재고추가() {
+        
+    }
+    func test_구매가능음료목록() {
+        
+    }
+    func test_음료수구매() {
+        
+    }
+    func test_잔액확인() {
+        
+    }
+    func test_재고종류별_리스트() {
+        
+    }
+    func test_유통기한지난음료_리스트() {
+        
+    }
+    func test_따뜻한음료_리스트() {
+        
+    }
+    func test_구매이력_리스트() {
+        
+    }
+}

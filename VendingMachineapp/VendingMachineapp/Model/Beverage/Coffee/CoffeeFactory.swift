b7e73f714de6cd652e5d9d4df1a094d1be0b72f8
shimyoungmin <dudals6965@gmail.com>
2021-03-02 23:05:57 +0900

    meta Type을 이용하여 Factory method 수정

--- VendingMachineapp/VendingMachineapp/Model/Beverage/Coffee/CoffeeFactory.swift
+++ VendingMachineapp/VendingMachineapp/Model/Beverage/Coffee/CoffeeFactory.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 class CoffeeFactory: BeverageFactory {
-    static func createBeverage(manufacturer: String, volume: Int, price: Int, brand: String, manufactured: Date, expiryTime: Date, calorie: Int, temperature: Int) -> Beverage? {
+    static func createBeverage(manufacturer: String, volume: Int, price: Int, brand: String, manufactured: Date, expiryTime: Date, calorie: Int, temperature: Int, ofType: Beverage.Type) -> Beverage? {
         
-        switch brand {
-        case "TOP아메리카노" :
+        switch ofType {
+        case is Top.Type :
             return Top(manufacturer: manufacturer, volume: volume, price: price, brand: brand, manufactured: manufactured, expiryTime: expiryTime, calorie: calorie, temperature: temperature, caffeineContent: 10, milkContent: 10)
             
-        case "칸타타" :
+        case is Cantata.Type :
             return Cantata(manufacturer: manufacturer, volume: volume, price: price, brand: brand, manufactured: manufactured, expiryTime: expiryTime, calorie: calorie, temperature: temperature, caffeineContent: 15, coldBrew: true)
             
-        case "조지아" :
+        case is Georgia.Type :
             return Georgia(manufacturer: manufacturer, volume: volume, price: price, brand: brand, manufactured: manufactured, expiryTime: expiryTime, calorie: calorie, temperature: temperature, caffeineContent: 20, condensedMilkContent: 5)
         default :
             return nil
--- VendingMachineapp/VendingMachineapp/Model/Beverage/Milk/MilkFactory.swift
+++ VendingMachineapp/VendingMachineapp/Model/Beverage/Milk/MilkFactory.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 class MilkFactory: BeverageFactory {
-    static func createBeverage(manufacturer: String, volume: Int, price: Int, brand: String, manufactured: Date, expiryTime: Date, calorie: Int, temperature: Int) -> Beverage? {
+    static func createBeverage(manufacturer: String, volume: Int, price: Int, brand: String, manufactured: Date, expiryTime: Date, calorie: Int, temperature: Int, ofType: Beverage.Type) -> Beverage? {
         
-        switch brand {
-        case "날마다딸기우유" :
+        switch ofType {
+        case is StrawberryMilk.Type :
             return StrawberryMilk(manufacturer: manufacturer, volume: volume, price: price, brand: brand, manufactured: manufactured, expiryTime: expiryTime, calorie: calorie, temperature: temperature, lowFat: 10, strawberryContent: 12)
             
-        case "날마다초코우유" :
+        case is ChocoMilk.Type :
             return ChocoMilk(manufacturer: manufacturer, volume: volume, price: price, brand: brand, manufactured: manufactured, expiryTime: expiryTime, calorie: calorie, temperature: temperature, lowFat: 20, chocoContent: 15)
             
-        case "날마다바나나우유" :
+        case is BananaMilk.Type :
             return BananaMilk(manufacturer: manufacturer, volume: volume, price: price, brand: brand, manufactured: manufactured, expiryTime: expiryTime, calorie: calorie, temperature: temperature, lowFat: 10, bananaContent: 13)
         default :
             return nil

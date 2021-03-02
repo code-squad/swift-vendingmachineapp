0e27d10a21cdeda93f6b21deef0550e01efdaea0
BMO <b38238496@gmail.com>
2021-03-02 18:06:47 +0900

    Milk Class의 속성, 인터페이스 추가
    유통기한을 설정하고 기한이 유효한지 알아보는 프로토콜 추가

--- VendinMachineApp/VendinMachineApp/Model/Beverages/Milk.swift
+++ VendinMachineApp/VendinMachineApp/Model/Beverages/Milk.swift
@@ -1,9 +1,32 @@
 
 import Foundation
 
-class Milk : Beverage {
+class Milk : Beverage, Drinkable {
+    private var expiredAt: Date
     
-    override init(brand: String, size: Int, price: Int, name: String, manufactureDate: Date) {
-        super.init(brand: brand, size: size, price: price, name: name, manufactureDate: manufactureDate)
+    init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, validWithin days: Int) {
+        self.expiredAt = manufactureAt.addingTimeInterval(Milk.valid(within: days))
+        super.init(brand: brand, size: size, price: price, name: name, manufactureAt: manufactureAt)
     }
+    
+    convenience init(brand: String, size: Int, price: Int, name: String, validWithin days: Int) {
+        self.init(brand: brand, size: size, price: price, name: name, manufactureAt: Date(), validWithin: days)
+    }
+    
+    static func valid(within days: Int) -> TimeInterval {
+        let validDays:TimeInterval = 86400 * Double(days)
+        return validDays
+    }
+    
+    func validate(with date: Date) -> Bool {
+        return expiredAt > date ? true : false
+    }
+    
 }
+
+
+protocol Drinkable {
+    func validate(with date: Date) -> Bool
+    static func valid(within days: Int) -> TimeInterval
+}
+

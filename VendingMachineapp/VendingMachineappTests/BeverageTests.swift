5c41624a7b46274cb5163d90cb53968dcd9c4a6c
shimyoungmin <dudals6965@gmail.com>
2021-03-02 18:44:21 +0900

    TestCase 수정

--- VendingMachineapp/VendingMachineappTests/BeverageTests.swift
+++ VendingMachineapp/VendingMachineappTests/BeverageTests.swift
@@ -15,47 +15,38 @@ class BeverageTests: XCTestCase {
  
     func testBeverage() {
         
-        let milk1: Milk = StrawberryMilk(manufacturer: "서울우유",
-                                         volume: 200, price: 1000,
-                                         brand: "날마다딸기우유",
-                                         manufactured: Date.stringToDate(date: "20171009"),
-                                         expirydate: Date.stringToDate(date: "20210228"),
-                                         calorie: 200,
-                                         temperature: 10,
-                                         lowFat: false)
+        let milk = MilkFactory.createBeverage(
+                             manufacturer: "서울우유",
+                             volume: 200, price: 1000,
+                             brand: "날마다딸기우유",
+                             manufactured: Date.stringToDate(date: "20171009"),
+                             expiryTime: Date.stringToDate(date: "20210227"),
+                             calorie: 200,
+                             temperature: 10)!
         
-        let milk2: Milk = StrawberryMilk(manufacturer: "서울우유",
-                                         volume: 200, price: 1000,
-                                         brand: "날마다딸기우유",
-                                         manufactured: Date.stringToDate(date: "20171009"),
-                                         expirydate: Date.stringToDate(date: "20210220"),
-                                         calorie: 200,
-                                         temperature: 10,
-                                         lowFat: false)
-        
-        let soda: Soda = Coke(manufacturer: "팹시",
-                              volume: 350,
-                              price: 2000,
-                              brand: "다이어트콜라",
-                              manufactured: Date.stringToDate(date: "20171005"),
-                              expirydate: Date.stringToDate(date: "20221005"),
-                              calorie: 100,
-                              temperature: 10,
-                              flavor: "콜라맛")
+        let coke = SodaFactory.createBeverage(
+                            manufacturer: "팹시",
+                            volume: 350,
+                            price: 2000,
+                            brand: "다이어트콜라",
+                            manufactured: Date.stringToDate(date: "20171005"),
+                            expiryTime: Date.stringToDate(date: "20221005"),
+                            calorie: 100,
+                            temperature: 10)!
 
-        let coffee: Coffee = Top(manufacturer: "맥심",
-                                 volume: 400,
-                                 price: 3000,
-                                 brand: "TOP아메리카노",
-                                 manufactured: Date.stringToDate(date: "20171010"),
-                                 expirydate: Date.stringToDate(date: "20211231"),
-                                 calorie: 350,
-                                 temperature: 10,
-                                 caffeineContent: 20)
+        let top = CoffeeFactory.createBeverage(
+                             manufacturer: "맥심",
+                             volume: 300,
+                             price: 3500,
+                             brand: "TOP아메리카노",
+                             manufactured: Date.stringToDate(date: "20210110"),
+                             expiryTime: Date.stringToDate(date: "20210330"),
+                             calorie: 350,
+                             temperature: 20)!
         
-        XCTAssertEqual(milk1.isHot(with: 50), false)
-        XCTAssertEqual(milk2.isLowFat(), false)
-        XCTAssertEqual(soda.isLowCalorie(with: 300), true)
-        XCTAssertEqual(coffee.validateExpiryTime(with: Date()), true)
+        XCTAssertEqual(milk.isHot(with: 50), false)
+        //XCTAssertEqual(milk.isLowFat(with: 200), false)
+        XCTAssertEqual(coke.isLowCalorie(with: 300), true)
+        XCTAssertEqual(top.validateExpiryTime(with: Date()), true)
     }
 }

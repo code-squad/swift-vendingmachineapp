177b5789b510f926b4cbd0cfde448f8f751f2c22
lena <sue822330@gmail.com>
2021-03-02 14:34:41 +0900

    add expiredAt property for drink

--- VendingMachineApp/VendingMachineApp/Models/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Models/Coffee.swift
@@ -10,8 +10,8 @@ import Foundation
 class Coffee: Drink {
     private let caffeineContent: Int
     
-    init(manufacturer: String, volume: Int, name: String, dateOfManufacture: Date, price: Int, caffeineContent: Int, temperature: Int, calorie: Int) {
+    init(manufacturer: String, volume: Int, name: String, dateOfManufacture: Date, price: Int, caffeineContent: Int, temperature: Int, calorie: Int, expiredAt: Date) {
         self.caffeineContent = caffeineContent
-        super.init(manufacturer: manufacturer, volume: volume, name: name, dateOfManufacture: dateOfManufacture, price: price, temperature: temperature, calorie: calorie)
+        super.init(manufacturer: manufacturer, volume: volume, name: name, dateOfManufacture: dateOfManufacture, price: price, temperature: temperature, calorie: calorie, expiredAt: expiredAt)
     }
 }

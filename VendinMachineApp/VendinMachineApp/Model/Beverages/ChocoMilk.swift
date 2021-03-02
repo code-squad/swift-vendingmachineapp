f02c660acd081828ef07ad4e2cfcc761770962ac
BMO <b38238496@gmail.com>
2021-03-03 02:05:06 +0900

    ChocoMilk SubClass 추가, Country 프로토콜 채택
    원산지 표기를 하도록 Country 프로토콜 채택

---
+++ VendinMachineApp/VendinMachineApp/Model/Beverages/ChocoMilk.swift
@@ -0,0 +1,22 @@
+
+import Foundation
+
+class ChocoMilk : Milk, Country {
+    private var country: Country
+    override var description: String {
+        return "\(super.description), 원산지: \(country)"
+    }
+    
+    init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, validWithin days: Int, madeIn: Country) {
+        self.country = madeIn
+        super.init(brand: brand, size: size, price: price, name: name, manufactureAt: manufactureAt, validWithin: days)
+    }
+    
+    convenience init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, madeIn: Country) {
+        self.init(brand: brand, size: size, price: price, name: name, manufactureAt: manufactureAt, validWithin: 3, madeIn: madeIn)
+    }
+    
+    func madeIn() -> Country {
+        return self.country
+    }
+}

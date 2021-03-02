3d1eba70fee09dade315ed1d400412c016db7323
dahun-lee-daji <ssss6596@gmail.com>
2021-03-02 19:37:34 +0900

    Modify TOP`s property : hot -> temperature
    뜨거운 여부 -> 온도
    70도 이상일 경우 뜨거운 음료로 판별하도록 로직 변경.

--- VendingMachineApp/VendingMachineApp/Model/TOP.swift
+++ VendingMachineApp/VendingMachineApp/Model/TOP.swift
@@ -9,18 +9,18 @@ import Foundation
 
 class TOP: Coffee {
     
-    private let hot : Bool
+    private let temperature : Double
     
-    init(brand : String, volume : Int, charge : Int, name :String, manufacturing : Date, caffeine : Double?, hot : Bool) {
-        self.hot = hot
+    init(brand : String, volume : Int, charge : Int, name :String, manufacturing : Date, caffeine : Double?, temperature : Double) {
+        self.temperature = temperature
         super.init(brand: brand, volume: volume, charge: charge, name: name, manufacturing: manufacturing, caffeine: caffeine)
     }
     
     convenience init() {
-        self.init(brand: "Maxim", volume: 275, charge: 980, name: "TOP 스위트 아메리카노", manufacturing: Date.init(), caffeine : 123.45, hot: true)
+        self.init(brand: "Maxim", volume: 275, charge: 980, name: "TOP 스위트 아메리카노", manufacturing: Date.init(), caffeine : 123.45, temperature: 73.3)
     }
     
     func isHot() -> Bool {
-        return hot
+        return temperature > 70
     }
 }

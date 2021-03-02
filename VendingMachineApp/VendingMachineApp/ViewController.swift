e16455a7f23af5e3f5f8d7f727eaabacfd749109
hoonha choi <sosqsz2@naver.com>
2021-03-02 21:12:55 +0900

    음료 이미지뷰 추가 및 코너값 추가

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     private let vendingMachine = VendingMachine()
-    private let vendingStackView = VendingStackView()
     
     private let strawberryMilk = StrawberryMilk()
     private let dietCola =  DietCola()
     private let topAmericano = TopAmericano()
     
     @IBOutlet var addStockButton: [UIButton]!
-    @IBOutlet var beverageImages: [UIImageView]!
+    @IBOutlet var beverageImages: [BeverageImageView]!
     @IBOutlet var numberOfStock: [UILabel]!
+    
+    @IBOutlet weak var BalanceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setImageCornerRadius()
         addBeverage()
-        
     }
     
     private func addBeverage() {
@@ -35,16 +34,16 @@ class ViewController: UIViewController {
         vendingMachine.addStock(beverage: topAmericano)
     }
     
-    private func setImageCornerRadius() {
-        beverageImages.forEach { (image) in
-            image.layer.cornerRadius = 30
-        }
-    }
-    
     @IBAction func BalanceIncreaseButtonTouched(_ sender: BalanceIncreasable) {
         sender.increase { money in
             self.vendingMachine.putPayMoney(money: money)
+            self.changeBalanceLabel()
         }
     }
+    
+    func changeBalanceLabel() {
+        self.BalanceLabel.text = self.vendingMachine.checkCurrentBalance().description
+    }
+    
 }
 

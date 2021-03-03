fa6e07717d6411c829b18e3a9b0f0677c8d6a50f
Song Lee <songlish233@gmail.com>
2021-03-03 17:58:35 +0900

    VendingMachineUpdator를 통해 View 업데이트 액션 구현

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var beverageCollection: [UIImageView]!
+    @IBOutlet var countCollection: [UILabel]!
+    @IBOutlet weak var moneyLabel: UILabel!
+
+    private var vendingMachine = VendingMachine(dateStandard: Date(),
+                                        temperatureStandard: 36.5,
+                                        sugarStandard: 1.0,
+                                        lactoStandard: 0.5)
+    
+    private var vendingMachineDelegate = VendingMachineUpdator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        vendingMachineDelegate.didTurnOn(images: beverageCollection, countLabels: countCollection, machine: vendingMachine, moneyLabel: moneyLabel)
+    }
+    
+    @IBAction func addMoneyTouched(_ sender: UIButton) {
+        vendingMachineDelegate.didAddMoneyTouched(sender: sender, machine: vendingMachine, label: moneyLabel)
+    }
+    
+    @IBAction func addStockTouched(_ sender: UIButton) {
+        vendingMachineDelegate.didAddStockTouched(sender: sender, machine: vendingMachine, labels: countCollection)
     }
 }
-

482315c7e86aaebaac11852810089333b37a6246
1song2 <1song2918@gmail.com>
2021-03-03 17:20:10 +0900

    Remove nested enum from 'Coffee' class

--- VendingMachineApp/VendingMachineApp/Coffee.swift
+++ VendingMachineApp/VendingMachineApp/Coffee.swift
@@ -9,16 +9,7 @@ import Foundation
 
 class Coffee: Beverage {
     
-    enum CoffeeType {
-        case top
-        case cantata
-        case georgia
-    }
-    
-    private let type: CoffeeType
-    
-    init(brand: String, volume: Int, price: Int, name: String, calorie: Int, manufactured: String, expiredAfter: String, type: CoffeeType) {
-        self.type = type
+    override init(brand: String, volume: Int, price: Int, name: String, calorie: Int, manufactured: String, expiredAfter: String) {
         super.init(brand: brand, volume: volume, price: price, name: name, calorie: calorie, manufactured: manufactured, expiredAfter: expiredAfter)
     }
 }

582f58b770691b9e8b23b3fc00a2bc41e62f6f7e
BMO <b38238496@gmail.com>
2021-03-03 12:12:16 +0900

    Coffee Class에 Hotable 프로토콜 채택

--- VendinMachineApp/VendinMachineApp/Model/Beverages/Coffee.swift
+++ VendinMachineApp/VendinMachineApp/Model/Beverages/Coffee.swift
@@ -1,10 +1,16 @@
 
 import Foundation
 
-class Coffee : Beverage {
+class Coffee : Beverage, Hotable {
     
-    override init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date) {
+    private var temperature: Int
+    
+    init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, temperature: Int) {
+        self.temperature = temperature
         super.init(brand: brand, size: size, price: price, name: name, manufactureAt: manufactureAt)
     }
     
+    func isHot(standard: Int) -> Bool {
+        return self.temperature > standard ? true : false
+    }
 }

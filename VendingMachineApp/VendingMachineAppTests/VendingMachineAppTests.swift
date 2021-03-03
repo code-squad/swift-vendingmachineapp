e5686ca0f3596787392166c50701effe5d79ccb6
Lia <judykim257@gmail.com>
2021-03-04 06:42:41 +0900

    step2 - create Test file

---
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -0,0 +1,24 @@
+//
+//  VendingMachineAppTests.swift
+//  VendingMachineAppTests
+//
+//  Created by Lia on 2021/03/04.
+//
+
+import XCTest
+
+class VendingMachineAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+}

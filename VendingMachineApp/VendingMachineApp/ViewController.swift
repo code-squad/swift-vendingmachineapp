e74fe6c8c0713d2285cbeef830bcc925eb80df54
zombietux <unknown>
2021-03-03 18:32:54 +0900

    ViewController내용 삭재

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -8,16 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    private let bananaMilkFactory = BananaMilkFactory()
-    private let cantatacoffeeFactory = CantataCoffeeFactory()
-    private let ciderFactory = CiderFactory()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let vendingMachine = VendingMachine(products: [bananaMilkFactory.produce(), cantatacoffeeFactory.produce(), ciderFactory.produce()])
-        OutputView.printBeverages(vendingMachine: vendingMachine)
     }
 }
 

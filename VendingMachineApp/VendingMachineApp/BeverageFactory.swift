e050913bb7387f3fb3422f979f2339d3c821643c
Song Lee <songlish233@gmail.com>
2021-03-03 19:19:51 +0900

    판매할 음료를 ViewController에서 리스트업하도록 수정

--- VendingMachineApp/VendingMachineApp/BeverageFactory.swift
+++ VendingMachineApp/VendingMachineApp/BeverageFactory.swift
@@ -14,26 +14,10 @@ protocol BeverageFactory {
 }
 
 enum BeverageFactory0303: BeverageFactory, CaseIterable {
-    case chocolateMilk, plainMilk, coke, milkis, americano, cafelatte
+    case americano, cafelatte, chocolateMilk, coke, milkis, plainMilk
     
     func create() -> Beverage {
         switch self {
-        case .chocolateMilk:
-            return Chocolate.init(brand: "푸르밀", name: "가나 초코우유", price: 1300, size: 225,
-                                       dateFactory: SixDigitFactory(), manufacturedInString: "210303",
-                                       expireAfter: 10, lactose: 3.5, package: .bottle)
-        case .plainMilk:
-            return Plain.init(brand: "매일", name: "소화가 잘되는 우유", price: 850, size: 190,
-                              dateFactory: SixDigitFactory(), manufacturedInString: "210303",
-                              expireAfter: 7, lactose: 0.2, sugar: 0.2)
-        case .coke:
-            return Coke.init(brand: "코카콜라", name: "코카콜라 제로", price: 2000, size: 500,
-                             dateFactory: SixDigitFactory(), manufacturedInString: "210303",
-                             package: .bottle , sugar: 0.0)
-        case .milkis:
-            return Milkis.init(brand: "", name: "밀키스", price: 1500, size: 340,
-                               dateFactory: SixDigitFactory(), manufacturedInString: "210303",
-                               package: .can, lactose: 3.5)
         case .americano:
             return Americano.init(brand: "스타벅스", name: "블랙커피", price: 1800, size: 275,
                                   dateFactory: SixDigitFactory(), manufacturedInString: "210303",
@@ -42,6 +26,22 @@ enum BeverageFactory0303: BeverageFactory, CaseIterable {
             return CafeLatte.init(brand: "코카콜라", name: "조지아 카페라떼", price: 900, size: 240,
                                   dateFactory: SixDigitFactory(), manufacturedInString: "210303",
                                   temperature: 8.5, package: .can, expireAfter: 10, lactose: 3.5)
+        case .chocolateMilk:
+            return Chocolate.init(brand: "푸르밀", name: "가나 초코우유", price: 1300, size: 225,
+                                       dateFactory: SixDigitFactory(), manufacturedInString: "210303",
+                                       expireAfter: 10, lactose: 3.5, package: .bottle)
+        case .coke:
+            return Coke.init(brand: "코카콜라", name: "코카콜라 제로", price: 2000, size: 500,
+                             dateFactory: SixDigitFactory(), manufacturedInString: "210303",
+                             package: .bottle , sugar: 0.0)
+        case .milkis:
+            return Milkis.init(brand: "", name: "밀키스", price: 1500, size: 340,
+                               dateFactory: SixDigitFactory(), manufacturedInString: "210303",
+                               package: .can, lactose: 3.5)
+        case .plainMilk:
+            return Plain.init(brand: "매일", name: "소화가 잘되는 우유", price: 850, size: 190,
+                              dateFactory: SixDigitFactory(), manufacturedInString: "210303",
+                              expireAfter: 7, lactose: 0.2, sugar: 0.2)
         }
     }
 }
--- VendingMachineApp/VendingMachineApp/VendingMachineUpdator.swift
+++ VendingMachineApp/VendingMachineApp/VendingMachineUpdator.swift
@@ -10,17 +10,17 @@ import UIKit
 
 protocol VendingMachineDelegate {
     
-    func didTurnOn(images: [UIImageView], countLabels: [UILabel], machine: VendingMachine, moneyLabel: UILabel)
+    func didTurnOn(images: [UIImageView], countLabels: [UILabel], machine: VendingMachine, moneyLabel: UILabel, beverageList: [Beverage])
     
     func didAddMoneyTouched(sender: UIButton, machine: VendingMachine, label: UILabel)
     
-    func didAddStockTouched(sender: UIButton, machine: VendingMachine, labels: [UILabel])
+    func didAddStockTouched(sender: UIButton, machine: VendingMachine, labels: [UILabel], beverageList: [Beverage])
     
 }
 
 class VendingMachineUpdator: VendingMachineDelegate {
     
-    func didTurnOn(images: [UIImageView], countLabels: [UILabel], machine: VendingMachine, moneyLabel: UILabel) {
+    func didTurnOn(images: [UIImageView], countLabels: [UILabel], machine: VendingMachine, moneyLabel: UILabel, beverageList: [Beverage]) {
         
         images.forEach { (image) in
             image.layer.cornerRadius = 15
@@ -30,7 +30,7 @@ class VendingMachineUpdator: VendingMachineDelegate {
             label.adjustsFontSizeToFitWidth = true
         }
         
-        updateCounts(for: countLabels, stockList: machine.allStocks())
+        updateCounts(for: countLabels, stockList: machine.allStocks(), beverageList: beverageList)
         updateBalance(for: moneyLabel, money: machine.moneyLeft())
     }
     
@@ -47,52 +47,52 @@ class VendingMachineUpdator: VendingMachineDelegate {
         label.text = "\(money)원"
     }
     
-    func didAddStockTouched(sender: UIButton, machine: VendingMachine, labels: [UILabel]) {
+    func didAddStockTouched(sender: UIButton, machine: VendingMachine, labels: [UILabel], beverageList: [Beverage]) {
         switch sender.restorationIdentifier {
         case "addAmericano":
-            machine.addStock(of: Americano())
+            machine.addStock(of: beverageList[0])
         case "addCafelatte":
-            machine.addStock(of: CafeLatte())
+            machine.addStock(of: beverageList[1])
         case "addChocolateMilk":
-            machine.addStock(of: Chocolate())
+            machine.addStock(of: beverageList[2])
         case "addCoke":
-            machine.addStock(of: Coke())
+            machine.addStock(of: beverageList[3])
         case "addMilkis":
-            machine.addStock(of: Milkis())
+            machine.addStock(of: beverageList[4])
         case "addPlainMilk":
-            machine.addStock(of: Plain())
+            machine.addStock(of: beverageList[5])
         default:
             return
         }
-        updateCounts(for: labels, stockList: machine.allStocks())
+        updateCounts(for: labels, stockList: machine.allStocks(), beverageList: beverageList)
     }
     
-    func countValidation(for count: Int?) -> String {
+    private func updateCounts(for labels: [UILabel], stockList: [Beverage: Int], beverageList: [Beverage]) {
+        for label in labels {
+            switch label.restorationIdentifier {
+            case "americano":
+                label.text = countValidation(for: stockList[beverageList[0]])
+            case "cafelatte":
+                label.text = countValidation(for: stockList[beverageList[1]])
+            case "chocolateMilk":
+                label.text = countValidation(for: stockList[beverageList[2]])
+            case "coke":
+                label.text = countValidation(for: stockList[beverageList[3]])
+            case "milkis":
+                label.text = countValidation(for: stockList[beverageList[4]])
+            case "plainMilk":
+                label.text = countValidation(for: stockList[beverageList[5]])
+            default:
+                return
+            }
+        }
+    }
+    
+    private func countValidation(for count: Int?) -> String {
         if let count = count {
             return "\(count)개"
         } else {
             return "품절"
-        }
-    }
-    
-    private func updateCounts(for labels: [UILabel], stockList: [Beverage: Int]) {
-        for label in labels {
-            switch label.restorationIdentifier {
-            case "americano":
-                label.text = countValidation(for: stockList[Americano()])
-            case "cafelatte":
-                label.text = countValidation(for: stockList[CafeLatte()])
-            case "chocolateMilk":
-                label.text = countValidation(for: stockList[Chocolate()])
-            case "coke":
-                label.text = countValidation(for: stockList[Coke()])
-            case "milkis":
-                label.text = countValidation(for: stockList[Milkis()])
-            case "plainMilk":
-                label.text = countValidation(for: stockList[Plain()])
-            default:
-                return
-            }
         }
     }
 }

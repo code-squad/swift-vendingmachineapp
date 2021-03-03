7c0b53aa8ac8d92af8dded68f7a70f0d24f677ba
BMO <b38238496@gmail.com>
2021-03-03 10:57:03 +0900

    Soda Class에 Package 프로토콜 채택

--- VendinMachineApp/VendinMachineApp/Model/Beverages/Soda.swift
+++ VendinMachineApp/VendinMachineApp/Model/Beverages/Soda.swift
@@ -1,10 +1,17 @@
 
 import Foundation
 
-class Soda : Beverage {
+class Soda : Beverage, Package {
     
-    override init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date) {
+    private var package: Package
+    
+    init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, package: Package) {
+        self.package = package
         super.init(brand: brand, size: size, price: price, name: name, manufactureAt: manufactureAt)
+    }
+
+    func isTransparentMaterial() -> Bool {
+        return self.package.isTransparentMaterial()
     }
     
 }

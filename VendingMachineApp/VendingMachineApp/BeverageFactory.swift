526f3d3fdb98834b7ef7cd56043dfe1a7569c954
jhpark-steve <park9368@naver.com>
2021-03-03 10:45:18 +0900

    서브 클래스 소다 정의, 음료 팩토리 메소드 정의

---
+++ VendingMachineApp/VendingMachineApp/BeverageFactory.swift
@@ -0,0 +1,12 @@
+//
+//  BeverageFactory.swift
+//  VendingMachineApp
+//
+//  Created by 박정하 on 2021/03/02.
+//
+
+import UIKit
+
+protocol BeverageFactroy {
+    func makeBeverage(_ product : Beverage) -> Beverage
+}
--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
         let coffee : Beverage = Beverage.init(brand: "스타벅스", volume: 255, price: 4100, name: "아메리카노", manufactured: Date().returnStringDate("20210101"))
         let milk : Beverage = Beverage.init(brand: "매일우유", volume: 180, price: 1300, name: "흰 우유", manufactured: Date().returnStringDate("20210102"))
         let soda : Beverage = Beverage.init(brand: "롯데칠성음료", volume: 330, price: 2500, name: "트레비", manufactured: Date().returnStringDate("20210103"))
-        let beverages : [CustomStringConvertible] = [coffee, milk, soda]
-        let vm = VendingMachine()
-        vm.printBeverages(beverages)
+        
+        let beverageFactroy : BeverageCreater = BeverageCreater()
+        let milk2 : Beverage = beverageFactroy.makeBeverage(Milk.init(brand: "", volume: 255, price: 4100, name: "아메리카노", manufactured: Date().returnStringDate("20210103")))
     }
 }
 

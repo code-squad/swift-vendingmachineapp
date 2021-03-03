2a2631118800a855b9c04ffc95caf169a7e147ae
BMO <b38238496@gmail.com>
2021-03-03 12:21:47 +0900

    Coffee Class에 Country 프로토콜 채택
    Coffee의 SubClass인 Americano와 Latte 모두
    Country 프로토콜을 채택하기에 상위로 올림

--- VendinMachineApp/VendinMachineApp/Model/Beverages/Coffee.swift
+++ VendinMachineApp/VendinMachineApp/Model/Beverages/Coffee.swift
@@ -1,16 +1,25 @@
 
 import Foundation
 
-class Coffee : Beverage, Hotable {
+class Coffee : Beverage, Hotable, Country {
     
     private var temperature: Int
+    private var country: Country
+    override var description: String {
+        return "\(super.description), 원산지: \(country)"
+    }
     
-    init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, temperature: Int) {
+    init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, temperature: Int, country: Country) {
         self.temperature = temperature
+        self.country = country
         super.init(brand: brand, size: size, price: price, name: name, manufactureAt: manufactureAt)
     }
     
     func isHot(standard: Int) -> Bool {
         return self.temperature > standard ? true : false
+    }
+    
+    func madeIn() -> Country {
+        return self.country
     }
 }

5dd27bab0efa6ece13e003cb256a219640598502
zombietux <unknown>
2021-03-03 18:33:29 +0900

    VendingMachine에서 음료를 사는 코드 추가

--- VendingMachineApp/VendingMachineApp/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/VendingMachine.swift
@@ -7,16 +7,23 @@
 
 import Foundation
 
-class VendingMachine: OutputViewPrintable {
-    private let products: [Beverage]
+class VendingMachine {
+    private let moneyManager: MoneyManagable
+    private let inventoryManager: InventoryManagable
+    private let purchaseHistoryManager: PurchaseHistoryManagable
     
-    init(products: [Beverage]) {
-        self.products = products
+    init(moneyManager: MoneyManagable, inventoryManager: InventoryManagable, purchaseHistoryManager: PurchaseHistoryManagable) {
+        self.moneyManager = moneyManager
+        self.inventoryManager = inventoryManager
+        self.purchaseHistoryManager = purchaseHistoryManager
     }
     
-    func printBeverage(handler: (Beverage) -> ()) {
-        products.forEach {
-            handler($0)
+    func purchaseBeverage(beverage: Beverage) {
+        guard beverage.isPurchasable(balance: moneyManager.readBalance()) else { return }
+        
+        if let purchased: Beverage = inventoryManager.removeInventory(beverage) {
+            self.purchaseHistoryManager.addPurchased(purchased)
+            self.moneyManager.decreaseBalance(purchased.price)
         }
     }
 }

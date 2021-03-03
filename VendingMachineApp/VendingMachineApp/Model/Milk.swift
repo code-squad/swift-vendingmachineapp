a553e580d9e4bd2cb1f29371e47367e0dfa539f6
Lia <judykim257@gmail.com>
2021-03-03 17:24:12 +0900

    step1 - manufacturingDate -> manufacteredAt & Drink.swift 파일 삭제

--- VendingMachineApp/VendingMachineApp/Model/Milk.swift
+++ VendingMachineApp/VendingMachineApp/Model/Milk.swift
@@ -10,32 +10,32 @@ import Foundation
 class Milk: Beverage {
     private let beverageType: BeverageCategory
     
-    override init(brand: String, volume: Int, price: Int, name: String, manufacturingDate: Date) {
+    override init(brand: String, volume: Int, price: Int, name: String, manufacturedAt: Date) {
         self.beverageType = .Milk
-        super.init(brand: brand, volume: volume, price: price, name: name, manufacturingDate: manufacturingDate)
+        super.init(brand: brand, volume: volume, price: price, name: name, manufacturedAt: manufacturedAt)
     }
     
     convenience init() {
-        self.init(brand: "brand", volume: 0, price: 0, name: "name", manufacturingDate: Date())
+        self.init(brand: "brand", volume: 0, price: 0, name: "name", manufacturedAt: Date())
     }
     
 }
 
 
 class StrawberryMilk: Milk {
-    override init(brand: String, volume: Int, price: Int, name: String, manufacturingDate: Date) {
-        super.init(brand: "서울우유", volume: 200, price: 600, name: "딸기우유", manufacturingDate: date)
+    override init(brand: String, volume: Int, price: Int, name: String, manufacturedAt: Date) {
+        super.init(brand: "서울우유", volume: 200, price: 600, name: "딸기우유", manufacturedAt: date)
     }
 }
 
 class ChocoMilk: Milk {
-    override init(brand: String, volume: Int, price: Int, name: String, manufacturingDate: Date) {
-        super.init(brand: "서울우유", volume: 200, price: 600, name: "초코우유", manufacturingDate: date)
+    override init(brand: String, volume: Int, price: Int, name: String, manufacturedAt: Date) {
+        super.init(brand: "서울우유", volume: 200, price: 600, name: "초코우유", manufacturedAt: date)
     }
 }
 
 class BananaMilk: Milk {
-    override init(brand: String, volume: Int, price: Int, name: String, manufacturingDate: Date) {
-        super.init(brand: "서울우유", volume: 200, price: 600, name: "바나나우유", manufacturingDate: date)
+    override init(brand: String, volume: Int, price: Int, name: String, manufacturedAt: Date) {
+        super.init(brand: "서울우유", volume: 200, price: 600, name: "바나나우유", manufacturedAt: date)
     }
 }

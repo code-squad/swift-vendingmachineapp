4eb1b225d0701f4fe013e1a0a2cc27e1d1bf74fe
Lia <judykim257@gmail.com>
2021-03-04 06:44:23 +0900

    step2 - VendingMachine 골격
    필요한 속성과 메소드를 추가하며 설계 중

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -7,16 +7,78 @@
 
 import Foundation
 
+struct Inventory {
+    var strawberryMilk = 0
+    var chocoMilk = 0
+    var bananaMilk = 0
+    var cola = 0
+    var cider = 0
+    var fanta = 0
+    var cantata = 0
+    var top = 0
+    var georgia = 0
+}
+
+
 class VendingMachine {
-    private let drinks: [Beverage]
+    
+    private var money: Int
+    private var inventory: Inventory
+    private var purchasedDrink: [Beverage]
     
     init(drinks: [Beverage]) {
-        self.drinks = drinks
+        self.money = 0
+        self.inventory = Inventory()
+        self.purchasedDrink = []
     }
     
-    func printDrinks() {
-        drinks.forEach { drink in
-            print(drink)
-        }
+    
+    //MARK:- Human to VendingMachine Method
+    
+    func collectingMoney(with money: Int) {
+        
     }
+    
+    func add(item: Beverage) {
+        
+    }
+    
+    func purchase(drink: Beverage) {
+        
+    }
+    
+    
+    //MARK:- VendingMachine to Human Method
+
+    func availableDrink() -> [Beverage] {
+        
+        return []
+    }
+    
+    func checkBalance() -> Int {
+        
+        return money
+    }
+    
+    func inventoryList() -> [Beverage:Int] {
+        
+        return [:]
+    }
+    
+    func expiredItems() -> [Beverage] {
+        
+        return []
+    }
+    
+    func hotDrinks() -> [Beverage] {
+        
+        return []
+    }
+    
+    func purchaseHistory() -> [Beverage] {
+        
+        return []
+    }
+    
+    
 }

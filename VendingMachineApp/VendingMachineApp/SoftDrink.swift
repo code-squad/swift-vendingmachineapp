4336b36d12ede966023e081a165fea7aa1519ffa
1song2 <1song2918@gmail.com>
2021-03-03 17:17:07 +0900

    Create a subclass 'ZeroCalorieSoftDrink' of 'SoftDrink'

--- VendingMachineApp/VendingMachineApp/SoftDrink.swift
+++ VendingMachineApp/VendingMachineApp/SoftDrink.swift
@@ -9,16 +9,7 @@ import Foundation
 
 class SoftDrink: Beverage {
     
-    enum SoftDrinkType {
-        case cola
-        case sprite
-        case fanta
-    }
-    
-    private let type: SoftDrinkType
-    
-    init(brand: String, volume: Int, price: Int, name: String, calorie: Int, manufactured: String, expiredAfter: String, type: SoftDrinkType) {
-        self.type = type
+    override init(brand: String, volume: Int, price: Int, name: String, calorie: Int, manufactured: String, expiredAfter: String) {
         super.init(brand: brand, volume: volume, price: price, name: name, calorie: calorie, manufactured: manufactured, expiredAfter: expiredAfter)
     }
 }
---
+++ VendingMachineApp/VendingMachineApp/ZeroCalorieSoftDrink.swift
@@ -0,0 +1,15 @@
+//
+//  ZeroCalorieSoftDrink.swift
+//  VendingMachineApp
+//
+//  Created by Song on 2021/03/03.
+//
+
+import Foundation
+
+class ZeroCalorieSoftDrink: SoftDrink {
+    
+    init(brand: String, volume: Int, price: Int, name: String, manufactured: String, expiredAfter: String, isCarbonated: Bool) {
+        super.init(brand: brand, volume: volume, price: price, name: name, calorie: 0, manufactured: manufactured, expiredAfter: expiredAfter)
+    }
+}

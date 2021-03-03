4b06a7d84acce906642d3276557d3a915fae361d
ellyheetov <yaaajcm@naver.com>
2021-03-03 16:10:14 +0900

    Test VendingMachineAppTest : add testSenario

--- VendingMachineAppTests/VendingMachineAppTests.swift
+++ VendingMachineAppTests/VendingMachineAppTests.swift
@@ -18,33 +18,41 @@ class VendingMachineAppTests: XCTestCase {
     }
     
     func testsSenario(){
-        // 재고를 추가한다.
-        vendingMachine.append(product: CoffeeFactory.createBeverage(type: Top.self)!)
-        vendingMachine.append(product: MilkFactory.createBeverage(type: ChocolateMilk.self)!)
+        
+        let top = CoffeeFactory.createBeverage(type: Top.self)!
+        let chocoMilk = MilkFactory.createBeverage(type: ChocolateMilk.self)!
         let georgia = CoffeeFactory.createBeverage(type: Georgia.self)!
+        let sprite = SodaFactory.createBeverage(type: Sprite.self)!
+        
+        var list = [top,chocoMilk,georgia,sprite]
+        
+        // 재고를 추가한다.
+        vendingMachine.append(product: top)
+        vendingMachine.append(product: chocoMilk)
         vendingMachine.append(product: georgia)
-        vendingMachine.append(product: SodaFactory.createBeverage(type: Sprite.self)!)
+        vendingMachine.append(product: sprite)
         
         // 잔액을 추가한다.
         vendingMachine.charge(coins: 10000)
         
         // 재고를 확인한다.
-//        vendingMachine.getTotalStock().forEach{print($0)}
+        XCTAssertEqual(vendingMachine.getTotalStock(), list.toDictionary(with: {$0.self}), "재고에 물품이 추가되지 않았습니다.")
         
         // 구매 가능한 상품을 출력한다.
-        vendingMachine.availableProducts().forEach{print($0)}
+        XCTAssertEqual(vendingMachine.availableProducts(), list, "구매 가능한 상품이 올바르지 않습니다.")
         
         // 상품을 구매한다.
         vendingMachine.sellProduct(product: georgia)
+        list.remove(at: 2)
         
         // 잔액을 확인한다.
         let guess = 9000
         XCTAssertEqual(vendingMachine.getCoins(), guess, "잔액이 올바르지 않습니다.")
         
         // 재고 상태를 확인한다.
-//        vendingMachine.getTotalStock().forEach{print($0)}
+        XCTAssertEqual(vendingMachine.getTotalStock(), list.toDictionary(with: {$0.self}), "재고에 물품이 제거되지 않았습니다.")
         
         // 구매 이력을 확인한다.
-        vendingMachine.getSoldProducts().forEach{print($0)}
+        XCTAssertEqual(vendingMachine.getSoldProducts(), [georgia], "물품을 구매하지 못했습니다.")
     }
 }

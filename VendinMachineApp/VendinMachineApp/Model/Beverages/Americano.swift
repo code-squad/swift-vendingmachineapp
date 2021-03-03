a7f1bac616e2af4b8511f2bd616ad7407ef7f0d6
BMO <b38238496@gmail.com>
2021-03-03 14:41:03 +0900

    TOP Class의 이름을 Americano 로 변경, 프로토콜 채택
    SubClass로 Americano와 Latte를 구현하기 때문

---
+++ VendinMachineApp/VendinMachineApp/Model/Beverages/Americano.swift
@@ -0,0 +1,20 @@
+
+import Foundation
+
+class Americano : Coffee, Shot {
+    
+    private var shot: Int
+    
+    init(brand: String, size: Int, price: Int, name: String, manufactureAt: Date, temperature: Int, country: Country, shot: Int) {
+        self.shot = shot
+        super.init(brand: brand, size: size, price: price, name: name, manufactureAt: manufactureAt, temperature: temperature, country: country)
+    }
+    
+    convenience init(brand: String, size: Int, price: Int, name: String, temperature: Int, country: Country, shot: Int) {
+        self.init(brand: brand, size: size, price: price, name: name, manufactureAt: Date(), temperature: temperature, country: country, shot: shot)
+    }
+    
+    func isMoreThan(shot: Int) -> Bool {
+        return self.shot > shot ? true : false
+    }
+}

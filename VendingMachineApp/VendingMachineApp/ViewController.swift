924a3d2f51bcdd67dcad189092a4ff12dfe07d2a
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-03 17:47:29 +0900

    Configure root view
    Added stack view in which I put beverage view
    Added labels and buttons on the right side of the root view

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     let machine = Machine()
     
+    @IBOutlet weak var moneyOnTransactionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        moneyOnTransactionLabel.text = "100000원"
     }
+    
+    @IBAction func thousandWonPlusButtonPressed(_ sender: UIButton) {
+    print("thousandWonPlusPressed")
+    }
+    
+    @IBAction func fiveThousandWonPlusButtonPressed(_ sender: UIButton) {
+    print("fiveThousnadWonPlusPressed")
+    }
+    
 }
 

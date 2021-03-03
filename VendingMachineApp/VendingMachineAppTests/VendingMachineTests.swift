5a334124230e79065bba4496cb76f2393b9f36a9
zombietux <>
2021-03-03 18:33:43 +0900

    테스트 코드 추가

--- VendingMachineApp/VendingMachineAppTests/VendingMachineTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineTests.swift
@@ -6,27 +6,71 @@
 //
 
 import XCTest
+@testable import VendingMachineApp
+
+/*
+ 각 메소드 동작을 검증할 수 있는 테스트 함수를 작성한다.
+ 테스트가 가능하다면 위에 동작 규칙을 다르게 구현해도 된다.
+ 전체 코드를 확인할 수 있는 통합 테스트 시나리오를 가지고 동작을 확인한다.
+ 예를 들어서, 재고 몇 개 추가하고 잔액 얼마 추가하면 어떤 상품 구매가능, 불가능인지 확인하고 가능한 상품 구매하고, 잔액 바뀌고 재고 상태 바뀌고, 구매 상품 이력을 확인한다.
+ */
 
 class VendingMachineTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    let moneyManager = Money()
+    let inventoryManager = Inventory()
+    let purchaseHistoryManager = PurchaseHistory()
+    
+    func testVendingMachine() {
+        assertAddInventory()
+        assertIncreaseBalance()
+        assertPurchasable()
+        assertPurchase()
+        assertPurchasedHistory()
+        assertDecreaseBalance()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    private func assertAddInventory() {
+        let bananaMilkFactory = BananaMilkFactory()
+        let cantatacoffeeFactory = CantataCoffeeFactory()
+        let ciderFactory = CiderFactory()
+        
+        inventoryManager.addInventory(bananaMilkFactory.produce())
+        inventoryManager.addInventory(bananaMilkFactory.produce())
+        inventoryManager.addInventory(cantatacoffeeFactory.produce())
+        inventoryManager.addInventory(cantatacoffeeFactory.produce())
+        inventoryManager.addInventory(cantatacoffeeFactory.produce())
+        inventoryManager.addInventory(ciderFactory.produce())
+        
+        XCTAssertEqual(inventoryManager.readInventores().count, 6)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    private func assertIncreaseBalance() {
+        moneyManager.increaseBalance(1000)
+        XCTAssertEqual(moneyManager.readBalance(), 1000)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    private func assertPurchasable() {
+        let purchasable: Inventory = inventoryManager.isPurchasableInventory(balance: moneyManager.readBalance())
+        XCTAssertEqual(purchasable.readInventores().count, 2)
     }
-
+    
+    private func assertPurchase() {
+        let vendingMachine = VendingMachine(moneyManager: moneyManager, inventoryManager: inventoryManager, purchaseHistoryManager: purchaseHistoryManager)
+        let bananaMilkFactory = BananaMilkFactory()
+        let bananaMilk = bananaMilkFactory.produce()
+        inventoryManager.addInventory(bananaMilk)
+        XCTAssertEqual(inventoryManager.readInventores().count, 7)
+        
+        vendingMachine.purchaseBeverage(beverage: bananaMilk)
+        XCTAssertEqual(inventoryManager.readInventores().count, 6)
+    }
+    
+    private func assertPurchasedHistory() {
+        XCTAssertEqual(purchaseHistoryManager.readHistory().count, 1)
+    }
+    
+    private func assertDecreaseBalance() {
+        XCTAssertEqual(moneyManager.readBalance(), 100)
+    }
 }

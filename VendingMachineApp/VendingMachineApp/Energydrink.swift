02e9a92646ca9708ac9926c15d78e53f45c80563
jhpark-steve <park9368@naver.com>
2021-03-03 10:54:46 +0900

    에너지 드링크 서브클래스 선언함

--- VendingMachineApp/VendingMachineApp/Energydrink.swift
+++ VendingMachineApp/VendingMachineApp/Energydrink.swift
@@ -8,6 +8,15 @@
 import Foundation
 
 class Energydrink : Beverage {
+    private var caffeine : Int
+    private var kcal : Int
+    
+    init(caffeine : Int, kcal : Int, brand: String, volume: Int, price: Int, name: String, manufactured: Date){
+        self.caffeine = caffeine
+        self.kcal = kcal
+        super.init(brand: brand, volume: volume, price: price, name: name, manufactured: manufactured)
+    }
+    
     enum taste {
         case monster, diablo
     }

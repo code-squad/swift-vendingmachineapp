f2160e8efbc5cd5cb239fda830869a282904de99
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-04 18:33:13 +0900

    Edit addArrangedSubViews logic

--- VendingMachineApp/VendingMachineApp/BeverageView.swift
+++ VendingMachineApp/VendingMachineApp/BeverageView.swift
@@ -8,13 +8,69 @@
 import UIKit
 
 class BeverageView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var stockAddButton: UIButton
+    var beverageImageView: UIImageView
+    var showStockLabel: UILabel
+    
+    override init(frame: CGRect) {
+        stockAddButton = {
+            let button = UIButton(type: .system)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitle("추가", for: .normal)
+            button.setTitleColor(UIColor.systemBlue, for: .normal)
+            return button
+        }()
+        beverageImageView = {
+            let imageView = UIImageView()
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.backgroundColor = UIColor.blue
+            imageView.contentMode = .scaleAspectFit
+            return imageView
+        }()
+        showStockLabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.backgroundColor = UIColor.green
+            label.text = "개"
+            return label
+        }()
+        super.init(frame: frame)
+        
+        addSubview(stockAddButton)
+        addSubview(beverageImageView)
+        addSubview(showStockLabel)
+    
+        configureStockAddButton()
+        configureBeverageImageView()
+        configureShowStockLabel()
     }
-    */
 
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureStockAddButton() {
+        stockAddButton.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.2).isActive = true
+        stockAddButton.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1).isActive = true
+        stockAddButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        stockAddButton.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+    }
+    
+    func configureBeverageImageView() {
+        beverageImageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.6).isActive = true
+        beverageImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1).isActive = true
+        beverageImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        beverageImageView.topAnchor.constraint(equalTo: stockAddButton.bottomAnchor).isActive = true
+    }
+    
+    func configureShowStockLabel() {
+        showStockLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.2).isActive = true
+        showStockLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1).isActive = true
+        showStockLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        showStockLabel.topAnchor.constraint(equalTo: beverageImageView.bottomAnchor).isActive = true
+    }
+    
+    func setLabelText(with string: String) {
+        self.showStockLabel.text = string
+    }
 }
--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -11,10 +11,24 @@ class ViewController: UIViewController {
     let machine = Machine()
     
     @IBOutlet weak var moneyOnTransactionLabel: UILabel!
+    @IBOutlet weak var beverageDisplayStackViewFirstRow: UIStackView!
+    @IBOutlet weak var beverageDisplayStackViewSecondRow: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        moneyOnTransactionLabel.text = "100000원"
+        let someBeverage = Beverage(brand: "종근당", size: 120, price: 1082, name: "쌩강쌍원진액", packageTime: Date())
+        let someStrawBerryMilk = StrawBerryMilk(brand: "남양유업", size: 180, price: 1000, name: "딸기에몽", packageTime: Date(), milkFarm: DomesticLocation(), strawBerryFarm: OverseasLocation(country: "이스라엘"), foodColoring: FoodColoring(color: "Red"))
+        let someBananaMilk = BananaMilk(brand: "빙그레", size: 240, price: 1500, name: "바나나맛우유", packageTime: Date(), milkFarm: DomesticLocation(), bananaFarm: OverseasLocation(country: "인도"), foodColoring: FoodColoring(color: "Yellow"))
+        let someCola = Cola(brand: "코카콜라", size: 190, price: 800, name: "코카-콜라", packageTime: Date())
+//        let someAmericano = Americano(brand: "스타벅스", size: 355, price: 4100, name: "아메리카노-톨", packageTime: Date(), bean: .robusta, caffeine: 150, inHeatingCabinet: false)
+        
+        machine.addStock(beverage: someBeverage, amount: 10)
+        machine.addStock(beverage: someStrawBerryMilk, amount: 20)
+        machine.addStock(beverage: someBananaMilk, amount: 30)
+        machine.addStock(beverage: someCola, amount: 40)
+//        machine.addStock(beverage: someAmericano, amount: 50)
+
+        InitialBeverageSetting()
     }
     
     @IBAction func thousandWonPlusButtonPressed(_ sender: UIButton) {
@@ -25,5 +39,33 @@ class ViewController: UIViewController {
     print("fiveThousnadWonPlusPressed")
     }
     
+    func createBeverageView(beveage: Beverage, count: Int) -> BeverageView {
+        let view = BeverageView()
+        view.setLabelText(with: "\(count)개")
+        return view
+    }
+    
+    func InitialBeverageSetting() {
+        let stock = machine.checkStock()
+        
+        for (index, item) in stock.enumerated() {
+            let view = createBeverageView(beveage: item.key, count: item.value)
+            if index < 5 {
+                beverageDisplayStackViewFirstRow.addArrangedSubview(view)
+            } else {
+                beverageDisplayStackViewSecondRow.addArrangedSubview(view)
+            }
+        }
+        
+        if stock.count < 5 {
+            for _ in 0..<(5 - stock.count) {
+                beverageDisplayStackViewFirstRow.addArrangedSubview(UIView())
+            }
+        } else if stock.count < 10 {
+            for _ in 0..<(10 - stock.count) {
+                beverageDisplayStackViewSecondRow.addArrangedSubview(UIView())
+            }
+        }
+    }
 }
 

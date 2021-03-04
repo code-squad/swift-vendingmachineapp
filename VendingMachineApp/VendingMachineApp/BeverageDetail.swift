cbd94493d29069404934a418a6c9d92d58aa016a
zombietux <unknown>
2021-03-04 14:12:09 +0900

    메서드 유연성, 테스트용이성 증가를 위한 코드 수정

--- VendingMachineApp/VendingMachineApp/BeverageDetail.swift
+++ VendingMachineApp/VendingMachineApp/BeverageDetail.swift
@@ -9,7 +9,7 @@ import Foundation
 
 protocol BeverageDetail {
     func isExpired(now date: Date) -> Bool
-    func isHot() -> Bool
-    func isLowCalorie() -> Bool
+    func isHot(temparature standard: Int) -> Bool
+    func isLowCalorie(calories standard: Int) -> Bool
     func isPurchasable(balance: Int) -> Bool
 }

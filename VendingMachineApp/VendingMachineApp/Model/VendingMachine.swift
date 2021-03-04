42ba3a9609bf0c77c6fae76eb82b05c288a043a9
Sonjh1306 <yale1306@gmail.com>
2021-03-04 23:37:42 +0900

    음료수 및 자판기 요구사항 구현

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -19,13 +19,17 @@ class VendingMachine {
         self.purchasedList = Beverages()
     }
     
-    // 재고를 추가하는 메소드
-    func addStock(beverage: Beverage) -> Beverage{
-        beverages.addBeverage(beverage: beverage)
-        return beverage
+    //MARK: 자판기 금액을 원하는 금액만큼 올리는 메소드
+    func addMoney(additionalAmount: Int) {
+        money.increaseAmount(additionalAmount: additionalAmount)
     }
     
-    // 현재 금액으로 구매 가능한 음료수 목록을 리턴하는 메소드
+    //MARK: 특정 상품 인스턴스를 넘겨서 재고를 추가하는 메소드
+    func addStock(beverage: Beverage) {
+        beverages.addBeverage(beverage: beverage)
+    }
+    
+    //MARK: 현재 금액으로 구매 가능한 음료수 목록을 리턴하는 메소드
     func showAvailableBeverage() -> [Beverage] {
         var availableList = [Beverage]()
         beverages.retrieveBeverage(completion: {
@@ -36,13 +40,51 @@ class VendingMachine {
         return availableList
     }
     
-    // 음료수를 구매하는 메소드
-    func buyBeverage(beverage: Beverage)  {
+    //MARK: 음료수를 구매하는 메소드
+    func buyBeverage(beverage: Beverage) -> Beverage  {
         if beverage.availableBeverage(currentAmount: money.showCurrentAmount()) {
             money.decreaseAmount(price: beverage.price)
             purchasedList.addBeverage(beverage: beverage)
             beverages.removeBeverage(beverage: beverage)
+            return beverage
         }
+        return beverage
     }
     
+    //MARK: 잔액을 확인하는 메소드
+    func showCurrentInputAmount() -> Int {
+        return money.showCurrentAmount()
+    }
+    
+    //MARK: 전체 상품 재고를 (사전으로 표현하는) 종류별로 리턴하는 메소드
+    func showCurrentStock() -> [Beverage: Int] {
+        return beverages.checkBeverageStock()
+    }
+    
+    //MARK: 유통기한 만료된 상품을 체크하는 메소드
+    func checkExpiredBeverage(date: Date) -> [Beverage] {
+        var expriedBeverageList = [Beverage]()
+        beverages.retrieveBeverage(completion: {
+            if !$0.isExpired(with: date){
+                expriedBeverageList.append($0)
+            }
+        })
+        return expriedBeverageList
+    }
+    
+    //MARK: 뜨거운 음료를 체크하는 메소드
+    func checkHotBeverage() -> [Beverage] {
+        var hotBeverageList = [Beverage]()
+        beverages.retrieveBeverage(completion: {
+            if $0.isHot(){
+                hotBeverageList.append($0)
+            }
+        })
+        return hotBeverageList
+    }
+    
+    //MARK: 시작이후 구매 상품 이력을 배열로 리턴하는 메소드
+    func showPurchasedList() -> Beverages {
+        return purchasedList
+    }
 }

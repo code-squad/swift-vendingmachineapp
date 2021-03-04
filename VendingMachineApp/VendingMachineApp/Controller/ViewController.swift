c93d682c07eff5ec7969d9d7f21290ff49f1ef6d
zeke <yangsin1031@naver.com>
2021-03-04 15:30:46 +0900

    View생성
    음료수와 버튼, 음료수들, 금액추가버튼 들을 스택뷰로 생성

---
+++ VendingMachineApp/VendingMachineApp/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 양준혁 on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let vendingMachine = VendingMachine()
+    }
+}
+
--- VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -17,9 +17,9 @@ class VendingMachineAppTests: XCTestCase {
     
     func test_자판기() {
         let vendingMachine = VendingMachine()
-        let strawberryMilk = StrawBerryMilk(brand: "서울우유", capacity: 300, price: 800, name: "딸기우유", manufacture: Date.input("20210302"), expirationDate: Date.input("20210310"), temperature: 5, pH: 6.5, color: StrawBerryMilk.Color.red)
-        let redBull = RedBull(brand: "RedBull", capacity: 250, price: 1000, name: "레드불", manufacture: Date.input("20210302"), expirationDate: Date.input("20210310"), temperature: 5, taurineContent: 1000, vitaminB: 2)
-        let top = TOP(brand: "동서식품", capacity: 380, price: 2000, name: "TOP", manufacture: Date.input("20210222"), expirationDate: Date.input("20210301"), temperature: 65, caffeineContent: 94, flavor: TOP.Flavor.americano)
+        let strawberryMilk = StrawBerryMilk(brand: "서울우유", capacity: 300, price: 800, name: "딸기우유", manufacturedAt: Date.input("20210302"), expirationAt: Date.input("20210310"), temperature: 5, pH: 6.5, color: StrawBerryMilk.Color.red)
+        let redBull = RedBull(brand: "RedBull", capacity: 250, price: 1000, name: "레드불", manufacturedAt: Date.input("20210302"), expirationAt: Date.input("20210310"), temperature: 5, taurineContent: 1000, vitaminB: 2)
+        let top = TOP(brand: "동서식품", capacity: 380, price: 2000, name: "TOP", manufacturedAt: Date.input("20210222"), expirationAt: Date.input("20210301"), temperature: 65, caffeineContent: 94, flavor: TOP.Flavor.americano)
         
         //자판기 금액 충전
         vendingMachine.rechargeCash(with: 10000)

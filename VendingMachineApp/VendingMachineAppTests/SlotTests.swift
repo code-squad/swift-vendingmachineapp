bbc86f8ef1fcec926d057184fbfdcaf518d718a0
1song2 <1song2918@gmail.com>
2021-03-04 16:38:23 +0900

    Create a unit test target

---
+++ VendingMachineApp/VendingMachineAppTests/SlotTests.swift
@@ -0,0 +1,24 @@
+//
+//  SlotTests.swift
+//  VendingMachineAppTests
+//
+//  Created by Song on 2021/03/04.
+//
+
+import XCTest
+
+class SlotTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func test_자판기앱_빈슬롯_생성() throws {
+        let emptySlot = Slot()
+        
+    }
+}

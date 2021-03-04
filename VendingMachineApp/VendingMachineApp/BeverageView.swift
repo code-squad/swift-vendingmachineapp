c59be8db84a6f3e5eeb7ad9f1e9b2b9a142a0578
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-04 14:23:56 +0900

    Add custom view
    Added customed view cosisting of beverage add button, beverage image view, and beverage stock label
    Added mock test code

---
+++ VendingMachineApp/VendingMachineApp/BeverageView.swift
@@ -0,0 +1,20 @@
+//
+//  BeverageView.swift
+//  VendingMachineApp
+//
+//  Created by 조중윤 on 2021/03/03.
+//
+
+import UIKit
+
+class BeverageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

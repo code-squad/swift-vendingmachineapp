c659c70eb854291b46e83eea70d153bb5fa926f1
Lia <judykim257@gmail.com>
2021-03-06 00:42:00 +0900

    step2 - create class Inventory
    and toDictionary Uility

--- VendingMachineApp/VendingMachineApp/Model/Beverage.swift
+++ VendingMachineApp/VendingMachineApp/Model/Beverage.swift
@@ -7,13 +7,21 @@
 
 import Foundation
 
-class Beverage: CustomStringConvertible {
+class Beverage: CustomStringConvertible, Hashable {
     private let brand: String
     private let volume: Int
     private let price: Int
     private let name: String
     private let createdAt: Date
     private let expiredAt: Date
+    
+    static func == (lhs: Beverage, rhs: Beverage) -> Bool {
+        return type(of: lhs) == type(of: rhs)
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(type(of: self)))
+    }
     
     init(brand: String, volume: Int, price: Int, name: String, createdAt: Date, expiredAt: Date) {
         self.brand = brand

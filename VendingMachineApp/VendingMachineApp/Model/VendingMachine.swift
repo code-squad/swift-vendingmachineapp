2cfea02830bf74877b29485ea1f5fd426515e534
Lia <judykim257@gmail.com>
2021-03-06 03:19:26 +0900

    step2 - filled  VendingMachine method
    add cheapItems in class Inventory for availableDrink()
    so also added testcase for InventoryTests

--- VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachine.swift
@@ -9,6 +9,13 @@ import Foundation
 
 class VendingMachine {
     
+    enum Money: Int {
+        case coin = 100
+        case coins = 500
+        case bill = 1000
+        case bills = 5000
+    }
+    
     private var money: Int
     private var inventory: Inventory
     private var purchasedDrink: [Beverage]
@@ -22,49 +29,45 @@ class VendingMachine {
     
     //MARK:- Human to VendingMachine Method
     
-    func collectingMoney(with money: Int) {
-        
+    func collectingMoney(with money: Money) {
+        self.money += money.rawValue
     }
     
     func add(item: Beverage) {
-        
+        inventory.append(with: item)
     }
     
     func purchase(drink: Beverage) {
-        
+        self.money -= drink.price
+        inventory.remove(item: drink)
+        purchasedDrink.append(drink)
     }
     
     
     //MARK:- VendingMachine to Human Method
 
     func availableDrink() -> [Beverage] {
-        
-        return []
+        return inventory.cheapItems(with: money)
     }
     
     func checkBalance() -> Int {
-        
         return money
     }
     
     func inventoryList() -> [Beverage:Int] {
-
-        return [:]
+        return inventory.getStockList()
     }
     
     func expiredItems() -> [Beverage] {
-        
-        return []
+        return inventory.expiredItems()
     }
     
-    func hotDrinks() -> [Beverage] {
-        
-        return []
+    func lowCaffeine() -> [Beverage] {
+        return inventory.lowCaffeineItems()
     }
     
     func purchaseHistory() -> [Beverage] {
-        
-        return []
+        return purchasedDrink
     }
     
     
--- VendingMachineApp/VendingMachineAppTests/InventoryTests.swift
+++ VendingMachineApp/VendingMachineAppTests/InventoryTests.swift
@@ -22,35 +22,49 @@ class InventoryTests: XCTestCase {
     }
 
     func test_Inventory_생성_및_append() {
-        inventory.append(item: drink)
+        inventory.append(with: drink)
         
         XCTAssertNotEqual(inventory, Inventory(), "Inventory 생성 및 append 성공")
     }
     
     func test_Inventory_삭제() {
-        inventory.append(item: drink)
+        inventory.append(with: drink)
         inventory.remove(item: drink)
         
         XCTAssertEqual(inventory, Inventory(), "Inventory 삭제 성공")
     }
 
     func test_재고목록_리턴() {
-        inventory.append(item: drink)
-        inventory.append(item: drink)
+        inventory.append(with: drink)
+        inventory.append(with: drink)
         
         XCTAssertEqual(inventory.getStockList(), [TOP():2], "재고목록 리턴 성공")
     }
     
     func test_유통기한_지난_재고() {
-        inventory.append(item: drink)
-        inventory.append(item: drink)
+        inventory.append(with: drink)
+        inventory.append(with: drink)
         
         XCTAssertEqual(inventory.expiredItems(), [TOP(), TOP()], "유통기한 지난 재고 리턴 성공")
     }
     
     func test_카페인_함량() {
-        inventory.append(item: drink)
+        inventory.append(with: drink)
         
         XCTAssertEqual(inventory.lowCaffeineItems(), [TOP()], "유통기한 지난 재고 리턴 성공")
+    }
+    
+    func test_낮은가격목록_성공() {
+        inventory.append(with: drink)
+        inventory.append(with: drink)
+        
+        XCTAssertEqual(inventory.cheapItems(with: 1000), [TOP(), TOP()], "구매 가능 목록 리턴 성공")
+    }
+    
+    func test_낮은가격목록_실패() {
+        inventory.append(with: drink)
+        inventory.append(with: drink)
+        
+        XCTAssertEqual(inventory.cheapItems(with: 800), [], "구매 가능 목록 리턴 실패")
     }
 }

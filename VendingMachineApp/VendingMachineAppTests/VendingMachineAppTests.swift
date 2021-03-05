89970eda4a6cc450daba4e3782b4334f3fee65d5
Lia <judykim257@gmail.com>
2021-03-06 04:52:02 +0900

    step2 - TestFile rename, relocation
    가장 먼저 생성한 파일이 타겟으로, 중심이 되는 것을 알게 되었다😅
    InventoryTest.swift -> VendingMachineAppTests 로 바꾸고,
    기존 내용을 새 파일에 옮기고, 중심 파일에 통합 테스트 구현할 예정

---
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -0,0 +1,24 @@
+//
+//  VendingMachineAppTests.swift
+//  VendingMachineAppTests
+//
+//  Created by Lia on 2021/03/04.
+//
+
+import XCTest
+
+class VendingMachineAppTests: XCTestCase {
+    
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+
+    func test() {
+        
+    }
+
+}

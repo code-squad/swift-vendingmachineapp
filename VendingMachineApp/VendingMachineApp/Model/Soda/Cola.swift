cdb834e998e87cdf8dfdead5330aa310d3d3d109
kiyoungj <unknown>
2021-03-06 02:38:24 +0900

    콜라 클래스에 enum타입 추가

--- VendingMachineApp/VendingMachineApp/Model/Soda/Cola.swift
+++ VendingMachineApp/VendingMachineApp/Model/Soda/Cola.swift
@@ -7,14 +7,19 @@
 
 import Foundation
 
+enum PackageType {
+    case bottle
+    case can
+    case plastic
+}
 
 class Cola: Soda {
     
-    private let inBottle: Bool
+    private let packageType: PackageType
     
-    init(inBottle: Bool, isSodaType: SodaType, factoryCode: String, brand: String, capacity: Int, price: Int, name: String,
+    init(packageType: PackageType, isSodaType: SodaType, factoryCode: String, brand: String, capacity: Int, price: Int, name: String,
          madeDate: Date,isHot:Bool) {
-        self.inBottle = inBottle
+        self.packageType = packageType
         super.init(isSodaType: isSodaType, factoryCode: factoryCode, brand: brand, capacity: capacity, price: price, name: name, madeDate: madeDate,isHot: isHot)
     }
 }

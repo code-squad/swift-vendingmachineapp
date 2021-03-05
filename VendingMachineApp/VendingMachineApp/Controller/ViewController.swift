e1651da874d4ad13fed43b21fc3e4f2f97f3ed0b
zeke <yangsin1031@naver.com>
2021-03-05 16:17:11 +0900

    UIImage의 cornerRadius 변경
    이미지들의 사각형의 각 꼭지점을 없애고 둥글게 만들기 위해 cornerRadius 변경

--- VendingMachineApp/VendingMachineApp/Controller/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/Controller/ViewController.swift
@@ -11,17 +11,21 @@ class ViewController: UIViewController {
     
     @IBOutlet var beverageLabels: [UILabel]!
     @IBOutlet var balanceLabel: UILabel!
+    @IBOutlet var beverageImages: [UIImageView]!
     
     let vendingMachine = VendingMachine()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        beverageImages.forEach{ (imageView) in
+            imageView.layer.cornerRadius = 50
+            imageView.clipsToBounds = true
+        }
     }
     
     @IBAction func addStock(_ sender: UIButton) {
         guard let id = sender.restorationIdentifier else {return}
         guard let beverage = DrinkFactory.createBeverage(with: id) else {return}
-        
         vendingMachine.addStock(as: beverage)
         let label = beverageLabels.filter{$0.restorationIdentifier == id+"Label"}[0]
         let stock = vendingMachine.showStock()

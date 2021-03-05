5924dd96f66c6e2109f4f1b1bf8293e501b4a1ec
dahun-lee-daji <ssss6596@gmail.com>
2021-03-05 11:53:19 +0900

    음료 StackView 뼈대 구성
    음료 상품 StackView에 들어갈 요소 생성
    View에 추가될 BeverageStackView 생성
    기존 View를 대체할 OutputView 수정
    상품 이미지 추가

---
+++ VendingMachineApp/VendingMachineApp/View/StockLabel.swift
@@ -0,0 +1,14 @@
+//
+//  StockLabel.swift
+//  VendingMachineApp
+//
+//  Created by 이다훈 on 2021/03/05.
+//
+
+import UIKit
+
+class StockLabel: UILabel {
+    func setting() {
+        self.text = "?개"
+    }
+}

ad7259fcb0a9722a2902119e21699f23f8ed4d56
kiyoungj <>
2021-03-05 19:22:44 +0900

    서브클래스속성 추가 및 메소드명 변경과 속성범위 지정

--- VendingMachineApp/VendingMachineApp/Model/Coffee/Cantata.swift
+++ VendingMachineApp/VendingMachineApp/Model/Coffee/Cantata.swift
@@ -9,7 +9,10 @@ import Foundation
 
 class Cantata: Coffee {
     
-    override init(isDecaffeinated: Bool,isHot:Bool, isCoffeeBrandName: CoffeeBrandName, brand: String, capacity: Int, price: Int, name: String, madeDate: Date, beverageType:BeverageType) {
+    private var isPremium: Bool
+    
+    init(isPremium: Bool, isDecaffeinated: Int,isHot:Bool, isCoffeeBrandName: CoffeeBrandName, brand: String, capacity: Int, price: Int, name: String, madeDate: Date, beverageType:BeverageType) {
+        self.isPremium = isPremium
         super.init(isDecaffeinated: isDecaffeinated,isHot: isHot, isCoffeeBrandName: isCoffeeBrandName, brand: brand, capacity: capacity, price: price, name: name, madeDate: madeDate, beverageType: BeverageType.Coffee)
     }
 }

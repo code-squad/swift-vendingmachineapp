61a7f463f24ccc4b19987d2e29293b330d695135
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-03-05 11:35:19 +0900

    음료 공장 객체를 생성했습니다.

--- VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
+++ VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
@@ -7,15 +7,35 @@
 
 import Foundation
 
-class BeverageFactory {
+class BeverageFactory: addBeverage {
     
-    func addBeverage(manufacturedAt: Date, expiration: Date) -> Beverage {
-        
-        return CantataAmericano(manufacturedAt: manufacturedAt, expiration: expiration)
+    static func addBeverage<T>(type sku: T, manufacturedAt: Date, expiration: Date) -> Beverage? {
+        switch sku {
+        case is CantataAmericano.Type:
+            return CantataAmericano(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is CantataLatte.Type:
+            return CantataLatte(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is BlueBottle.Type:
+            return BlueBottle(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is SeoulSterilized.Type:
+            return SeoulSterilized(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is SeoulStrawberry.Type:
+            return SeoulStrawberry(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is SeoulChoco.Type:
+            return SeoulChoco(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is DrPepper.Type:
+            return DrPepper(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is FantaOrange.Type:
+            return FantaOrange(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is Sprite.Type:
+            return Sprite(manufacturedAt: manufacturedAt, expiration: expiration)
+        default:
+            return nil
+        }
     }
     
 }
 
 protocol addBeverage {
-    func addBeverage(specific: a.Type, manufacturedAt: Date, expiration: Date) -> Beverage?
+    static func addBeverage<T>(type sku: T, manufacturedAt: Date, expiration: Date) -> Beverage?
 }

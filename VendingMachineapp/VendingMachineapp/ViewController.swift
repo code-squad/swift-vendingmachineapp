f5536f348fdf00d68aea30219b7818bb9b379fc8
shimyoungmin <dudals6965@gmail.com>
2021-03-05 12:09:32 +0900

    스토리보드에 음료사진 추가

--- VendingMachineapp/VendingMachineapp/ViewController.swift
+++ VendingMachineapp/VendingMachineapp/ViewController.swift
@@ -8,8 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let imageArray = [
+        "top", "cantata", "georgia",
+        "coke", "sprite", "fanta",
+        "strawberry", "banana", "choco"
+    ]
+    
+    @IBOutlet var mainStackView: UIStackView!
+    @IBOutlet var lineStackView: [UIStackView]!
+    @IBOutlet var beverageStackView: [UIStackView]!
+    
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var imageViews: [UIImageView]!
+    @IBOutlet var labels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainStackView.backgroundColor = .gray
+        setImageViews()
     }
+    
+    func setImageViews() {
+        var index = 0
+        for img in imageViews {
+            img.image = UIImage(named: "\(imageArray[index])")
+            index += 1
+        }
+    }
+
 }

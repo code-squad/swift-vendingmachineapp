f619bf44af34bf28e2ba5ab3db4f70ab348c9fc7
okstring <milkoi7801@gmail.com>
2021-03-05 15:42:29 +0900

    메뉴 추가버튼 클릭 시 label 변경까지 구현
     - SelectPanelStackView에 delegate 채택
     - addDrink 매개변수를 타입으로 받도록 구현
     - tag를 이용해 식별, showStock을 통해 재고 확인
     - VendingMachine 초기화 시 직접 stock과 coins를 만들도록 수정

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+enum DrinkTags: Int {
+    case blueBottleColdBrewBold
+    case fantaOrange
+    case bananaMilk
+    case seoulStrawberryMilk
+    case sanpellegrino
+    case starbucksColdBrewBlack
+}
+
+class ViewController: UIViewController, SelectPanelStackViewDelegate {
+    @IBOutlet weak var selectPanelStackView: SelectPanelStackView!
+    var vendingMachine: VendingMachine!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        vendingMachine = VendingMachine()
+        selectPanelStackView.delegate = self
     }
-
+    
+    func didAddDrink(typeOf drinkType: Drink.Type) {
+        vendingMachine.addDrink(drinkType)
+        loadSelectPanelStackViewLabels()
+    }
 }
 
+extension ViewController {
+    private func findDrinkTypeToTag(of tag: Int) -> Drink.Type? {
+        switch tag {
+        case 0: return BlueBottleColdBrew.self
+        case 1: return Fanta.self
+        case 2: return BingBananaMilk.self
+        case 3: return SeoulStrawberryMilk.self
+        case 4: return Sanpellegrino.self
+        case 5: return StarbucksColdBrew.self
+        default: return nil
+        }
+    }
+    
+    private func loadSelectPanelStackViewLabels() {
+        let stock = vendingMachine.showStock()
+        selectPanelStackView.stockDrinkLabels.enumerated().forEach { (index, label) in
+            guard let type = findDrinkTypeToTag(of: label.tag) else { return }
+            let typeKey = ObjectIdentifier(type)
+            if stock[typeKey] != nil {
+                label.text = "\(stock[typeKey]!.count)개"
+            } else {
+                label.text = "0개"
+            }
+        }
+    }
+}

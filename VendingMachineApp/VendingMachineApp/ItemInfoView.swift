a477183f58f3e22925a6ae182e945c756e75a046
1song2 <1song2918@gmail.com>
2021-03-08 23:14:21 +0900

    Create custom UIView with Xib

---
+++ VendingMachineApp/VendingMachineApp/ItemInfoView.swift
@@ -0,0 +1,35 @@
+//
+//  ItemInfoView.swift
+//  VendingMachineApp
+//
+//  Created by Song on 2021/03/08.
+//
+
+import UIKit
+
+class ItemInfoView: UIView {
+    private let nibName = "ItemInfoView"
+    
+    @IBOutlet weak var itemQuantityIncrementButton: UIButton!
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemQuantityLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        guard let nibObjects = Bundle.main.loadNibNamed(nibName, owner: self, options: nil) else { return }
+        guard let nibView = nibObjects.first as? UIView else { return }
+        nibView.frame = self.bounds
+        itemImageView.layer.cornerRadius = 15.0
+        itemImageView.layer.masksToBounds = true
+        self.addSubview(nibView)
+    }
+}

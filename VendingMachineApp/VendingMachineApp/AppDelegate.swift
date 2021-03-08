da7709bf8ccd5abd917f58dfee94a46baa9eca8f
Song Lee <songlish233@gmail.com>
2021-03-08 15:34:52 +0900

    VendingMachine 객체 생성 주체를 AppDelegate로 변경
    - ViewController에 appDelegate 생성하여 VendingMachine 변수 사용할 수 있도록 함

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -9,6 +9,19 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var vendingMachine: VendingMachine!
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        /// 데이터 불러오기 - 없을 경우 새로 생성
+        vendingMachine = VendingMachine(dateStandard: Date(),
+                                            temperatureStandard: 36.5,
+                                            sugarStandard: 1.0,
+                                            lactoStandard: 0.5)
+        return true
+    }
 
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        /// 데이터 저장
+    }
 }
-

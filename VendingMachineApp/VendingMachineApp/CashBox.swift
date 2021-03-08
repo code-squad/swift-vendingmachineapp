ef85861481b8988f00cc1a104c83fca23982c4da
1song2 <1song2918@gmail.com>
2021-03-08 16:36:55 +0900

    Add struct 'CashBox'

--- VendingMachineApp/VendingMachineApp/CashBox.swift
+++ VendingMachineApp/VendingMachineApp/CashBox.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct CashBox {
+    private var totalRevenue: Int
+    private var moneyDeposited: Int
+    
+    init(totalRevenue: Int, moneyDeposited: Int) {
+        self.totalRevenue  = totalRevenue
+        self.moneyDeposited = moneyDeposited
+    }
+    
+    mutating func insertMoney(amount: Int) {
+        moneyDeposited += amount
+    }
+    
+    mutating func increaseRevenue(by amount: Int) {
+        moneyDeposited -= amount
+        totalRevenue += amount
+    }
+    
+    func showRemainingBalance() -> Int {
+        return moneyDeposited
+    }
+}

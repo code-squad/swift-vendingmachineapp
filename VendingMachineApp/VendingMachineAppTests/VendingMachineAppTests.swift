ee91cbd13beac4a68aa043e3fa3e9263f2b1f30c
okstring <milkoi7801@gmail.com>
2021-03-08 22:13:45 +0900

    수정 - 테스트 케이스 각각의 행동을 묶어 수행하도록 수정

--- VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -12,29 +12,20 @@ class VendingMachineAppTests: XCTestCase {
     var stock: StockManageable!
     var coinCounter: CoinCounter!
     var vendingMachine: VendingMachine!
+    var fanta: Fanta!
+    var seoulMilk: SeoulStrawberryMilk!
     
     override func setUp() {
         super.setUp()
         stock = Stock()
         coinCounter = CoinCounter()
         vendingMachine = VendingMachine()
-    }
-    
-    override func tearDown() {
-        stock = nil
-        vendingMachine = nil
-        coinCounter = nil
-        super.tearDown()
-    }
-    
-    // 통합 테스트 시나리오
-    func testVendingMachineScenario() {
         guard let blueBottle = DrinkFactory.makeDrink(of: BlueBottleColdBrew.self) else { return }
         guard let starbucks = DrinkFactory.makeDrink(of: StarbucksColdBrew.self) else { return }
-        guard let seoulMilk = DrinkFactory.makeDrink(of: SeoulStrawberryMilk.self) else { return }
+        guard case self.seoulMilk = DrinkFactory.makeDrink(of: SeoulStrawberryMilk.self) else { return }
         guard let bananaMilk = DrinkFactory.makeDrink(of: BingBananaMilk.self) else { return }
         guard let sanpellegrino = DrinkFactory.makeDrink(of: Sanpellegrino.self) else { return }
-        guard let fanta = DrinkFactory.makeDrink(of: Fanta.self, manufactured: Date() - 365) else { return }
+        guard case self.fanta = DrinkFactory.makeDrink(of: Fanta.self, manufactured: Date() - 365) else { return }
         
         // Productization을 채택하지 않은 Soda 클래스
         let weirdSoda = Soda(brand: "탄산회사", capacity: 500, price: 1500, name: "정체모를 탄산수", isZeroCalorie: true, manufactured: Date(), temperature: 15, calorie: 150)
@@ -46,19 +37,36 @@ class VendingMachineAppTests: XCTestCase {
         vendingMachine.addDrink(sanpellegrino)
         vendingMachine.addDrink(fanta)
         vendingMachine.addDrink(weirdSoda)
-        
-        XCTAssertEqual(vendingMachine.showStock().count,
-                       6)
-        
+    }
+    
+    override func tearDown() {
+        stock = nil
+        vendingMachine = nil
+        coinCounter = nil
+        seoulMilk = nil
+        fanta = nil
+        super.tearDown()
+    }
+    
+    func 보관중인_Drink_갯수() {
+        XCTAssertEqual(vendingMachine.showStock().count, 6)
+    }
+    
+    func 창고안에서_따듯한_Drink_반환() {
         XCTAssertEqual(vendingMachine.findWarmDrinks(), [])
-        
+    }
+    
+    func 유통기한_지난_Drink_반환() {
         XCTAssertEqual(vendingMachine.findExpiredDrinks(), [fanta])
-        
+    }
+    
+    func 동전_투입_후_이용가능한_Drink_반환() {
         vendingMachine.insertCoin(1000)
-        
         XCTAssertEqual(vendingMachine.availableDrink(), [seoulMilk])
+    }
+    
+    func 구입_후_구매이력_반환() {
         _ = vendingMachine.buy(typeOf: SeoulStrawberryMilk.self)
         XCTAssertEqual(vendingMachine.showPurchasehistory(), [seoulMilk])
-        
     }
 }

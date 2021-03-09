7814dcb79f6acf519c06b7e261b0c3bc5367a122
Ray <readingod@naver.com>
2021-03-09 10:50:59 +0900

    BeverageFactory Class 수정
    NSCoding 과 Codable의 차이 및 다형성 학습 중 수정 필요한 부분 발견하여 Beverage로 통일해서 만들었던 음료를 각 Class타입으로 수정

--- VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
+++ VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
@@ -9,12 +9,12 @@ struct BeverageFactory {
     private(set) var cantata: Beverage
     
     init() {
-        chocoMilk = Beverage(brand: .namyang, volume: 200, price: 1000, productName: "초코에몽", manufacturedDay: Date(), sellByDate: Date(), calories: 100, temparature: 0)
-        strawberryMilk = Beverage(brand: .seoul, volume: 200, price: 1200, productName: "딸기꿀단지", manufacturedDay: Date(), sellByDate: Date(), calories: 110, temparature: 0)
-        coke = Beverage(brand: .coca, volume: 180, price: 1300, productName: "코카콜라제로", manufacturedDay: Date(), sellByDate: Date(), calories: 0, temparature: 0)
-        sprite = Beverage(brand: .coca, volume: 200, price: 1300, productName: "스프라이트", manufacturedDay: Date(), sellByDate: Date(), calories: 100, temparature: 0)
-        top = Beverage(brand: .maxim, volume: 180, price: 1800, productName: "TOP", manufacturedDay: Date(), sellByDate: Date(), calories: 190, temparature: 110)
-        cantata = Beverage(brand: .lotte, volume: 210, price: 1900, productName: "콘트라베이스블랙", manufacturedDay: Date(), sellByDate: Date(), calories: 90, temparature: 100)
+        chocoMilk = ChocolateMilk(brand: .namyang, volume: 200, price: 1000, productName: "초코에몽", manufacturedDay: Date(), sellByDate: Date(), calories: 100, temparature: 0, farmCode: "f1234", additive: "Chocolate")
+        strawberryMilk = StrawBerryMilk(brand: .seoul, volume: 180, price: 1200, productName: "딸기꿀단지", manufacturedDay: Date(), sellByDate: Date(), calories: 110, temparature: 0, farmCode: "f1243", additive: "Strawberry")
+        coke = Coke(brand: .coca, volume: 200, price: 1300, productName: "코카콜라제로", manufacturedDay: Date(), sellByDate: Date(), calories: 0, temparature: 0, zeroCalories: true, flavor: .Original)
+        sprite = Sprite(brand: .coca, volume: 340, price: 1800, productName: "스프라이트", manufacturedDay: Date(), sellByDate: Date(), calories: 80, temparature: 0, zeroCalories: false, container: .Bottle)
+        top = Top(brand: .maxim, volume: 180, price: 1500, productName: "TOP", manufacturedDay: Date(), sellByDate: Date(), calories: 100, temparature: 100, decaffeinated: false, dairyProducts: true)
+        cantata = Cantata(brand: .lotte, volume: 180, price: 1700, productName: "콘트라베이스블랙", manufacturedDay: Date(), sellByDate: Date(), calories: 10, temparature: 110, decaffeinated: true, dairyProducts: false)
     }
     
     

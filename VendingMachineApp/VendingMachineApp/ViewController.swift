c5c1c2e85cee7c8dca52ab439e11320104b9c17e
zzisun <jisunkim98@gmail.com>
2021-03-09 12:36:21 +0900

    Delete Staff class

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -8,18 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let vendingMachine = VendingMachine()
-    private let staff = Staff()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        staff.fill(vendingMachine: vendingMachine, beverage: Milk(brand: "빙그레", capacity: 240, price: 1400, name: "바나나맛우유", dateString: "20210225"))
-        staff.fill(vendingMachine: vendingMachine, beverage: Milk(brand: "빙그레", capacity: 240, price: 1400, name: "바나나맛우유", dateString: "20210226"))
-        staff.fill(vendingMachine: vendingMachine, beverage: Milk(brand: "코카콜라", capacity: 250, price: 1500, name: "코카콜라제로", dateString: "20210225"))
-        staff.fill(vendingMachine: vendingMachine, beverage: Milk(brand: "맥심", capacity: 275, price: 2200, name: "TOP아메리카노", dateString: "20210225"))
-
-        
-        print(staff.printStock(vendingMachine: vendingMachine))
+//        staff.fill(type: .ChocoMilk, expireDate: "20210331")
+//        staff.fill(type: .EnergyDrink, expireDate: "20210101")
+//
+//        staff.printAllStock {
+//            print($0)
+//        }
     }
 }
 

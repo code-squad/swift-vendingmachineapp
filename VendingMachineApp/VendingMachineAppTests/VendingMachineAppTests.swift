7241cd6d1a401e67c18e7ee73c8494862bb2b2e8
Song Lee <songlish233@gmail.com>
2021-03-09 16:16:45 +0900

    Test: 라면 자판기 Mock class 생성, 금액 관련 테스트 진행

--- VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -15,4 +15,44 @@ class VendingMachineAppTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
     }
+    
+    func test_is_자판기_돈_투입() throws {
+        let sinRamen = SinRamen()
+        let vendingMachine = VendingMachine(storage: RamenStorage(item: sinRamen),
+                                            dispensedList: RamenList(),
+                                            moneyBox: MoneyBox(),
+                                            beverageManager: RamenManager())
+        
+        vendingMachine.insert(money: 1000)
+        XCTAssertEqual(vendingMachine.moneyLeft(), 1000)
+    }
+    
+    func test_is_구매가능품목() throws {
+        let sinRamen: Shopable = SinRamen()
+        let sinRamenBlack: Shopable = SinRamenBlack()
+        let vendingMachine = VendingMachine(storage: RamenStorage(item: sinRamen),
+                                            dispensedList: RamenList(),
+                                            moneyBox: MoneyBox(),
+                                            beverageManager: RamenManager())
+        vendingMachine.insert(money: 2000)
+        
+        vendingMachine.addStock(of: sinRamenBlack)
+        
+        XCTAssertEqual(vendingMachine.affordables(),
+                       [ObjectIdentifier(type(of: sinRamen))])
+    }
+    
+    func test_is_구매후_잔액() throws {
+        let sinRamenBlack: Shopable = SinRamenBlack()
+        let vendingMachine = VendingMachine(storage: RamenStorage(item: sinRamenBlack),
+                                            dispensedList: RamenList(),
+                                            moneyBox: MoneyBox(),
+                                            beverageManager: RamenManager())
+        vendingMachine.insert(money: 5000)
+        
+        vendingMachine.buy(item: sinRamenBlack)
+        vendingMachine.buy(item: sinRamenBlack) //구매하지 못하고 넘어감
+        
+        XCTAssertEqual(vendingMachine.moneyLeft(), 2000)
+    }
 }

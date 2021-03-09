26f014a052ccc8fb7046a838ae474499a9cd6f23
Sonjh1306 <yale1306@gmail.com>
2021-03-09 14:14:10 +0900

    ViewController image, button, label 추가, BeverageFactory 생성

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,9 +9,48 @@ import UIKit
 
 class VendingMachineViewController: UIViewController {
     
+    @IBOutlet var beverageImages: [UIImageView]!
+    @IBOutlet weak var coffeeInventoryQuantity: UILabel!
+    @IBOutlet weak var milkInventoryQuantity: UILabel!
+    @IBOutlet weak var sodaInventoryQuantity: UILabel!
+    @IBOutlet weak var currentTotalMoney: UILabel!
+    private var vendingMachine = VendingMachine()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpImageView()
+    
+    }
+    
+    //MARK: 음료 이미지 라운드
+    func setUpImageView() {
+        for image in beverageImages {
+            image.layer.cornerRadius = 20
+        }
+    }
+    
+    
+    //MARK: 재고 추가 버튼
+    @IBAction func addCoffeeButton(_ sender: Any) {
         
     }
+    
+    @IBAction func addMilkButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addSodaButton(_ sender: Any) {
+        
+    }
+    
+    //MARK: 금액 추가 버튼
+    @IBAction func add1000Money(_ sender: Any) {
+        
+    }
+    @IBAction func add5000Money(_ sender: Any) {
+        
+    }
+    
 }
 

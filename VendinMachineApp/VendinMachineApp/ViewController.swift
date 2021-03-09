4ea89708d4cb9951bd7735f0b6729bc75a5d786c
BMO <b38238496@gmail.com>
2021-03-09 20:57:44 +0900

    이미지뷰 IBOutletCollection 추가 및 속성 설정
    음료 이미지 모서리 둥글게

--- VendinMachineApp/VendinMachineApp/ViewController.swift
+++ VendinMachineApp/VendinMachineApp/ViewController.swift
@@ -4,12 +4,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var beverageImages: [UIImageView]!
+    @IBOutlet var addBeverageStockButton: [UIButton]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        setUpBeverageImages()
     }
 
-
+    func setUpBeverageImages() {
+        beverageImages.forEach { imageView in
+            imageView.contentMode = .scaleToFill
+            imageView.layer.borderWidth = 2
+            imageView.layer.borderColor = UIColor.lightGray.cgColor
+            imageView.layer.cornerRadius = 10
+        }
+    }    
 }
+
 

22539fb49844b38e722c8dae9709733fdd98b730
hoonha choi <sosqsz2@naver.com>
2021-03-09 12:00:35 +0900

    appDelegate 아카이브 변경

--- VendingMachineApp/VendingMachineApp/Archive.swift
+++ VendingMachineApp/VendingMachineApp/Archive.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 struct ObjectArchive {
-    static func archive(with things: VendingMachine) -> Data {
+    static func save(with things: VendingMachine) -> Data {
         guard let archived = try? NSKeyedArchiver.archivedData(withRootObject: things, requiringSecureCoding: false) else { return Data() }
         return archived
     }
 
-    static func unarchive(with text: Data) -> VendingMachine? {
+    static func load(with text: Data) -> VendingMachine? {
         guard let object = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(text)
          else { return nil }
         return object as? VendingMachine
--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        changeBalanceLabel()
     }
     
     @IBAction func buyBeverageButtonTouched(_ sender: BeveragesButton) {

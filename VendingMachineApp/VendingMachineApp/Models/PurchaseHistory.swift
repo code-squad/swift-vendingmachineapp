007f74b2211d250029d83075edd23eff7e63322f
zzisun <jisunkim98@gmail.com>
2021-03-09 12:39:47 +0900

    Add: PurchaseHistory class

--- VendingMachineApp/VendingMachineApp/Models/PurchaseHistory.swift
+++ VendingMachineApp/VendingMachineApp/Models/PurchaseHistory.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
-class PurchaseLog {
+class PurchaseHistory {
+    private var history: [Beverage]
     
+    init() {
+        history = []
+    }
+    
+    func add(beverage: Beverage){
+        history.append(beverage)
+    }
+    
+//    func show() -> [Beverage] -> 이거보다 좋은 방법이 없을까..?
+    func show(_ handler: (Beverage) -> Void){
+        history.forEach { handler($0) }
+    }
 }

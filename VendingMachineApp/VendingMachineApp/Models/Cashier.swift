00c27516fcbf6df4f67208e9f401aa56a10cb951
zzisun <jisunkim98@gmail.com>
2021-03-09 12:38:18 +0900

    Add: Cashier in vendingMachine

--- VendingMachineApp/VendingMachineApp/Models/Cashier.swift
+++ VendingMachineApp/VendingMachineApp/Models/Cashier.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class Cashier {
+    private(set) var moneyDeposited: Int
+    
+    init() {
+        moneyDeposited = 0
+    }
+    
+    func insert(money: Int) {
+        moneyDeposited += money
+    }
+    
+    func getPurchasableBeverages(with stock: Stock) -> [Beverage] {
+        return stock.purchasableBeverages(with: moneyDeposited)
+    }
+    
+    func purchase(beverage: Beverage, stock: Stock) -> Beverage {
+        moneyDeposited -= beverage.price
+        return stock.remove(beverage: beverage)
+    }
+}

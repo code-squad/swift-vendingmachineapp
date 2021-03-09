9e4a13737055cd0120b4646939454c5f0f814263
zeke <yangsin1031@naver.com>
2021-03-09 18:00:27 +0900

    팩토리 패턴 수정
    기존 간편생성자에서 지정생성자로 변경

--- VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
+++ VendingMachineApp/VendingMachineApp/Model/BeverageFactory.swift
@@ -13,13 +13,13 @@ class BeverageFactory {
     func createBeverage(with beverageType: BeverageType) -> Productable {
         switch beverageType {
         case .cola:
-            return Cola()
+            return Cola(brand: "코카콜라", capacity: 500, price: 1000, name: "콜라", manufacturedAt: Date.input("20210302"), expirationAt: Date.input("20210310"), temperature: 5, glycemicIndex: 50, packageAttribute: Cola.Package.can)
         case .strawberrymilk:
-            return StrawBerryMilk()
+            return StrawBerryMilk(brand: "서울우유", capacity: 300, price: 800, name: "딸기우유", manufacturedAt: Date.input("20210302"), expirationAt: Date.input("20210310"), temperature: 5, pH: 6.5, color: StrawBerryMilk.Color.red)
         case .redbull:
-            return RedBull()
+            return RedBull(brand: "RedBull", capacity: 250, price: 1000, name: "레드불", manufacturedAt: Date.input("20210302"), expirationAt: Date.input("20210310"), temperature: 5, taurineContent: 1000, vitaminB: 2)
         case .top:
-            return TOP()
+            return TOP(brand: "동서식품", capacity: 380, price: 2000, name: "TOP", manufacturedAt: Date.input("20210222"), expirationAt: Date.input("20210301"), temperature: 65, caffeineContent: 94, flavor: TOP.Flavor.americano)
         }
     }
 }

0e6085aac1e679a204fc3db5310d7b1875949224
zzisun <jisunkim98@gmail.com>
2021-03-09 12:40:30 +0900

    Add: Stock class interface

--- VendingMachineApp/VendingMachineApp/Models/Stock.swift
+++ VendingMachineApp/VendingMachineApp/Models/Stock.swift
@@ -8,19 +8,50 @@
 import Foundation
 
 class Stock {
-    private var beverages: [Beverage]
+    private var stock: [Beverage: Int]
     
     init() {
-        self.beverages = []
+        self.stock = [:]
     }
     
     func add(beverage: Beverage) {
-        beverages.append(beverage)
+        stock[beverage] = (stock[beverage] ?? 0) + 1
     }
     
-    func show(handler: (Beverage) -> Void) {
-        self.beverages.forEach { 
-            handler($0)
+    // return에 handler적용해보기
+    func getAllStock() -> [Beverage: Int] {
+        return stock
+    }
+    
+    func purchasableBeverages(with money: Int) -> [Beverage] {
+        return stock.map{ $0.key }.filter{ $0.price <= money }
+    }
+    
+    func remove(beverage: Beverage) -> Beverage { // 음료가 없을 때, 어떻게 처리해줄까 -> 오류 검증 부분을 어디서 해주지! 일단 오류 없다고 작성
+        stock[beverage] = (stock[beverage] ?? 0) - 1
+        return beverage
+    }
+    
+    func expiredBeverages() -> [Beverage] {
+        var beverages = [Beverage]()
+        for (key, value) in stock {
+//            (0..<value).forEach{ beverages.append(key) }
+            for _ in 0..<value {
+                beverages.append(key)
+            }
+        }
+        
+        return beverages.filter{ beverage in
+            return !beverage.isValid(with: Date())
         }
     }
+    
+    func hotBeverages() -> [Beverage] {
+        return stock.filter{ (key, value) in
+            if let coffee = key as? Coffee {
+                return coffee.isHot()
+            }else { return false }
+        }.map{ $0.key }
+    }
+    
 }
--- VendingMachineApp/VendingMachineApp/Models/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/Models/VendingMachine.swift
@@ -9,17 +9,68 @@ import Foundation
 
 class VendingMachine {
     private let stock: Stock
+    private let cashier: Cashier
+    private let purchaseHistory: PurchaseHistory
     
     init() {
         stock = Stock()
+        cashier = Cashier()
+        purchaseHistory = PurchaseHistory()
     }
     
-    func add(beverage: Beverage){
-        stock.add(beverage: beverage)
+    func add(type: BeverageType, expireDate: String){
+        switch type {
+        case .BananaMilk:
+            stock.add(beverage: BananaMilk(expireDate: expireDate))
+        case .ChocoMilk:
+            stock.add(beverage: ChocoMilk(expireDate: expireDate))
+        case .Americano:
+            stock.add(beverage: Americano(expireDate: expireDate))
+        case .CaffeLatte:
+            stock.add(beverage: CaffeLatte(expireDate: expireDate))
+        case .Coke:
+            stock.add(beverage: Coke(expireDate: expireDate))
+        case .EnergyDrink:
+            stock.add(beverage: EnergyDrink(expireDate: expireDate))
+        }
     }
     
-    func showStock() {
-        stock.show { print($0) }
+    func insert(money: Int) {
+        cashier.insert(money: money)
+    }
+    
+    func purchase(beverage: Beverage) -> Beverage {
+        
+        purchaseHistory.add(beverage: beverage)
+        return cashier.purchase(beverage: beverage, stock: stock)
+    }
+    
+    func showAllStock() -> [Beverage:Int] {
+        return stock.getAllStock()
+    }
+    
+    func showPurchasableBeverages() -> [Beverage] {
+        return cashier.getPurchasableBeverages(with: stock)
+    }
+    
+    func showExpiredBeverages() -> [Beverage] {
+        return stock.expiredBeverages()
+    }
+    
+    func showHotBeverages() -> [Beverage] {
+        return stock.hotBeverages()
+    }
+    
+    func showPurchaseHistory() -> [Beverage] {
+        var history = [Beverage]()
+        purchaseHistory.show {
+            history.append($0)
+        }
+        return history
+    }
+    
+    func showBalance() -> Int {
+        return cashier.moneyDeposited
     }
 }
 

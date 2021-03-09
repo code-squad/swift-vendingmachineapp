e90845830ea9f8a70736a8421ef1245ac9a88ccb
hoonha choi <sosqsz2@naver.com>
2021-03-09 12:00:35 +0900

    appDelegate 아카이브 변경

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -10,6 +10,20 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
     var vendingMachine = VendingMachine()
+
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool  {
+        print("willFinishLaunchingWithOptions")
+        guard let vendingMachine = ObjectArchive.load(with: UserDefaults.standard.data(forKey: "vendingMachine") ?? Data()) else { return true }
+        self.vendingMachine = vendingMachine
+        return true
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication){
+        UserDefaults.standard.setValue(ObjectArchive.save(with: vendingMachine), forKey: "vendingMachine")
+        print("applicationDidEnterBackground")
+    }
+
 }
 
--- VendingMachineApp/VendingMachineApp/SceneDelegate.swift
+++ VendingMachineApp/VendingMachineApp/SceneDelegate.swift
@@ -5,29 +5,4 @@
 //  Created by HOONHA CHOI on 2021/02/23.
 //
 
-import UIKit
-import Foundation
-
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
-    var window: UIWindow?
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
-    
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        let _ = ObjectArchive.unarchive(with: UserDefaults.standard.data(forKey: "vendingMachine") ?? Data())
-    }
-    
-    func sceneWillResignActive(_ scene: UIScene) {}
-    
-    func sceneWillEnterForeground(_ scene: UIScene) {}
-    
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        let vendingMachine = appDelegate.vendingMachine
-        UserDefaults.standard.setValue(ObjectArchive.archive(with: vendingMachine), forKey: "vendingMachine")
-    }
-}
 

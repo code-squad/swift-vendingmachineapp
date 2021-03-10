74a849a5fa37710f0f65eb068f17474607dca418
zombietux <>
2021-03-10 11:06:13 +0900

    Button Sender index를 Beverage.Type으로 바꿔주는 코드 추가

---
+++ VendingMachineApp/VendingMachineApp/BeverageMapper.swift
@@ -0,0 +1,31 @@
+//
+//  BeverageMapper.swift
+//  VendingMachineApp
+//
+//  Created by zombietux on 2021/03/08.
+//
+
+import Foundation
+
+struct BeverageMapper {
+    private let map: [Int: Beverage.Type]
+    private let beverageTypes: [Beverage.Type] = [Banana.self, Strawberry.self,
+                                                  TOP.self, Cantata.self,
+                                                  Cola.self, Cider.self
+                                                  ]
+    
+    init() {
+        var map: [Int: Beverage.Type] = [:]
+        let tags = 0...self.beverageTypes.count
+    
+        for (tag, beverageType) in zip(tags, beverageTypes) {
+            map[tag] = beverageType
+        }
+        
+        self.map = map
+    }
+    
+    func mapping(by tag: Int) -> Beverage.Type? {
+        return map[tag]
+    }
+}
---
+++ VendingMachineApp/VendingMachineApp/Noti+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Noti+Ext.swift
+//  VendingMachineApp
+//
+//  Created by zombietux on 2021/03/09.
+//
+
+import Foundation

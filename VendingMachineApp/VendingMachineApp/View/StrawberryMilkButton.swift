bd72de492b7a3aae571e5fabf0444e9a92d2fc1d
zeke <yangsin1031@naver.com>
2021-03-10 17:03:08 +0900

    Button 클래스 수정
    메소드와 속성들 전부 삭제

--- VendingMachineApp/VendingMachineApp/View/StrawberryMilkButton.swift
+++ VendingMachineApp/VendingMachineApp/View/StrawberryMilkButton.swift
@@ -8,9 +8,5 @@
 import Foundation
 
 class StrawberryMilkButton: BeverageButtons {
-    var strawberryMilk = BeverageFactory.shared.createBeverage(with: .strawberrymilk)
-    
-    override func action(completion: (Beverage) -> Void) {
-        completion(strawberryMilk as! Beverage)
-    }
+
 }

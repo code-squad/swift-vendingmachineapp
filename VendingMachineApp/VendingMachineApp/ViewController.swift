7292517c8dacef460ba57a18c370c1d914871a8c
Lia <judykim257@gmail.com>
2021-03-11 06:33:11 +0900

    step3 - 재고 추가 버튼 삽질 중

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     private var vendingMachine: VendingMachine
     @IBOutlet weak var currentMoney: UILabel!
+    @IBOutlet var amountLabels: [UILabel]!
+    
     
     required init?(coder: NSCoder) {
         self.vendingMachine = VendingMachine()
@@ -29,5 +31,17 @@ class ViewController: UIViewController {
         
     }
     
+    
+    @IBAction func addStockButtonTouched(_ sender: UIButton) {
+        let name = [0: StrawberryMilk(), 1: ChocoMilk(), 2: BananaMilk(), 3: Coke(), 4: Cider(), 5: Fanta(), 6: Cantata(), 7: TOP(), 8: Georgia()]
+        let index = sender.tag
+        let drink = name[sender.tag] ?? TOP()
+        vendingMachine.add(item: drink)
+        amountLabels[index].text = ""
+        amountLabels.forEach { label in
+            
+        }
+        
+    }
 }
 

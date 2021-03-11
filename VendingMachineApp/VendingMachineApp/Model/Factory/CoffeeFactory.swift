d4c28ed830b715fafe5f113b4457e89986aadba0
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-03-11 15:57:05 +0900

    음료 카테고리별 팩토리로 나눴습니다.

---
+++ VendingMachineApp/VendingMachineApp/Model/Factory/CoffeeFactory.swift
@@ -0,0 +1,25 @@
+//
+//  CoffeeFactory.swift
+//  VendingMachineApp
+//
+//  Created by Jun Ho JANG on 2021/03/09.
+//
+
+import Foundation
+
+class CoffeeFactory: addBeverage {
+    
+    static func addBeverage<T>(type sku: T, manufacturedAt: Date, expiration: Date) -> Beverage? {
+        switch sku {
+        case is CantataAmericano.Type:
+            return CantataAmericano(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is CantataLatte.Type:
+            return CantataLatte(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is BlueBottle.Type:
+            return BlueBottle(manufacturedAt: manufacturedAt, expiration: expiration)
+        default:
+            return nil
+        }
+    }
+    
+}

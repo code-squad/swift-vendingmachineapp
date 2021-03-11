5c096fe9c5056c2b5faa8af703318b815d56f841
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-03-11 16:50:24 +0900

    nonoe

--- VendingMachineApp/VendingMachineApp/Model/Factory/CoffeeFactory.swift
+++ VendingMachineApp/VendingMachineApp/Model/Factory/CoffeeFactory.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-<<<<<<< HEAD
 
 class CoffeeFactory: addBeverage {
     
@@ -24,5 +23,3 @@ class CoffeeFactory: addBeverage {
     }
     
 }
-=======
->>>>>>> b3a143a0... 개별 음료 뷰를 하나의 스택뷰로 묶을 수 있는 객체를 만들었습니다.
--- VendingMachineApp/VendingMachineApp/Model/Factory/MilkFactory.swift
+++ VendingMachineApp/VendingMachineApp/Model/Factory/MilkFactory.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-<<<<<<< HEAD
 
 class MilkFactory: addBeverage {
     
@@ -24,5 +23,3 @@ class MilkFactory: addBeverage {
     }
     
 }
-=======
->>>>>>> b3a143a0... 개별 음료 뷰를 하나의 스택뷰로 묶을 수 있는 객체를 만들었습니다.

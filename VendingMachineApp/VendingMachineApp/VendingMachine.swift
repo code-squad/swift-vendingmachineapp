e9603daf269f6f36d811ba496855f066473679be
zombietux <>
2021-03-11 15:42:11 +0900

    vendingMachine속성 (set) 없애고 매서드를 통해 접근하는 방식으로 수정

--- VendingMachineApp/VendingMachineApp/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/VendingMachine.swift
@@ -7,24 +7,16 @@
 
 import Foundation
 
-class VendingMachine: NSObject, NSCoding {
-    private (set) var moneyManager: MoneyManagable
-    private (set) var inventoryManager: InventoryManagable
-    private (set) var purchaseHistoryManager: PurchaseHistoryManagable
+class VendingMachine: NSObject,VendingMachineManagable, NSCoding {
+    
+    private var moneyManager: MoneyManagable
+    private var inventoryManager: InventoryManagable
+    private var purchaseHistoryManager: PurchaseHistoryManagable
     
     init(moneyManager: MoneyManagable, inventoryManager: InventoryManagable, purchaseHistoryManager: PurchaseHistoryManagable) {
         self.moneyManager = moneyManager
         self.inventoryManager = inventoryManager
         self.purchaseHistoryManager = purchaseHistoryManager
-    }
-    
-    func purchaseBeverage(beverage: Beverage) {
-        guard beverage.isPurchasable(balance: moneyManager.readBalance()) else { return }
-        
-        if let purchased: Beverage = inventoryManager.removeBeverage(beverage) {
-            self.purchaseHistoryManager.addPurchased(purchased)
-            self.moneyManager.decreaseBalance(purchased.price)
-        }
     }
     
     func encode(with coder: NSCoder) {
@@ -37,5 +29,38 @@ class VendingMachine: NSObject, NSCoding {
         self.moneyManager = coder.decodeObject(forKey: "moneyManager") as! MoneyManagable
         self.inventoryManager = coder.decodeObject(forKey: "inventoryManager") as! InventoryManagable
         self.purchaseHistoryManager = coder.decodeObject(forKey: "purchaseHistoryManager") as! PurchaseHistoryManagable
+    }
+    
+    func readBalance() -> Int {
+        return moneyManager.readBalance()
+    }
+    
+    func increaseBalance(_ price: Int) {
+        moneyManager.increaseBalance(price)
+    }
+    
+    func tagToBeverageType(by tag: Int) -> Beverage.Type? {
+        return inventoryManager.tagToBeverageType(by: tag)
+    }
+    
+    func readInventoryCount(index: Int, allInventores: [ObjectIdentifier : [Beverage]]) -> Int {
+        return inventoryManager.readInventoryCount(index: index, allInventores: allInventores)
+    }
+    
+    func purchaseBeverage(beverage: Beverage) {
+        guard beverage.isPurchasable(balance: moneyManager.readBalance()) else { return }
+        
+        if let purchased: Beverage = inventoryManager.removeBeverage(beverage) {
+            self.purchaseHistoryManager.addPurchased(purchased)
+            self.moneyManager.decreaseBalance(purchased.price)
+        }
+    }
+    
+    func readInventores() -> [ObjectIdentifier : [Beverage]] {
+        inventoryManager.readInventores()
+    }
+    
+    func addBeverage(_ beverage: Beverage) {
+        inventoryManager.addBeverage(beverage)
     }
 }

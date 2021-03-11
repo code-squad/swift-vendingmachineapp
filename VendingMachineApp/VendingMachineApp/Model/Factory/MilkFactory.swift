19b7ddd9980e366f2e44286e90eae47f3e40e029
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-03-11 15:57:05 +0900

    음료 카테고리별 팩토리로 나눴습니다.

---
+++ VendingMachineApp/VendingMachineApp/Model/Factory/MilkFactory.swift
@@ -0,0 +1,25 @@
+//
+//  MilkFactory.swift
+//  VendingMachineApp
+//
+//  Created by Jun Ho JANG on 2021/03/09.
+//
+
+import Foundation
+
+class MilkFactory: addBeverage {
+    
+    static func addBeverage<T>(type sku: T, manufacturedAt: Date, expiration: Date) -> Beverage? {
+        switch sku {
+        case is SeoulSterilized.Type:
+            return SeoulSterilized(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is SeoulStrawberry.Type:
+            return SeoulStrawberry(manufacturedAt: manufacturedAt, expiration: expiration)
+        case is SeoulChoco.Type:
+            return SeoulChoco(manufacturedAt: manufacturedAt, expiration: expiration)
+        default:
+            return nil
+        }
+    }
+    
+}

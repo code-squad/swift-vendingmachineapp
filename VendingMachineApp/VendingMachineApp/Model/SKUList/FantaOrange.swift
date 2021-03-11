9136ecd32bf6836b1baaf979b04be34c19c0eec5
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-03-11 15:58:54 +0900

    none

--- VendingMachineApp/VendingMachineApp/Model/SKUList/FantaOrange.swift
+++ VendingMachineApp/VendingMachineApp/Model/SKUList/FantaOrange.swift
@@ -10,15 +10,15 @@ import Foundation
 
 class FantaOrange: Soda {
     
-    private var fruity: Bool
+    private var carbohydrateContent: Int
     
-    init(manufacturer: String, volume: Int, price: Int, subbrand: String, transparent: Bool, flavour: String, manufacturedAt: Date, expiration: Date, fruity: Bool, skuName: String) {
-        self.fruity = fruity
-        super.init(manufacturer: manufacturer, volume: volume, price: price, subbrand: subbrand, transparent: transparent, manufacturedAt: manufacturedAt, expiration: expiration, skuName: skuName)
+    init(manufacturer: String, volume: Int, price: Int, subbrand: String, sugarContent: Int, flavour: String, manufacturedAt: Date, expiration: Date, carbohydrateContent: Int, skuName: String) {
+        self.carbohydrateContent = carbohydrateContent
+        super.init(manufacturer: manufacturer, volume: volume, price: price, subbrand: subbrand, sugarContent: sugarContent, manufacturedAt: manufacturedAt, expiration: expiration, skuName: skuName)
     }
     
     convenience init(manufacturedAt: Date, expiration: Date) {
-        self.init(manufacturer: "CocaCola", volume: 355, price: 1300, subbrand: "환타 오렌지맛", transparent: false, flavour: "Orange", manufacturedAt: manufacturedAt, expiration: expiration, fruity: true, skuName: "환타 오렌지맛 355ml 낱개")
+        self.init(manufacturer: "CocaCola", volume: 355, price: 1300, subbrand: "환타 오렌지맛", sugarContent: 41, flavour: "Orange", manufacturedAt: manufacturedAt, expiration: expiration, carbohydrateContent: 39, skuName: "환타 오렌지맛 355ml 낱개")
     }
     
 }
--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -9,8 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+    private var vendingMachine = VendingMachine()
+    
+    @IBAction func insertCashOne(_ sender: Any) {
+        vendingMachine.addCash(cash: 1000)
+        updateTotalCash()
+    }
+    
+    @IBAction func insertCashFive(_ sender: Any) {
+        vendingMachine.addCash(cash: 5000)
+        updateTotalCash()
+    }
+    
+    @IBOutlet weak var totalCash: UILabel!
+    
+    private func updateTotalCash() {
+        totalCash.text = "\(vendingMachine.showAccount())"
+    }
+    
+    private func addMainStackView() {
+        let skuStackView = SKUStackView()
+        let mainStackView = skuStackView.makeMainStackView()
+        self.view.addSubview(mainStackView)
+        mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive = true
+        mainStackView.leadingAnchor.constraint(equalTo:view.safeAreaLayoutGuide.leadingAnchor, constant: 150).isActive = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addMainStackView()
+        updateTotalCash()
+    }
 }
+
+

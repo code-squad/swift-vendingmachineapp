76cf6ccb8197623177c188416695ee737bb8871a
BMO <b38238496@gmail.com>
2021-03-12 19:56:39 +0900

    VendingMachineElements 수정
    setUp기능을 모아놓은 setUpAll 메소드 추가
    VendingMachine의 Beverages나 cashBox를 얻기 위해
    VendingMachine 자체를 가져 오던 것을 필요한 것만 가져오는 것으로 수정

--- VendinMachineApp/VendinMachineApp/Model/VendingMachineElements.swift
+++ VendinMachineApp/VendinMachineApp/Model/VendingMachineElements.swift
@@ -13,29 +13,50 @@ class VendingMachineElements {
         self.beverageList = [BananaMilk.self, ChocoMilk.self, Coke.self, Cider.self, Americano.self, Latte.self]
     }
     
-    func pressedAddBeverage(button: UIButton) -> Beverage.Type? {
-        return self.addBeverageButtons[button]
-    }
-    
     func pressedInsertCash(button: UIButton) -> Int? {
         return self.insertCashButtons[button]
     }
     
-    func setUpAddBeverage(buttons: [UIButton]) {
-        for (button, beverageType) in zip(buttons, beverageList) {
+    func pressedAddBeverage(button: UIButton) -> Beverage.Type? {
+        return self.addBeverageButtons[button]
+    }
+    
+    func setUpBeverageImages(collection: [UIImageView]) {
+        collection.forEach { imageView in
+            imageView.contentMode = .scaleToFill
+            imageView.layer.borderWidth = 2
+            imageView.layer.borderColor = UIColor.lightGray.cgColor
+            imageView.layer.cornerRadius = 10
+        }
+    }
+    
+    func setUpAddBeverageButtons(collection: [UIButton]) {
+        for (button, beverageType) in zip(collection, beverageList) {
             addBeverageButtons.updateValue(beverageType, forKey: button)
         }
     }
     
-    func setUpInsertCash(buttons: [UIButton]) {
-        for (button, money) in zip(buttons, VendingMachineMoney.allCases) {
+    func setUpInsertCashButtons(collection: [UIButton]) {
+        for (button, money) in zip(collection, VendingMachineMoney.allCases) {
             insertCashButtons.updateValue(money.rawValue, forKey: button)
         }
     }
     
-    func updateBeverageStock(labels: [UILabel], in vendingMachine: VendingMachine) {
+    func updateCurrentCash(label: UILabel, cashBox: Int) {
+        label.text = "\(cashBox)"
+    }
+    
+    func updateBeverageStock(labels: [UILabel], beverageStock: Beverages) {
         for (beverageLabel, beverageType) in zip(labels, beverageList) {
-            beverageLabel.text = "\(vendingMachine.beverageCount(beverageType: beverageType))"
+            beverageLabel.text = "\(beverageStock.beverageCount(elementType: beverageType))"
         }
+    }
+    
+    func setUpAll(images: [UIImageView], beverageButtons: [UIButton], beverageStockLabels: [UILabel], beverageStock: Beverages, cashButtons: [UIButton], cashLabel: UILabel, cashBox: Int) {
+        self.setUpBeverageImages(collection: images)
+        self.setUpInsertCashButtons(collection: cashButtons)
+        self.setUpAddBeverageButtons(collection: beverageButtons)
+        self.updateCurrentCash(label: cashLabel, cashBox: cashBox)
+        self.updateBeverageStock(labels: beverageStockLabels, beverageStock: beverageStock)
     }
 }

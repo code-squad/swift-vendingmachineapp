6e33d39fb8533231fd7ceb7c215645ee850a3dd2
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-12 14:18:46 +0900

    Move machine instance toAppDelegate and connect it with ViewController

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -9,8 +9,10 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    let machine = Machine()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         return true
     }
 

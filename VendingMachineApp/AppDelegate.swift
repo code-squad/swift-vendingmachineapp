c2b00aa10e2728e8eb09010ceb06d0cab7b000e4
ellyheetov <yaaajcm@naver.com>
2021-03-12 18:52:12 +0900

    Update AppDelegate : initialize vendingmachine in WillFinishLaunchingWithOptions

--- VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/AppDelegate.swift
@@ -12,21 +12,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var vendingMachine : VendingMachine?
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
-    {
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         vendingMachine = DataManager.load()
         return true
     }
-
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        return true
+    }
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         DataManager.save(data: vendingMachine!)
     }

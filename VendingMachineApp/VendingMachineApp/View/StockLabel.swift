95514760b22a44963adc955b958449469ecc7024
dahun-lee-daji <ssss6596@gmail.com>
2021-03-12 19:47:29 +0900

    재고, 잔액 추가 event 구현
    VC 내부 AddStockButton event명 변경 및 test code 삭제
    NSNotification.Name extention하여 name 추가.
    BalanceButton 내부 delegate test code 삭제.
    잔액 및 재고 label에 observer pattern 적용

--- VendingMachineApp/VendingMachineApp/View/StockLabel.swift
+++ VendingMachineApp/VendingMachineApp/View/StockLabel.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 class StockLabel: UILabel {
+    var beverageType : Drink
+        
+    init(frame: CGRect, type: Drink) {
+        beverageType = type
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder, type: Drink) {
+        beverageType = Drink()
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     func setting(count : Int) {
         self.text = "\(count)개"
     }

2c5e3b8b92d9501afe16d89edf0620f222a1d078
1song2 <1song2918@gmail.com>
2021-03-13 16:59:04 +0900

    Move instance of VendingMachine to SceneDelegate

--- VendingMachineApp/VendingMachineApp/SceneDelegate.swift
+++ VendingMachineApp/VendingMachineApp/SceneDelegate.swift
@@ -8,8 +8,15 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    var vendingMachine: VendingMachine!
+    
+    private func initialSetupVendingMachine() {
+        vendingMachine = VendingMachine(numberOfSlots: 5)
+        let beverageFactoryList: [BeverageFactory] = [DenmarkStrawberryMilkFactory(), MaeilChocolateMilkFactory(), ZeroSugarCokeFactory(), GeorgiaMaxFactory(), RedBullFactory()]
+        beverageFactoryList.forEach { factory in
+            vendingMachine.bulkInsert(itemFrom: factory, quantity: 5, manufactured: Date().formattedDate(from: "20210222"), expiredAfter: Date().formattedDate(from: "20210302"))
+        }
+    }
 }
-

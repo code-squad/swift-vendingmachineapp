8ae93ca2bbb0bb279053bf6a892126769c26a6c5
1song2 <1song2918@gmail.com>
2021-03-13 17:31:24 +0900

    Change 'Order' to class and let it conform to protocols

--- VendingMachineApp/VendingMachineApp/Models/Order.swift
+++ VendingMachineApp/VendingMachineApp/Models/Order.swift
@@ -7,12 +7,22 @@
 
 import Foundation
 
-struct Order {
+class Order: NSObject, NSCoding {
     private let purchased: Date
     private let item: Beverage
     
     init(purchased: Date, item: Beverage) {
         self.purchased = purchased
         self.item = item
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(purchased, forKey: "purchased")
+        coder.encode(item, forKey: "item")
+    }
+    
+    required init?(coder: NSCoder) {
+        self.purchased = coder.decodeObject(forKey: "purchased") as! Date
+        self.item = coder.decodeObject(forKey: "item") as! Beverage
     }
 }

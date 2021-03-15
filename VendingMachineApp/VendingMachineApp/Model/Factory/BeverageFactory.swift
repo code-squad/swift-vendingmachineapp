66dfb8b72e90f41e708fede05ef1b16421486635
zeke <yangsin1031@naver.com>
2021-03-15 16:48:19 +0900

    팩토리 메소드 수정
    팩토리 메소드를 편리하게 사용하기 위해 switch문을 사용하도록 수정

--- VendingMachineApp/VendingMachineApp/Model/Factory/BeverageFactory.swift
+++ VendingMachineApp/VendingMachineApp/Model/Factory/BeverageFactory.swift
@@ -8,8 +8,19 @@
 import Foundation
 
 class BeverageFactory {
-    static func releaseBeverage(with factory: Produceable) -> Beverage {
-        return factory.createBeverage()
+    static func releaseBeverage(with factory: Beverage.Type) -> Beverage? {
+        switch factory {
+        case is StrawBerryMilk.Type:
+            return StrawberryMilkFactory.createBeverage()
+        case is RedBull.Type:
+            return RedBullFactory.createBeverage()
+        case is TOP.Type:
+            return TOPFactory.createBeverage()
+        case is Cola.Type:
+            return ColaFactory.createBeverage()
+        default:
+            return nil
+        }
     }
 }
 
--- VendingMachineApp/VendingMachineApp/Model/Factory/RedBullFactory.swift
+++ VendingMachineApp/VendingMachineApp/Model/Factory/RedBullFactory.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-class RedBullFactory: Produceable {
-    func createBeverage() -> Beverage {
+class RedBullFactory {
+    static func createBeverage() -> Beverage {
         return RedBull(brand: "레드불", capacity: 500, price: 1200, name: "레드불", manufacturedAt: Date.input("20210301"), expirationAt: Date.input("20210310"), temperature: 5, taurineContent: 2000, vitaminB: 5)
     }
 }

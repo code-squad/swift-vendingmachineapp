50a01a979f3ac9e6bfab6f896d5f3ed96a82e279
BMO <b38238496@gmail.com>
2021-03-15 17:04:07 +0900

    Country프로토콜 수정, CountryOfOrigin 클래스 생성
    Country 프로토콜을 채택하는 하위 클래스를 하나의 타입으로 변경

--- VendinMachineApp/VendinMachineApp/Model/Country.swift
+++ VendinMachineApp/VendinMachineApp/Model/Country.swift
@@ -2,50 +2,25 @@
 import Foundation
 
 protocol Country : CustomStringConvertible {
-    func madeIn() -> Country
+    func nameOfCountry() -> String
 }
 
-class Korea : Country {
+class CountryOfOrigin : NSObject, Country, NSCoding{
     private var name: String
-    var description: String {
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    required init?(coder: NSCoder) {
+        self.name = coder.decodeObject(forKey: "countryOfOrigin") as! String
+    }
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(self.name, forKey: "countryOfOrigin")
+    }
+    
+    func nameOfCountry() -> String {
         return self.name
-    }
-    
-    init() {
-        self.name = "한국"
-    }
-    
-    func madeIn() -> Country {
-        return self
-    }
-}
-
-class Ghana : Country {
-    private var name: String
-    var description: String {
-        return self.name
-    }
-    
-    init() {
-        self.name = "가나"
-    }
-    
-    func madeIn() -> Country {
-        return self
-    }
-}
-
-class Ethiopia : Country {
-    private var name: String
-    var description: String {
-        return self.name
-    }
-    
-    init() {
-        self.name = "에티오피아"
-    }
-    
-    func madeIn() -> Country {
-        return self
     }
 }

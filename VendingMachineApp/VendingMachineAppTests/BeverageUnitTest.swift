8ec2792f44c9d039a6f97e224a14b26f8d27c5fb
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-03-15 16:57:47 +0900

    Reactivate methods and test modules

--- VendingMachineApp/VendingMachineAppTests/BeverageUnitTest.swift
+++ VendingMachineApp/VendingMachineAppTests/BeverageUnitTest.swift
@@ -1,129 +1,60 @@
-////
-////  BeverageUnitTest.swift
-////  VendingMachineAppTests
-////
-////  Created by 조중윤 on 2021/02/28.
-////
-////You need to import your project first
 //
-//import XCTest
+//  BeverageUnitTest.swift
+//  VendingMachineAppTests
 //
-////FIXME
-//class BeverageUnitTest: XCTestCase {
-//    //And call the desirable module to be tested
-//    var expensiveAndOldBeverage: Beverage!
-//    var expensiveButFreshBeverage: Beverage!
-//    var cheapAndFreshAndDomesticMilk: Milk!
-//    var expensiveAndOldOverseasBananaMilkWithFoodColoring: BananaMilk!
-//    var cheapButOldDomesticStrawberryMilkWithoutFoodColoring: StrawBerryMilk!
-//    var cheapAndFreshZeroCalorieSoda: Soda!
-//    var expensiveAndOldZeroCalorieCola: Cola!
-//    var cheapAndFreshFantaWithHighCaffeineAndFoodColoring: Fanta!
-//    var cheapAndFreshFantaWithHighCaffeineAndNoFoodColoring: Fanta!
-//    var expensiveButFreshCoffee: Coffee!
-//    var cheapAndFreshHotAmericanoWithLowCaffeine: Americano!
-//    var expensiveAndOldHotCaffeeLatteWithHighCaffeine: CaffeLatte!
-//    var expensiveAndOldColdCaffeeLatteWithHighCaffeine: CaffeLatte!
-//    
-//    override func setUp() {
-//        super.setUp()
+//  Created by 조중윤 on 2021/02/28.
 //
-//        // 5000 and above considerd expensive
-////        expensiveAndOldBeverage = BeverageFactory.makeBeverage(price: .high, packageTime: .old)
-////        expensiveButFreshBeverage = BeverageFactory.makeBeverage(price: .high, packageTime: .fresh)
-////        cheapAndFreshAndDomesticMilk = BeverageFactory.makeMilk(price: .low, packageTime: .fresh, location: .domestic)
-////        expensiveAndOldOverseasBananaMilkWithFoodColoring = BeverageFactory.makeBananaMilk(price: .high, packageTime: .old, location: .overseas, bananaFarm: .overseas, foodColoring: .foodColoring)
-////        cheapButOldDomesticStrawberryMilkWithoutFoodColoring = BeverageFactory.makeStrawBerryMilk(price: .low, packageTime: .old, location: .domestic, strawBerryFarm: .domestic, foodColoring: .noFoodColoring)
-////        cheapAndFreshZeroCalorieSoda = BeverageFactory.makeSoda(price: .low, packageTime: .fresh, calories: 0)
-////        expensiveAndOldZeroCalorieCola = BeverageFactory.makeCola(price: .high, packageTime: .old)
-////        cheapAndFreshFantaWithHighCaffeineAndFoodColoring = BeverageFactory.makeFanta(price: .low, packageTime: .fresh, calories: 0, foodColoring: .foodColoring, caffeine: .high)
-////        cheapAndFreshFantaWithHighCaffeineAndNoFoodColoring = BeverageFactory.makeFanta(price: .low, packageTime: .fresh, calories: 0, foodColoring: .noFoodColoring, caffeine: .high)
-////        expensiveButFreshCoffee = BeverageFactory.makeCoffee(price: .high, packageTime: .fresh)
-////        cheapAndFreshHotAmericanoWithLowCaffeine = BeverageFactory.makeAmericano(price: .low, packageTime: .fresh, caffeine: .low, temperature: 50)
-////        expensiveAndOldHotCaffeeLatteWithHighCaffeine = BeverageFactory.makeCaffeLatte(price: .high, packageTime: .old, caffeine: .high, temperature: 50)
-////        expensiveAndOldColdCaffeeLatteWithHighCaffeine = BeverageFactory.makeCaffeLatte(price: .high, packageTime: .old, caffeine: .high, temperature: 50)
-//    }
-//    
-//    override func tearDown() {
-//        expensiveAndOldBeverage = nil
-//        expensiveButFreshBeverage = nil
-//        cheapAndFreshAndDomesticMilk = nil
-//        expensiveAndOldOverseasBananaMilkWithFoodColoring = nil
-//        cheapButOldDomesticStrawberryMilkWithoutFoodColoring = nil
-//        cheapAndFreshZeroCalorieSoda = nil
-//        expensiveAndOldZeroCalorieCola = nil
-//        cheapAndFreshFantaWithHighCaffeineAndFoodColoring = nil
-//        expensiveButFreshCoffee = nil
-//        cheapAndFreshHotAmericanoWithLowCaffeine = nil
-//        expensiveAndOldHotCaffeeLatteWithHighCaffeine = nil
-//        expensiveAndOldColdCaffeeLatteWithHighCaffeine = nil
-//        super.tearDown()
-//    }
-//    
-//    func test_cheap_beverages_are_purchasable() {
-//        let highestPriceAmongCheapBeverage: Int = 4999
-//        let zeroBudget: Int = 0
-//        XCTAssertTrue(cheapAndFreshAndDomesticMilk.isPurchasable(with: highestPriceAmongCheapBeverage))
-//        XCTAssertTrue(cheapButOldDomesticStrawberryMilkWithoutFoodColoring.isPurchasable(with: highestPriceAmongCheapBeverage))
-//        XCTAssertTrue(cheapAndFreshZeroCalorieSoda.isPurchasable(with: highestPriceAmongCheapBeverage))
-//        XCTAssertTrue(cheapAndFreshFantaWithHighCaffeineAndFoodColoring.isPurchasable(with: highestPriceAmongCheapBeverage))
-//        XCTAssertTrue(cheapAndFreshHotAmericanoWithLowCaffeine.isPurchasable(with: highestPriceAmongCheapBeverage))
-//        XCTAssertFalse(cheapAndFreshHotAmericanoWithLowCaffeine.isPurchasable(with: zeroBudget))
-//    }
-//    
-//    func test_expensive_beverages_are_not_purchasable() {
-//        let highestPriceAmongCheapBeverage: Int = 4999
-//        let hugeAmountOfMoney: Int = 999_999
-//        XCTAssertFalse(expensiveAndOldBeverage.isPurchasable(with: highestPriceAmongCheapBeverage))
-//        XCTAssertFalse(expensiveButFreshBeverage.isPurchasable(with: highestPriceAmongCheapBeverage))
-//        XCTAssertTrue(expensiveAndOldHotCaffeeLatteWithHighCaffeine.isPurchasable(with: hugeAmountOfMoney))
-//    }
-//    
-//    
-//    func test_old_beverages_are_expired() {
-//        XCTAssertTrue(expensiveAndOldBeverage.isExpired())
-//        XCTAssertTrue(expensiveAndOldOverseasBananaMilkWithFoodColoring.isExpired())
-//        XCTAssertTrue(cheapButOldDomesticStrawberryMilkWithoutFoodColoring.isExpired())
-//        XCTAssertTrue(expensiveAndOldZeroCalorieCola.isExpired())
-//        XCTAssertTrue(expensiveAndOldHotCaffeeLatteWithHighCaffeine.isExpired())
-//    }
-//    
-//    func test_fresh_beverages_are_not_expired() {
-//        XCTAssertFalse(cheapAndFreshZeroCalorieSoda.isExpired())
-//        XCTAssertFalse(expensiveButFreshBeverage.isExpired())
-//        XCTAssertFalse(expensiveButFreshCoffee.isExpired())
-//        XCTAssertFalse(cheapAndFreshAndDomesticMilk.isExpired())
-//    }
-//    
-//    func test_hot_beverages_are_hot() {
-//        XCTAssertTrue(cheapAndFreshHotAmericanoWithLowCaffeine.isHot(temperature: 40))
-//        XCTAssertTrue(expensiveAndOldHotCaffeeLatteWithHighCaffeine.isHot(temperature: 40))
-//    }
-//    
-//    func test_normal_beverages_are_not_hot() {
-//        XCTAssertFalse(expensiveAndOldColdCaffeeLatteWithHighCaffeine.isHot(temperature: 100))
-//    }
-//    
-//    func test_highCaffeine_beverages_contain_highCaffein() {
-//        XCTAssertTrue(expensiveAndOldColdCaffeeLatteWithHighCaffeine.highCaffeine(caffeine: 100))
-//        XCTAssertTrue(expensiveAndOldHotCaffeeLatteWithHighCaffeine.highCaffeine(caffeine: 100))
-//        XCTAssertTrue(cheapAndFreshFantaWithHighCaffeineAndFoodColoring.highCaffeine(caffeine: 100))
-//    }
-//    
-//    func test_beverages_with_highCaffeine_and_foodColoring_are_bad_for_body() {
-//        XCTAssertTrue(cheapAndFreshFantaWithHighCaffeineAndFoodColoring.isBadForBody(caffeine: 100))
-//    }
-//    
-//    func test_beverages_with_highCaffeine_and_noFoodColoring_are_not_bad_for_body() {
-//        XCTAssertFalse(cheapAndFreshFantaWithHighCaffeineAndNoFoodColoring.isBadForBody(caffeine: 100))
-//    }
-//    
-//    
-//    func test_hot_beverages_with_highCaffeine_are_energyBoosting() {
-//        XCTAssertTrue(expensiveAndOldHotCaffeeLatteWithHighCaffeine.isEnergyBoosting(caffeine: 100, temperature: 40))
-//    }
-//    
-//    func test_hot_but_lowCaffeine_beverages_are_not_energyBoosting() {
-//        XCTAssertFalse(cheapAndFreshHotAmericanoWithLowCaffeine.isEnergyBoosting(caffeine: 100, temperature: 40))
-//    }
-//}
+import XCTest
+
+class BeverageUnitTest: XCTestCase {
+    var someBeverage: Beverage!
+    
+    override func setUp() {
+        super.setUp()
+        someBeverage = BeverageFactory.makeBeverageWithType(brand: "동아제약", price: .high, name: "박카스", packageTime: .fresh, beverageType: Beverage.self)
+    }
+    
+    override func tearDown() {
+        someBeverage = nil
+        super.tearDown()
+    }
+    
+    func test_cheap_beverages_are_purchasable() {
+    }
+    
+    func test_expensive_beverages_are_not_purchasable() {
+        let highestPriceAmongCheapBeverage: Int = 4999
+        let hugeAmountOfMoney: Int = 999_999
+        XCTAssertFalse(someBeverage.isPurchasable(with: highestPriceAmongCheapBeverage))
+        XCTAssertTrue(someBeverage.isPurchasable(with: hugeAmountOfMoney))
+    }
+    
+    
+    func test_old_beverages_are_expired() {
+    }
+    
+    func test_fresh_beverages_are_not_expired() {
+        XCTAssertFalse(someBeverage.isExpired(on: Date()))
+    }
+    
+    func test_hot_beverages_are_hot() {
+    }
+    
+    func test_normal_beverages_are_not_hot() {
+    }
+    
+    func test_highCaffeine_beverages_contain_highCaffein() {
+    }
+    
+    func test_beverages_with_highCaffeine_and_foodColoring_are_bad_for_body() {
+    }
+    
+    func test_beverages_with_highCaffeine_and_noFoodColoring_are_not_bad_for_body() {
+    }
+    
+    func test_hot_beverages_with_highCaffeine_are_energyBoosting() {
+    }
+    
+    func test_hot_but_lowCaffeine_beverages_are_not_energyBoosting() {
+    }
+}
--- VendingMachineApp/VendingMachineAppTests/MachineUnitTest.swift
+++ VendingMachineApp/VendingMachineAppTests/MachineUnitTest.swift
@@ -1,124 +1,79 @@
-////
-////  MachineUnitTest.swift
-////  VendingMachineAppTests
-////
-////  Created by 조중윤 on 2021/03/02.
-////
 //
-//import XCTest
+//  MachineUnitTest.swift
+//  VendingMachineAppTests
 //
-////FIXME
-//class MachineUnitTest: XCTestCase {
-//    var testMachine: Machine!
-//    var expensiveAndOldBeverage: Beverage!
-//    var expensiveButFreshBeverage: Beverage!
-//    var cheapAndFreshAndDomesticMilk: Milk!
-//    var expensiveAndOldOverseasBananaMilkWithFoodColoring: BananaMilk!
-//    var cheapButOldDomesticStrawberryMilkWithoutFoodColoring: StrawBerryMilk!
-//    var cheapAndFreshZeroCalorieSoda: Soda!
-//    var expensiveAndOldZeroCalorieCola: Cola!
-//    var cheapAndFreshFantaWithHighCaffeineAndFoodColoring: Fanta!
-//    var cheapAndFreshFantaWithHighCaffeineAndNoFoodColoring: Fanta!
-//    var expensiveButFreshCoffee: Coffee!
-//    var cheapAndFreshHotAmericanoWithLowCaffeine: Americano!
-//    var expensiveAndOldHotCaffeeLatteWithHighCaffeine: CaffeLatte!
-//    var expensiveAndOldColdCaffeeLatteWithHighCaffeine: CaffeLatte!
-//    var purchaseHistory: [Beverage]!
-//    
-//    override func setUpWithError() throws {
-//        testMachine = Machine()
-//        
-////        expensiveButFreshBeverage = BeverageFactory.makeBeverage(price: .high, packageTime: .fresh)
-////        cheapAndFreshAndDomesticMilk = BeverageFactory.makeMilk(price: .low, packageTime: .fresh, location: .domestic)
-////        expensiveAndOldOverseasBananaMilkWithFoodColoring = BeverageFactory.makeBananaMilk(price: .high, packageTime: .old, location: .overseas, bananaFarm: .overseas, foodColoring: .foodColoring)
-////        cheapButOldDomesticStrawberryMilkWithoutFoodColoring = BeverageFactory.makeStrawBerryMilk(price: .low, packageTime: .old, location: .domestic, strawBerryFarm: .domestic, foodColoring: .noFoodColoring)
-////        cheapAndFreshZeroCalorieSoda = BeverageFactory.makeSoda(price: .low, packageTime: .fresh, calories: 0)
-////        expensiveAndOldZeroCalorieCola = BeverageFactory.makeCola(price: .high, packageTime: .old)
-////        cheapAndFreshFantaWithHighCaffeineAndFoodColoring = BeverageFactory.makeFanta(price: .low, packageTime: .fresh, calories: 0, foodColoring: .foodColoring, caffeine: .high)
-////        cheapAndFreshFantaWithHighCaffeineAndNoFoodColoring = BeverageFactory.makeFanta(price: .low, packageTime: .fresh, calories: 0, foodColoring: .noFoodColoring, caffeine: .high)
-////        expensiveButFreshCoffee = BeverageFactory.makeCoffee(price: .high, packageTime: .fresh)
-////        cheapAndFreshHotAmericanoWithLowCaffeine = BeverageFactory.makeAmericano(price: .low, packageTime: .fresh, caffeine: .low, temperature: 40)
-////        expensiveAndOldHotCaffeeLatteWithHighCaffeine = BeverageFactory.makeCaffeLatte(price: .high, packageTime: .old, caffeine: .high, temperature: 40)
-////        expensiveAndOldColdCaffeeLatteWithHighCaffeine = BeverageFactory.makeCaffeLatte(price: .high, packageTime: .old, caffeine: .high, temperature: 40)
-//    }
+//  Created by 조중윤 on 2021/03/02.
 //
-//    override func tearDownWithError() throws {
-//        testMachine = nil
-//    }
-//
-//    func test_can_initialize_machine() {
-//        let machine = Machine()
-//    }
-//    
-//    func test_add_4_different_beverages_in_stock() {
-//        testMachine.addStock(beverage: expensiveAndOldBeverage, amount: 10)
-//        testMachine.addStock(beverage: cheapAndFreshAndDomesticMilk, amount: 20)
-//        testMachine.addStock(beverage: cheapAndFreshFantaWithHighCaffeineAndFoodColoring, amount: 30)
-//        testMachine.addStock(beverage: expensiveAndOldHotCaffeeLatteWithHighCaffeine, amount: 40)
-//        testMachine.addStock(beverage: expensiveAndOldBeverage, amount: 100)
-//        
-//        XCTAssertEqual(testMachine.checkStock()[expensiveAndOldBeverage], 110)
-//        XCTAssertEqual(testMachine.checkStock()[cheapAndFreshAndDomesticMilk], 20)
-//        XCTAssertEqual(testMachine.checkStock()[cheapAndFreshFantaWithHighCaffeineAndFoodColoring], 30)
-//        XCTAssertEqual(testMachine.checkStock()[expensiveAndOldHotCaffeeLatteWithHighCaffeine], 40)
-//    }
-//    
-//    func test_customer_insert_money() {
-//        let customerBudget = 10_000
-//        testMachine.receiveMoney(amount: customerBudget)
-//        XCTAssertEqual(testMachine.showInsertedCashBalance(), customerBudget)
-//    }
-//    
-//    func test_show_purchasables() {
-//        testMachine.addStock(beverage: expensiveAndOldBeverage, amount: 10)
-//        testMachine.addStock(beverage: cheapAndFreshAndDomesticMilk, amount: 20)
-//        let purchasables = testMachine.showPurchasables(with: 4999)
-//        XCTAssertEqual(purchasables.count, 1)
-//        XCTAssertEqual(purchasables[cheapAndFreshAndDomesticMilk], 20)
-//    }
-//    
-//    func test_show_expried() {
-//        testMachine.addStock(beverage: expensiveAndOldBeverage, amount: 10)
-//        testMachine.addStock(beverage: cheapAndFreshAndDomesticMilk, amount: 20)
-//        let expired = testMachine.showExpired()
-//        XCTAssertEqual(expired.count, 1)
-//        XCTAssertEqual(expired[expensiveAndOldBeverage], 10)
-//    }
-//    
-//    func test_purchase_beverage_successfully_with_enough_money() {
-//        testMachine.receiveMoney(amount: 1_000_000)
-//        testMachine.addStock(beverage: expensiveAndOldBeverage, amount: 10)
-//        let itemPrice = expensiveAndOldBeverage.showPrice()
-//        testMachine.purchaseBeverage(beverage: expensiveAndOldBeverage)
-//        testMachine.purchaseBeverage(beverage: expensiveAndOldBeverage)
-//        XCTAssertEqual(testMachine.checkStock()[expensiveAndOldBeverage], 8)
-//        XCTAssertEqual(testMachine.showPurchaseHistory(), [expensiveAndOldBeverage,expensiveAndOldBeverage])
-//    }
-//    
-//    func test_fail_purchase_beverage_with_not_enough_money() {
-//        testMachine.receiveMoney(amount: 10)
-//        testMachine.addStock(beverage: expensiveAndOldBeverage, amount: 10)
-//        testMachine.purchaseBeverage(beverage: expensiveAndOldBeverage)
-//        XCTAssertEqual(testMachine.checkStock()[expensiveAndOldBeverage], 10)
-//    }
-//    
-//    func test_return_change_to_customer_when_transaction_stop_button_pressed() {
-//        let insertedMoney = 100_000
-//        testMachine.receiveMoney(amount: insertedMoney)
-//        testMachine.addStock(beverage: expensiveAndOldBeverage, amount: 10)
-//        testMachine.purchaseBeverage(beverage: expensiveAndOldBeverage)
-//        let itemPrice = expensiveAndOldBeverage.showPrice()
-//        XCTAssertEqual(testMachine.transactionStopButtonPressed(), insertedMoney - itemPrice)
-//    }
-//    
-//    func test_save_purchase_history() {
-//        let insertedMoney = 100_000
-//        testMachine.receiveMoney(amount: insertedMoney)
-//        testMachine.addStock(beverage: expensiveAndOldBeverage, amount: 10)
-//        testMachine.purchaseBeverage(beverage: expensiveAndOldBeverage)
-//        testMachine.purchaseBeverage(beverage: expensiveAndOldZeroCalorieCola)
-//        XCTAssertEqual(testMachine.showPurchaseHistory(), [expensiveAndOldBeverage])
-//        _ = testMachine.transactionStopButtonPressed()
-//        XCTAssertEqual(testMachine.showPurchaseHistory(), [])
-//    }
-//}
+
+import XCTest
+
+class MachineUnitTest: XCTestCase {
+    var testMachine: Machine!
+    var someBeverage: Beverage!
+    var purchaseHistory: [Beverage]!
+    
+    override func setUpWithError() throws {
+        testMachine = Machine()
+        someBeverage = BeverageFactory.makeBeverageWithType(brand: "동아제약", price: .high, name: "박카스", packageTime: .fresh, beverageType: Beverage.self)
+        purchaseHistory = []
+    }
+
+    override func tearDownWithError() throws {
+        testMachine = nil
+        someBeverage = nil
+        purchaseHistory = nil
+    }
+
+    func test_can_initialize_machine() {
+        let machine = Machine()
+    }
+    
+    func test_customer_insert_money() {
+        let customerBudget = 10_000
+        testMachine.receiveMoney(amount: customerBudget)
+        XCTAssertEqual(testMachine.showInsertedCashBalance(), customerBudget)
+    }
+    
+    func test_show_purchasables() {
+        testMachine.addStock(beverage: someBeverage, amount: 10)
+        let purchasables = testMachine.showPurchasables(with: 999_999)
+        XCTAssertEqual(purchasables.count, 1)
+    }
+    
+    func test_show_expried() {
+    }
+    
+    func test_purchase_beverage_successfully_with_enough_money() {
+        testMachine.receiveMoney(amount: 1_000_000)
+        testMachine.addStock(beverage: someBeverage, amount: 10)
+        testMachine.purchaseBeverage(beverage: someBeverage)
+        XCTAssertEqual(testMachine.checkSpecificTypeOfBeverage(beverage: someBeverage),9)
+    }
+    
+    func test_fail_purchase_beverage_with_not_enough_money() {
+        testMachine.receiveMoney(amount: 10)
+        testMachine.addStock(beverage: someBeverage, amount: 10)
+        testMachine.purchaseBeverage(beverage: someBeverage)
+        XCTAssertEqual(testMachine.checkSpecificTypeOfBeverage(beverage: someBeverage), 10)
+    }
+    
+    func test_return_change_to_customer_when_transaction_stop_button_pressed() {
+        let insertedMoney = 100_000
+        testMachine.receiveMoney(amount: insertedMoney)
+        testMachine.addStock(beverage: someBeverage, amount: 10)
+        testMachine.purchaseBeverage(beverage: someBeverage)
+        let itemPrice = someBeverage.showPrice()
+        XCTAssertEqual(testMachine.transactionStopButtonPressed(), insertedMoney - itemPrice)
+    }
+    
+    func test_save_purchase_history() {
+        let insertedMoney = 100_000
+        testMachine.receiveMoney(amount: insertedMoney)
+        testMachine.addStock(beverage: someBeverage, amount: 10)
+        testMachine.purchaseBeverage(beverage: someBeverage)
+        testMachine.purchaseBeverage(beverage: someBeverage)
+        XCTAssertEqual(testMachine.showPurchaseHistory(), [someBeverage, someBeverage])
+        _ = testMachine.transactionStopButtonPressed()
+        XCTAssertEqual(testMachine.showPurchaseHistory(), [])
+    }
+}

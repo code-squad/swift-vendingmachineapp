2f6322e669099dd773a8df18d005b1ac3f8fd380
Sonjh1306 <yale1306@gmail.com>
2021-03-15 23:28:38 +0900

    Archive 추가 생성

---
+++ VendingMachineApp/VendingMachineApp/Archive.swift
@@ -0,0 +1,33 @@
+//
+//  Archive.swift
+//  VendingMachineApp
+//
+//  Created by sonjuhyeong on 2021/03/15.
+//
+
+import Foundation
+
+class Archive {
+    
+    func archive(with things: VendingMachine) -> Data {
+        do {
+            let archived = try NSKeyedArchiver.archivedData(withRootObject: things, requiringSecureCoding: false)
+            return archived
+        }
+        catch {
+            print(error)
+        }
+        return Data()
+    }
+
+    func unarchive(with text: Data) -> VendingMachine? {
+        do {
+            let object = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(text)
+            return object as? VendingMachine
+        }
+        catch {
+            print(error)
+        }
+        return nil
+    }
+}

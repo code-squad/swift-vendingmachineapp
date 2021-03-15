704eed7259618256138d6493dbe9213b51a84034
Song Lee <songlish233@gmail.com>
2021-03-15 19:36:55 +0900

    제품 구매 시 ScrollView에 음료 이미지 업데이트
    - 제품 구매에 관한 Notification 추가

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -15,9 +15,11 @@ class ViewController: UIViewController {
     @IBOutlet var countLabelCollection: [UILabel]!
     @IBOutlet var stockButtonCollection: [UIButton]!
     @IBOutlet var moneyButtonCollection: [UIButton]!
+    @IBOutlet var buyButtonCollection: [UIButton]!
     
     @IBOutlet weak var moneyLabel: UILabel!
-
+    @IBOutlet weak var scrollContentView: UIScrollView!
+    
     private let beverageImages = [#imageLiteral(resourceName: "americano"), #imageLiteral(resourceName: "cafelatte"), #imageLiteral(resourceName: "chocolatemilk"), #imageLiteral(resourceName: "coke"), #imageLiteral(resourceName: "milkis"), #imageLiteral(resourceName: "plainmilk")]
     private let moneyUnits = [1000, 5000, 10000]
     
@@ -53,17 +55,29 @@ class ViewController: UIViewController {
                                                selector: #selector(didBalanceChanged(_:)),
                                                name: VendingMachine.NotiKeys.balanceUpdate,
                                                object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(didBoughtItem(_:)),
+                                               name: VendingMachine.NotiKeys.dispensdListUpdate,
+                                               object: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateDispensedList()
     }
     
     private func updateOutletCollections() {
         countLabelCollection = []
         stockButtonCollection = []
+        buyButtonCollection = []
         
         for view in productStackView.arrangedSubviews {
             let stackView = view as! ProductStackView
             countLabelCollection.append(stackView.countLabel)
             stockButtonCollection.append(stackView.addButton)
+            buyButtonCollection.append(stackView.buyButton)
             stackView.addButton.addTarget(self, action: #selector(self.addStockTouched(_:)), for: .touchUpInside)
+            stackView.buyButton.addTarget(self, action: #selector(self.buyItemTouched(_:)), for: .touchUpInside)
         }
     }
     
@@ -78,6 +92,35 @@ class ViewController: UIViewController {
                                 label: moneyLabel)
     }
     
+    @objc func didBoughtItem(_ notification: Notification) {
+        updateDispensedList()
+    }
+    
+    private func updateDispensedList() {
+        for views in scrollContentView.subviews {
+            views.removeFromSuperview()
+        }
+        
+        let purchased = appDelegate.vendingMachine.purchased()
+        let count = purchased.count
+        let sizeUnit: CGFloat = 200
+        let yPosition = scrollContentView.bounds.height/2 - sizeUnit/2
+        scrollContentView.contentSize.width = CGFloat(count + 1) * sizeUnit/2
+        
+        for i in 1...count {
+            let targetIdx = beverageList.firstIndex { (listItem) -> Bool in
+                ObjectIdentifier(type(of: listItem)) == ObjectIdentifier(type(of: purchased[count-i]))
+            }
+            
+            if let targetIdx = targetIdx {
+                let newView = UIImageView(frame: CGRect(x: CGFloat(i-1) * sizeUnit/2, y: yPosition, width: sizeUnit, height: sizeUnit))
+                newView.image = beverageImages[targetIdx]
+                newView.contentMode = .scaleAspectFit
+                scrollContentView.addSubview(newView)
+            }
+        }
+    }
+    
     @IBAction func addStockTouched(_ sender: UIButton) {
         if let targetIdx = stockButtonCollection.firstIndex(of: sender) {
             let targetBeverage = beverageList[targetIdx]
@@ -89,6 +132,13 @@ class ViewController: UIViewController {
         if let targetIdx = moneyButtonCollection.firstIndex(of: sender) {
             let amount = moneyUnits[targetIdx]
             appDelegate.vendingMachine.insert(money: amount)
+        }
+    }
+    
+    @IBAction func buyItemTouched(_ sender: UIButton) {
+        if let targetIdx = buyButtonCollection.firstIndex(of: sender) {
+            let targetBeverage = beverageList[targetIdx]
+            appDelegate.vendingMachine.buy(item: targetBeverage)
         }
     }
 }

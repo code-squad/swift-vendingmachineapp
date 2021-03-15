270bba7cc6bda23c5f2834a78385077e0a6a09c0
jhpark-steve <park9368@naver.com>
2021-03-15 15:48:59 +0900

    vendingMachine struct -> class로 재변경

--- VendingMachineApp/VendingMachineApp/VendingMachine.swift
+++ VendingMachineApp/VendingMachineApp/VendingMachine.swift
@@ -7,21 +7,21 @@
 
 import Foundation
 
-struct VendingMachine{
+class VendingMachine : NSObject, NSCoding{
     static let Instance : VendingMachine = VendingMachine.init()
     private var money : Money
     private var currentBeverages : Beverages
     private var bought : Beverages
     private var BeverageFactory : [Beverage] = []
     
-    private init() {
+    private override init() {
         self.money = Money.init(money: 0)
         self.currentBeverages = Beverages()
         self.bought = Beverages()
         currentBeverages.makeBeverageofFactroy(beverages: &self.BeverageFactory)
     }
     
-    mutating func encode(with coder: NSCoder) {
+    func encode(with coder: NSCoder) {
         if let tempMoney = coder.decodeObject(forKey: "Money") as? Money {
             self.money = tempMoney
         }
@@ -34,6 +34,17 @@ struct VendingMachine{
         if let tempBeverageFactory = coder.decodeObject(forKey: "BeverageFactory") as? [Beverage] {
             self.BeverageFactory = tempBeverageFactory
         }
+    }
+    
+    required init?(coder: NSCoder) {
+        self.money = Money.init(money: 0)
+        self.currentBeverages = Beverages()
+        self.bought = Beverages()
+        currentBeverages.makeBeverageofFactroy(beverages: &self.BeverageFactory)
+        coder.encode(self.money, forKey: "Money")
+        coder.encode(self.currentBeverages, forKey: "currentBeverages")
+        coder.encode(self.bought, forKey: "boughtList")
+        coder.encode(self.BeverageFactory, forKey: "BeverageFactory")
     }
     
     func addBeverage(beverage : Beverage){

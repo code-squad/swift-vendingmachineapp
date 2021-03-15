6faca3cdf662fef5530bfdcb78649f3518b7c699
BMO <b38238496@gmail.com>
2021-03-15 17:02:40 +0900

    앱 종료, 시작 시점 데이터 저장 및 로드

--- VendinMachineApp/VendinMachineApp/AppDelegate.swift
+++ VendinMachineApp/VendinMachineApp/AppDelegate.swift
@@ -10,5 +10,34 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var vendingMachine = VendingMachine()
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        if let data = UserDefaults.standard.data(forKey: "vendingMachine3"), let vendingMachineDate = unarchive(with: data) {
+            vendingMachine = vendingMachineDate
+        }
+        return true
+    }
+    
+    func archive(with things: VendingMachine) -> Data {
+        do {
+            let archived = try NSKeyedArchiver.archivedData(withRootObject: things, requiringSecureCoding: false)
+            return archived
+        }
+        catch {
+            print(error)
+        }
+        return Data()
+    }
+    
+    func unarchive(with things: Data) -> VendingMachine? {
+        do {
+            let object = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(things)
+            return object as? VendingMachine
+        }
+        catch {
+            print(error)
+        }
+        return nil
+    }
 }
 

1e65109a10625e6501d9e5f871a11be8f8db32bf
Ray <readingod@naver.com>
2021-03-16 14:11:50 +0900

    Dissmiss 기능 버튼 추가

--- VendingMachineApp/VendingMachineApp/AdminViewController.swift
+++ VendingMachineApp/VendingMachineApp/AdminViewController.swift
@@ -5,4 +5,7 @@ class AdminViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }

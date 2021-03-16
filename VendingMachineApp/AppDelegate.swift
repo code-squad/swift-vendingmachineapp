acc698469193a49b4fea62bf5f01fb906396e9b0
ellyheetov <yaaajcm@naver.com>
2021-03-16 15:07:56 +0900

    Update save data

--- VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
-        DataManager.save(data: vendingMachine!)
+        
     }
 }
 

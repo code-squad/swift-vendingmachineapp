9caed86c0c1ab06760721f8799053281e2d73485
Song Lee <songlish233@gmail.com>
2021-03-16 12:01:19 +0900

    프로토콜 파일 정리: 개별 파일로 분리

---
+++ VendingMachineApp/VendingMachineApp/Model/Protocols/FoodManagable.swift
@@ -0,0 +1,20 @@
+//
+//  FoodManagable.swift
+//  VendingMachineApp
+//
+//  Created by Song on 2021/03/16.
+//
+
+import Foundation
+
+protocol FoodManagable {
+    
+    func expiredItems(fromItemsIn storage: Storage) -> [ObjectIdentifier: Int]
+    
+    func hotItems(fromItemsIn storage: Storage) -> [ObjectIdentifier]
+    
+    func transportables(fromItemsIn storage: Storage) -> [ObjectIdentifier]
+    
+    func healthyItems(fromItemsIn storage: Storage) -> [ObjectIdentifier]
+    
+}

1e47b21d6b2bd1db791995752ecdaa3f7317a6c3
okstring <milkoi7801@gmail.com>
2021-03-16 19:13:25 +0900

    Design: topPanelView roundCorners, backgroundColoe 적용

--- VendingMachineApp/VendingMachineApp/View/TopPanel.swift
+++ VendingMachineApp/VendingMachineApp/View/TopPanel.swift
@@ -27,3 +27,14 @@ class TopPanel: UIView {
         delegate?.didInsertedCoin(amound: amount)
     }
 }
+
+extension TopPanel {
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let maskPath = UIBezierPath(roundedRect: bounds,
+                                    byRoundingCorners: corners,
+                                    cornerRadii: CGSize(width: radius, height: radius))
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = maskPath.cgPath
+        layer.mask = maskLayer
+    }
+}

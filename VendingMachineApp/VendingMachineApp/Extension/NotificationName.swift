e407bb684beac26c6fe3e57d67652ff19f8dfeda
zeke <yangsin1031@naver.com>
2021-03-16 12:02:54 +0900

    Notification.Name 추가
    vendingMachineNotification으로 name추가

---
+++ VendingMachineApp/VendingMachineApp/Extension/NotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName.swift
+//  VendingMachineApp
+//
+//  Created by 양준혁 on 2021/03/16.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let vendingMachineNotification = Notification.Name("vendingMachineNotification")
+}

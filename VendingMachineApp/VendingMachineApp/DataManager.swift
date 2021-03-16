faeb28ba17bf8b4e65cea9ada77b8d9fd39710fc
hoonha choi <sosqsz2@naver.com>
2021-03-16 14:38:07 +0900

    appdelegate archive 분리

--- VendingMachineApp/VendingMachineApp/DataManager.swift
+++ VendingMachineApp/VendingMachineApp/DataManager.swift
@@ -9,11 +9,13 @@ import Foundation
 
 class DataManager {
     
-    func save() {
-        
+    func save() -> VendingMachine? {
+        let loadData = UserDefaults.standard.data(forKey: "vendingMachine") ?? Data()
+        let loadMachine =  ObjectArchive.load(with: loadData)
+        return loadData.isEmpty ? VendingMachine() : loadMachine
     }
     
-    func load() {
-        
+    func load(vendingMachine : VendingMachine) {
+        UserDefaults.standard.setValue(ObjectArchive.save(with: vendingMachine), forKey: "vendingMachine")
     }
 }

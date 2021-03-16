5abbf041e183e1ff5912779baac266be432f78e1
lena <sue822330@gmail.com>
2021-03-16 11:31:59 +0900

    Add ChargeUnit type

---
+++ VendingMachineApp/VendingMachineApp/Models/ChargeUnit.swift
@@ -0,0 +1,13 @@
+//
+//  ChargeUnit.swift
+//  VendingMachineApp
+//
+//  Created by Ador on 2021/03/16.
+//
+
+import Foundation
+
+enum ChargeUnit: Int, CaseIterable {
+    case _1000 = 1000
+    case _5000 = 5000
+}

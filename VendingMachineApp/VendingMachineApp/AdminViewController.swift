782c8de2fa78c80a86a97312780457c5cef02ac1
Ray <readingod@naver.com>
2021-03-16 16:56:46 +0900

    기능구현 제외하고 Admin View만 생성 완료

--- VendingMachineApp/VendingMachineApp/AdminViewController.swift
+++ VendingMachineApp/VendingMachineApp/AdminViewController.swift
@@ -2,10 +2,25 @@ import UIKit
 
 class AdminViewController: UIViewController {
     
+    private var beverageStackView: AdminBeverageStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        beverageStackView = AdminBeverageStackView()
+        configureBeverageStackView()
     }
+    
     @IBAction func closeButtonTouched(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension AdminViewController {
+    
+    private func configureBeverageStackView() {
+        view.addSubview(beverageStackView)
+        beverageStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive = true
+        beverageStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
     }
 }

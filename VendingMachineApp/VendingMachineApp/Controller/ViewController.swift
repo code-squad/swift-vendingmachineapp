4691d49e43dc9b91d48fe5c598f62a73d1437fec
HongzCloud <jihong21511@gmail.com>
2021-03-16 18:36:13 +0900

    🎨 Separate view generation
    BeverageView 생성과 모델 값과 뷰인스턴스 매칭하는 부분을 따로 떼어냈다.

---
+++ VendingMachineApp/VendingMachineApp/Controller/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 오킹 on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var inventoryStackView: UIStackView!
+    @IBOutlet weak var moneyLabel: UILabel!
+  
+    private var vendingMachineInfo = VendingMachineInfo()
+    
+    var delegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpInventoryStackView()
+        addEventOfBeverageStockButton()
+        setUpMoneyLabel()
+    }
+    
+    private func setUpInventoryStackView() {
+        vendingMachineInfo.repeatForBeverageView { beverageView in
+            inventoryStackView.addArrangedSubview(beverageView)
+        }
+    }
+    
+    private func addEventOfBeverageStockButton() {
+        vendingMachineInfo.repeatForButton { UIButton in
+            UIButton.addTarget(self, action: #selector(addBeverage), for: .touchUpInside)
+        }
+    }
+    
+    private func setUpMoneyLabel() {
+        moneyLabel.text = "잔액: \(delegate.vendingMachine.showCurrentMoney()) 원"
+    }
+    
+    @objc
+    private func addBeverage(_ sender: UIButton!) {
+        guard let beverage = vendingMachineInfo.beverageTypeButtons[sender]?.init() else { return }
+        
+        delegate.vendingMachine.appendInventory(beverage)
+        
+        vendingMachineInfo.beverageStockLabels[sender]!.text = "\(delegate.vendingMachine.showAllBeverageList()[ObjectIdentifier(type(of: beverage))]?.count ?? 0)"
+    }
+    
+    @IBAction func addMoney5000(_ sender: Any) {
+        delegate.vendingMachine.put(in: .fiveThousand)
+        setUpMoneyLabel()
+    }
+    
+    @IBAction func addMoney1000(_ sender: Any) {
+        delegate.vendingMachine.put(in: .thousand)
+        setUpMoneyLabel()
+    }
+}
---
+++ VendingMachineApp/VendingMachineApp/Model/VendingMachineInfo.swift
@@ -0,0 +1,55 @@
+//
+//  ModelUpdater.swift
+//  VendingMachineApp
+//
+//  Created by 오킹 on 2021/03/16.
+//
+
+import UIKit
+
+class VendingMachineInfo {
+    private(set) var matchModelAndViewHelper: [ObjectIdentifier: BeverageView] = [:]
+    private(set) var beverageTypeButtons: [UIButton: Beverage.Type] = [:]
+    private(set) var beverageStockLabels: [UIButton: UILabel] = [:]
+    
+    init() {
+        renewVendingMachineInfo()
+    }
+    
+    public func renewVendingMachineInfo() {
+        matchModelAndViewHelper.removeAll()
+        
+        let delegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let beverages = delegate.vendingMachine.showAllBeverageList()
+        beverages.keys.forEach { key in
+            if let beverage = beverages[key]?.first {
+                let image = UIImage(named: beverage.name) ?? UIImage()
+                let text = "\(beverages[key]?.count ?? 0)"
+                let beverageView =  BeverageView(beverageImage: image, stockLabelText: text )
+                
+                matchModelAndViewHelper[key] = beverageView
+                beverageTypeButtons[beverageView.addButton] = type(of: beverage)
+                beverageStockLabels[beverageView.addButton] = beverageView.stockLabel
+            }
+        }
+    }
+    
+    public func repeatForButton(closure: (UIButton)->()) {
+        renewVendingMachineInfo()
+        
+        beverageTypeButtons.keys.forEach { key in
+            closure(key)
+        }
+    }
+    
+    public func repeatForBeverageView(closure: (BeverageView)->()) {
+        renewVendingMachineInfo()
+        
+        matchModelAndViewHelper.keys.forEach { (objectIdentifier) in
+            guard let beverageView = matchModelAndViewHelper[objectIdentifier] else { return }
+            closure(beverageView)
+        }
+    }
+}
+

980f766f6ddbe0b4d995c8c29543fed349e33906
Lia <judykim257@gmail.com>
2021-03-17 17:30:54 +0900

    step4 - VendingMachine 객체 AppDelegate 로 옮김
    아카이브 일부 실패

--- VendingMachineApp/VendingMachineApp/AppDelegate.swift
+++ VendingMachineApp/VendingMachineApp/AppDelegate.swift
@@ -10,5 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var vendingMachine: VendingMachine?
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        vendingMachine = ArchiveManager.unarchive()
+        
+        return true
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        ArchiveManager.archive(data: vendingMachine ?? VendingMachine())
+    }
+    
 }
 

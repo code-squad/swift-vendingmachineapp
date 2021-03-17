e7a4ecef2356aadfa1d684694e3f9c19a08a68d2
hoonha choi <sosqsz2@naver.com>
2021-03-17 15:52:38 +0900

    구매목록 스크롤뷰 추가 및 이미지 추가

--- VendingMachineApp/VendingMachineApp/BeverageFactory.swift
+++ VendingMachineApp/VendingMachineApp/BeverageFactory.swift
@@ -6,10 +6,15 @@
 //
 
 import Foundation
-
+import UIKit
 
 class BeverageFactory {
     static func make(_ drinkType : Beverage.Type) -> Beverage {
         return drinkType.init()
+    }
+    
+    static func makeImage(_ drinkType : Beverage.Type) -> UIImage? {
+        let image = UIImage(named: "\(drinkType)")
+        return image
     }
 }

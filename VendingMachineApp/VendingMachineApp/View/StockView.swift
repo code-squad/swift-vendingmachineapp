b302b832cb8a1ce803f8f724c5c1c3d1f8597e2f
Ray <readingod@naver.com>
2021-03-17 11:34:12 +0900

    코드로 구현했던 버튼이 눌리지 않았던 오류 해결, UIImageView를 UIView로 수정

--- VendingMachineApp/VendingMachineApp/View/StockView.swift
+++ VendingMachineApp/VendingMachineApp/View/StockView.swift
@@ -1,11 +1,11 @@
 import UIKit
 
-class StockView: UIImageView {
+class StockView: UIView {
     
     private var stockImage: UIImage!
     private var stockImageView: UIImageView!
     private var beverageStock = UILabel()
-    private var addBeverage = UIButton()
+    private var addBeverage = UIButton(type: .system)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,30 +31,30 @@ class StockView: UIImageView {
     }
     
     private func configureImageView() {
-        let imageViewWidth = NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 120)
+        let imageViewWidth = NSLayoutConstraint(item: self, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 250)
         let imageViewHeight = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 120)
         addConstraints([imageViewWidth, imageViewHeight])
     }
     
     private func setUpImage() {
         stockImageView = UIImageView(image: stockImage)
+        addSubview(stockImageView)
         stockImageView.contentMode = .scaleAspectFit
         stockImageView.frame = CGRect(x: 0, y: 0, width: 70, height: 70)
-        addSubview(stockImageView)
     }
     
     private func setUpLabel() {
+        addSubview(beverageStock)
         beverageStock.text = "0개"
         beverageStock.textAlignment = .center
         beverageStock.frame = CGRect(x: 100, y: 5, width: 30, height: 20)
-        addSubview(beverageStock)
     }
     
     private func setUpButton() {
-        addBeverage.setTitle("재고추가", for: .normal)
-        addBeverage.setImage(UIImage(named: "button"), for: .normal)
-        addBeverage.frame = CGRect(x: 100, y: 30, width: 80, height: 40)
-        addBeverage.setTitleColor(UIColor.blue, for: .normal)
         addSubview(addBeverage)
+        addBeverage.setTitle("추가", for: .normal)
+        addBeverage.setTitleColor(.systemBlue, for: .normal)
+        addBeverage.backgroundColor = .systemYellow
+        addBeverage.frame = CGRect(x: 100, y: 30, width: 80, height: 40)
     }
 }

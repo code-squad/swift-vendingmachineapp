0d69aa30517d2d2bfc4856f11b337a0104a5b66b
Lia <judykim257@gmail.com>
2021-03-17 15:59:24 +0900

    sugar 오타 수정😅

--- VendingMachineApp/VendingMachineApp/Model/Soda/Cider.swift
+++ VendingMachineApp/VendingMachineApp/Model/Soda/Cider.swift
@@ -11,14 +11,14 @@ class Cider: Soda {
     
     private let flat: Bool
     
-    init(brand: String, volume: Int, price: Int, name: String, createdAt: Date, expiredAt: Date, suger: Int, flat: Bool) {
+    init(brand: String, volume: Int, price: Int, name: String, createdAt: Date, expiredAt: Date, sugar: Int, flat: Bool) {
         self.flat = flat
-        super.init(brand: brand, volume: volume, price: price, name: name, createdAt: createdAt, expiredAt: expiredAt, suger: suger)
+        super.init(brand: brand, volume: volume, price: price, name: name, createdAt: createdAt, expiredAt: expiredAt, sugar: sugar)
     }
 
     required init() {
         flat = false
-        super.init(brand: "코카콜라", volume: 185, price: 1000, name: "스프라이트", createdAt: date, expiredAt: date, suger: 21)
+        super.init(brand: "코카콜라", volume: 185, price: 1000, name: "스프라이트", createdAt: date, expiredAt: date, sugar: 21)
     }
     
     required init?(coder decoder: NSCoder) {

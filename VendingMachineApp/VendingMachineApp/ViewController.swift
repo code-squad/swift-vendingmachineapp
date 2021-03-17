31baae2484df0fefaa5ff970611fc538955f49f9
Lia <judykim257@gmail.com>
2021-03-17 17:30:54 +0900

    step4 - VendingMachine 객체 AppDelegate 로 옮김
    아카이브 일부 실패

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -8,20 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private var vendingMachine: VendingMachine
+    private var vendingMachine: VendingMachine!
     @IBOutlet weak var currentMoney: UILabel!
     @IBOutlet var amountLabels: [UILabel]!
     
     
-    required init?(coder: NSCoder) {
-        self.vendingMachine = VendingMachine()
-        
-        super.init(coder: coder)
-    }
+//    required init?(coder: NSCoder) {
+//        self.vendingMachine = VendingMachine()
+//        vendingMachine = (UIApplication.shared.delegate as! AppDelegate).vendingMachine!
+//        super.init(coder: coder)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        vendingMachine = (UIApplication.shared.delegate as! AppDelegate).vendingMachine!
+        currentMoney.text = "\(vendingMachine.checkBalance())원"
+        updateAmountLabel()
     }
 
     @IBAction func moneyButtonTouched(_ sender: UIButton) {
@@ -31,9 +33,9 @@ class ViewController: UIViewController {
         
     }
     
+    private let name = [0: StrawberryMilk.self, 1: ChocoMilk.self, 2: BananaMilk.self, 3: Coke.self, 4: Cider.self, 5: Fanta.self, 6: Cantata.self, 7: TOP.self, 8: Georgia.self]
     
     @IBAction func addStockButtonTouched(_ sender: UIButton) {
-        let name = [0: StrawberryMilk.self, 1: ChocoMilk.self, 2: BananaMilk.self, 3: Coke.self, 4: Cider.self, 5: Fanta.self, 6: Cantata.self, 7: TOP.self, 8: Georgia.self]
         let index = sender.tag
         let drinkType = name[index] ?? TOP.self
         let drink = drinkType.init()
@@ -45,6 +47,18 @@ class ViewController: UIViewController {
         
         amountLabels[index].text = "\(num)개"
         
+    }
+    
+    private func updateAmountLabel() {
+        let drinkList = vendingMachine.inventoryList()
+        
+        (0...8).forEach{ index in
+            let drinkType = name[index] ?? TOP.self
+            let drink = drinkType.init()
+            let num = drinkList[ObjectIdentifier(type(of: drink))] ?? 0
+            
+            amountLabels[index].text = "\(num)개"
+        }
     }
 }
 

c7c8ff4dcb59e91568b3f669510719898ee4b098
Ray <readingod@naver.com>
2021-03-17 11:34:12 +0900

    코드로 구현했던 버튼이 눌리지 않았던 오류 해결, UIImageView를 UIView로 수정

--- VendingMachineApp/VendingMachineApp/AdminViewController.swift
+++ VendingMachineApp/VendingMachineApp/AdminViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class AdminViewController: UIViewController {
     
     private var beverageStackView: AdminBeverageStackView!
+    private var uibutton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,5 +23,12 @@ extension AdminViewController {
         view.addSubview(beverageStackView)
         beverageStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive = true
         beverageStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+    }
+}
+
+extension AdminViewController {
+    
+    @objc func updateBeveragestock(_ sender: UIButton) {
+        print(1)
     }
 }

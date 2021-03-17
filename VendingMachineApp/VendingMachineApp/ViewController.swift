f06d8a65f93e83279786abf1b1a7d9d532b1fef3
Lia <judykim257@gmail.com>
2021-03-17 18:10:30 +0900

    step4 - viewController refactoring

--- VendingMachineApp/VendingMachineApp/ViewController.swift
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -16,43 +16,53 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         vendingMachine = (UIApplication.shared.delegate as! AppDelegate).vendingMachine!
+        
         currentMoney.text = "\(vendingMachine.checkBalance())원"
-        updateAmountLabel()
+        reloadAmountLabels()
     }
-
+    
+    
     @IBAction func moneyButtonTouched(_ sender: UIButton) {
         let amount = Int(sender.currentTitle ?? "0")!
         let collectedMoney = vendingMachine.collectMoney(with: Cash(rawValue: amount)!)
+
         currentMoney.text = "\(collectedMoney)원"
-        
     }
     
-    private let name = [0: StrawberryMilk.self, 1: ChocoMilk.self, 2: BananaMilk.self, 3: Coke.self, 4: Cider.self, 5: Fanta.self, 6: Cantata.self, 7: TOP.self, 8: Georgia.self]
-    
     @IBAction func addStockButtonTouched(_ sender: UIButton) {
-        let index = sender.tag
-        let drinkType = name[index] ?? TOP.self
-        let drink = drinkType.init()
+        let drink = searchDrinkFromButton(at: sender.tag)
         
         vendingMachine.add(item: drink)
-        
+        updateAmountLabel(at: sender.tag)
+    }
+    
+    
+    private func reloadAmountLabels() {
+        (0...8).forEach{ index in
+            updateAmountLabel(at: index)
+        }
+    }
+    
+    private func updateAmountLabel(at index: Int) {
+        let drink = searchDrinkFromButton(at: index)
         let drinkList = vendingMachine.inventoryList()
         let num = drinkList[ObjectIdentifier(type(of: drink))] ?? 0
         
         amountLabels[index].text = "\(num)개"
-        
     }
     
-    private func updateAmountLabel() {
-        let drinkList = vendingMachine.inventoryList()
-        
-        (0...8).forEach{ index in
-            let drinkType = name[index] ?? TOP.self
-            let drink = drinkType.init()
-            let num = drinkList[ObjectIdentifier(type(of: drink))] ?? 0
-            
-            amountLabels[index].text = "\(num)개"
-        }
+    private func searchDrinkFromButton(at index: Int) -> Beverage {
+        let name = [0: StrawberryMilk.self,
+                    1: ChocoMilk.self,
+                    2: BananaMilk.self,
+                    3: Coke.self,
+                    4: Cider.self,
+                    5: Fanta.self,
+                    6: Cantata.self,
+                    7: TOP.self,
+                    8: Georgia.self]
+        let drinkType = name[index] ?? TOP.self
+        return drinkType.init()
     }
 }
 

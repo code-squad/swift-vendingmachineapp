0bc61a04d21a9e5bc255601675dc8695b10658b7
Jisoo Kim <ghis22130@naver.com>
2021-03-17 11:18:28 +0900

    Test 작성

--- VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
+++ VendingMachineApp/VendingMachineAppTests/VendingMachineAppTests.swift
@@ -25,34 +25,19 @@ class VendingMachineAppTests: XCTestCase {
         }
     }
     
-    func test_50원입력_남은크레딧출력() {
-        vendingMachine.insertCoin(coin: .fifty)
-        XCTAssertEqual(vendingMachine.nowCredit(), 50)
-    }
-    
-    func test_50원입력_다른크레딧출력() {
-        vendingMachine.insertCoin(coin: .fifty)
-        XCTAssertNotEqual(vendingMachine.nowCredit(), 100)
-        XCTAssertNotEqual(vendingMachine.nowCredit(), 500)
-    }
-    
-    
-    func test_딸기우유입력후구매() {
-        let drink = StrawberryMilk(date: Date().date("20210220"), fat: 30, container: .bottle, expiration: Date().date("20210310"), hot: false, calorie: 250)
-        vendingMachine.append(drink)
-        vendingMachine.insertCoin(coin: .fifvethousand)
-        let buyDrink = vendingMachine.buy(with: drink)
+    func testArchived() throws {
+        vendingMachine.append(StrawberryMilk())
+        vendingMachine.append(Sprite())
         
-        XCTAssertEqual(buyDrink, drink)
-    }
-    
-    
-    func test_상품구매로그() {
-        let drink = StrawberryMilk(date: Date().date("20210220"), fat: 10, container: .bottle, expiration: Date().date("20210310"), hot: false, calorie: 250)
-        vendingMachine.append(drink)
-        vendingMachine.insertCoin(coin: .fifvethousand)
-        _ = vendingMachine.buy(with: drink)
-        XCTAssertEqual(vendingMachine.showLog(), ["서울우유, 300ml, 1800원, Strawberry Milk Bottle, 20210220"])
+        guard let data = try? NSKeyedArchiver.archivedData(withRootObject: vendingMachine, requiringSecureCoding: false) else {
+            XCTAssert(false, "vendingMachine Archived 실패")
+            return
+        }
+        guard let vendingMachine2 = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? VendingMachine else {
+            XCTAssert(false, "vendingMachine UnArchived 실패")
+            return
+        }
+        XCTAssertEqual(vendingMachine, vendingMachine2)
     }
 
 }

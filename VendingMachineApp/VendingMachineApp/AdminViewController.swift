53e69f3376bb64fa201597917c92849be4af2d72
Song Lee <songlish233@gmail.com>
2021-03-17 17:31:28 +0900

    AdminMode 스토리보드 구성 / AdminViewController 추가

---
+++ VendingMachineApp/VendingMachineApp/AdminViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AdminViewController.swift
+//  VendingMachineApp
+//
+//  Created by Song on 2021/03/17.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

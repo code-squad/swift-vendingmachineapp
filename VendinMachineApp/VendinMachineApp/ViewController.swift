fd4340077485a0613bec15f2e750170a06bf92d9
BMO <b38238496@gmail.com>
2021-03-17 16:54:12 +0900

    Notification 적용

--- VendinMachineApp/VendinMachineApp/ViewController.swift
+++ VendinMachineApp/VendinMachineApp/ViewController.swift
@@ -19,8 +19,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         vendingMachineElements.setUpAll(images: beverageImages, beverageButtons: beverageAddButtons, cashButtons: cashButtons)
-        updateCurrentCash(label: cashLabel, cashBox: appDelegate.vendingMachine.cashBox)
-        updateBeverageStock(labels: beverageStockLabels, beverageStock: appDelegate.vendingMachine.totalBeverageStockList(), beverageTypeList: vendingMachineElements.beverageList)
+ 
+        NotificationCenter.default.addObserver(self, selector: #selector(updateCurrentCash(notification:)), name: .insertCash, object: appDelegate.vendingMachine)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateBeverageStock(notification:)), name: .addBeverageStock, object: appDelegate.vendingMachine)
     }
     
     @IBAction func beverageMake(_ sender: UIButton) {
@@ -37,16 +38,19 @@ class ViewController: UIViewController {
             return
         }
         appDelegate.vendingMachine.insertCash(amount: cash)
-        vendingMachineElements.updateCurrentCash(label: cashLabel, cashBox: appDelegate.vendingMachine.cashBox)
+    }
+
+    @objc func updateCurrentCash(notification: Notification) {
+        if let cash = notification.userInfo?[VendingMachine.NotificationUserInfo.cashBox] as? Int {
+            cashLabel.text = "\(cash)"
+        }
     }
     
-    func updateCurrentCash(label: UILabel, cashBox: Int) {
-        label.text = "\(cashBox)"
-    }
-    
-    func updateBeverageStock(labels: [UILabel], beverageStock: Beverages, beverageTypeList: [Beverage.Type]) {
-        for (beverageLabel, beverageType) in zip(labels, beverageTypeList) {
-            beverageLabel.text = "\(beverageStock.beverageCount(elementType: beverageType))"
+    @objc func updateBeverageStock(notification: Notification) {
+        if let totalBeverages = notification.userInfo?[VendingMachine.NotificationUserInfo.beverageStock] as? Beverages {
+            for (beverageLabel, beverageType) in zip(beverageStockLabels, vendingMachineElements.beverageList) {
+                beverageLabel.text = "\(totalBeverages.beverageCount(elementType: beverageType))"
+            }
         }
     }
 }

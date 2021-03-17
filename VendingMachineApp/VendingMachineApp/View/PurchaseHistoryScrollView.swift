212a28f7cbca4acb0d281d34c9a66cfb2f2b6c4c
okstring <milkoi7801@gmail.com>
2021-03-17 12:42:21 +0900

    Simplify: PurchaseHistoryScrollView 파일 추가

---
+++ VendingMachineApp/VendingMachineApp/View/PurchaseHistoryScrollView.swift
@@ -0,0 +1,13 @@
+//
+//  PurchaseHistoryScrollView.swift
+//  VendingMachineApp
+//
+//  Created by Issac on 2021/03/17.
+//
+
+import UIKit
+
+class PurchaseHistoryScrollView: UIScrollView {
+    @IBOutlet var purchaseHistoryStackView: UIStackView!
+}
+

cfedd0549e19b2b456d6c2e726e7dbbf6ee852fe
jhpark-steve <park9368@naver.com>
2021-03-19 06:29:03 +0900

    금액 추가 금액 표시 레이블 묶어 하위 뷰로 생성

--- VendingMachineApp/VendingMachineApp/MoneyView.swift
+++ VendingMachineApp/VendingMachineApp/MoneyView.swift
@@ -8,24 +8,29 @@
 import UIKit
 
 class MoneyView : UIView{
-    private var MoneyaddButtons : [UIButton]!
-    private var currentMoneyLabel : UILabel!
+    private var vendingMachine : VendingMachine!
+    private let appDelegate : AppDelegate
+    private var MoneyaddButtons : [UIButton]
+    private var currentMoneyLabel : UILabel
     
     override init(frame: CGRect) {
+        self.appDelegate = UIApplication.shared.delegate as! AppDelegate
+        self.vendingMachine = appDelegate.vendingMachine
+        MoneyaddButtons = []
+        currentMoneyLabel = UILabel()
         super.init(frame: frame)
-    }
-    
-    init(frame: CGRect, moneyMethod : Selector) {
-        super.init(frame: frame)
-        initMoneyaddButtons(method: moneyMethod)
+        initMoneyaddButtons()
         initCurrentMoneyLabel()
     }
     
     required init?(coder: NSCoder) {
+        self.appDelegate = UIApplication.shared.delegate as! AppDelegate
+        MoneyaddButtons = []
+        currentMoneyLabel = UILabel()
         super.init(coder: coder)
     }
     
-    func initMoneyaddButtons(method : Selector){
+    func initMoneyaddButtons(){
         for i in 0..<2 {
             MoneyaddButtons.append(UIButton())
             MoneyaddButtons[i].translatesAutoresizingMaskIntoConstraints = false
@@ -35,10 +40,10 @@ class MoneyView : UIView{
             MoneyaddButtons[i].heightAnchor.constraint(equalToConstant: 50).isActive = true
             MoneyaddButtons[i].centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
             MoneyaddButtons[i].tag = i
-            MoneyaddButtons[i].addTarget(self, action: method, for: .touchUpInside)
+            MoneyaddButtons[i].addTarget(self, action: #selector(addMoney(sender:)), for: .touchUpInside)
         }
-        MoneyaddButtons[0].leftAnchor.constraint(equalTo: self.leftAnchor, constant: 35).isActive = true
-        MoneyaddButtons[1].leftAnchor.constraint(equalTo: MoneyaddButtons[0].rightAnchor, constant: 25).isActive = true
+        MoneyaddButtons[0].leftAnchor.constraint(equalTo: self.leftAnchor, constant: 5).isActive = true
+        MoneyaddButtons[1].rightAnchor.constraint(equalTo: self.rightAnchor, constant: -5).isActive = true
         MoneyaddButtons[0].setTitle("+1000원", for: .normal)
         MoneyaddButtons[1].setTitle("+5000원", for: .normal)
     }
@@ -50,9 +55,17 @@ class MoneyView : UIView{
         self.addSubview(currentMoneyLabel)
         currentMoneyLabel.widthAnchor.constraint(equalToConstant: 75).isActive = true
         currentMoneyLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        currentMoneyLabel.leftAnchor.constraint(equalTo: MoneyaddButtons[0].centerXAnchor).isActive = true
-        currentMoneyLabel.centerYAnchor.constraint(equalTo: MoneyaddButtons[0].bottomAnchor, constant: 30).isActive = true
+        currentMoneyLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        currentMoneyLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 40).isActive = true
+    }
+
+    func updateMoeny(money: String){
+        currentMoneyLabel.text = money
     }
     
-
+    @objc private func addMoney(sender: UIButton){
+        if let tempMoney = Money.moneyUnit.init(index: sender.tag) {
+            vendingMachine.addMoney(money: Money.init(money: tempMoney))
+        }
+    }
 }
